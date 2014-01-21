06723960b638898a276d23bd503964802f223af8
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-01-21 13:48:19 +0100

    Fixed test cases according to pull request comments

--- cpp/src/test_.cpp
+++ cpp/src/test_.cpp
@@ -4,11 +4,12 @@
 
 using namespace sail;
 
-TEST(ContinuousRangeTest, test001)
+TEST(ContinuousRangeTest, TestIntersects)
 {
-	ASSERT_TRUE(ContinuousRange(Arrayd::args(1, 2), false).intersects(ContinuousRange(Arrayd::args(1.5, 3), false)));
-	ASSERT_TRUE(!ContinuousRange(Arrayd::args(1, 2), false).intersects(ContinuousRange(Arrayd::args(3, 4), false)));
-	ASSERT_TRUE(ContinuousRange(Arrayd::args(0.0, 0.3), true).intersects(ContinuousRange(Arrayd::args(0.1 + 4.0*M_PI, 0.3), true)));
-	ASSERT_TRUE(!ContinuousRange(Arrayd::args(0.0, 0.3), true).intersects(ContinuousRange(Arrayd::args(0.4 + 4.0*M_PI, 0.31), true)));
-	ASSERT_TRUE(ContinuousRange(Arrayd::args(0.0, 0.3), true).intersects(ContinuousRange(Arrayd::args(0.0, 0.3), true)));
+	EXPECT_TRUE(ContinuousRange(Arrayd::args(1, 2), false).intersects(ContinuousRange(Arrayd::args(1.5, 3), false)));
+	EXPECT_FALSE(ContinuousRange(Arrayd::args(1, 2), false).intersects(ContinuousRange(Arrayd::args(3, 4), false)));
+	EXPECT_TRUE(ContinuousRange(Arrayd::args(0.0, 0.3), true).intersects(ContinuousRange(Arrayd::args(0.1 + 4.0*M_PI, 0.3), true)));
+	EXPECT_FALSE(ContinuousRange(Arrayd::args(0.0, 0.3), true).intersects(ContinuousRange(Arrayd::args(0.4 + 4.0*M_PI, 0.31), true)));
+	EXPECT_TRUE(ContinuousRange(Arrayd::args(0.0, 0.3), true).intersects(ContinuousRange(Arrayd::args(0.0, 0.3), true)));
 }
+
