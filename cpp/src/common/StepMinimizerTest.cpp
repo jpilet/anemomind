586ff077951417af47eade035ec3693bd89ceabd
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-01-22 12:12:27 +0100

    Added testcase

---
+++ cpp/src/common/StepMinimizerTest.cpp
@@ -0,0 +1,77 @@
+/*
+ * StepMinimizerTest.cpp
+
+ *
+ *  Created on: 22 janv. 2014
+ *      Author: jonas
+ */
+#include "gtest/gtest.h"
+#include "StepMinimizer.h"
+#include <cmath>
+
+using namespace sail;
+
+
+TEST(StepMinimizerTest, MinimizeConvex)
+{
+	const int coefCount = 3;
+	double coefs[coefCount] = {2.0, 3.0, 4.0};
+
+	double initX = 300;
+	double initStep = 100;
+	for (int i = 0; i < coefCount; i++)
+	{
+		double a = coefs[i];
+		for (int j = 0; j < coefCount; j++)
+		{
+			double b = coefs[j];
+			double c = 1.0;
+
+			auto fun = [&] (double x) {return a*x*x + b*x + c;};
+
+			// 2*a*x + b = 0 <=> 2*a*x = -b <=> x = -b/(2*a)
+			double Xopt = -b/(2*a);
+
+			StepMinimizer minimizer;
+			StepMinimizerState initialState(initX, initStep, fun(initX));
+
+			StepMinimizerState finalState = minimizer.minimize(initialState, fun);
+			double dif = finalState.getX() - Xopt;
+			double err = std::abs(dif);
+			EXPECT_LE(err, 1.0e-6);
+		}
+	}
+}
+
+TEST(StepMinimizerTest, MinimizeConvexStepByStep)
+{
+	const int coefCount = 3;
+	double coefs[coefCount] = {2.0, 3.0, 4.0};
+
+	double initX = 300;
+	double initStep = 100;
+	for (int i = 0; i < coefCount; i++)
+	{
+		double a = coefs[i];
+		for (int j = 0; j < coefCount; j++)
+		{
+			double b = coefs[j];
+			double c = 1.0;
+
+			auto fun = [&] (double x) {return a*x*x + b*x + c;};
+
+			// 2*a*x + b = 0 <=> 2*a*x = -b <=> x = -b/(2*a)
+			double Xopt = -b/(2*a);
+
+			StepMinimizer minimizer;
+			StepMinimizerState state(initX, initStep, fun(initX));
+
+			for (int k = 0; k < 30; k++)
+			{
+				state = minimizer.takeStep(state, fun);
+			}
+			double err = std::abs(state.getX() - Xopt);
+			EXPECT_LE(err, 1.0e-6);
+		}
+	}
+}
