1003dc61c00a91f80c9bdfa3fa367b46eb19af4f
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-01-24 10:44:50 +0100

    New class here for state assignment

---
+++ cpp/src/common/StateAssign.cpp
@@ -0,0 +1,24 @@
+/*
+ * StateAssign.cpp
+ *
+ *  Created on: 24 janv. 2014
+ *      Author: jonas
+ */
+
+#include "StateAssign.h"
+
+namespace sail
+{
+
+StateAssign::StateAssign()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+StateAssign::~StateAssign()
+{
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace sail */
---
+++ cpp/src/common/StateAssign.h
@@ -0,0 +1,23 @@
+/*
+ * StateAssign.h
+ *
+ *  Created on: 24 janv. 2014
+ *      Author: jonas
+ */
+
+#ifndef STATEASSIGN_H_
+#define STATEASSIGN_H_
+
+namespace sail
+{
+
+class StateAssign
+{
+public:
+	StateAssign();
+	virtual ~StateAssign();
+};
+
+} /* namespace sail */
+
+#endif /* STATEASSIGN_H_ */
