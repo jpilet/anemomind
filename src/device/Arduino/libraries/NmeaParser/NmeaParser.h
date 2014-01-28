694149e5375d26bfba9201dcdf5a33d873f9d3e0
Julien Pilet <julien.pilet@gmail.com>
2014-01-28 11:33:07 +0100

    Adds an NMEA parser library.
    works on arduino and on PC.

---
+++ src/device/Arduino/libraries/NmeaParser/NmeaParser.h
@@ -0,0 +1,184 @@
+/*
+ * A light NMEA library. Julien Pilet, 2008-2013.
+ *
+ * This library not only parses GPS sentences, but also other common marine
+ * NMEA sentences such as MVW or MLW.
+ *
+ * This code also runs on Arduino.
+ */
+#ifndef NMEAPARSER_H
+#define NMEAPARSER_H
+
+typedef long DWord;
+typedef short Word;
+typedef unsigned char Byte;
+
+#define NP_MAX_DATA_LEN 80
+#define NP_MAX_ARGS 16
+
+class AccAngle {
+  public:
+
+    AccAngle();
+    AccAngle(double angle) { set(angle); }
+    AccAngle(Word deg, Word min, Word mc) : deg_(deg), min_(min), mc_(mc) { }
+
+    double toDouble() const;
+
+    void set(double a);
+    void set(Word deg, Word min, Word mc) {
+        deg_ = deg; min_ = min; mc_ = mc;
+    }
+
+    Word deg() const { return deg_; }
+    Word min() const { return min_; }
+    Word mc() const { return mc_; }
+
+    AccAngle &operator = (double a) { set(a); return *this; }
+
+  private:
+    Word deg_, min_, mc_;
+};
+
+class GeoPos {
+  public:
+    GeoPos() { }
+    GeoPos(int latDeg, int latMin, int latMc, int lonDeg, int lonMin, int lonMc) :
+        lon(lonDeg, lonMin, lonMc), lat(latDeg, latMin, latMc) { }
+
+    AccAngle lon, lat;
+};
+
+class GeoRef;
+class ProjectedPos {
+  public:
+    ProjectedPos() { }
+    ProjectedPos(const GeoRef &ref, const GeoPos &pos);
+
+    double dist(const ProjectedPos &p) const;
+
+    double x() const { return xy_[0]; }
+    double y() const { return xy_[1]; }
+
+  private:
+    double xy_[2];
+};
+
+class GeoRef {
+  public:
+    GeoRef() { }
+    GeoRef(const GeoPos &pos, double altitude) { set(pos, altitude); }
+    void set(const GeoPos &pos, double altitude);
+
+    void project(const GeoPos &pos, double xy[2]) const;
+  private:
+    double reflon, reflat;
+    double dlon, dlat;
+};
+
+class NmeaParser {
+  public:
+
+    enum NmeaSentence {
+        NMEA_NONE=0,
+        NMEA_UNKNOWN,
+        NMEA_TIME_POS,
+        NMEA_AW,
+        NMEA_TW,
+        NMEA_WAT_SP_HDG,
+        NMEA_VLW,
+    };
+
+    NmeaParser();
+    NmeaSentence processByte(Byte data);
+    void printSentence();
+    void getSentenceString(int bufLen, char *buffer);
+
+    DWord cumulativeWaterDistance() const { return cwd_; }
+    DWord waterDistance() const { return wd_; }
+
+    const GeoPos &pos() const { return pos_; }
+
+    void setXTE(float dist, bool left);
+
+    // GPS data
+    Word gpsSpeed() const { return gpsSpeed_; } // 1/256 knots
+    short gpsBearing() const { return gpsBearing_; } // degrees
+    char hour() const { return hour_; }
+    char min() const { return min_; }
+    char sec() const { return sec_; }
+    char day() const { return day_; }
+    char month() const { return month_; }
+    char year() const { return year_; }
+
+    // Wind data
+    short awa() const { return awa_; } // aparent wind angle [degrees]
+    short twa() const { return twa_; } // true wind angle [degrees]
+    Word aws() const { return aws_; } // apparent wind speed [1/256 knots]
+    Word tws() const { return tws_; } // true wind speed [1/256 knots]
+    Word magHdg() const { return magHdg_; } // magnetic heading [degrees]
+    Word watSpeed() const { return watSpeed_; } // water speed [1/256 knots]
+    DWord cwd() const { return cwd_; } // cumulative water distance [nautical miles]
+    DWord wd() const { return wd_; } // water distance since reset [1/10 nautical miles]
+
+    DWord numBytes() const { return numBytes_; }
+    Word numErr() const { return numErr_; }
+    Word numSentences() const { return numSentences_; }
+
+    bool ignoreWrongChecksum() const { return ignoreWrongChecksum_; }
+    void setIgnoreWrongChecksum(bool val) { ignoreWrongChecksum_ = val; }
+  private:
+    enum NPState {
+        NP_STATE_SOM, 	        // Search for start of message
+        NP_STATE_CMD,		// Get command and args
+        NP_STATE_CHECKSUM_1,	// Get first checksum character
+        NP_STATE_CHECKSUM_2,	// get second checksum character
+        NP_STATE_IMPL_EOS,	// implicit end of sentence
+    };
+
+    // GPS data
+    Word gpsSpeed_; // 1/256 knots
+    short gpsBearing_; // degrees
+    char hour_, min_, sec_;
+    char day_, month_, year_;
+
+    // Wind data
+    short awa_; // aparent wind angle [degrees]
+    short twa_; // true wind angle [degrees]
+    Word aws_; // apparent wind speed [1/256 knots]
+    Word tws_; // true wind speed [1/256 knots]
+    Word magHdg_; // magnetic heading [degrees]
+    Word watSpeed_; // water speed [1/256 knots]
+    GeoPos pos_;
+    DWord cwd_; // cumulative water distance [nautical miles]
+    DWord wd_; // water distance since reset [1/10 nautical miles]
+
+    NPState state_;
+    Byte checksum_;
+    int index_;
+    char allow_implicit_eos_;
+
+    char data_[NP_MAX_DATA_LEN];
+    char *argv_[NP_MAX_ARGS];
+    Byte argc_;
+
+    Byte receivedChecksum_;
+    DWord numBytes_;
+    Word numErr_;
+    Word numSentences_;
+
+    bool ignoreWrongChecksum_;
+
+    char computeChecksum() const;
+
+    NmeaSentence processCommand();
+    NmeaSentence processGPRMC();
+    NmeaSentence processMWV();
+    NmeaSentence processVHW();
+    NmeaSentence processVLW();
+};
+
+double geoPosDist(GeoPos *a, GeoPos *b);
+void secToStr(int sec, char *str);
+
+#endif
