9ced33b8dc63a48ab73f6966c8b54d54233e266e
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-02-11 14:24:40 +0100

    Added files meant to contain example code

---
+++ src/server/nautical/CalibrationExamples.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Created on: 2014-02-11
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "CalibrationExamples.h"
+
+namespace sail {
+
+void calibEx001() {
+
+}
+
+} /* namespace sail */
