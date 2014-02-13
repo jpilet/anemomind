819ead4d973ab3e0916c64b71438405eebf131bb
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-02-13 11:24:36 +0100

    Cleaned up some example code

--- src/server/nautical/CalibrationExamples.cpp
+++ src/server/nautical/CalibrationExamples.cpp
@@ -15,57 +15,86 @@
 namespace sail {
 
 
+namespace {
+  class CalibSetup { // temp class with all the test data we need
+   public:
+    CalibSetup();
+    Array<Nav> navs;
+    LocalRace race;
+    Grid3d wgrid;
+    Grid3d cgrid;
+
+    std::shared_ptr<BoatData> boatData;
+
+    DataCalib calib;
+
+    Array<Arrayb> windSplits;
+    Array<Arrayb> currentSplits;
+
+    arma::sp_mat Pwind;
+    arma::sp_mat windRegSpace;
+    arma::sp_mat windRegTime;
+
+    arma::sp_mat Pcurrent;
+    arma::sp_mat currentRegSpace;
+    arma::sp_mat currentRegTime;
+  };
+
+  CalibSetup::CalibSetup() {
+    Array<Nav> allNavs = loadNavsFromText(Nav::AllNavsPath, false);
+    Array<Array<Nav> > splitNavs = splitNavsByDuration(allNavs,
+                                   Duration::minutes(10).getDurationSeconds());
+    navs = splitNavs.first();
+    double spaceStep = 1000; // metres
+    double timeStep = Duration::minutes(20).getDurationSeconds();
+
+    race = LocalRace(navs, spaceStep, timeStep);
+    wgrid = race.getWindGrid();
+    cgrid = race.getCurrentGrid();
+
+    boatData = std::shared_ptr<BoatData>(new BoatData(&race, navs));
+
+    calib.addBoatData(boatData);
+    Array<Arrayb> windSplits = makeRandomSplits(4, calib.windDataCount());
+    Array<Arrayb> currentSplits = makeRandomSplits(4, calib.currentDataCount());
+
+    Pwind = kronWithSpEye(calib.makeP(wgrid), 2);
+    windRegSpace = kronWithSpEye(vcat(wgrid.makeFirstOrderReg(0),
+        wgrid.makeFirstOrderReg(1)), 2);
+    windRegTime = kronWithSpEye(wgrid.makeFirstOrderReg(2), 2);
+
+    Pcurrent = kronWithSpEye(calib.makeP(cgrid), 2);
+    currentRegSpace = kronWithSpEye(vcat(cgrid.makeFirstOrderReg(0),
+        cgrid.makeFirstOrderReg(1)), 2);
+    currentRegTime = kronWithSpEye(cgrid.makeFirstOrderReg(2), 2);
+  }
+}
+
 
 void calibEx001() {
-  Array<Nav> allNavs = loadNavsFromText(Nav::AllNavsPath, false);
-  Array<Array<Nav> > splitNavs = splitNavsByDuration(allNavs,
-                                 Duration::minutes(10).getDurationSeconds());
-  Array<Nav> navs = splitNavs.first();
-  double spaceStep = 1000; // metres
-  double timeStep = Duration::minutes(20).getDurationSeconds();
+  CalibSetup s;
+  WindData windData(s.calib);
+  CurrentData currentData(s.calib);
 
-  LocalRace race(navs, spaceStep, timeStep);
-  Grid3d wgrid = race.getWindGrid();
-  Grid3d cgrid = race.getCurrentGrid();
-
-  std::shared_ptr<BoatData> boatData(new BoatData(&race, navs));
-
-  DataCalib calib;
-  calib.addBoatData(boatData);
-
-  WindData windData(calib);
-  CurrentData currentData(calib);
-
-  arma::sp_mat Pwind = kronWithSpEye(calib.makeP(wgrid), 2);
-  arma::sp_mat windRegSpace = kronWithSpEye(vcat(wgrid.makeFirstOrderReg(0),
-                                   wgrid.makeFirstOrderReg(1)), 2);
-  arma::sp_mat windRegTime = kronWithSpEye(wgrid.makeFirstOrderReg(2), 2);
-
-  arma::sp_mat Pcurrent = kronWithSpEye(calib.makeP(cgrid), 2);
-  arma::sp_mat currentRegSpace = kronWithSpEye(vcat(cgrid.makeFirstOrderReg(0),
-                                   cgrid.makeFirstOrderReg(1)), 2);
-  arma::sp_mat currentRegTime = kronWithSpEye(cgrid.makeFirstOrderReg(2), 2);
 
 
   const double initialRegWeight = 0.1;
 
   const int splitCount = 4;
-  Array<Arrayb> windSplits = makeRandomSplits(4, calib.windDataCount());
-  Array<Arrayb> currentSplits = makeRandomSplits(4, calib.currentDataCount());
 
   double windCurrentBalance = 0.5;
-  std::shared_ptr<GridFit> windTerm(new GridFit(Pwind, &windData,
-      Array<arma::sp_mat>::args(windRegSpace, windRegTime), windSplits,
+  std::shared_ptr<GridFit> windTerm(new GridFit(s.Pwind, &windData,
+      Array<arma::sp_mat>::args(s.windRegSpace, s.windRegTime), s.windSplits,
       Arrayd::args(initialRegWeight, initialRegWeight),
       Array<std::string>::args("Wind-space", "Wind-time"),
       windCurrentBalance));
-  std::shared_ptr<GridFit> currentTerm(new GridFit(Pcurrent, &currentData,
-      Array<arma::sp_mat>::args(currentRegSpace, currentRegTime), currentSplits,
+  std::shared_ptr<GridFit> currentTerm(new GridFit(s.Pcurrent, &currentData,
+      Array<arma::sp_mat>::args(s.currentRegSpace, s.currentRegTime), s.currentSplits,
       Arrayd::args(initialRegWeight, initialRegWeight),
       Array<std::string>::args("Current-space", "Current-time"),
       1.0 - windCurrentBalance));
 
-  arma::mat X = calib.makeInitialParameters();
+  arma::mat X = s.calib.makeInitialParameters();
 
   GridFitter gf;
   gf.setPretuneWeightsIters(3);
@@ -73,8 +102,8 @@ void calibEx001() {
   gf.add(windTerm);
   gf.add(currentTerm);
 
-  std::cout << "Solving for " << wgrid.getVertexCount() << " wind grid 2d vertices and \n";
-  std::cout << "  for " << cgrid.getVertexCount() << " current grid 2d vertices.\n";
+  std::cout << "Solving for " << s.wgrid.getVertexCount() << " wind grid 2d vertices and \n";
+  std::cout << "  for " << s.cgrid.getVertexCount() << " current grid 2d vertices.\n";
   std::cout << "Number of calibration parameters: " << X.n_elem << "\n";
   std::cout << "Number of wind obs: " << windTerm->getData().outDims() << "\n";
   std::cout << "Number of current obs: " << currentTerm->getData().outDims() << "\n";
@@ -84,10 +113,10 @@ void calibEx001() {
 
   double *x = X.memptr();
   std::cout << "Final calibration: " << std::endl;
-  std::cout << EXPR_AND_VAL_AS_STRING(boatData->magneticCompassOffset(x)) << std::endl;
-  std::cout << EXPR_AND_VAL_AS_STRING(boatData->windDirectionOffset(x)) << std::endl;
-  std::cout << EXPR_AND_VAL_AS_STRING(boatData->waterSpeedCoef(x)) << std::endl;
-  std::cout << EXPR_AND_VAL_AS_STRING(boatData->windSpeedCoef(x)) << std::endl;
+  std::cout << EXPR_AND_VAL_AS_STRING(s.boatData->magneticCompassOffset(x)) << std::endl;
+  std::cout << EXPR_AND_VAL_AS_STRING(s.boatData->windDirectionOffset(x)) << std::endl;
+  std::cout << EXPR_AND_VAL_AS_STRING(s.boatData->waterSpeedCoef(x)) << std::endl;
+  std::cout << EXPR_AND_VAL_AS_STRING(s.boatData->windSpeedCoef(x)) << std::endl;
 }
 
 } /* namespace sail */
