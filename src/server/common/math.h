b8576bbc98b01469a546becf6042ffa9d51b5496
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-02-13 21:21:54 +0100

    Removed some functions that are replaced by templates and improved macro names

--- src/server/common/math.h
+++ src/server/common/math.h
@@ -96,10 +96,10 @@ T normdif(T *a, T *b) {
 }
 
 
-#define MAKE_UNIT_CONVERTER(fwdName, invName, factor) template <typename T> T fwdName(T x) {return (factor)*x;} template <typename T> T invName(T x) {return (1.0/(factor))*x;}
-MAKE_UNIT_CONVERTER(deg2rad, rad2deg, M_PI/180.0);
-MAKE_UNIT_CONVERTER(nm2m, m2nm, 1852.0);
-MAKE_UNIT_CONVERTER(knots2MPS, MPS2knots, 1852.0/3600.0);
+#define MAKE_UNIT2OTHERUNIT_CONVERTER(fwdName, invName, factor) template <typename T> T fwdName(T x) {return (factor)*x;} template <typename T> T invName(T x) {return (1.0/(factor))*x;}
+MAKE_UNIT2OTHERUNIT_CONVERTER(deg2rad, rad2deg, M_PI/180.0);
+MAKE_UNIT2OTHERUNIT_CONVERTER(nm2m, m2nm, 1852.0);
+MAKE_UNIT2OTHERUNIT_CONVERTER(knots2MPS, MPS2knots, 1852.0/3600.0);
 
 
 } /* namespace sail */
