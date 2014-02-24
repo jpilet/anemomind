6c1e9cdcf35c3689e3331bccf520d8756ff5469d
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-02-24 11:17:11 +0100

    Moved logging to separate file

--- src/server/common/logging.cpp
+++ src/server/common/logging.cpp
@@ -48,12 +48,6 @@
 #include <stdlib.h>
 #include <string>
 
-// To verify that ScopedLog is only used in a single thread.
-#include <thread>
-#include <sstream>
-
-
-
 using std::string;
 
 namespace internal {
@@ -150,78 +144,3 @@ void SetLogHandler(void (*log_handler)(LogLevel level, const char* filename, int
                                        const std::string& message)) {
   internal::LogHandler = log_handler;
 }
-
-ScopedLog::ScopedLog(const char* filename, int line, std::string message) :
-    _filename(filename),
-    _line(line),
-    _message(message) {
-  verifyThread();
-  _finalScope = _depth == _depthLimit-1;
-
-  if (_finalScope) {
-    disp(_filename, _line, LOGLEVEL_INFO, _message);
-  } else {
-    dispScopeLimit("BEGIN: ");
-  }
-  _depth++;
-}
-
-ScopedLog::ScopedLog(const ScopedLog &x) : _filename(nullptr), _line(-1), _finalScope(false) {
-  LOG(FATAL) << "This object is not meant to be relocated.";
-}
-
-void ScopedLog::operator= (const ScopedLog &x) {
-  LOG(FATAL) << "This object is not meant to be relocated.";
-
-}
-
-
-ScopedLog::~ScopedLog() {
-  _depth--;
-  if (!_finalScope) {
-    dispScopeLimit("END:   ");
-  }
-}
-
-void ScopedLog::setDepthLimit(int l) {
-  _depthLimit = l;
-}
-
-void ScopedLog::dispScopeLimit(const char *label) {
-  disp(_filename, _line, LOGLEVEL_INFO, std::string(label) + _message);
-}
-
-void ScopedLog::disp(const char *filename, int line, LogLevel level, std::string s) {
-  if ((_depth < _depthLimit) || (level != LOGLEVEL_INFO)) {
-    std::string data = makeIndentation() + s;
-    internal::LogFinisher() = internal::LogMessage(level, filename, line) << data;
-  }
-}
-
-std::string ScopedLog::makeIndentation() {
-  return std::string(2*_depth, ' ');
-}
-
-int ScopedLog::_depth = 0;
-int ScopedLog::_depthLimit = 3000;
-
-
-namespace {
-  ScopedLog::ThreadId getThreadId() {
-    std::stringstream ss;
-    ss << std::this_thread::get_id();
-    return ss.str();
-  }
-}
-
-void ScopedLog::verifyThread() {
-  if (!(getThreadId() == _commonThreadId)) {
-    LOG(FATAL) << "Only use the ScopedLog class within a single thread.";
-  }
-}
-
-ScopedLog::ThreadId ScopedLog::_commonThreadId = getThreadId();
-
-
-
-
--- src/server/common/logging.h
+++ src/server/common/logging.h
@@ -127,44 +127,4 @@ void SetLogHandler(void (*log_handler)(LogLevel level, const char* filename, int
                                        const std::string& message));
 
 
-
-
-// Construct this object on the stack in the beginning of a new scope.
-// It helps you follow the program flow.
-class ScopedLog {
- public:
-  typedef std::string ThreadId;
-
-  ScopedLog(const char* filename, int line, std::string message);
-  ~ScopedLog();
-  static void setDepthLimit(int l);
-  void disp(const char *file, int line, LogLevel level, std::string s);
- private:
-  ScopedLog(const ScopedLog &x);
-  void operator= (const ScopedLog &x);
-  const char *_filename;
-  int _line;
-  std::string _message;
-  static int _depth;      // <-- How much indentation there should be
-  static int _depthLimit; // <-- Upper non-inclusive limit beyond which nothing will be displayed.
-  void dispScopeLimit(const char *label);
-  static std::string makeIndentation();
-  bool _finalScope;
-
-
-  static ThreadId _commonThreadId;
-  static void verifyThread();
-};
-
-// This macro is flawed in the sense that the
-// name of the resulting object is not auto-generated
-// to be unique. Be careful.
-#define ENTERSCOPE(SCOPENAME) ScopedLog _slog(__FILE__, __LINE__, SCOPENAME)
-
-// TODO: Provide stream-like syntax, so that we have
-//    LOG(INFO) << ...
-//  and
-//    SCOPED(INFO) << ...
-#define SCOPEDMESSAGE(LEVEL, MESSAGE) _slog.disp(__FILE__, __LINE__, LOGLEVEL_##LEVEL, MESSAGE)
-
 #endif  // _LOGGING_H
