cb34211b207275cdc0aaae077890814cee8ce590
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-02-24 18:05:52 +0100

    More tests

--- src/server/math/MatExprTest.cpp
+++ src/server/math/MatExprTest.cpp
@@ -87,13 +87,15 @@ TEST(MatExprTest, TestVCat) {
   EXPECT_NEAR(arma::norm(Y0 - Y2, 2), 0.0, tol);
 }
 
-void makePowerOf2(MatExprBuilder &builder, int deg) {
-  if (deg == 0) {
-    builder.push(arma::speye(5, 5));
-  } else {
-    builder.push(arma::sp_mat(2.0*arma::speye(5, 5)));
-    makePowerOf2(builder, deg-1);
-    builder.mul();
+namespace {
+  void makePowerOf2(MatExprBuilder &builder, int deg) {
+    if (deg == 0) {
+      builder.push(arma::speye(5, 5));
+    } else {
+      builder.push(arma::sp_mat(2.0*arma::speye(5, 5)));
+      makePowerOf2(builder, deg-1);
+      builder.mul();
+    }
   }
 }
 
@@ -105,5 +107,59 @@ TEST(MatExprTest, Power2) {
   arma::mat Y = builder.top()->mulWithDense(arma::ones(5, 1));
   for (int i = 0; i < 5; i++) {
     EXPECT_NEAR(Y[i], 256.0, 1.0e-9);
+  }
+}
+
+namespace {
+  void makeSumOf2(MatExprBuilder &builder, int deg) {
+    if (deg > 0) {
+      builder.push(arma::sp_mat(2.0*arma::speye(5, 5)));
+      builder.add();
+      makeSumOf2(builder, deg-1);
+    }
+  }
+}
+
+TEST(MatExprTest, Sum2) {
+  int deg = 8;
+  MatExprBuilder builder;
+
+  builder.push(arma::zeros(5, 5));
+  makeSumOf2(builder, deg);
+
+  EXPECT_EQ(builder.size(), 1);
+  arma::mat Y = builder.top()->mulWithDense(arma::ones(5, 1));
+  for (int i = 0; i < 5; i++) {
+    EXPECT_NEAR(Y[i], 16.0, 1.0e-9);
+  }
+}
+
+namespace {
+  void makeRepeater(MatExprBuilder &builder, int count, int dim) {
+    builder.push(arma::sp_mat(1.0*arma::speye(dim, dim)));
+    if (count > 1) {
+      makeRepeater(builder, count-1, dim);
+      builder.vcat();
+    }
+  }
+}
+
+TEST(MatExprTest, Repeat) {
+  int count = 3;
+  const int dim = 3;
+
+  MatExprBuilder builder;
+  makeRepeater(builder, count, dim);
+
+  double xdata[dim] = {0.0, 1.0, 2.0};
+  arma::mat X(xdata, dim, 1, false, true);
+
+  EXPECT_EQ(builder.size(), 1);
+  arma::mat Y = builder.top()->mulWithDense(X);
+  int elemCount = count*dim;
+  EXPECT_EQ(Y.n_rows, elemCount);
+
+  for (int i = 0; i < elemCount; i++) {
+    EXPECT_NEAR(Y[i], i % dim, 1.0e-9);
   }
 }
