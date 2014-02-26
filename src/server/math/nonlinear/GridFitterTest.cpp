6be85347c6c2d594d7b7ba9e87a22fe393e113e7
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-02-26 18:02:18 +0100

    Turned one more example into a test case

--- src/server/math/nonlinear/GridFitterTest.cpp
+++ src/server/math/nonlinear/GridFitterTest.cpp
@@ -13,6 +13,100 @@
 
 using namespace sail;
 
+
+namespace {
+  class GridFitterTestData {
+   public:
+    GridFitterTestData();
+
+    int sampleCount;
+    Arrayd X, Ygt, Ynoisy;
+
+    Array<Arrayb> splits;
+  };
+}
+
+GridFitterTestData::GridFitterTestData() {
+  const int sampleCount_ = 30;
+  double Xdata[sampleCount_] = {-0.967399, -0.782382, -0.740419, -0.725537, -0.716795, -0.686642, -0.604897, -0.563486, -0.514226, -0.444451, -0.329554, -0.270431, -0.211234, -0.198111, -0.0452059, 0.0268018, 0.10794, 0.213938, 0.257742, 0.271423, 0.434594, 0.536459, 0.566198, 0.59688, 0.608354, 0.680375, 0.823295, 0.83239, 0.904459, 0.997849};
+  double Ygtdata[sampleCount_] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+  double Ynoisydata[sampleCount_] = {-0.994827, -0.864355, -0.954944, -1.08159, -0.944979, -0.990285, -1.00257, -0.81089, -1.08299, -0.891457, -0.989302, -0.892034, -1.03991, -0.843388, -1.08667, 0.940983, 1.12309, 1.16761, 0.827902, 1.17973, 1.0104, 0.834422, 0.876886, 1.06529, 1.15609, 0.939557, 0.825669, 0.808009, 0.983081, 0.825238};
+  X = Arrayd(sampleCount_, Xdata).dup();
+  Ygt = Arrayd(sampleCount_, Ygtdata).dup();
+  Ynoisy = Arrayd(sampleCount_, Ynoisydata).dup();
+  sampleCount = sampleCount_;
+
+  bool splitdata0[sampleCount_] = {1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0};
+  bool splitdata1[sampleCount_] = {1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0};
+  bool splitdata2[sampleCount_] = {0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1};
+  bool splitdata3[sampleCount_] = {1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1};
+  bool splitdata4[sampleCount_] = {0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1};
+  bool splitdata5[sampleCount_] = {1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1};
+  bool splitdata6[sampleCount_] = {0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0};
+  bool splitdata7[sampleCount_] = {1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0};
+  bool splitdata8[sampleCount_] = {1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0};
+
+  const int splitCount = 9;
+  Arrayb splitsdata[splitCount] = {Arrayb(sampleCount_, splitdata0).dup(),
+      Arrayb(sampleCount_, splitdata1).dup(),
+      Arrayb(sampleCount_, splitdata2).dup(),
+      Arrayb(sampleCount_, splitdata3).dup(),
+      Arrayb(sampleCount_, splitdata4).dup(),
+      Arrayb(sampleCount_, splitdata5).dup(),
+      Arrayb(sampleCount_, splitdata6).dup(),
+      Arrayb(sampleCount_, splitdata7).dup(),
+      Arrayb(sampleCount_, splitdata8).dup()};
+   splits = Array<Arrayb>(splitCount, splitsdata).dup();
+}
+
+
+TEST(GridFitterTest, TestFixedReg) {
+  BBox1d bbox(Span(-1.0, 1.0));
+  double spacing[1] = {0.03};
+  Grid1d grid(bbox, spacing);
+
+  arma::sp_mat A = grid.makeFirstOrderReg(0);
+
+  GridFitterTestData td;
+
+
+  arma::sp_mat P = grid.makeP(MDArray2d(td.X));
+
+  {
+    // Validate P
+    MDArray2d V = grid.getGridVertexCoords();
+    arma::mat Vmat(V.getData(), V.rows(), V.cols(), false, true);
+    arma::mat Xmat(td.X.getData(), td.X.size(), 1, false, true);
+    assert(arma::norm(P*Vmat - Xmat, 2) <= 1.0e-6);
+  }
+
+  NoisyStep data(td.X, td.Ynoisy);
+
+  GridFitter gridFitter;
+
+
+  std::shared_ptr<GridFit> gf(new GridFit(P, &data, Array<arma::sp_mat>::args(A), td.splits, Arrayd::args(100.0)));
+  gridFitter.add(gf);
+
+  std::shared_ptr<MatExpr> Pinv = gf->makeDataToParamMat();
+
+
+  arma::mat params(1, 1);
+  params[0] = 30.0;
+  gridFitter.solveFixedReg(&params);
+
+  Arrayd Yfitted(td.sampleCount);
+  data.eval(params.memptr(), Yfitted.getData());
+  int counter = 0;
+  for (int i = 0; i < td.sampleCount; i++) {
+    counter += ((Yfitted[i] > 0) == (td.Ygt[i] > 0)? 1 : 0);
+  }
+
+  double tol = (0.25*td.sampleCount);
+  EXPECT_NEAR(counter, td.sampleCount, tol);
+}
+
+
 TEST(GridFitterTest, TestAutoTuneFirstOrder) { // Based on the example: SignalFitExampleAutoRegFirstOrder.cpp
   ScopedLog::setDepthLimit(0);
   BBox1d bbox(Span(-1.0, 1.0));
@@ -21,58 +115,28 @@ TEST(GridFitterTest, TestAutoTuneFirstOrder) { // Based on the example: SignalFi
 
   arma::sp_mat A = grid.makeFirstOrderReg(0);
 
-  const int sampleCount = 30;
-
-  double Xdata[sampleCount] = {-0.967399, -0.782382, -0.740419, -0.725537, -0.716795, -0.686642, -0.604897, -0.563486, -0.514226, -0.444451, -0.329554, -0.270431, -0.211234, -0.198111, -0.0452059, 0.0268018, 0.10794, 0.213938, 0.257742, 0.271423, 0.434594, 0.536459, 0.566198, 0.59688, 0.608354, 0.680375, 0.823295, 0.83239, 0.904459, 0.997849};
-  double Ygtdata[sampleCount] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-  double Ynoisydata[sampleCount] = {-0.994827, -0.864355, -0.954944, -1.08159, -0.944979, -0.990285, -1.00257, -0.81089, -1.08299, -0.891457, -0.989302, -0.892034, -1.03991, -0.843388, -1.08667, 0.940983, 1.12309, 1.16761, 0.827902, 1.17973, 1.0104, 0.834422, 0.876886, 1.06529, 1.15609, 0.939557, 0.825669, 0.808009, 0.983081, 0.825238};
-
-  Arrayd X(sampleCount, Xdata);
-  Arrayd Ygt(sampleCount, Ygtdata);
-  Arrayd Ynoisy(sampleCount, Ynoisydata);
+  GridFitterTestData td;
 
 
-  arma::sp_mat P = grid.makeP(MDArray2d(X));
+  arma::sp_mat P = grid.makeP(MDArray2d(td.X));
 
   {
     // Validate P
     MDArray2d V = grid.getGridVertexCoords();
     arma::mat Vmat(V.getData(), V.rows(), V.cols(), false, true);
-    arma::mat Xmat(X.getData(), X.size(), 1, false, true);
+    arma::mat Xmat(td.X.getData(), td.X.size(), 1, false, true);
     EXPECT_NEAR(arma::norm(P*Vmat - Xmat, 2), 0.0, 1.0e-6);
   }
 
-  bool splitdata0[sampleCount] = {1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0};
-  bool splitdata1[sampleCount] = {1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0};
-  bool splitdata2[sampleCount] = {0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1};
-  bool splitdata3[sampleCount] = {1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1};
-  bool splitdata4[sampleCount] = {0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1};
-  bool splitdata5[sampleCount] = {1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1};
-  bool splitdata6[sampleCount] = {0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0};
-  bool splitdata7[sampleCount] = {1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0};
-  bool splitdata8[sampleCount] = {1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0};
 
-  const int splitCount = 9;
-  Arrayb splitsdata[splitCount] = {Arrayb(sampleCount, splitdata0),
-      Arrayb(sampleCount, splitdata1),
-      Arrayb(sampleCount, splitdata2),
-      Arrayb(sampleCount, splitdata3),
-      Arrayb(sampleCount, splitdata4),
-      Arrayb(sampleCount, splitdata5),
-      Arrayb(sampleCount, splitdata6),
-      Arrayb(sampleCount, splitdata7),
-      Arrayb(sampleCount, splitdata8)};
-
-  Array<Arrayb> splits(splitCount, splitsdata);
-
-  NoisyStep data(X, Ynoisy);
+  NoisyStep data(td.X, td.Ynoisy);
 
   GridFitter gridFitter;
 
 
   double initReg = 1.0; // works
 
-  std::shared_ptr<GridFit> gf(new GridFit(P, &data, Array<arma::sp_mat>::args(A), splits, Arrayd::args(initReg)));
+  std::shared_ptr<GridFit> gf(new GridFit(P, &data, Array<arma::sp_mat>::args(A), td.splits, Arrayd::args(initReg)));
   gridFitter.add(gf);
 
   arma::mat params(1, 1);
@@ -89,11 +153,10 @@ TEST(GridFitterTest, TestAutoTuneFirstOrder) { // Based on the example: SignalFi
   std::shared_ptr<MatExpr> Pinv = gf->makeDataToParamMat();
 
 
-  Arrayd Yfitted(sampleCount);
+  Arrayd Yfitted(td.sampleCount);
   data.eval(params.memptr(), Yfitted.getData());
   arma::mat D(Yfitted.getData(), Yfitted.size(), 1, false, true);
-  arma::mat vertices = Pinv->mulWithDense(D);
-  for (int i = 0; i < sampleCount; i++) {
-    EXPECT_NEAR(Yfitted[i], Ygt[i], 0.25); // Be quite tolerant here
+  for (int i = 0; i < td.sampleCount; i++) {
+    EXPECT_NEAR(Yfitted[i], td.Ygt[i], 0.25); // Be quite tolerant here
   }
 }
