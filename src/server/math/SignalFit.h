1c9682cf0070f5d7e46e1a6152939a8da024f7d9
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-03-12 21:00:20 +0100

    Fit a signal to noisy observations with regularization

---
+++ src/server/math/SignalFit.h
@@ -0,0 +1,14 @@
+/*
+ *  Created on: 2014-03-12
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef SIGNALFIT_H_
+#define SIGNALFIT_H_
+
+namespace sail {
+
+
+} /* namespace sail */
+
+#endif /* SIGNALFIT_H_ */
---
+++ src/server/math/SignalFitExample.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Created on: 2014-03-12
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "SignalFit.h"
+#include <iostream>
+
+int main() {
+  std::cout << "SignalFitExample mjao" << std::endl;
+  return 0;
+}
+
+
