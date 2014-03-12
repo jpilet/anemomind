422deb7965b87ae4ae84ef1de2a8de4a118f0736
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-03-12 11:09:16 +0100

    Test

--- src/server/common/PeriodicHistTest.cpp
+++ src/server/common/PeriodicHistTest.cpp
@@ -56,7 +56,7 @@ TEST(PeriodicHistTest, PHTestPlotData) {
 TEST(PeriodicHistTest, PHTestDiv) {
 
   const int bc = 3;
-  PeriodicHistIndexer indexer(3);
+  PeriodicHistIndexer indexer(bc);
   double adata[bc] = {1.0, 2.0, 2.0};
   double bdata[bc] = {0.5, 2.0, 4.0};
   double expectedCdata[bc] = {2.0, 1.0, 0.5};
