ccf1fe943d82e57913fcd8b0ac7e0aae49cbb6bb
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-03-13 08:59:45 +0100

    Added d-a-c

---
+++ src/server/math/nonlinear/SignalFitExampleDivideAndConquer.cpp
@@ -0,0 +1,12 @@
+/*
+ *  Created on: 2014-03-13
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+#include <iostream>
+
+int main() {
+  std::cout << "Divide-and-conquer" << std::endl;
+  return 0;
+}
+
+
