4dc3d22f14789c3a85c33963d8b14bcd9426abe8
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-03-13 09:41:58 +0100

    Modified refernce

--- src/server/math/nonlinear/SignalFitExampleDivideAndConquer.cpp
+++ src/server/math/nonlinear/SignalFitExampleDivideAndConquer.cpp
@@ -2,14 +2,22 @@
  *  Created on: 2014-03-13
  *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
  */
-#include <server/math/nonlinear/NoisyStep.h>
+#include <server/math/nonlinear/GridFitterTestData.h>
+
+namespace sail {
+
+
 
 void sfexDAC() {
+  GridFitterTestData data;
+}
+
+
 
 }
 
 int main() {
-  sfexDAC();
+  sail::sfexDAC();
   return 0;
 }
 
