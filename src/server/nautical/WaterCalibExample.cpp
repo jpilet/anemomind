79e0220c8310f1047dd919aa170b99d47d244286
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-03-24 14:35:30 +0100

    Added a new file

---
+++ src/server/nautical/WaterCalibExample.cpp
@@ -0,0 +1,19 @@
+/*
+ *  Created on: 24 mars 2014
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "WaterCalibExample.h"
+
+namespace sail {
+
+WaterCalibExample::WaterCalibExample() {
+  // TODO Auto-generated constructor stub
+
+}
+
+WaterCalibExample::~WaterCalibExample() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace sail */
