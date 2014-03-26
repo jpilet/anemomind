28fa83546e446d2574d9b4faf171411cfcd19062
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-03-26 14:02:21 +0100

    Added files to read Nav from Nmea

---
+++ src/server/nautical/NavNmea.cpp
@@ -0,0 +1,11 @@
+/*
+ *  Created on: 2014-03-26
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "NavNmea.h"
+
+namespace sail {
+
+
+} /* namespace sail */
---
+++ src/server/nautical/NavNmea.h
@@ -0,0 +1,15 @@
+/*
+ *  Created on: 2014-03-26
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef NAVNMEA_H_
+#define NAVNMEA_H_
+
+namespace sail {
+
+
+
+} /* namespace sail */
+
+#endif /* NAVNMEA_H_ */
