400584fae5914728160776d7e7e8d8053c36c838
Julien Pilet <julien.pilet@gmail.com>
2014-03-31 09:12:02 +0200

    Removed macros.

--- src/server/common/PhysicalQuantityJson.h
+++ src/server/common/PhysicalQuantityJson.h
@@ -2,7 +2,8 @@
  *  Created on: 2014-03-28
  *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
  *
- *  Json interface for Physical quantities
+ *  Json interface for Physical quantities.
+ *  Provides readField() and writeField() for most Quantity<Type> combinations.
  */
 
 #ifndef PHYSICALQUANTITYJSON_H_
@@ -15,17 +16,71 @@
 namespace sail {
 namespace json {
 
-#define MAKE_PHYSQUANT_JSON_INTERFACE_H(TypeName) \
-    void readField(Poco::JSON::Object::Ptr obj, std::string fieldPrefix, TypeName<double> *out, bool require = true); \
-    void writeField(Poco::JSON::Object::Ptr obj, std::string fieldPrefix, const TypeName<double> &x);
+namespace {
 
-MAKE_PHYSQUANT_JSON_INTERFACE_H(Duration)
-MAKE_PHYSQUANT_JSON_INTERFACE_H(Velocity)
-MAKE_PHYSQUANT_JSON_INTERFACE_H(Angle)
-MAKE_PHYSQUANT_JSON_INTERFACE_H(Length)
-MAKE_PHYSQUANT_JSON_INTERFACE_H(Mass)
+template <class Quantity, typename Value>
+struct JsonQuantityTraits {
+};
 
-#undef MAKE_PHYSQUANT_JSON_INTERFACE_H
+template <typename Value>
+struct JsonQuantityTraits<Duration<Value>, Value> {
+    static double serialize(const Duration<Value>& duration) { return duration.seconds(); }
+    static Duration<Value> deserialize(double v) { return Duration<Value>::seconds(v); }
+    static const char* suffix() { return  "-s"; };
+};
+
+template <typename Value>
+struct JsonQuantityTraits<Velocity<Value>, Value> {
+    static double serialize(const Velocity<Value>& a) { return a.metersPerSecond(); }
+    static Velocity<Value> deserialize(double v) { return Velocity<Value>::metersPerSecond(v); }
+    static const char* suffix() { return "-mps"; };
+};
+
+template <typename Value>
+struct JsonQuantityTraits<Angle<Value>, Value> {
+    static double serialize(const Angle<Value>& a) { return a.radians(); }
+    static Angle<Value> deserialize(double v) { return Angle<Value>::radians(v); }
+    static const char* suffix() { return "-mps"; };
+};
+
+template <typename Value>
+struct JsonQuantityTraits<Length<Value>, Value> {
+    static double serialize(const Length<Value>& a) { return a.meters(); }
+    static Length<Value> deserialize(double v) { return Length<Value>::meters(v); }
+    static const char* suffix() { return "-m"; };
+};
+
+}  // namespace
+
+template<class Quantity>
+bool readField(Poco::JSON::Object::Ptr obj, std::string fieldPrefix,
+               Quantity *out, bool require) {
+    std::string fname = fieldPrefix + JsonQuantityTraits<Quantity, typename Quantity::ValueType>::suffix();
+    bool is = obj->has(fname);
+    if (is) {
+        *out = JsonQuantityTraits<Quantity, typename Quantity::ValueType>::deserialize(
+            obj->getValue<double>(fname));
+        return true;
+    } else if (require) {
+        throw MissingFieldException(obj, fname);
+    } else {
+        *out = Quantity();
+    }
+    return false;
+}
+
+template<class Quantity>
+void writeField(Poco::JSON::Object::Ptr obj, std::string fieldPrefix,
+                const Quantity &x) {
+  double val =
+      JsonQuantityTraits<Quantity, typename Quantity::ValueType>::serialize(x);
+  if (!std::isnan(val)) {
+    obj->set(
+        fieldPrefix +
+        JsonQuantityTraits<Quantity, typename Quantity::ValueType>::suffix(),
+        val);
+  }
+}
 
 }
 } /* namespace sail */
