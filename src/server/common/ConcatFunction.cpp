c17c7573434cb6ad5f33b34b31d656068228fce4
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-04-02 08:30:46 +0200

    Added ConcatFunction

---
+++ src/server/common/ConcatFunction.cpp
@@ -0,0 +1,44 @@
+/*
+ *  Created on: 14 mars 2014
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "ConcatFunction.h"
+#include <assert.h>
+
+namespace sail {
+
+
+ConcatFunction::ConcatFunction(Function &a, Function &b) : _a(a), _b(b) {
+  assert(_a.inDims() == _b.inDims());
+  _Jtemp.create(std::max(_a.outDims(), _b.outDims())*inDims());
+}
+
+MDArray2d ConcatFunction::Atemp() {
+  return MDArray2d(_a.outDims(), _a.inDims(), _Jtemp.ptr());
+}
+MDArray2d ConcatFunction::Btemp() {
+  return MDArray2d(_b.outDims(), _b.inDims(), _Jtemp.ptr());
+}
+
+
+void ConcatFunction::eval(double *Xin, double *Fout, double *Jout) {
+  Arrayd Fdst(outDims(), Fout);
+  MDArray2d Jdst;
+  bool outputJ = Jout != nullptr;
+  if (outputJ) {
+    Jdst = MDArray2d(outDims(), inDims(), Jout);
+  }
+
+  _a.eval(Xin, Fout, tempPtr(Jout));
+  if (outputJ) {
+    Atemp().copyToSafe(Jdst.sliceRowsTo(_a.outDims()));
+  }
+
+  _b.eval(Xin, Fout + _a.outDims(), tempPtr(Jout));
+  if (outputJ) {
+    Btemp().copyToSafe(Jdst.sliceRowsFrom(_a.outDims()));
+  }
+}
+
+} /* namespace sail */
---
+++ src/server/common/ConcatFunction.h
@@ -0,0 +1,31 @@
+/*
+ *  Created on: 14 mars 2014
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef CONCATFUNCTION_H_
+#define CONCATFUNCTION_H_
+
+#include <server/common/Function.h>
+#include <server/common/MDArray.h>
+
+namespace sail {
+
+class ConcatFunction : public Function {
+ public:
+  ConcatFunction(Function &a, Function &b);
+  int inDims() {return _a.inDims();}
+  int outDims() {return _a.outDims() + _b.outDims();}
+  void eval(double *Xin, double *Fout, double *Jout);
+ private:
+  Function &_a, &_b;
+  Arrayd _Jtemp;
+
+  MDArray2d Atemp();
+  MDArray2d Btemp();
+  double *tempPtr(double *Jout) {return (Jout == nullptr? nullptr : _Jtemp.ptr());}
+};
+
+} /* namespace sail */
+
+#endif /* CONCATFUNCTION_H_ */
