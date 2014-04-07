9ffab3c6a443612d0e8a17475186459fbb6ce7cf
Xavier Righetti <xavier@thefabric.com>
2014-04-07 10:56:38 +0200

    additional cleanup

--- test/server/upload/upload.test.js
+++ test/server/upload/upload.test.js
@@ -2,45 +2,21 @@
 
 var app = require('../../../server'),
     supertest = require('supertest'),
-    request = supertest(app),
-    mongoose = require('mongoose');
+    request = supertest(app);
 
 describe('POST /api/upload', function() {
   it('should refuse unauthorized uploads', function(done) {
-    request(app)
+    request
       .post('/api/upload')
       .attach('text', 'test/server/upload/sample.txt')
       .expect(401, done);
   });
 
   it('should store authorized uploads', function(done) {
-    request(app)
+    var agent = supertest.agent(app);
+    agent
       .post('/api/upload')
       .attach('text', 'test/server/upload/sample.txt')
       .expect(401, done);
   });
-});
-
-
-
-var supertest = require('supertest');
-var app = require('../lib/your_app_location');
-
-var request = supertest(app);
-
-describe('when user not logged in', function() {
-    describe('POST /api/posts', function() {
-        var agent1 = supertest.agent(app);
-
-        agent1
-            .post(API.url('posts'))
-            .set('Accept', 'application/json')
-            .send(post: data)
-            .(end(function(err, res) {
-                should.not.exist(err);
-                res.should.have.status(401);
-                should.exist(res.headers['set-cookie']);
-                done();
-            }));
-    });
 });
