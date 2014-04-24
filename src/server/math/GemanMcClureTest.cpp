a3e0e388c6c662890946585e0360b3babd5ee3fb
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-04-24 15:53:46 +0200

    Added test case

---
+++ src/server/math/GemanMcClureTest.cpp
@@ -0,0 +1,19 @@
+/*
+ *  Created on: 2014-04-24
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+#include <gtest/gtest.h>
+#include <server/math/GemanMcClure.h>
+
+using namespace sail;
+
+TEST(GemanMcClureTest, WeightTest) {
+  GemanMcClure gmc(10.0, 10.0, 1, 1);
+  EXPECT_NEAR(gmc.getWeight(0), 0.5, 1.0e-6);
+  gmc.addResiduals(10.0);
+  EXPECT_NEAR(gmc.getWeight(0), 0.5, 1.0e-6);
+  gmc.addResiduals(10.1);
+  EXPECT_LE(gmc.getWeight(0), 0.5);
+}
+
+
