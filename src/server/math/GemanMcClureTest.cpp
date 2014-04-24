6648bce5f6d7f016cadf696bfcb55eff142005be
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-04-24 15:56:10 +0200

    Gmc test that passes

--- src/server/math/GemanMcClureTest.cpp
+++ src/server/math/GemanMcClureTest.cpp
@@ -10,10 +10,10 @@ using namespace sail;
 TEST(GemanMcClureTest, WeightTest) {
   GemanMcClure gmc(10.0, 10.0, 1, 1);
   EXPECT_NEAR(gmc.getWeight(0), 0.5, 1.0e-6);
-  gmc.addResiduals(10.0);
+  gmc.addResiduals(Arrayd::args(10.0));
   EXPECT_NEAR(gmc.getWeight(0), 0.5, 1.0e-6);
-  gmc.addResiduals(10.1);
-  EXPECT_LE(gmc.getWeight(0), 0.5);
+  gmc.addResiduals(Arrayd::args(10000.1));
+  EXPECT_LE(gmc.getWeight(0), 0.4);
 }
 
 
