068aef5dba9c5a3c278f560f8e36f13b53cd4f82
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-05-12 11:53:30 +0200

    Command line interface

---
+++ src/server/cmdapp.cpp
@@ -0,0 +1,15 @@
+/*
+ *  Created on: 2014-05-12
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <iostream>
+#include <Poco/Util/Application.h>
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
+
+
+
