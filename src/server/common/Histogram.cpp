c2e5939403d52240da92681f6cba0ba20f42ec46
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-05-20 11:24:24 +0200

    works well

---
+++ src/server/common/Histogram.cpp
@@ -0,0 +1,91 @@
+/*
+ *  Created on: 2014-05-20
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "Histogram.h"
+#include  <assert.h>
+
+namespace sail {
+
+HistogramMap::HistogramMap(int count, double leftBd, double rightBd) {
+  init(count, leftBd, rightBd);
+}
+
+
+HistogramMap::HistogramMap(int count, Arrayd values) {
+  double minv = values[0];
+  double maxv = minv;
+  int n = values.size();
+  for (int i = 1; i < n; i++) {
+    double x = values[i];
+    minv = std::min(minv, x);
+    maxv = std::max(maxv, x);
+  }
+  const double marg = 1.0e-9;
+  init(count, minv - marg, maxv + marg);
+}
+
+
+int HistogramMap::toBin(double value) const {
+  int index = int(_index2left.inv(value));
+  assert(validIndex(index));
+  return index;
+}
+
+double HistogramMap::toLeftBound(int binIndex) const {
+  assert(validIndex(binIndex));
+  return _index2left(binIndex);
+}
+
+double HistogramMap::toRightBound(int binIndex) const {
+  assert(validIndex(binIndex));
+  return _index2left(binIndex + 1.0);
+}
+
+double HistogramMap::toCenter(int binIndex) const {
+  assert(validIndex(binIndex));
+  return _index2left(binIndex + 0.5);
+}
+Arrayi HistogramMap::countPerBin(Arrayd values) const {
+  Arrayi hist(_binCount);
+  hist.setTo(0);
+  int count = values.size();
+  for (int i = 0; i < count; i++) {
+    hist[toBin(values[i])]++;
+  }
+  return hist;
+}
+
+Arrayi HistogramMap::assignBins(Arrayd values) const {
+  return values.map<int>([&](double x) {return toBin(x);});
+}
+
+
+void HistogramMap::init(int count, double leftBd, double rightBd) {
+  _binCount = count;
+  _index2left = LineKM(0, count, leftBd, rightBd);
+}
+
+namespace {
+  Array<Arrayd> allocateGroups(Arrayi cpb) {
+    return cpb.map<Arrayd>([&](int i) {return Arrayd(i);});
+  }
+}
+
+Array<Arrayd> HistogramMap::groupValuesByBin(Arrayd X, Arrayd Y) const {
+  Arrayi cpb = countPerBin(X);
+  Array<Arrayd> groups = allocateGroups(cpb);
+  assert(groups.size() == _binCount);
+  Arrayi counters = Arrayi::fill(_binCount, 0);
+  int n = X.size();
+  assert(n == Y.size());
+  for (int i = 0; i < n; i++) {
+    int index = toBin(X[i]);
+    groups[index][counters[index]] = Y[i];
+    counters[index]++;
+  }
+  return groups;
+}
+
+} /* namespace sail */
---
+++ src/server/common/HistogramTest.cpp
@@ -0,0 +1,50 @@
+/*
+ *  Created on: 2014-05-20
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <gtest/gtest.h>
+#include <server/common/Histogram.h>
+
+using namespace sail;
+
+TEST(HistgramTest, BasicMapping) {
+  const double marg = 1.0e-6;
+  HistogramMap h(2, 1.0, 3.0);
+  EXPECT_EQ(h.binCount(), 2);
+  EXPECT_EQ(h.toBin(1.5), 0);
+  EXPECT_EQ(h.toBin(2.5), 1);
+  EXPECT_NEAR(h.toLeftBound(0), 1.0, marg);
+  EXPECT_NEAR(h.toCenter(0), 1.5, marg);
+  EXPECT_NEAR(h.toRightBound(0), 2.0, marg);
+  EXPECT_TRUE(h.validIndex(0));
+  EXPECT_TRUE(h.validIndex(1));
+  EXPECT_FALSE(h.validIndex(2));
+}
+
+TEST(HistgramTest, Counting) {
+  const double marg = 1.0e-6;
+  HistogramMap h(2, 1.0, 3.0);
+  const int xn = 3;
+  double xdata[xn] = {1.2, 1.3, 2.9};
+  Arrayd X(xn, xdata);
+  Arrayi counts = h.countPerBin(X);
+  EXPECT_EQ(counts.size(), 2);
+  EXPECT_EQ(counts[0], 2);
+  EXPECT_EQ(counts[1], 1);
+  Arrayi inds = h.assignBins(X);
+  EXPECT_EQ(inds.size(), 3);
+  EXPECT_EQ(inds[0], 0);
+  EXPECT_EQ(inds[1], 0);
+  EXPECT_EQ(inds[2], 1);
+
+  int ydata[xn] = {119, 611, 249};
+  Arrayi Y(xn, ydata);
+  Array<Arrayi> groups = h.groupValuesByBin(X, Y);
+  EXPECT_EQ(groups.size(), 2);
+  EXPECT_EQ(groups[0].size(), 2);
+  EXPECT_EQ(groups[1].size(), 1);
+  EXPECT_EQ(groups[1][0], 249);
+}
+
+
