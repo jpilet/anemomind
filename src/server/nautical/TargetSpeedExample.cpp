4557e8c2589306f04b8b6932a6c4148d07fcd4f7
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-05-20 15:33:28 +0200

    an example

---
+++ src/server/nautical/TargetSpeedExample.cpp
@@ -0,0 +1,15 @@
+/*
+ *  Created on: May 20, 2014
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+namespace {
+  void targetSpeedPlot() {
+
+  }
+}
+
+int main() {
+  targetSpeedPlot();
+  return 0;
+}
