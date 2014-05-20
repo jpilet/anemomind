2ac3734f99b438ef2ce368085d395febec8427b4
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-05-20 12:02:08 +0200

    bra

--- src/server/common/Histogram.cpp
+++ src/server/common/Histogram.cpp
@@ -30,6 +30,8 @@ HistogramMap::HistogramMap(int count, Arrayd values) {
 int HistogramMap::toBin(double value) const {
   int index = int(_index2left.inv(value));
   assert(validIndex(index));
+  assert(toLeftBound(index) <= value);
+  assert(value < toRightBound(index));
   return index;
 }
 
