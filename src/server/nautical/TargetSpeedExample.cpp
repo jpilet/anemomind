2bc6ae0229f9e6d882ed7f88e2647853862e6de1
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-05-20 15:41:53 +0200

    working on it

--- src/server/nautical/TargetSpeedExample.cpp
+++ src/server/nautical/TargetSpeedExample.cpp
@@ -1,11 +1,22 @@
 /*
- *  Created on: May 20, 2014
+ *  Created on: 2014-05-20
  *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
  */
 
+#include <server/nautical/TargetSpeed.h>
+#include <server/common/Env.h>
+#include <server/common/PathBuilder.h>
+
+using namespace sail;
+
 namespace {
   void targetSpeedPlot() {
-
+    Poco::Path srcpath = PathBuilder::makeDirectory(Env::SOURCE_DIR).
+        pushDirectory("datasets").
+        pushDirectory("regates").get();
+    Array<Nav> navs = scanNmeaFolder(srcpath, Nav::debuggingBoatId());
+    Arrayb upwind = guessUpwindNavs(navs);
+    Arrayb downwind = neg(upwind);
   }
 }
 
