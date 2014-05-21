db6e5051f819be750fcc8f2d8b0150d726806ada
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-05-21 18:38:51 +0200

    Added files for synthetic test data

---
+++ src/server/nautical/SynthTestData.cpp
@@ -0,0 +1,12 @@
+/*
+ *  Created on: 2014-05-21
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "SynthTestData.h"
+
+namespace sail {
+
+
+
+} /* namespace sail */
---
+++ src/server/nautical/SynthTestData.h
@@ -0,0 +1,15 @@
+/*
+ *  Created on: 2014-05-21
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef SYNTHTESTDATA_H_
+#define SYNTHTESTDATA_H_
+
+namespace sail {
+
+
+
+} /* namespace sail */
+
+#endif /* SYNTHTESTDATA_H_ */
