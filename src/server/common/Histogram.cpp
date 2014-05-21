057eb5461992f4eea22cf70cca0d06ce8559e65f
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-05-21 17:30:34 +0200

    removed assertion

--- src/server/common/Histogram.cpp
+++ src/server/common/Histogram.cpp
@@ -25,8 +25,6 @@ HistogramMap::HistogramMap(int count, Arrayd values) {
 int HistogramMap::toBin(double value) const {
   int index = int(_index2left.inv(value));
   if (validIndex(index)) {
-    assert(toLeftBound(index) <= value);
-    assert(value < toRightBound(index));
     return index;
   } else {
     return -1;
