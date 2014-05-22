3ee2a1259e8be54c990a21b1573910d4a46886e6
Xavier Righetti <xavier@thefabric.com>
2014-05-22 13:28:29 +0000

    removed unwanted file

--- scripts/notify.js
+++
@@ -1,9 +0,0 @@
-var upload = require('../lib/controllers/upload'),
-	id = process.argv[2],
-	polar = process.argv[3],
-	race = process.argv[4];
-
-upload.storeUpload(id, polar, race);
-// process.argv.forEach(function (val, index, array) {
-//   console.log(index + ': ' + val);
-// });
