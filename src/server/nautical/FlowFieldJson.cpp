7b72b310ffb6c19805ca8599510c4c5274fee3eb
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-05-23 11:50:00 +0200

    Added json files

---
+++ src/server/nautical/FlowFieldJson.cpp
@@ -0,0 +1,30 @@
+/*
+ *  Created on: May 23, 2014
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "FlowFieldJson.h"
+
+namespace sail {
+namespace json {
+
+CommonJson::Ptr serialize(const FlowField &x) {
+  CommonJson::Ptr obj = CommonJsonObject::make();
+  obj->toObject()->set("grid", serialize(x.grid()));
+  obj->toObject()->set("flow", serialize(x.flow()));
+  return obj;
+}
+
+void deserialize(CommonJson::Ptr obj, FlowField *dst) {
+  Array<FlowField::InternalFlowVector> flow;
+  deserialize(obj->toObject()->get("flow"), &flow);
+
+  Grid3d grid;
+  deserialize(obj->toObject()->get("grid"), &grid);
+
+  *dst = FlowField(grid, flow);
+}
+
+
+}
+} /* namespace sail */
