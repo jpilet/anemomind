919191179bf9b3c9e6dad82f27be415c704dec48
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-06-03 16:14:48 +0200

    target speed example

--- src/server/nautical/TargetSpeedExample.cpp
+++ src/server/nautical/TargetSpeedExample.cpp
@@ -7,10 +7,9 @@
 #include <server/common/Env.h>
 #include <server/common/PathBuilder.h>
 #include <server/nautical/NavNmeaScan.h>
-#include <server/common/Span.h>
 #include <iostream>
-#include <server/plot/extra.h>
-#include <server/nautical/grammars/Grammar001.h>
+#include <server/nautical/Calibrator.h>
+#include <server/nautical/BoatLogProcessor.h>
 
 using namespace sail;
 
@@ -21,38 +20,13 @@ namespace {
         pushDirectory("regates").get();
     Array<Nav> allnavs = scanNmeaFolder(srcpath, Nav::debuggingBoatId());
 
-    Grammar001Settings settings;
-    Grammar001 g(settings);
+    Calibrator c;
+    c.calibrate(allnavs);
+    TargetSpeedData uw, dw;
+    computeTargetSpeedData(c, &uw, &dw);
 
-
-    std::shared_ptr<HTree> tree = g.parse(allnavs);
-
-    Arrayb upwind = markNavsByDesc(tree, g.nodeInfo(), allnavs, "upwind-leg");
-    assert(!upwind.empty());
-    assert(countTrue(upwind) > 0);
-
-
-    Array<Nav> upwindNavs = allnavs.slice(upwind);
-
-    const int binCount = 25;
-    Array<Velocity<double> > tws = estimateTws(upwindNavs);
-    Array<Velocity<double> > vmg = calcUpwindVmg(upwindNavs);
-    Array<Velocity<double> > gss = getGpsSpeed(upwindNavs);
-
-    Arrayd gssd = gss.map<double>([&](Velocity<double> x) {return x.metersPerSecond();});
-    Arrayd twsd = tws.map<double>([&](Velocity<double> x) {return x.metersPerSecond();});
-    Arrayd vmgd = vmg.map<double>([&](Velocity<double> x) {return x.metersPerSecond();});
-
-    std::cout << "GPS-span (m/s): " << Spand(gssd) << std::endl;
-    std::cout << "TWS-span (m/s): " << Spand(twsd) << std::endl;
-    std::cout << "VMG-span (m/s): " << Spand(vmgd) << std::endl;
-
-    Velocity<double> minvel = Velocity<double>::metersPerSecond(4.0);
-    Velocity<double> maxvel = Velocity<double>::metersPerSecond(17.0);
-    TargetSpeedData tgt(tws, vmg, binCount,
-        minvel, maxvel);
-
-    tgt.plot();
+    TargetSpeedData which = uw;
+    which.plot();
   }
 }
 
