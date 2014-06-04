df11a587e59d7cd6f1c989ccaa76463513728d37
Julien Pilet <julien.pilet@gmail.com>
2014-06-04 08:31:05 +0200

    Initial attempt to port Nav to arduino.

---
+++ src/device/Arduino/libraries/Nav/Nav.cpp
@@ -0,0 +1,348 @@
+/*
+ * Nav.cpp
+ *
+ *  Created on: 16 janv. 2014
+ *      Author: jonas
+ */
+
+#include "Nav.h"
+
+#ifndef ANEMOMIND_DEVICE
+#include <server/common/ArrayIO.h>
+#include <algorithm>
+#include <server/plot/gnuplot_i.hpp>
+#include <server/common/LineKM.h>
+#include <server/common/PhysicalQuantity.h>
+#include <server/plot/extra.h>
+#include <server/nautical/Ecef.h>
+#include <ctime>
+#include <server/nautical/WGS84.h>
+#include <server/common/string.h>
+#endif
+
+namespace sail {
+
+#ifndef ANEMOMIND_DEVICE
+Nav::Nav() : _time(TimeStamp::makeUndefined()) {
+  _cwd = -1;
+  _wd = -1;
+}
+#endif
+
+
+//tm_sec	int	seconds after the minute	0-60*
+//tm_min	int	minutes after the hour	0-59
+//tm_hour	int	hours since midnight	0-23
+//tm_mday	int	day of the month	1-31
+//tm_mon	int	months since January	0-11
+//tm_year	int	years since 1900
+//tm_wday	int	days since Sunday	0-6
+//tm_yday	int	days since January 1	0-365
+//tm_isdst	int	Daylight Saving Time flag
+
+
+namespace NavDataConversion {
+#ifndef ANEMOMIND_DEVICE
+  TimeStamp makeTimeNmeaFromYMDhms(double yearSince2000, double
+      month, double day, double hour,
+      double minute, double second) {
+
+    return TimeStamp::UTC(int(yearSince2000 + 2000), int(month), int(day),
+              int(hour), int(minute), second);
+  }
+#else
+  TimeStamp makeTimeNmeaFromYMDhms(double yearSince2000, double
+      month, double day, double hour,
+      double minute, double second) {
+    TimeStamp ts;
+    ts._year = (yearSince2000 > 80 ? 1900 : 2000);
+    ts._month = month;
+    ts._day = day;
+    ts._hour = hour;
+    ts._min = minute;
+    ts._sec = second;
+    return ts;
+  }
+#endif
+}
+
+
+#ifndef ANEMOMIND_DEVICE
+Nav::Nav(MDArray2d row) {
+  assert(row.rows() == 1);
+  assert(row.cols() == 23 || row.cols() == 22);
+
+  _gpsSpeed = Velocity<double>::knots(row(0, 6));
+  _awa = Angle<double>::degrees(row(0, 7));
+  _aws = Velocity<double>::knots(row(0, 8));
+
+  _twaFromFile = Angle<double>::degrees(row(0, 9));
+  _twsFromFile = Velocity<double>::knots(row(0, 10));
+
+  _magHdg = Angle<double>::degrees(row(0, 11));
+  _watSpeed = Velocity<double>::knots(row(0, 12));
+  _gpsBearing = Angle<double>::degrees(row(0, 13));
+
+
+  Angle<double> lat = Angle<double>::degMinMc(row(0, 14), row(0, 15), row(0, 16));
+  Angle<double> lon = Angle<double>::degMinMc(row(0, 17), row(0, 18), row(0, 19));
+  _pos = GeographicPosition<double>(lon, lat);
+
+  _cwd = row(0, 20); // week day
+  _wd = row(0, 21);
+
+  double year = row(0, 0);
+  double month = row(0, 1);
+  double dayOfTheMonth = row(0, 2);
+  double hour = row(0, 3);
+  double minute = row(0, 4);
+  double second = row(0, 5);
+  _time = NavDataConversion::makeTimeNmeaFromYMDhms(year, month, dayOfTheMonth, hour, minute, second);
+}
+#endif
+
+bool Nav::operator== (const Nav &other) const {
+  return _gpsSpeed.eqWithNan(other._gpsSpeed) &&
+      _awa.eqWithNan(other._awa) &&
+      _aws.eqWithNan(other._aws) &&
+#ifndef ANEMOMIND_DEVICE
+      _boatId == other._boatId &&
+#endif
+      _twaFromFile.eqWithNan(other._twaFromFile) &&
+      _twsFromFile.eqWithNan(other._twsFromFile) &&
+      _magHdg.eqWithNan(other._magHdg) &&
+      _watSpeed.eqWithNan(other._watSpeed) &&
+      _gpsBearing.eqWithNan(other._gpsBearing) &&
+      _pos == other._pos && (strictEquality(_cwd, other._cwd)) && (strictEquality(_wd, other._wd));
+}
+
+
+HorizontalMotion<double> Nav::gpsVelocity() const {
+  return HorizontalMotion<double>::polar(gpsSpeed(), gpsBearing());
+}
+
+
+#ifndef ANEMOMIND_DEVICE
+Nav::Id Nav::id() const {
+  if (hasId()) {
+    int64_t time = _time.toMilliSecondsSince1970();
+    static_assert(sizeof(time) == 8, "The size of the time datatype seems to have changed.");
+    return _boatId + int64ToHex(time);
+  } else {
+    return "";
+  }
+}
+
+bool Nav::hasId() const {
+  return hasBoatId() && _time.defined();
+}
+
+// From load_data.m
+//year = 1;
+//month = 2;
+//dayOfTheMonth = 3;
+//hour = 4;
+//minute = 5;
+//second = 6;
+//gpsSpeed = 7;
+//awa = 8;
+//aws = 9;
+//twa = 10;
+//tws = 11;
+//magHdg = 12;
+//watSpeed = 13;
+//gpsBearing = 14;
+//pos_lat_deg = 15;
+//pos_lat_min = 16;
+//pos_lat_mc = 17;
+//pos_lon_deg = 18;
+//pos_lon_min = 19;
+//pos_lon_mc = 20;
+//cwd = 21;
+//wd = 22;
+//days = 23;
+//days_data = datenum(unsorted_data(:,year) + 2000, unsorted_data(:,month), unsorted_data(:, dayOfTheMonth), unsorted_data(:,hour), unsorted_data(:,minute), unsorted_data(:,second));
+
+Array<Velocity<double> > getExternalTws(Array<Nav> navs) {
+  return navs.map<Velocity<double> >([&](const Nav &n) {return n.externalTws();});
+}
+
+Array<Angle<double> > getExternalTwa(Array<Nav> navs) {
+  return navs.map<Angle<double> >([&](const Nav &n) {return n.externalTwa();});
+}
+
+Array<Velocity<double> > getGpsSpeed(Array<Nav> navs) {
+  return navs.map<Velocity<double> >([&](const Nav &n) {return n.gpsSpeed();});
+}
+
+Array<Nav> loadNavsFromText(std::string filename, bool sort) {
+  MDArray2d data = loadMatrixText<double>(filename);
+  int count = data.rows();
+
+  std::vector<Nav> navs(count);
+  for (int i = 0; i < count; i++) {
+    navs[i] = Nav(data.sliceRow(i));
+  }
+
+  if (sort) {
+    std::sort(navs.begin(), navs.end());
+  }
+
+  return Array<Nav>::referToVector(navs).dup();
+}
+
+bool areSortedNavs(Array<Nav> navs) {
+  int count = navs.size();
+  for (int i = 0; i < count-1; i++) {
+    if (navs[i].time() > navs[i+1].time()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void plotNavTimeVsIndex(Array<Nav> navs) {
+  Gnuplot plot;
+  int count = navs.size();
+
+  std::vector<double> X(count), Y(count);
+  TimeStamp start = navs[0].time();
+  for (int i = 0; i < count; i++) {
+    X[i] = i;
+    Y[i] = (navs[i].time() - start).days();
+  }
+
+  plot.set_style("lines");
+  plot.plot_xy(X, Y);
+  plot.set_xautoscale();
+  plot.set_yautoscale();
+  plot.set_xlabel("Index");
+  plot.set_ylabel("Time");
+  plot.showonscreen();
+  sleepForever();
+}
+
+double getNavsMaxInterval(Array<Nav> navs) {
+  int count = navs.size();
+  double m = 0.0;
+  for (int i = 0; i < count-1; i++) {
+    m = std::max(m, (navs[i+1].time() - navs[i].time()).seconds());
+  }
+  return m;
+}
+
+void dispNavTimeIntervals(Array<Nav> navs) {
+  assert(areSortedNavs(navs));
+  double mintime = 0.0;
+  double maxtime = (navs[navs.size()-1].time() - navs[0].time()).seconds();
+
+  double m = getNavsMaxInterval(navs);
+  std::cout << "Max interval (seconds): " << m << std::endl;
+  std::cout << "Span (seconds): " << maxtime - mintime << std::endl;
+  int navCount = navs.size();
+  int binCount = 30;
+  LineKM line(log(2.0), log(m+1), 1, binCount);
+
+  Arrayi bins(binCount);
+  bins.setTo(0);
+  for (int i = 0; i < navCount-1; i++) {
+    double span = (navs[i+1].time() - navs[i].time()).seconds();
+    int index = std::max(0, int(floor(line(log(span)))));
+    bins[index]++;
+  }
+  for (int i = 0; i < binCount; i++) {
+    cout << "Bin " << i+1 << "/" << binCount << ": " << bins[i]
+      << " intervals longer than the previous but shorter than "
+      << Duration<double>::seconds(exp(line.inv(i+1))).str() << endl;
+  }
+}
+
+int countNavSplitsByDuration(Array<Nav> navs, double durSeconds) {
+  int count = navs.size();
+  int counter = 0;
+  for (int i = 0; i < count-1; i++) {
+    if ((navs[i+1].time() - navs[i].time()).seconds() > durSeconds) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+Array<Array<Nav> > splitNavsByDuration(Array<Nav> navs, double durSeconds) {
+  int count = 1 + countNavSplitsByDuration(navs, durSeconds);
+  Array<Array<Nav> > dst(count);
+  int navCount = navs.size();
+  int from = 0;
+  int counter = 0;
+  for (int i = 0; i < navCount-1; i++) {
+    if ((navs[i+1].time() - navs[i].time()).seconds() > durSeconds) {
+      dst[counter] = navs.slice(from, i+1);
+      counter++;
+      from = i+1;
+    }
+  }
+  dst.last() = navs.sliceFrom(from);
+  assert(counter + 1 == count);
+  return dst;
+}
+
+MDArray2d calcNavsEcefTrajectory(Array<Nav> navs) {
+  int count = navs.size();
+  MDArray2d data(count, 3);
+  for (int i = 0; i < count; i++) {
+    Nav &nav = navs[i];
+
+    Length<double> xyz[3];
+    WGS84<double>::toXYZ(nav.geographicPosition(), xyz);
+
+
+    for (int j = 0; j < 3; j++) {
+      data(i, i) = xyz[j].meters();
+    }
+  }
+  return data;
+}
+
+Array<MDArray2d> calcNavsEcefTrajectories(Array<Array<Nav> > navs) {
+  int count = navs.size();
+  Array<MDArray2d> dst(count);
+  for (int i = 0; i < count; i++) {
+    dst[i] = calcNavsEcefTrajectory(navs[i]);
+  }
+  return dst;
+}
+
+void plotNavsEcefTrajectory(Array<Nav> navs) {
+  assert(areSortedNavs(navs));
+
+  GnuplotExtra plot;
+  plot.set_style("lines");
+  plot.plot(calcNavsEcefTrajectory(navs));
+  //plot.cmd("set view equal xyz");
+  plot.show();
+}
+
+void plotNavsEcefTrajectories(Array<Array<Nav> > navs) {
+  int count = navs.size();
+  LineKM hue(0, count, 0.0, 360.0);
+
+  GnuplotExtra plot;
+  plot.set_style("lines");
+  for (int i = 0; i < count; i++) {
+    plot.plot(calcNavsEcefTrajectory(navs[i]));
+  }
+  plot.cmd("set view equal xyz");
+  plot.show();
+}
+
+int countNavs(Array<Array<Nav> > navs) {
+  int counter = 0;
+  int count = navs.size();
+  for (int i = 0; i < count; i++) {
+    counter += navs[i].size();
+  }
+  return counter;
+}
+#endif
+
+} /* namespace sail */
---
+++ src/device/Arduino/libraries/Nav/Nav.h
@@ -0,0 +1,171 @@
+/*
+ * Nav.h
+ *
+ *  Created on: 16 janv. 2014
+ *      Author: jonas
+ */
+
+#ifndef NAV_H_
+#define NAV_H_
+
+#ifdef ANEMOMIND_SERVER
+#include <string>
+#include <server/common/MDArray.h>
+#include <server/common/math.h>
+#include <server/common/PhysicalQuantity.h>
+#include <server/nautical/GeographicPosition.h>
+#include <server/common/TimeStamp.h>
+#else
+#ifndef ANEMOMIND_DEVICE
+#define ANEMOMIND_DEVICE
+#endif
+#include "PhysicalQuantity.h"
+#include "GeographicPosition.h"
+class TimeStamp {
+  public:
+    TimeStamp() { }
+
+    bool operator <(const TimeStamp& other) const {
+      if (_year < other._year) return true;
+      if (_year > other._year) return false;
+      if (_month < other._month) return true;
+      if (_month > other._month) return false;
+      if (_day < other._day) return true;
+      if (_day > other._day) return false;
+      if (_hour < other._hour) return true;
+      if (_hour > other._hour) return false;
+      if (_min < other._min) return true;
+      if (_min > other._min) return false;
+      if (_sec < other._sec) return true;
+      if (_sec > other._sec) return false;
+      return true;
+    }
+
+    int _year, _month, _day, _hour, _min, _sec;
+};
+#endif
+
+namespace sail {
+
+// Helper functions to convert from the format of
+// NmeaParser
+namespace NavDataConversion {
+  TimeStamp makeTimeNmeaFromYMDhms(double yearSince2000, double month, double day, double hour, double minute, double second);
+}
+
+// Represents a single recording of data from the devices onboard.
+class Nav {
+ public:
+#ifndef ANEMOMIND_DEVICE
+  typedef std::string Id; // We may want to replace this typedef by a special type for Id's
+
+  static Nav::Id debuggingBoatId() {return "FFFFFFFF";}
+  Nav(MDArray2d row);
+  Nav();
+#else
+  Nav() { }
+#endif
+
+
+  // For sorting
+  bool operator< (const Nav &other) const {
+    return _time < other._time;
+  }
+
+  TimeStamp time() const {
+    return _time;
+  }
+
+  const GeographicPosition<double> &geographicPosition() const {return _pos;}
+  Angle<double> awa() const {return _awa;}
+  Velocity<double> aws() const {return _aws;}
+  Angle<double> magHdg() const {return _magHdg;}
+  Angle<double> gpsBearing() const {return _gpsBearing;}
+  Velocity<double> gpsSpeed() const {return _gpsSpeed;}
+  Velocity<double> watSpeed() const {return _watSpeed;}
+  Angle<double> externalTwa() const {return _twaFromFile;}
+  Velocity<double> externalTws() const {return _twsFromFile;}
+
+  HorizontalMotion<double> gpsVelocity() const;
+
+
+  void setAwa(Angle<double> awa_) {_awa = awa_;}
+  void setAws(Velocity<double> aws_) {_aws = aws_;}
+  void setMagHdg(Angle<double> magHdg_) {_magHdg = magHdg_;}
+  void setGpsBearing(Angle<double> gpsBearing_) {_gpsBearing = gpsBearing_;}
+  void setGpsSpeed(Velocity<double> gpsSpeed_) {_gpsSpeed = gpsSpeed_;}
+  void setWatSpeed(Velocity<double> watSpeed_) {_watSpeed = watSpeed_;}
+  void setTime(const TimeStamp &t) {_time = t;}
+  void setGeographicPosition(GeographicPosition<double> pos) {_pos = pos;}
+
+  void setExternalTwa(Angle<double> twa_) {_twaFromFile = twa_;}
+  void setExternalTws(Velocity<double> tws_) {_twsFromFile = tws_;}
+
+  bool operator== (const Nav &other) const;
+
+#ifndef ANEMOMIND_DEVICE
+  void setBoatId(const Id &bi) {_boatId = bi;}
+  bool hasBoatId() const {return !_boatId.empty();}
+
+  // TODO: Require this method to return true before a Nav is inserted to a database.
+  bool isIndexed() const {return hasId() && hasBoatId();}
+
+  Id id() const;
+  bool hasId() const;
+  const Id &boatId() const {return _boatId;}
+#endif
+
+ private:
+#ifndef ANEMOMIND_DEVICE
+  Id _boatId;
+#endif
+
+  Velocity<double> _gpsSpeed;
+  Angle<double> _awa;
+  Velocity<double> _aws;
+
+  // Can we trust these estimates of the true wind? Don't think so. We'd better reconstruct them
+  // with a good model.
+  Angle<double> _twaFromFile;
+  Velocity<double> _twsFromFile;
+
+  Angle<double> _magHdg;
+  Velocity<double> _watSpeed;
+  Angle<double> _gpsBearing;
+
+  GeographicPosition<double> _pos;
+
+  // What does cwd and wd stand for? I forgot...
+  // See NmeaParser: "Cumulative Water Distance" and "Water Distance"
+  double _cwd;
+  double _wd;
+
+
+
+  // TIME RELATED
+  TimeStamp _time;
+};
+
+#ifndef ANEMOMIND_DEVICE
+Array<Velocity<double> > getExternalTws(Array<Nav> navs);
+Array<Angle<double> > getExternalTwa(Array<Nav> navs);
+Array<Velocity<double> > getGpsSpeed(Array<Nav> navs);
+
+
+Array<Nav> loadNavsFromText(std::string filename, bool sort = true);
+bool areSortedNavs(Array<Nav> navs);
+void plotNavTimeVsIndex(Array<Nav> navs);
+void dispNavTimeIntervals(Array<Nav> navs);
+Array<Array<Nav> > splitNavsByDuration(Array<Nav> navs, double durSeconds);
+MDArray2d calcNavsEcefTrajectory(Array<Nav> navs);
+Array<MDArray2d> calcNavsEcefTrajectories(Array<Array<Nav> > navs);
+void plotNavsEcefTrajectory(Array<Nav> navs);
+void plotNavsEcefTrajectories(Array<Array<Nav> > navs);
+int countNavs(Array<Array<Nav> > navs);
+
+#endif
+
+
+} /* namespace sail */
+
+#endif /* NAV_H_ */
