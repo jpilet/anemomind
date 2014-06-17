489b1fff6031faffdfe824ebd03d5716b08598f5
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-06-17 15:16:34 +0200

    bra

---
+++ src/server/nautical/PlotLeeway.cpp
@@ -0,0 +1,17 @@
+/*
+ *  Created on: 2014-06-17
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <server/nautical/TestdataNavs.h>
+
+
+using namespace sail;
+
+int main() {
+  Array<Nav> navs = getTestdataNavs();
+
+  return 0;
+}
+
+
