d0f466788243bcc6b09969bc2f1aeb970c78f53f
Julien Pilet <julien.pilet@gmail.com>
2014-06-17 17:05:11 +0200

    abs. path for logfile. default to production mode.

--- www/server.js
+++ www/server.js
@@ -10,7 +10,7 @@ var express = require('express'),
  */
 
 // Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 // Application Config
 var config = require('./lib/config/config');
@@ -37,7 +37,7 @@ var app = express();
 app.use(express.bodyParser({uploadDir:__dirname + '/uploads'}));
 
 //Log accesses to file
-var logFile = fs.createWriteStream('./logFile.log', {flags: 'a'}); //use {flags: 'w'} to open in write mode
+var logFile = fs.createWriteStream('/var/log/nodejs_server.log', {flags: 'a'});
 app.use(express.logger({stream: logFile}));
 
 // Hack for psaros33
