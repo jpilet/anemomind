691747e915b7c3bc1aaa0e48bf509dfa2c7e3883
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-06-18 15:13:06 +0200

    completed the coding now

--- src/server/nautical/PlotLeeway.cpp
+++ src/server/nautical/PlotLeeway.cpp
@@ -38,8 +38,8 @@ namespace {
 
 
   Array<std::string> getPlottableValues() {
-    const int count = 4;
-    static std::string v[count] = {"awa", "aws", "leeway", "gps-speed"};
+    const int count = 6;
+    static std::string v[count] = {"awa", "aws", "leeway", "gps-speed", "time", "wat-speed"};
     return Array<std::string>(count, v);
   }
 
@@ -75,13 +75,82 @@ namespace {
     Array<std::string> defaultPlottables(ndef, def);
 
     if (extracted.empty()) {
-      return defaultPlottables();
+      return defaultPlottables;
     } else if (extracted.size() != 2 && extracted.size() != 3) {
       std::cout << "Can only make a 2d or 3d scatter plot. Please provide 2 or 3 values to plot, not " << extracted.size() << ":\n";
-      std::cout << EXPR_AND_VAL_AS_STRING(extracted) << std::endl;
+      for (auto e : extracted) {
+        std::cout << "  " << e << std::endl;
+      }
       return defaultPlottables;
     }
     return extracted;
+  }
+
+  void getBounds(int argc, const char **argv, int *fromOut, int *toOut) {
+    std::string tag = "--slice";
+    for (int i = 0; i < argc-2; i++) {
+      if (tag == argv[i]) {
+        *fromOut = atoi(argv[i+1]);
+        *toOut = atoi(argv[i+2]);
+        std::cout << "Slice from " << *fromOut << " to " << *toOut << std::endl;
+        return;
+      }
+    }
+  }
+
+  void transferValues(std::function<double(const Nav &)> f,
+      Array<Nav> navs,
+      MDArray2d dst) {
+    assert(dst.cols() == 1);
+    assert(dst.rows() == navs.size());
+    int count = navs.size();
+    for (int i = 0; i < count; i++) {
+      dst(i, 0) = f(navs[i]);
+    }
+  }
+
+  #define NAVEXPR(expr) [&] (const Nav &x) {return expr;}
+  #define TRANSFER(expr) transferValues(NAVEXPR(expr), navs, dst)
+
+  void extractValues(std::string tag, Array<Nav> navs, MDArray2d dst) {
+    if (tag == "awa") {
+      TRANSFER(x.awa().degrees());
+    } else if (tag == "aws") {
+      TRANSFER(x.aws().knots());
+    } else if (tag == "leeway") {
+      TRANSFER((x.magHdg() - x.gpsBearing()).degrees());
+    } else if (tag == "time") {
+      TimeStamp reftime = TimeStamp::UTC(2014, 06, 18, 15, 8, 00);
+      TRANSFER((x.time() - reftime).seconds());
+    } else if (tag == "gps-speed") {
+      TRANSFER(x.gpsSpeed().knots());
+    } else if (tag == "wat-speed") {
+      TRANSFER(x.watSpeed().knots());
+    } else {
+      dst.setAll(0.0);
+      std::cout << "Unknown tag: " << tag << std::endl;
+    }
+  }
+
+  void makePlot(Array<Nav> navs, Array<std::string> toPlot) {
+    int navCount = navs.size();
+    int toPlotCount = toPlot.size();
+    assert(toPlotCount == 2 || toPlotCount == 3);
+
+    Array<std::string> labels = toPlot;
+    MDArray2d plotData(navCount, toPlotCount);
+    for (int i = 0; i < toPlotCount; i++) {
+      extractValues(toPlot[i], navs, plotData.sliceCol(i));
+    }
+
+    GnuplotExtra plot;
+    plot.plot(plotData);
+    plot.set_xlabel(labels[0]);
+    plot.set_ylabel(labels[1]);
+    if (toPlotCount >= 3) {
+      plot.set_zlabel(labels[2]);
+    }
+    plot.show();
   }
 }
 
@@ -92,21 +161,11 @@ int main(int argc, const char **argv) {
   Array<Nav> allnavs = getTestdataNavs(argc, argv);
   std::cout << "Done loading " << allnavs.size() << " navs." << std::endl;
 
+  int from = 0;
+  int to = allnavs.size();
+  getBounds(argc, argv, &from, &to);
 
-  Grammar001Settings s;
-  Grammar001 g(s);
-
-  std::cout << "Parsing..." << std::endl;
-  std::shared_ptr<HTree> tree = g.parse(allnavs);
-  std::cout << "Done extracting a tree with " << tree->childCount() << " children." << std::endl;
-
-  std::shared_ptr<HTree> ch = tree->child(index);
-  Array<Nav> navs = allnavs.slice(ch->left(), ch->right());
-  std::cout << "Create a plot from " << navs.size() << " navs." << std::endl;
-
-  GnuplotExtra plot;
-  plot.plot(getDataToPlot(navs).sliceColsFrom(int(plot2d)));
-  plot.show();
+  makePlot(allnavs.slice(from, to), toPlot);
 
   return 0;
 }
