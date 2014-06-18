23f56e2f3409987b6c5dd911753cfffbee1b21ce
Julien Pilet <julien.pilet@gmail.com>
2014-06-18 11:22:08 +0200

    fix logfile path

--- www/lib/config/env/production.js
+++ www/lib/config/env/production.js
@@ -3,7 +3,7 @@
 module.exports = {
   env: 'production',
   port: process.env.PORT || 8080,
-  logfile: '/var/log/www/nodejs_server.log',
+  logfile: '/srv/data/var/log/www/nodejs_server.log',
   mongo: {
     uri: process.env.MONGOLAB_URI ||
          process.env.MONGOHQ_URL ||
