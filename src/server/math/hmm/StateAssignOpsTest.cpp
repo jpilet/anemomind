9dc41c22700af1fb226cc654e18c29f5f5c337ae
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-06-18 11:24:14 +0200

    bra va

---
+++ src/server/math/hmm/StateAssignOpsTest.cpp
@@ -0,0 +1,13 @@
+/*
+ *  Created on: 2014-06-18
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <server/math/hmm/StateAssignOps.h>
+#include <gtest/gtest.h>
+
+TEST(StateAssignOpsTest, SimpleParsingWithHints) {
+  EXPECT_TRUE(true);
+}
+
+
