40814dfb9acf27528fae6c92185e6e412207766c
Julien Pilet <julien.pilet@gmail.com>
2014-06-19 09:06:22 +0200

    Add a SpanOverlap unit test.

--- src/server/common/SpanOverlapTest.cpp
+++ src/server/common/SpanOverlapTest.cpp
@@ -37,4 +37,21 @@ TEST(SpanOverlap, Subset) {
   EXPECT_EQ(overlaps[2], SpanOverlap(Spani(9, 28), Arrayi::args(0)));
 }
 
+TEST(SpanOverlap, Complex) {
+  Spani A(1, 28);
+  Spani B(4, 9);
+  Spani C(4, 12);
+  Spani D(5, 11);
+
+  Array<SpanOverlap> overlaps = computeSpanOverlaps(Array<Spani>::args(A, B, C, D));
+  EXPECT_EQ(overlaps.size(), 7);
+  EXPECT_EQ(overlaps[0], SpanOverlap(Spani(1, 4), Arrayi::args(0)));
+  EXPECT_EQ(overlaps[1], SpanOverlap(Spani(4, 5), Arrayi::args(0, 1, 2)));
+  EXPECT_EQ(overlaps[2], SpanOverlap(Spani(5, 9), Arrayi::args(0, 1, 2, 3)));
+  EXPECT_EQ(overlaps[3], SpanOverlap(Spani(9, 11), Arrayi::args(0, 2, 3)));
+  EXPECT_EQ(overlaps[4], SpanOverlap(Spani(11, 12), Arrayi::args(0, 2)));
+  EXPECT_EQ(overlaps[6], SpanOverlap(Spani(12, 28), Arrayi::args(0)));
+}
+
+
 
