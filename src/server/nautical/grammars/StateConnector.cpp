c9d63d38073373cff4ecb37db909ed99578f8b9b
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-06-20 11:10:12 +0200

    bra

---
+++ src/server/nautical/grammars/StateConnector.cpp
@@ -0,0 +1,19 @@
+/*
+ *  Created on: Jun 20, 2014
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "StateConnector.h"
+
+namespace sail {
+
+StateConnector::StateConnector() {
+  // TODO Auto-generated constructor stub
+
+}
+
+StateConnector::~StateConnector() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace sail */
---
+++ src/server/nautical/grammars/StateConnector.h
@@ -0,0 +1,19 @@
+/*
+ *  Created on: Jun 20, 2014
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef STATECONNECTOR_H_
+#define STATECONNECTOR_H_
+
+namespace sail {
+
+class StateConnector {
+public:
+  StateConnector();
+  virtual ~StateConnector();
+};
+
+} /* namespace sail */
+
+#endif /* STATECONNECTOR_H_ */
