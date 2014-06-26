4fa66db5a1dd323bd21097059e7b704bd39af3eb
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-06-26 10:38:23 +0200

    extra file

---
+++ src/server/nautical/grammars/TrzParserExample.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Created on: Jun 26, 2014
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <iostream>
+#include <server/nautical/grammars/TrzParser.h>
+
+using namespace sail;
+
+int main() {
+  std::cout << "Trz parser" << std::endl;
+  return 0;
+}
