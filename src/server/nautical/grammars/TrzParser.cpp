cc29fca06a49e104d759864405ca1a7d0437dd52
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-06-26 10:34:13 +0200

    extra files

---
+++ src/server/nautical/grammars/TrzParser.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Created on: 2014-06-26
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "TrzParser.h"
+
+namespace mmm {
+
+namespace {
+
+}
+
+}
---
+++ src/server/nautical/grammars/TrzParser.h
@@ -0,0 +1,15 @@
+/*
+ *  Created on: 2014-06-26
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef TRZPARSER_H_
+#define TRZPARSER_H_
+
+namespace sail {
+
+
+
+}
+
+#endif
