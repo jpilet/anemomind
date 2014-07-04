be8536ee0762d84f8d4a62141b4800aacad5d1f6
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-07-04 17:17:10 +0200

    bra

--- src/server/nautical/TemporalPlot.cpp
+++ src/server/nautical/TemporalPlot.cpp
@@ -4,6 +4,10 @@
  */
 
 #include <server/nautical/TestdataNavs.h>
+#include <server/common/logging.h>
+#include <server/plot/extra.h>
+#include <server/common/string.h>
+#include <server/common/ArrayBuilder.h>
 
 namespace sail {
 
@@ -24,37 +28,95 @@ namespace sail {
     Arrayd _values;
   };
 
+  class PlotEnv;
   class PlotCmd {
    public:
+    // String that triggers this command
     virtual const char *cmd() = 0;
-    virtual void apply(std::vector<Plottable> *stack, GnuplotExtra *dst) = 0;
+
+    // Should return a single string explaining how to use it
+    virtual const char *help() = 0;
+
+
+    virtual void apply(PlotEnv *dst) = 0;
+
     virtual ~PlotCmd() {}
   };
 
   class PlotEnv {
    public:
-    int run(Array<Nav> navs, int argc, const char **argv);
+    PlotEnv(Array<Nav> navs_);
+
+    int run(int argc, const char **argv);
+
+    // Access navs
+    Array<Nav> navs() const {return _navs;}
+
+    // Accessors allowing for mutation
+    std::vector<Plottable> &stack() {return _stack;}
+    GnuplotExtra &plot() {return _plot;}
+    void dispHelp();
+    void dispCommands();
    private:
+    Array<Nav> _navs;
+    Array<PlotCmd*> _commands;
     std::vector<Plottable> _stack;
     GnuplotExtra _plot;
+    bool parsePlotCommand(const std::string &cmd);
   };
 
-  int PlotEnv::run(Array<Nav> navs, int argc, const char **argv) {
+  template <typename T>
+  void registerCmd(ArrayBuilder<PlotCmd*> *dst) {
+    static T instance;
+    dst->add(&instance);
+  }
+
+  PlotEnv::PlotEnv(Array<Nav> navs_) : _navs(navs_) {
+    ArrayBuilder<PlotCmd*> builder;
+    //registerCmd<Add>(&builder);
+    _commands = builder.get();
+  }
+
+  int PlotEnv::run(int argc, const char **argv) {
     int begin = findArg(argc, argv, "begin");
     if (begin == -1) {
-      std::cout << "Please begin your plot command sequence with 'begin'." << std::endl;
+      dispHelp();
       return -1;
     }
     for (int i = begin+1; i < argc; i++) {
-      parsePlotCommand();
+      std::string cmd(argv[i]);
+      if (!parsePlotCommand(cmd)) {
+        std::cout << "  No such command: " << cmd << std::endl;
+      }
     }
     return 0;
+  }
+
+  bool PlotEnv::parsePlotCommand(const std::string &cmd) {
+    for (auto x : _commands) {
+      if (x->cmd() == cmd) {
+        x->apply(this);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void PlotEnv::dispHelp() {
+    std::cout << "Please begin your plot command sequence with 'begin'." << std::endl;
+    std::cout << "After that, you can use any of the following commands:" << std::endl;
+    dispCommands();
+  }
+
+  void PlotEnv::dispCommands() {
+    for (auto x : _commands) {
+      std::cout << "  " << x->cmd() << ": " << x->help() << std::endl;
+    }
   }
 }
 
 int main(int argc, const char **argv) {
   using namespace sail;
-  Array<Nav> navs = getTestdataNavs();
-  PlotEnv env;
-  return env.run(navs, argc, argv);
+  PlotEnv env(getTestdataNavs(argc, argv));
+  return env.run(argc, argv);
 }
