ab3cdc6945f8e03c93b8d4aa4ae6050832698871
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-08-29 21:34:37 +0200

    added source

---
+++ src/server/common/ArgMap.cpp
@@ -0,0 +1,19 @@
+/*
+ *  Created on: 2014-
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "ArgMap.h"
+
+namespace mmm {
+
+ArgMap::ArgMap() {
+  // TODO Auto-generated constructor stub
+
+}
+
+ArgMap::~ArgMap() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace mmm */
---
+++ src/server/common/ArgMap.h
@@ -0,0 +1,20 @@
+/*
+ *  Created on: 2014-
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef ARGMAP_H_
+#define ARGMAP_H_
+
+namespace mmm {
+
+class ArgMap {
+ public:
+  ArgMap();
+  ~ArgMap();
+ private:
+};
+
+} /* namespace mmm */
+
+#endif /* ARGMAP_H_ */
