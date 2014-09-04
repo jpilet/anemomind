6ce246a2cce9b62a292ebc4de84ad725c90c6b71
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-09-04 15:31:24 +0200

    added test

---
+++ src/server/common/PolarHistogramTest.cpp
@@ -0,0 +1,13 @@
+/*
+ *  Created on: 2014-09-04
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <gtest/gtest.h>
+#include <server/common/PolarHistogram.h>
+
+TEST(PolarHistogramTest, BasicTest) {
+
+}
+
+
