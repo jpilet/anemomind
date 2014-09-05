c29a50ae062ef4f0aa70ed022f95d65f8eef50a2
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-09-05 19:13:14 +0200

    added testcase

--- src/server/common/Histogram.cpp
+++ src/server/common/Histogram.cpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <cmath>
 #include <server/common/Span.h>
+#include <server/common/LineKM.h>
 
 namespace sail {
 namespace HistogramInternal {
@@ -26,6 +27,23 @@ void drawBin(double left, double right, double height, MDArray2d dst) {
     dst(2, 0) = right;
     dst(2, 1) = height;
   }
+
+
+void drawPolarBin(double leftRadians, double rightRadians, double radius,
+    MDArray2d dst0, int segmentsPerBin, bool nautical) {
+  assert(segmentsPerBin + 2 == dst0.rows());
+  assert(2 == dst0.cols());
+  dst0.sliceRow(0).setAll(0);
+  int arcPts = segmentsPerBin + 1;
+  MDArray2d dst = dst0.sliceRowsFrom(1);
+  LineKM map(0, segmentsPerBin, leftRadians, rightRadians);
+  int k = (nautical? 1 : 0);
+  for (int i = 0; i < arcPts; i++) {
+    double angle = map(i);
+    dst(i, 0 + k) = radius*cos(angle);
+    dst(i, 1 - k) = radius*sin(angle);
+  }
+}
 
 }
 } /* namespace sail */
