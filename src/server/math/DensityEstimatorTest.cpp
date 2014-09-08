d98e04927378c494ebff68471eb908ac23afc1ef
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-09-08 20:14:06 +0200

    asdf

--- src/server/math/DensityEstimatorTest.cpp
+++ src/server/math/DensityEstimatorTest.cpp
@@ -16,13 +16,13 @@ TEST(DensityEstimatorTest, NaiveTest1d) {
   // A single training sample for the distribution.
   double c = 3.9;
 
-  KDE1 est(1.0, Array<KDE1::Vec>::args(KDE1::Vec{c}));
+  KDE1 estimator(1.0, Array<KDE1::Vec>::args(KDE1::Vec{c}));
 
   for (int i = 0; i < 30; i++) {
     double a = sin(i*123.324342);
     double b = sin(i*857.374522);
     EXPECT_EQ(std::abs(a - c) < std::abs(b - c),
-              est.density(KDE1::Vec{a}) > est.density(KDE1::Vec{b}));
+              estimator.density(KDE1::Vec{a}) > estimator.density(KDE1::Vec{b}));
   }
 }
 
@@ -31,7 +31,7 @@ TEST(DensityEstimatorTest, NaiveTest1dWithTwoPoints) {
   double c = 3.9;
   double d = 34.0;
 
-  KDE1 est(1.0, Array<KDE1::Vec>::args(KDE1::Vec{c},
+  KDE1 estimator(1.0, Array<KDE1::Vec>::args(KDE1::Vec{c},
                                     KDE1::Vec{d}));
 
   for (int i = 0; i < 30; i++) {
@@ -40,16 +40,16 @@ TEST(DensityEstimatorTest, NaiveTest1dWithTwoPoints) {
     double amin = std::min(std::abs(a - c), std::abs(a - d));
     double bmin = std::min(std::abs(b - c), std::abs(b - d));
     EXPECT_EQ(amin < bmin,
-              est.density(KDE1::Vec{a}) > est.density(KDE1::Vec{b}));
+              estimator.density(KDE1::Vec{a}) > estimator.density(KDE1::Vec{b}));
   }
 
   double middle = 0.5*(c + d);
-  double middleDensity = est.density(KDE1::Vec{middle});
+  double middleDensity = estimator.density(KDE1::Vec{middle});
 
   for (int i = 0; i < 30; i++) {
     double x = double(i)/29;
     double y = c + x*(d - c);
-    EXPECT_LE(middleDensity, est.density(KDE1::Vec{y}));
+    EXPECT_LE(middleDensity, estimator.density(KDE1::Vec{y}));
   }
 }
 
@@ -68,9 +68,9 @@ TEST(DensityEstimatorTest, TwoDims) {
   KDE2::Vec b = KDE2::Vec{-1, -1};
   KDE2::Vec c = KDE2::Vec{-0.5, 20};
 
-  KDE2 est(1, Array<KDE2::Vec>::args(a));
-  EXPECT_LE(est.density(c), est.density(b));
-  EXPECT_LE(est.density(b), est.density(a));
+  KDE2 estimator(1, Array<KDE2::Vec>::args(a));
+  EXPECT_LE(estimator.density(c), estimator.density(b));
+  EXPECT_LE(estimator.density(b), estimator.density(a));
 }
 
 
