0d731ffd5e19f232a2930d44d7066cd6423a2602
Julien Pilet <julien.pilet@gmail.com>
2014-09-10 08:30:47 +0200

    Added comments.

--- www/lib/models/tile.js
+++ www/lib/models/tile.js
@@ -11,19 +11,26 @@ var TileSchema = new Schema({
     created: Date,
     curves: [{
       curveId: String,
+
+      // Sequence of connected points that are part of 'curveId'.
+      // the curve might begin or continue on other tiles.
       points: [{
         time: Date,
+
+        // a 2-D array of OSM coordinates between 0 and 1.
+        // see
+        // http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#C.2FC.2B.2B
         pos: [Number],
-        gpsBearing: Number,
-        gpsSpeed: Number,
-        awa: Number,
-        aws: Number,
-        magHdg: Number,
-        watSpeed: Number,
-        externalTwa: Number,
-        externalTws: Number,
-        twdir: Number,
-        tws: Number
+        gpsBearing: Number,  // in degrees
+        gpsSpeed: Number,  // in knots
+        awa: Number,  // in degrees
+        aws: Number,  // in knots
+        magHdg: Number,  // in degrees
+        watSpeed: Number,  // in knots
+        externalTwa: Number,  // in degrees, relative to boat orientation
+        externalTws: Number,  // in knots
+        twdir: Number,  // in degrees, relative to north
+        tws: Number  // in knots
         }]
       }
     ]
