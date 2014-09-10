4cebbd53ec47d4a9104d4d251f22cb28eec1dd87
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-09-10 21:22:44 +0200

    good

--- src/server/nautical/FilteredPolar.cpp
+++ src/server/nautical/FilteredPolar.cpp
@@ -5,11 +5,25 @@
 
 #include <server/common/ArgMap.h>
 #include <server/plot/extra.h>
+#include <server/nautical/TestdataNavs.h>
 
 using namespace sail;
 
 int main(int argc, const char **argv) {
-  ArgMap amap;
+  double lambda = 0.0;
+  double stepSizeKnots = 0.0;
 
+  ArgMap amap;
+  registerGetTestdataNavs(amap);
+  amap.registerOption("--reg", "Set the regularization parameter").setArgCount(1).store(&lambda);
+  amap.registerOption("--step", "Set the step size in the quantization").setArgCount(1).store(&stepSizeKnots);
+
+  if (amap.parseAndHelp(argc, argv)) {
+    Array<Nav> navs = getTestdataNavs(amap);
+
+    //MDArray2i inds = quantFilterVel(Velocity<double>::knots(stepSizeKnots), );
+
+    return 0;
+  }
   return -1;
 }
