dff6e90565f0ba1fdc193ec7fba9d201c2776cb5
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-09-11 20:13:30 +0200

    curve

---
+++ src/server/math/CurveParam.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Created on: 2014-
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "CurveParam.h"
+
+namespace sail {
+
+MDArray2d parameterizeOpenCurve(int vertexCount, Arrayi ctrlInds, int regDeg) {
+
+}
+
+} /* namespace mmm */
---
+++ src/server/math/CurveParam.h
@@ -0,0 +1,15 @@
+/*
+ *  Created on: 2014-
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef CURVEPARAM_H_
+#define CURVEPARAM_H_
+
+namespace sail {
+
+MDArray2d parameterizeOpenCurve(int vertexCount, Arrayi ctrlInds, int regDeg = 2);
+
+} /* namespace mmm */
+
+#endif /* CURVEPARAM_H_ */
