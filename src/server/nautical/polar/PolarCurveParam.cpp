f18f70e06b4fa84d733af727afe698c024e9eabf
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-09-12 08:53:59 +0200

    good

--- src/server/nautical/polar/PolarCurveParam.cpp
+++ src/server/nautical/polar/PolarCurveParam.cpp
@@ -6,6 +6,7 @@
 #include <server/nautical/polar/PolarCurveParam.h>
 #include <server/math/CurveParam.h>
 #include <server/common/LineKM.h>
+#include <server/math/PolarCoordinates.h>
 #include <cassert>
 
 namespace sail {
@@ -18,7 +19,11 @@ PolarCurveParam::PolarCurveParam(int segsPerCtrlSpan, int ctrlCount, bool mirror
       _paramCount = ctrlCount;
     }
 
-    _P = parameterizeCurve(vertexCount(), makeCtrlInds(), 2, true);
+    MDArray2d PtempCtrl = parameterizeCurve(vertexCount(), makeCtrlInds(), 2, true);
+    arma::mat PmatCtrl = arma::mat(PtempCtrl.getData(), PtempCtrl.rows(), PtempCtrl.cols(), false, true);
+    _Pmat = arma::kron(PmatCtrl, arma::eye(2, 2))*makeP2CMat();
+    assert(_Pmat.n_rows == vertexDim());
+    assert(_Pmat.n_cols == paramDim());
 }
 
 Angle<double> PolarCurveParam::ctrlAngle(int ctrlIndex) const {
@@ -34,6 +39,18 @@ Arrayi PolarCurveParam::makeCtrlInds() const {
     inds[i] = ctrlToVertexIndex(i);
   }
   return inds;
+}
+
+arma::mat PolarCurveParam::makeP2CMat() const {
+  arma::mat M = arma::zeros(2*_ctrlCount, _paramCount);
+  for (int i = 0; i < _ctrlCount; i++) {
+    int rowOffset = 2*(1 + i);
+    int col = ctrlToParamIndex(i);
+    Angle<double> alpha = ctrlAngle(i);
+    M(rowOffset + 0, col) = calcPolarX(true, 1.0, alpha);
+    M(rowOffset + 1, col) = calcPolarY(true, 1.0, alpha);
+  }
+  return M;
 }
 
 int PolarCurveParam::ctrlToParamIndex(int ctrlIndex) const {
--- src/server/nautical/polar/PolarCurveParam.h
+++ src/server/nautical/polar/PolarCurveParam.h
@@ -8,6 +8,7 @@
 
 #include <device/Arduino/libraries/PhysicalQuantity/PhysicalQuantity.h>
 #include <server/common/MDArray.h>
+#include <armadillo>
 
 namespace sail {
 
@@ -18,7 +19,6 @@ namespace sail {
 class PolarCurveParam {
  public:
   PolarCurveParam(int segsPerCtrlSpan, int ctrlCount, bool mirrored);
-  Angle<double> ctrlAngle(int ctrlIndex) const;
 
   int ctrlToVertexIndex(int ctrlIndex) const {
     return _segsPerCtrlSpan*(1 + ctrlIndex);
@@ -28,14 +28,44 @@ class PolarCurveParam {
     return lastVertex() + 1;
   }
 
+
   int paramCount() const {
     return _paramCount;
   }
 
-  int ctrlToParamIndex(int paramIndex) const;
+  // Returns the dimension of the vertex vector
+  int vertexDim() const {
+    return 2*vertexCount(); // Every vertex has a 2d coordinate.
+  }
 
+  // Returns the dimension of the parameter vector
+  int paramDim() const {
+    return paramCount();
+  }
+
+
+  template <typename T>
+  void paramToVertices(Array<T> src,
+      Array<T> dst /*destination should be preallocated
+      and of the right size, for safety*/) {
+    assert(src.size() == paramDim());
+    assert(dst.size() == vertexDim());
+
+    arma::Mat<T> srcMat(src.getData(), src.size(), 1, false, true);
+    arma::Mat<T> dstMat(dst.getData(), dst.size(), 1, false, true);
+    dstMat = _Pmat*srcMat;
+
+    // Make sure armadillo didn't reallocate anything.
+    assert(dstMat.memptr() == dst.getData());
+  }
+
+
+
+  int ctrlToParamIndex(int paramIndex) const;
  private:
-  MDArray2d _P;
+  Angle<double> ctrlAngle(int ctrlIndex) const;
+
+  arma::mat _Pmat;
   int _segsPerCtrlSpan, _ctrlCount, _paramCount;
   bool _mirrored;
 
@@ -44,6 +74,7 @@ class PolarCurveParam {
   }
 
   Arrayi makeCtrlInds() const;
+  arma::mat makeP2CMat() const;
 };
 
 } /* namespace mmm */
