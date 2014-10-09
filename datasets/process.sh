f26ae0eda7d76fbb00226400433050a2df0ee36c
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-10-09 14:54:39 +0200

    bug found

--- datasets/process.sh
+++ datasets/process.sh
@@ -12,9 +12,9 @@ for boat in Irene; do #exocet psaros33_Banque_Sturdza; do
   #"${NAUTICAL}/nautical_processBoatLogs" "$boat"
   #echo "Generating polar for $boat"
   #"${NAUTICAL}/polar/nautical_polar_FilteredPolarExample" --navpath "$boat" --save "$boat/processed/filtered.json"
-  "${NAUTICAL}/polar/nautical_polar_FilteredPolarExample" --view-spans "$boat/processed/filtered.json" 400 \
+  #"${NAUTICAL}/polar/nautical_polar_FilteredPolarExample" --view-spans "$boat/processed/filtered.json" 400 \
     --optimize 7 32 --build-table "${boat}/processed/polar.dat"
-  echo Generating tiles.
+  #echo Generating tiles.
   "${NAUTICAL}/tiles/tiles_generateAndUpload" --id $boat --maxpoints 32 --navpath $boat --scale 20 \
     --polarDat "${boat}/processed/polar.dat" --boatDat "${boat}/processed/boat.dat"
 done
