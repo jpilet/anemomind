fdfc7d145baf928016d82557031ec0c0d6db7efd
Julien Pilet <julien.pilet@gmail.com>
2014-10-09 09:16:53 +0200

    Fix a time bug in device simulator.
    adds a unit test to expose the bug.

---
+++ src/device/Arduino/NMEAStats/test/ScreenRecordingSimulatorTest.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <device/Arduino/NMEAStats/test/ScreenRecordingSimulator.h>
+#include <server/common/Env.h>
+#include <server/nautical/NavNmea.h>
+
+using namespace sail;
+
+TEST(ScreenRecordingTest, screenAtTest) {
+  ScreenRecordingSimulator simulator;
+
+  std::string nmeaFile(std::string(Env::SOURCE_DIR)
+                       + std::string("/datasets/tinylog.txt"));
+
+  simulator.prepare("", "");
+  simulator.simulate(nmeaFile);
+
+  Array<Nav> navs = flattenAndSort(
+      Array<ParsedNavs>::args(loadNavsFromNmea(nmeaFile, "")),
+      ParsedNavs::makeGpsWindMask());
+
+  EXPECT_EQ(5, navs.size());
+  for (int i = 0; i < navs.size(); ++i) {
+    ScreenInfo info;
+    EXPECT_TRUE(simulator.screenAt(navs[i].time(), &info));
+    EXPECT_NEAR((info.time - navs[i].time()).seconds(), 0, 2);
+  }
+}
