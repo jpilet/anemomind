b7ee8a818a7701b97565e985f0e891c5a8e4f31b
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-10-23 14:25:31 +0200

    fixed test case

--- src/server/nautical/CalibrationModelTest.cpp
+++ src/server/nautical/CalibrationModelTest.cpp
@@ -6,8 +6,14 @@
 #include <server/nautical/CalibrationModel.h>
 #include <gtest/gtest.h>
 
-TEST(CalibModelTest, CountTest) {
+using namespace sail;
 
+TEST(CalibModelTest, CountTest) {
+  DefaultCorrectorSet<double> set;
+  EXPECT_EQ(set.magneticHeadingCorrector().paramCount(), 1);
+  EXPECT_EQ(set.awaCorrector().paramCount(), 1);
+  EXPECT_EQ(set.awsCorrector().paramCount(), 4);
+  EXPECT_EQ(set.waterSpeedCorrector().paramCount(), 4);
 }
 
 
--- src/server/nautical/SpeedCalibTest.cpp
+++ src/server/nautical/SpeedCalibTest.cpp
@@ -12,7 +12,11 @@
 using namespace sail;
 
 TEST(SpeedCalibTest, TestNoError) {
-  SpeedCalib<double> c(0.0, 0.0, 0.0, 0.0);
+  //SpeedCalib<double> c(0.0, 0.0, 0.0, 0.0);
+  SpeedCalib<double> c(SpeedCalib<double>::initKParam(),
+      SpeedCalib<double>::initMParam(),
+      SpeedCalib<double>::initCParam(),
+      SpeedCalib<double>::initAlphaParam());
   EXPECT_NEAR(c.eval(Velocity<double>::knots(0.1)).knots(), 0.1, 1.0e-6);
   EXPECT_NEAR(c.eval(Velocity<double>::knots(1.0)).knots(), 1.0, 1.0e-6);
 }
