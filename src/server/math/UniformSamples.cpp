84471d2acff7e6df670003813996fb3e34c0bc74
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-10-23 14:42:04 +0200

    make uniform samples a template

--- src/server/math/UniformSamples.cpp
+++ src/server/math/UniformSamples.cpp
@@ -8,39 +8,20 @@
 
 namespace sail {
 
-UniformSamples::UniformSamples(LineKM sampling_, Arrayd samples_) :
-    _sampling(sampling_), _samples(samples_) {}
-
-double UniformSamples::interpolateLinear(double x) const {
-  int I[2];
-  double W[2];
-  _sampling.makeInterpolationWeights(x, I, W);
-  return W[0]*_samples[I[0]] + W[1]*_samples[I[1]];
-}
+double UniformSamples::interpolateLinear(double x) const
 
 Arrayd UniformSamples::interpolateLinear(Arrayd X) const {
-  return X.map<double>([&](double x) {return interpolateLinear(x);});
+
 }
 
-double UniformSamples::interpolateLinearDerivative(double x) const {
-  int I[2];
-  double W[2];
-  _sampling.makeInterpolationWeights(x, I, W);
-  return (_samples[I[1]] - _samples[I[0]])/_sampling.getK();
-}
+double UniformSamples::interpolateLinearDerivative(double x) const
 
 Arrayd UniformSamples::interpolateLinearDerivative(Arrayd X) const {
-  return X.map<double>([&](double x) {return interpolateLinearDerivative(x);});
+
 }
 
 Arrayd UniformSamples::makeCentredX() {
-  int sampleCount = _samples.size() - 1;
-  LineKM map(0, 1, _sampling(0.5), _sampling(1.5));
-  Arrayd dst(sampleCount);
-  for (int i = 0; i < sampleCount; i++) {
-    dst[i] = map(i);
-  }
-  return dst;
+
 }
 
 
--- src/server/math/UniformSamples.h
+++ src/server/math/UniformSamples.h
@@ -14,16 +14,37 @@
 
 namespace sail {
 
+template <typename T>
 class UniformSamples {
  public:
+  typedef Array<T> ArrayType;
+
   UniformSamples() {}
 
-  UniformSamples(LineKM sampling_, Arrayd samples_);
+  UniformSamples(LineKM sampling_, ArrayType samples_) :
+    _sampling(sampling_), _samples(samples_) {}
 
-  double interpolateLinear(double x) const;
-  Arrayd interpolateLinear(Arrayd X) const;
-  double interpolateLinearDerivative(double x) const;
-  Arrayd interpolateLinearDerivative(Arrayd x) const;
+  T interpolateLinear(double x) const {
+    int I[2];
+    double W[2];
+    _sampling.makeInterpolationWeights(x, I, W);
+    return W[0]*_samples[I[0]] + W[1]*_samples[I[1]];
+  }
+
+  ArrayType interpolateLinear(Arrayd X) const {
+    return X.map<T>([&](double x) {return interpolateLinear(x);});
+  }
+
+  double interpolateLinearDerivative(double x) const {
+    int I[2];
+    double W[2];
+    _sampling.makeInterpolationWeights(x, I, W);
+    return (1.0/_sampling.getK())*(_samples[I[1]] - _samples[I[0]]);
+  }
+
+  ArrayType interpolateLinearDerivative(Arrayd X) const {
+    return X.map<T>([&](double x) {return interpolateLinearDerivative(x);});
+  }
 
   const LineKM &sampling() const {
     return _sampling;
@@ -41,13 +62,21 @@ class UniformSamples {
     return _samples.empty();
   }
 
-  Arrayd makeCentredX();
+  Arrayd makeCentredX() const {
+    int sampleCount = _samples.size() - 1;
+    LineKM map(0, 1, _sampling(0.5), _sampling(1.5));
+    Arrayd dst(sampleCount);
+    for (int i = 0; i < sampleCount; i++) {
+      dst[i] = map(i);
+    }
+    return dst;
+  }
  private:
   // Maps sample indices to time
   LineKM _sampling;
 
   // The samples
-  Arrayd _samples;
+  ArrayType _samples;
 };
 
 } /* namespace mmm */
