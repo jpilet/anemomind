cb23e57d70915c162b7b60207295406f62f6fa55
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-10-24 13:53:06 +0200

    argmap

--- src/server/nautical/CalibrationDemo.cpp
+++ src/server/nautical/CalibrationDemo.cpp
@@ -2,7 +2,24 @@
  *  Created on: 2014-
  *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
  */
+#include <server/common/ArgMap.h>
+#include <server/nautical/CalibratedNavData.h>
+#include <server/nautical/NavNmeaScan.h>
+
+using namespace sail;
+
+int main(int argc, const char **argv) {
+  ArgMap amap;
+  amap.registerOption("--ex0", "Run a preconfigured example");
+
+  if (!amap.parse(argc, argv)) {
+    return -1;
+  }
+  if (amap.help()) {
+    return 0;
+  }
 
 
 
-
+  return 0;
+}
