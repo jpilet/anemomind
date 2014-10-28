a0c687f01c8136ef9f463eb05519614e59f0a947
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-10-28 16:04:56 +0100

    addressed comments

--- src/server/math/nonlinear/GeneralizedTV.h
+++ src/server/math/nonlinear/GeneralizedTV.h
@@ -10,6 +10,37 @@
 
 namespace sail {
 
+/*
+ * Performs generalized TV denoising of a signal. Here is a
+ * relevant paper on that topic:
+ *
+      @article{karahanoglu2011signal,
+        title={A signal processing approach to generalized 1-D total variation},
+        author={Karahanoglu, Fikret Isik and Bayram, Ilker and Van De Ville, Dimitri},
+        journal={Signal Processing, IEEE Transactions on},
+        volume={59},
+        number={11},
+        pages={5265--5274},
+        year={2011},
+        publisher={IEEE}
+}
+
+  To minimize the objective function, the Majorization-Minimization
+  algorithm is used:
+
+  @INPROCEEDINGS{4107109,
+    author={Figueiredo, M.A.T. and Dias, J.B. and Oliveira, J.P. and Nowak, R.D.},
+    booktitle={Image Processing, 2006 IEEE International Conference on},
+    title={On Total Variation Denoising: A New Majorization-Minimization Algorithm and an Experimental Comparisonwith Wavalet Denoising},
+    year={2006},
+    month={Oct},
+    pages={2633-2636},
+    keywords={image denoising;wavelet transforms;image denoising;majorization-minimization algorithm;total variation method;wavelet-based method;Bayesian methods;Focusing;Image denoising;Image restoration;Noise reduction;Partial differential equations;TV;Telecommunication computing;Wavelet coefficients;Wavelet domain;Image restoration;image denoising;majorization-minimization algorithms;total variation},
+    doi={10.1109/ICIP.2006.313050},
+    ISSN={1522-4880},}
+
+ *
+ */
 class GeneralizedTV {
  public:
   GeneralizedTV(int iters = 30, double minv = 1.0e-6,
