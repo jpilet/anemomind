015af7e9db2fadccd0fbfddf2936aecef7dc81d3
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-11-04 16:09:07 +0100

    added a few more files

---
+++ src/server/nautical/CalibrationModelTest.cpp
@@ -0,0 +1,34 @@
+/*
+ *  Created on: 2014-
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <server/nautical/CalibrationModel.h>
+#include <gtest/gtest.h>
+#include <server/common/Array.h>
+
+using namespace sail;
+
+TEST(CalibModelTest, CountTest) {
+  DefaultCorrectorSet<double> set;
+  EXPECT_EQ(set.magneticHeadingCorrector().paramCount(), 1);
+  EXPECT_EQ(set.awaCorrector().paramCount(), 1);
+  EXPECT_EQ(set.awsCorrector().paramCount(), 4);
+  EXPECT_EQ(set.waterSpeedCorrector().paramCount(), 4);
+  EXPECT_EQ(set.waterSpeedCorrector().paramCount(), 4);
+  EXPECT_EQ(set.driftAngle().paramCount(), 2);
+  EXPECT_EQ(set.paramCount(), 1 + 1 + 4 + 4 + 2);
+}
+
+
+TEST(CalibModelTest, InitTest) {
+  DefaultCorrectorSet<double> set;
+  Array<double> params = Array<double>::fill(set.paramCount(), 1.0e6);
+  set.initialize(params.ptr(), false);
+  for (double p : params) {
+    EXPECT_LT(std::abs(p), 1.0e2);
+  }
+}
+
+
+
