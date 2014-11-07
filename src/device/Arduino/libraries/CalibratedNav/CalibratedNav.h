1a47fc806f13c267f603bf53b3206290ce2981ae
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-11-07 15:18:37 +0100

    removed comment

--- src/device/Arduino/libraries/CalibratedNav/CalibratedNav.h
+++ src/device/Arduino/libraries/CalibratedNav/CalibratedNav.h
@@ -44,12 +44,6 @@ class DefinedValue {
 
   bool defined() const {return _defined;}
   bool undefined() const {return !_defined;}
-
-  // TODO:
-  // Would it make sense to make
-  // the assignment operator private
-  // for this class, so that we are obliged
-  // to use the set/get methods?
  private:
   bool _defined;
   T _value;
