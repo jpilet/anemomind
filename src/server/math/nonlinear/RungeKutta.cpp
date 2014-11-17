b95e215325e40555327665d76dfd24b2b320ec87
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-11-17 11:01:37 +0100

    fixed cmkae

---
+++ src/server/math/nonlinear/RungeKutta.cpp
@@ -0,0 +1,49 @@
+/*
+ *  Created on: 2014-
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "RungeKutta.h"
+#include <server/common/logging.h>
+#include <cassert>
+
+namespace sail {
+
+RungeKutta::RungeKutta(std::shared_ptr<Function> fun) :
+    _fun(fun), _dim(_fun->inDims()) {
+  CHECK(_fun->inDims() == _fun->outDims());
+  _k1 = Arrayd(_dim);
+  _k2 = Arrayd(_dim);
+  _k3 = Arrayd(_dim);
+  _k4 = Arrayd(_dim);
+  _temp = Arrayd(_dim);
+}
+
+namespace {
+  void addWeighted(Arrayd a, double lambda, Arrayd b, Arrayd dst) {
+    int dim = a.size();
+    assert(dim == b.size());
+    assert(dim == dst.size());
+    for (int i = 0; i < dim; i++) {
+      dst[i] = a[i] + lambda*b[i];
+    }
+  }
+}
+
+
+void RungeKutta::step(Arrayd stateVector, double stepSize) {
+  _fun->eval(stateVector.ptr(), _k1.ptr());
+  addWeighted(stateVector, 0.5*stepSize, _k1, _temp);
+  _fun->eval(_temp.ptr(), _k2.ptr());
+  addWeighted(stateVector, 0.5*stepSize, _k2, _temp);
+  _fun->eval(_temp.ptr(), _k3.ptr());
+  addWeighted(stateVector, stepSize, _k3, _temp);
+  _fun->eval(_temp.ptr(), _k4.ptr());
+  double factor = stepSize/6;
+  for (int i = 0; i < _dim; i++) {
+    stateVector[i] = stateVector[i] + factor*(_k1[i] + 2*(_k2[i] + _k3[i]) + _k4[i]);
+  }
+}
+
+
+}
