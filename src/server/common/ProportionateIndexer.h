25a6ec6562f59bc7e175d6feaef430fb39366f26
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-11-17 19:21:07 +0100

    added a couple of files

---
+++ src/server/common/ProportionateIndexer.h
@@ -0,0 +1,54 @@
+/*
+ *  Created on: 2014-
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef PROPORTIONATESAMPLER_H_
+#define PROPORTIONATESAMPLER_H_
+
+#include <server/common/Array.h>
+
+namespace sail {
+
+class ProportionateIndexer {
+ public:
+  ProportionateIndexer(Arrayd proportions);
+
+  int get(double x) const;
+  void remove(int index);
+  int getAndRemove(double x);
+
+  Arrayd proportions() const;
+  Arrayb selected() const;
+  Arrayb remaining() const;
+
+
+
+  // For advanced use.
+  class Result {
+   public:
+    Result(int index_, double localX_) :
+      index(index_), localX(localX_) {}
+    int index;
+    double localX;
+  };
+  Result getBySum(int node, double x) const;
+
+  double sum() const {return _values[0];}
+ private:
+  int _offset, _count;
+  Arrayd _values;
+
+  double fillInnerNodes(int root);
+  static int parent(int index);
+  static int leftChild(int index);
+  static int rightChild(int index);
+
+  bool isLeaf(int index) const {
+    return _offset <= index;
+  }
+};
+
+}
+
+#endif /* PROPORTIONATESAMPLER_H_ */
---
+++ src/server/common/ProportionateIndexerTest.cpp
@@ -0,0 +1,48 @@
+/*
+ *  Created on: 2014-
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <gtest/gtest.h>
+#include <server/common/ProportionateIndexer.h>
+
+using namespace sail;
+
+TEST(PropTest, Test) {
+  Arrayd arr(3);
+  arr[0] = 1.0;
+  arr[1] = 2.0;
+  arr[2] = 1.0;
+  ProportionateIndexer s(arr);
+  {
+    Arrayb remaining = s.remaining();
+    Arrayb selected = s.selected();
+    for (int i = 0; i < 3; i++) {
+      EXPECT_FALSE(selected[i]);
+      EXPECT_TRUE(remaining[i]);
+    }
+  }
+  EXPECT_EQ(0, s.get(0.5/4));
+  EXPECT_EQ(1, s.get(1.5/4));
+  EXPECT_EQ(1, s.get(2.5/4));
+  EXPECT_EQ(2, s.get(3.5/4));
+  s.remove(1);
+  EXPECT_EQ(0, s.get(0.25));
+  EXPECT_EQ(2, s.get(0.75));
+  s.remove(0);
+  for (double x = 0.01; x < 1; x += 0.032344) {
+    EXPECT_EQ(2, s.get(x));
+  }
+  s.remove(2);
+
+  {
+    Arrayb remaining = s.remaining();
+    Arrayb selected = s.selected();
+    Arrayd props = s.proportions();
+    for (int i = 0; i < 3; i++) {
+      EXPECT_TRUE(selected[i]);
+      EXPECT_FALSE(remaining[i]);
+      EXPECT_NEAR(props[i], 0.0, 1.0e-6);
+    }
+  }
+}
