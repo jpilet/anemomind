918654325c05482f55dee5fa4ee7fd543123486d
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-11-17 14:54:50 +0100

    bra

--- src/server/nautical/synthtest/SynthBoatTrajectoryTest.cpp
+++ src/server/nautical/synthtest/SynthBoatTrajectoryTest.cpp
@@ -5,6 +5,8 @@
 
 #include <gtest/gtest.h>
 #include <server/nautical/synthtest/SynthBoatTrajectory.h>
+#include <armadillo>
+#include <server/common/string.h>
 
 using namespace sail;
 
@@ -61,6 +63,46 @@ TEST(SynthBoatTrajectoryTest, TangentTest3) {
 
   EXPECT_NEAR(maxsol, 270, 1.0e-5);
   EXPECT_NEAR(minsol, 180, 1.0e-5);
+}
+
+namespace {
+
+  SynthBoatTrajectory::WayPt fitWayPt(Angle<double> a, Angle<double> b, double rad) {
+    arma::mat A(2, 2);
+    A(0, 0) = cos(a);
+    A(0, 1) = sin(a);
+    A(1, 0) = cos(b);
+    A(1, 1) = sin(b);
+    arma::mat B(2, 1);
+    B(0, 0) = rad;
+    B(1, 0) = rad;
+    arma::mat X = arma::solve(A, B);
+    return makeWayPt(X[0], X[1], rad);
+  }
+}
+
+TEST(SynthBoatTrajectoryTest, TangentTest4) {
+  Angle<double> angleA = Angle<double>::degrees(90);
+  Angle<double> angleB = Angle<double>::degrees(270 + 45);
+
+  auto v = fitWayPt(Angle<double>::degrees(90),
+      Angle<double>::degrees(0), 1.0);
+  EXPECT_NEAR(v.pos[0].meters(), 1.0, 1.0e-6);
+  EXPECT_NEAR(v.pos[1].meters(), 1.0, 1.0e-6);
+  EXPECT_NEAR(v.radius.meters(), 1.0, 1.0e-6);
+
+  auto a = fitWayPt(angleA, angleB, 3.4);
+  auto b = fitWayPt(angleA, angleB, 9.76);
+
+  Angle<double> sol1, sol2;
+  SynthBoatTrajectory::WayPt::solveCircleTangentLine(a, true, b, true, &sol1, &sol2);
+  sol1 = sol1.positiveMinAngle();
+  sol2 = sol2.positiveMinAngle();
+  double minsol = std::min(sol1.degrees(), sol2.degrees());
+  double maxsol = std::max(sol1.degrees(), sol2.degrees());
+  EXPECT_NEAR(minsol, angleB.degrees() - 180.0, 1.0e-5);
+  EXPECT_NEAR(maxsol, angleA.degrees() + 180.0, 1.0e-5);
+
 }
 
 
