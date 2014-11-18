c9d260c0fda65fbf6daadf9fd79af140e0bc4907
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-11-18 18:32:36 +0100

    it passes and works

--- src/server/nautical/synthtest/BoatSimTest.cpp
+++ src/server/nautical/synthtest/BoatSimTest.cpp
@@ -37,4 +37,27 @@ TEST(BoatSimTest, SimLimit) {
   EXPECT_NEAR(129, states.last().twaWater.degrees(), 1.0);
 }
 
+TEST(BoatSimTest, SimLimit2) {
+  BoatCharacteristics ch;
+  auto windfun = makeConstantFlow(Velocity<double>::metersPerSecond(8),
+                                  Angle<double>::degrees(0));
+  auto currentfun = makeConstantFlow(Velocity<double>::knots(0.1),
+                                  Angle<double>::degrees(90));
+
+  BoatSimulator::TwaDirective d1(Duration<double>::minutes(2.0),
+                                Angle<double>::degrees(129));
+  BoatSimulator::TwaDirective d2(Duration<double>::minutes(2.0),
+                                Angle<double>::degrees(199));
+
+  auto twadir = Array<BoatSimulator::TwaDirective>::args(d1, d2);
+  BoatSimulator simulator(windfun, currentfun, ch, twadir);
+
+  Array<BoatSimulator::FullBoatState> states = simulator.simulate(Duration<double>::minutes(3.99),
+      Duration<double>::seconds(1.0), 20);
+
+
+  EXPECT_NEAR(129, states[states.middle()].twaWater.degrees(), 10.0);
+  EXPECT_NEAR(199, states.last().twaWater.degrees(), 10.0);
+}
+
 
