864396a097526cb776d21546817d1e4330fb3793
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-12-01 12:05:02 +0100

    more testing

--- src/server/nautical/synthtest/TestcaseTest.cpp
+++ src/server/nautical/synthtest/TestcaseTest.cpp
@@ -20,11 +20,11 @@ TEST(TestcaseTest, Test1) {
       Angle<double>::degrees(77));
 
 
-  TimeStamp timeoffset = TimeStamp::UTC(2012, 10, 13,
-                                5, 34, 23);
-  GeographicReference georef(
-      GeographicPosition<double>(Angle<double>::degrees(30),
-          Angle<double>::degrees(45)));
+  TimeStamp timeoffset = TimeStamp::UTC(2012, 10, 13, 5, 34, 23);
+
+  auto pos = GeographicPosition<double>(Angle<double>::degrees(30),
+                                        Angle<double>::degrees(45));
+  GeographicReference georef(pos);
 
   Testcase::FlowFun windfun = Testcase::constantFlowFun(wind);
   Testcase::FlowFun currentfun = Testcase::constantFlowFun(wind);
@@ -32,6 +32,12 @@ TEST(TestcaseTest, Test1) {
   std::default_random_engine e;
   Testcase tc(e, georef, timeoffset, windfun, currentfun,
     Array<Testcase::BoatSimDirs>(0));
+
+  EXPECT_NEAR(tc.toLocalTime(timeoffset).seconds(), 0.0, 1.0e-6);
+  auto m = tc.geoRef().map(pos);
+  EXPECT_NEAR(m[0].meters(), 0.0, 1.0e-4);
+  EXPECT_NEAR(m[1].meters(), 0.0, 1.0e-4);
+
 }
 
 
