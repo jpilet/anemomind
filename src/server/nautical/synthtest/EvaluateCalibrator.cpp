7519d70317c4169c084094b3b6e51afb61b20843
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-12-16 15:44:10 +0100

    evaluation of maneuver-based approach

--- src/server/nautical/synthtest/EvaluateCalibrator.cpp
+++ src/server/nautical/synthtest/EvaluateCalibrator.cpp
@@ -3,8 +3,39 @@
  *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
  */
 
+#include <server/nautical/synthtest/NavalSimulation.h>
+#include <server/nautical/Calibrator.h>
+#include <server/common/string.h>
+
+
+namespace {
+  using namespace sail;
+
+  void evaluate() {
+    auto sim = makeNavSimUpwindDownwindLong();
+    auto boatData = sim.boatData(0);
+    Array<Nav> navs = boatData.navs();
+    WindOrientedGrammarSettings gs;
+    WindOrientedGrammar grammar(gs);
+    auto tree = grammar.parse(navs);
+    bool calibrate(const Array<Nav>& navs,
+                   std::shared_ptr<HTree> tree,
+                   Nav::Id boatId);
+    Calibrator calib(grammar);
+    assert(calib.calibrate(navs, tree, Nav::debuggingBoatId()));
+
+    Array<Nav> navsWithTrueWind = navs.dup();
+    calib.simulate(&navsWithTrueWind);
+    Array<HorizontalMotion<double> > trueWind =
+        navsWithTrueWind.map<HorizontalMotion<double> >([=](const Nav &x) {
+          return x.trueWind();
+    });
+    auto fitness = boatData.evaluateFitnessPerNav(trueWind,
+        Array<HorizontalMotion<double> >());
+    std::cout << EXPR_AND_VAL_AS_STRING(fitness) << std::endl;
+  }
+}
+
 int main() {
   return 0;
 }
-
-
