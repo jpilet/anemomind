084835b37c74206c4444be84e468c7060c6b1661
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-12-18 17:24:03 +0100

    works well

--- src/server/math/SubdivFractalsTest.cpp
+++ src/server/math/SubdivFractalsTest.cpp
@@ -12,8 +12,11 @@ TEST(SubdivFractalsTest, IndexBox) {
   EXPECT_EQ(IndexBox<2>().numel(), 0);
   IndexBox<2> rulle = IndexBox<1>(2) + IndexBox<1>(3);
   EXPECT_EQ(rulle.numel(), 2*3);
-  EXPECT_EQ(rulle.slice(0, 1).numel(), 3);
-  EXPECT_EQ(rulle.slice(1, 1).numel(), 2);
+  auto a = rulle.slice(0, 1);
+  auto b = rulle.slice(1, 1);
+  EXPECT_EQ(a.numel(), 3);
+  EXPECT_EQ(b.numel(), 2);
+  EXPECT_EQ(rulle.slice(1, 0, 2).numel(), 2*2);
 }
 
 
