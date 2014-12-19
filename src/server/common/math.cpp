f17d673f2deb36c2aa25f2fc62289c20ba04b5db
Jonas Östlund <uppfinnarjonas@gmail.com>
2014-12-19 16:56:08 +0100

    assertions

--- src/server/common/math.cpp
+++ src/server/common/math.cpp
@@ -4,6 +4,8 @@
 namespace sail {
 
 void makeTriBasisVector(int N, int index, double *dst) {
+  assert(0 <= N);
+  assert(0 <= index && index < N);
   double lambda0, lambda1;
   solveQuadratic<double>(N, -2, -1, &lambda0, &lambda1);
   assert(std::isfinite(lambda0));
