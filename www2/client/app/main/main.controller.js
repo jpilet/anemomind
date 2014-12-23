4d7fa16a11fbf2b719ca3cd94135a72819d44fca
Julien Pilet <julien.pilet@gmail.com>
2014-12-23 11:03:46 +0100

    group all map related scope stuff in a "map" object.

--- www2/client/app/main/main.controller.js
+++ www2/client/app/main/main.controller.js
@@ -4,49 +4,51 @@ angular.module('www2App')
 
  .controller('PathController', [ '$scope', function($scope) {
   angular.extend($scope, {
-    lausanne: {
-      lat: 46.5198, 
-      lng: 6.6335,
-      zoom: 11
-    },
-    defaults: {
-      tileLayer: "http://{s}.tiles.mapbox.com/v3/openplans.map-g4j0dszr/{z}/{x}/{y}.png",
-      tileLayerOptions: {
-        attribution: '© anemomind 2014',
-        opacity: 1,
-        detectRetina: true,
-        reuseTiles: true,
-      }
-    },
-    layers: {
-      baselayers: {
-        osm:{
-          name: "Mapbox",
-          type: "xyz",
-          url: 'http://{s}.tiles.mapbox.com/v3/openplans.map-g4j0dszr/{z}/{x}/{y}.png',
-          layerOptions: {
-            subdomains: ['a', 'b', 'c'],
-            attribution: '&copy; <a href="http://anemomind.com">anemomind</a> & <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            continuousWorld: true
-          }
+    map: {
+      position: {
+        lat: 46.5198, 
+        lng: 6.6335,
+        zoom: 11
+      },
+      defaults: {
+        tileLayer: "http://{s}.tiles.mapbox.com/v3/openplans.map-g4j0dszr/{z}/{x}/{y}.png",
+        tileLayerOptions: {
+          attribution: '© anemomind 2014',
+          opacity: 1,
+          detectRetina: true,
+          reuseTiles: true,
         }
       },
-      overlays: {
-        sessions: {
-          name:'Sessions',
-          type: 'geoJSON',
-          url:'../api/tiles/{z}/{x}/{y}/Irene',
-          layerOptions: {
-            style: {
-              "color": "#00D",
-              "fillColor": "#00D",
-              "weight": 1.0,
-              "opacity": 0.6,
-              "fillOpacity": .2
+      layers: {
+        baselayers: {
+          osm:{
+            name: "Mapbox",
+            type: "xyz",
+            url: 'http://{s}.tiles.mapbox.com/v3/openplans.map-g4j0dszr/{z}/{x}/{y}.png',
+            layerOptions: {
+              subdomains: ['a', 'b', 'c'],
+              attribution: '&copy; <a href="http://anemomind.com">anemomind</a> & <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+              continuousWorld: true
             }
-          },
-          pluginOptions:{
-            cliptiles: true
+          }
+        },
+        overlays: {
+          sessions: {
+            name:'Sessions',
+            type: 'geoJSON',
+            url:'../api/tiles/{z}/{x}/{y}/Irene',
+            layerOptions: {
+              style: {
+                "color": "#00D",
+                "fillColor": "#00D",
+                "weight": 1.0,
+                "opacity": 0.6,
+                "fillOpacity": .2
+              }
+            },
+            pluginOptions:{
+              cliptiles: true
+            }
           }
         }
       }
