203e50d60b24a6cb6c6d540ec044b471afa42109
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-01-02 14:44:55 +0100

    added a few files

---
+++ src/server/nautical/synthtest/FractalFlow.cpp
@@ -0,0 +1,18 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <server/nautical/synthtest/FractalFlow.h>
+
+namespace sail {
+
+Flow makeFractalFlow(Length<double> spaceUnit,
+                     Duration<double> timeUnit,
+                     SubdivFractals::Fractal<3> xFlow,
+                     SubdivFractals::Fractal<3> yFlow) {
+
+}
+
+
+} /* namespace mmm */
---
+++ src/server/nautical/synthtest/FractalFlow.h
@@ -0,0 +1,21 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef FRACTALFLOW_H_
+#define FRACTALFLOW_H_
+
+#include <server/nautical/synthtest/Flow.h>
+#include <server/math/SubdivFractals.h>
+
+namespace sail {
+
+Flow makeFractalFlow(Length<double> spaceUnit,
+                     Duration<double> timeUnit,
+                     SubdivFractals::Fractal<3> xFlow,
+                     SubdivFractals::Fractal<3> yFlow);
+
+}
+
+#endif /* FRACTALFLOW_H_ */
