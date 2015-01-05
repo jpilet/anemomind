91fff0d834036498dd259c03a82e3a0b9bfeb294
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-01-05 16:07:12 +0100

    corrected version

--- src/server/nautical/synthtest/FractalFlow.cpp
+++ src/server/nautical/synthtest/FractalFlow.cpp
@@ -9,20 +9,20 @@ namespace sail {
 
 namespace {
   Flow::VelocityFunction makeFractalFlowSub(Length<double> unitLength,
-      Duration<double> unitTime,
+      Duration<double> unitTime, Velocity<double> unitVelocity,
       SubdivFractals::Fractal<3> velocityFractal,
       SubdivFractals::Fractal<3> angleFractal, double phase) {
     return [=](Flow::ProjectedPosition pos, Duration<double> time) {
       double local[3] = {pos[0]/unitLength, pos[1]/unitLength, time/unitTime};
-      double angle = angleFractal.eval(local);
-      double vel = velocityFractal.eval(local);
-      return sin(angle + phase)*vel;
+      double angle = angleFractal.evalOrtho(local);
+      double vel = velocityFractal.evalOrtho(local);
+      return sin(angle + phase);
     };
   }
 }
 
 Flow makeFractalFlow(Length<double> unitLength,
-    Duration<double> unitTime,
+    Duration<double> unitTime, Velocity<double> unitVelocity,
     SubdivFractals::Fractal<3> velocityFractal,
     SubdivFractals::Fractal<3> angleFractal) {
 
@@ -30,10 +30,46 @@ Flow makeFractalFlow(Length<double> unitLength,
   // more or less the same thing... Twice as much work. Maybe, if it turns out
   // that we only need flows to be defined as fractals, we can restructure the
   // code to avoid this.
-  return Flow(makeFractalFlowSub(unitLength, unitTime,
+  return Flow(makeFractalFlowSub(unitLength, unitTime, unitVelocity,
                                  velocityFractal, angleFractal, 0.5*M_PI),
-              makeFractalFlowSub(unitLength, unitTime,
+              makeFractalFlowSub(unitLength, unitTime, unitVelocity,
                                  velocityFractal, angleFractal, 0.0));
+}
+
+using namespace SubdivFractals;
+
+namespace {
+
+}
+
+
+
+Flow makeWindFlow001() {
+
+  std::default_random_engine e;
+
+  Length<double> unitLength = Length<double>::nauticalMiles(1000);
+  Duration<double> unitTime = Duration<double>::days(7);
+
+  // Tuned so that the wind is in range.
+  Velocity<double> unitVelocity = Velocity<double>::metersPerSecond(1.0);
+
+  int depth = 25;
+
+  double bd = 0.1;
+
+  int classCount = 4;
+  MaxSlope maxSlope(bd, 1.0);
+  MDArray<Rule::Ptr, 2> angleRules = makeRandomAngleRules(classCount, e);
+  MDArray<Rule::Ptr, 2> velRules = makeRandomBoundedRules(classCount, maxSlope, e);
+
+  Array<Vertex> angleCtrl =
+  Array<Vertex> velCtrl =
+
+
+  Flow result = makeFractalFlow(unitLength, unitTime, unitVelocity,
+    Fractal<3>(velRules, velCtrl), angleRules);
+
 }
 
 
