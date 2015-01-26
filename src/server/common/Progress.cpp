4779f5c6468bc327058b2221afb94fe74fd6f2c4
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-01-26 14:30:48 +0100

    Class that tells when the user should be notified about the progress of an algorithm

---
+++ src/server/common/Progress.cpp
@@ -0,0 +1,38 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <server/common/Progress.h>
+#include <server/common/logging.h>
+#include <cassert>
+
+namespace sail {
+
+Progress::Progress(int totalIterations,
+    Duration<double> notificationPeriod) :
+    _totalIterations(totalIterations),
+    _notificationPeriod(notificationPeriod),
+    _counter(0) {
+  setNextUpdateFromNow();
+}
+
+
+void Progress::setNextUpdateFromNow() {
+  _nextUpdate = _timeOffset.elapsed() + _notificationPeriod;
+}
+
+// Once an iteration is complete, call this method. It will
+// return true if a notification should be produced.
+bool Progress::endOfIteration() {
+  _counter++;
+  assert(_counter <= _totalIterations);
+  if (_timeOffset.elapsed() >= _nextUpdate) {
+    setNextUpdateFromNow();
+    return true;
+  }
+  return false;
+}
+
+
+} /* namespace mmm */
---
+++ src/server/common/Progress.h
@@ -0,0 +1,78 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef PROGRESS_H_
+#define PROGRESS_H_
+
+#include <device/Arduino/libraries/PhysicalQuantity/PhysicalQuantity.h>
+
+namespace sail {
+
+/*
+ *  Class used to give the user notifications on the progress of an
+ *  algorithm that takes long to run.
+ *
+ *  Usage:
+ *
+ *  int iters = ... ;
+ *  Progress prog(iters);
+ *  for (int i = 0; i < iters; i++) {
+ *
+ *    ... computation ...
+ *
+ *    if (prog.endOfIteration()) {
+ *      std::cout << "Time left: " << prog.timeLeft() << std::endl;
+ *    }
+ *  }
+ *
+ */
+class Progress {
+ public:
+  Progress(int totalIterations,
+      Duration<double> notificationPeriod = Duration<double>::seconds(3.0));
+
+  // Once an iteration is complete, call this method. It will
+  // return true if a notification should be produced.
+  bool endOfIteration();
+
+  Duration<double> timeLeft() const;
+
+  int remainingIterations() const {
+    return _totalIterations - _counter;
+  }
+
+  Duration<double> timeElapsed() const {
+    return _timeOffset.elapsed();
+  }
+
+  Duration<double> averageTimePerIteration() const {
+    return (1.0/_counter)*timeElapsed();
+  }
+
+  Duration<double> remainingTime() const {
+    return double(remainingIterations())*averageTimePerIteration();
+  }
+
+  // How many iterations so far.
+  int iterationsSoFar() const {
+    return _counter;
+  }
+
+  // Total number of iterations
+  int totalIterations() const {
+    return _totalIterations;
+  }
+ private:
+  TimeOffset _timeOffset;
+  Duration<double> _nextUpdate, _notificationPeriod;
+  int _counter;
+  int _totalIterations;
+
+  void setNextUpdateFromNow();
+};
+
+}
+
+#endif /* PROGRESS_H_ */
