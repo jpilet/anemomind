7b32a8295370aa763f0bcd985f8b882998342451
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-01-27 11:54:35 +0100

    nke parser

---
+++ src/server/nautical/NKEParser.cpp
@@ -0,0 +1,19 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "NKEParser.h"
+
+namespace mmm {
+
+NKEParser::NKEParser() {
+  // TODO Auto-generated constructor stub
+
+}
+
+NKEParser::~NKEParser() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace mmm */
---
+++ src/server/nautical/NKEParser.h
@@ -0,0 +1,19 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef NKEPARSER_H_
+#define NKEPARSER_H_
+
+namespace mmm {
+
+class NKEParser {
+public:
+  NKEParser();
+  virtual ~NKEParser();
+};
+
+} /* namespace mmm */
+
+#endif /* NKEPARSER_H_ */
