8c572fbc2ec0c34a9210456ca0cc0b6e1697861f
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-02-01 12:01:18 +0100

    fixed span test

--- src/server/common/Span.h
+++ src/server/common/Span.h
@@ -44,17 +44,10 @@ class Span {
 
   Span(T value) : _minv(value), _maxv(value), _initialized(true) {}
   Span(Array<T> arr) {
-    if (arr.empty()) {
-      _initialized = false;
-    } else {
-      _initialized = false;
-      int count = arr.size();
-      _minv = arr[0];
-      _maxv = _minv;
-      for (int i = 1; i < count; i++) {
-        extend(arr[i]);
-      }
-      _initialized = true;
+    _initialized = false;
+    int count = arr.size();
+    for (int i = 0; i < count; i++) {
+      extend(arr[i]);
     }
   }
 
