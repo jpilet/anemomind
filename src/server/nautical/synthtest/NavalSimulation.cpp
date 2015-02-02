cc98db77407f3643f312d9e7c892816af4623901
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-02-02 12:28:49 +0100

    m

--- src/server/nautical/synthtest/NavalSimulation.cpp
+++ src/server/nautical/synthtest/NavalSimulation.cpp
@@ -377,7 +377,7 @@ Array<BoatSimulationSpecs> makeSpecs001() {
   Array<BoatSimulationSpecs> specs(n);
   for (int i = 0; i < n; i++) {
     specs[i] = BoatSimulationSpecs(BoatCharacteristics(),
-          dirs,
+          dirs.sliceTo(2),
           sets[i]);
   }
   return specs;
