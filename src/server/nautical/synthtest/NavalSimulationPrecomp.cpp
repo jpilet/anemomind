4f5705e5870218186b1fabb1b0716966a9d1f798
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-02-03 09:21:44 +0100

    added a few files related to naval simulation

---
+++ src/server/nautical/synthtest/NavalSimulationPrecomp.cpp
@@ -0,0 +1,19 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include "NavalSimulationPrecomp.h"
+
+namespace mmm {
+
+NavalSimulationPrecomp::NavalSimulationPrecomp() {
+  // TODO Auto-generated constructor stub
+
+}
+
+NavalSimulationPrecomp::~NavalSimulationPrecomp() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace mmm */
---
+++ src/server/nautical/synthtest/NavalSimulationPrecomp.h
@@ -0,0 +1,15 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef SERVER_NAUTICAL_SYNTHTEST_NAVALSIMULATIONPRECOMP_H_
+#define SERVER_NAUTICAL_SYNTHTEST_NAVALSIMULATIONPRECOMP_H_
+
+namespace sail {
+
+
+
+}
+
+#endif /* SERVER_NAUTICAL_SYNTHTEST_NAVALSIMULATIONPRECOMP_H_ */
