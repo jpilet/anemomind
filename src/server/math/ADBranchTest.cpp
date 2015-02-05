3ecb297d6cd38e6eb36cc19931dbd0df373762e1
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-02-05 16:41:11 +0100

    branching test

---
+++ src/server/math/ADBranchTest.cpp
@@ -0,0 +1,17 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <gtest/gtest.h>
+#include <ceres/ceres.h>
+#include <server/math/ADFunction.h>
+
+TEST(ADBranchTest, Adolc) {
+  class TestFun : public AutoDiffFunction {
+
+  };
+}
+
+
+
