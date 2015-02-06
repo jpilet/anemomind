cfd78e6c2aee813464b690ece456d53e512bff48
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-02-06 11:24:17 +0100

    added a test case

---
+++ src/server/common/DestructorInvocationTest.cpp
@@ -0,0 +1,56 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <gtest/gtest.h>
+#include <server/common/string.h>
+
+namespace {
+  class MyStringClass {
+   public:
+    MyStringClass(char *stringData) : _stringData(stringData) {
+      _stringData[0] = '1';
+    }
+
+    const char *c_str() const {
+      return _stringData;
+    }
+
+    ~MyStringClass() {
+      _stringData[0] = '0';
+    }
+   private:
+    char *_stringData;
+  };
+
+  class MockObj {
+   public:
+    MockObj() {
+      _stringData[0] = ' ';
+      _stringData[1] = 0;
+    }
+    MyStringClass str() {
+      return MyStringClass(_stringData);
+    }
+
+
+
+    char _stringData[2];
+  };
+
+  std::string result;
+
+  void assignResult(const char *x) {
+    result = x;
+  }
+}
+
+TEST(DestructorInvocationTest, StringTest) {
+  MockObj obj;
+  assignResult(obj.str().c_str());
+  EXPECT_EQ(std::string(obj._stringData), "0");
+  EXPECT_EQ(result, "1");
+}
+
+
