ac3c9c29c6082967d30c524adc354cc4c7fbf366
Julien Pilet <julien.pilet@gmail.com>
2015-03-09 17:12:51 +0100

    removed boat from socket.io

--- www2/server/config/socketio.js
+++ www2/server/config/socketio.js
@@ -18,7 +18,6 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
-  require('../api/boat/boat.socket').register(socket);
   require('../api/tiles/tiles.socket').register(socket);
   require('../api/upload/upload.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
