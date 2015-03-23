f50628d9a82f72780bfd9d992c4ad2d0faf288f1
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-03-23 14:35:38 +0100

    check that it works

--- mail/syncdemo.js
+++ mail/syncdemo.js
@@ -1,14 +1,36 @@
 // A simulation of mailbox synchronizations
 var mb = require("./mail.sqlite.js");
+var assert = require('assert');
 var async = require("async");
 var q = require("q");
 
 var boxnames = ["A", "B", "C"];
 
 
+function fillWithPackets(count, srcMailbox, dstMailboxName, cb) {
+    assert(typeof count == 'number');
+    assert(typeof dstMailboxName == 'string');
+    if (count == 0) {
+	cb();
+    } else {
+	srcMailbox.sendPacket(
+	    dstMailboxName,
+	    "Some-label" + count,
+	    "some-data " + count,
+	    fillWithPackets(count-1, srcMailbox, dstMailboxName, cb)
+	);
+    }
+}
+
 function mailboxesCreated(err, mailboxes) {
-    console.log("Err = %j", err);
-    console.log("Intiated these mailboxes: %j", mailboxes);
+    fillWithPackets(
+	39,
+	mailboxes[0],
+	mailboxes[2].mailboxName,
+	function() {
+	    console.log("Filled with packets");
+	}
+    );
 }
 
 
