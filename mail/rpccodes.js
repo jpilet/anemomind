696102699eb59deb8c33b3b41648349aa24b8e85
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-03-28 14:29:05 +0100

    rpc codes added

--- mail/rpccodes.js
+++ mail/rpccodes.js
@@ -45,26 +45,7 @@ module.exports.setForeignDiaryNumber = new Call(
     undefined
 );
 
-module.exports.setForeignDiaryNumber = new Call(
-    // Args
-    new Coder(
-	function(obj) {
-	    assert(bigint.isBigInt(obj.mailboxName));
-	    assert(bigint.isBigInt(obj.diaryNumber));
-	    return bigint.serialize(obj.mailboxName + obj.diaryNumber);
-	}, function(wrappedObj) {
-	    var arr = bigint.deserializeBigInts(wrappedObj, defaultWidth);
-	    return {
-		mailboxName: arr[0],
-		diaryNumber: arr[1]
-	    };
-	}
-    ),
-
-    // No data to return => no need for a Coder.
-    undefined
-);
-
+var packetCoder = new Coder(pkt.serialize, pkt.deserialize);
 
 module.exports.getFirstPacketStartingFrom = new Call(
     // Args
@@ -85,14 +66,40 @@ module.exports.getFirstPacketStartingFrom = new Call(
 	    var diaryNumber = bigint.deserializeBigIntFromBuffer(
 		buf,
 		bigint.defaultWidth/2, bigint.defaultWidth);
-	    var lightWeight = (buf.readUInt8(bigint.defaultWidth) == 0? false : true);
+	    var lightWeight = buf.readUInt8(bigint.defaultWidth) != 0;
 	}
     ),
 
     // Return value
+    packetCoder
+);
+
+module.exports.handleIncomingPacket = new Call(
+    packetCoder,
+    undefined
+);
+
+module.exports.isAdmissible = new Call(
     new Coder(
-	function(packet) {
-	    
+	function(obj) {
+	    return bigint.serialize([obj.src, obj.dst]);
+	}, function(wrappedObj) {
+	    var arr = bigint.deserializeBigInts(wrappedObj, bigint.defaultWidth);
+	    return {
+		src: arr[0],
+		dst: arr[1]
+	    };
+	}
+    ),
+    new Coder(
+	function(p) {
+	    assert(typeof p == 'boolean');
+	    var buf = new Buffer(1);
+	    buf.writeUInt8(p? 1 : 0, 0);
+	    return buf;
+	}, function(buf) {
+	    assert(buf.length == 1);
+	    return buf.readUInt8(0) != 0;
 	}
     )
 );
