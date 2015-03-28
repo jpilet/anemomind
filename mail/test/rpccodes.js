d4cfbe41ebb7905cb1843cf48e9e476a6a365c03
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-03-28 16:15:34 +0100

    rpccodes

---
+++ mail/test/rpccodes.js
@@ -0,0 +1,54 @@
+var c = require('../rpccodes.js');
+var bigint = require('../bigint.js');
+var assert = require('assert');
+var eq = require('../eq.js');
+
+    
+
+function wrapAndUnwrap(coder, x) {
+    return coder.unwrap(coder.wrap(x));
+}
+
+
+
+it(
+    'rpccodes',
+    function() {
+	describe(
+	    'Should validate setForeignDiaryNumber',
+	    function() {
+		var args = {mailboxName: bigint.make(12), diaryNumber: bigint.make(32)};
+		var args2 = wrapAndUnwrap(c.setForeignDiaryNumber.args, args);
+		assert(eq.eqv(args, args2));
+	    }
+	);
+
+	describe(
+	    'Should validate getFirstPacketStartingFrom',
+	    function() {
+		{
+		    var args = {
+			mailboxName: bigint.make(12),
+			diaryNumber: bigint.make(13),
+			lightWeight: true,
+		    };
+		    var args2 = wrapAndUnwrap(c.getFirstPacketStartingFrom.args, args);
+		    assert(eq.eqv(args, args2, true));
+		}{
+		    var packet = {
+			diaryNumber: bigint.make(39),
+			src: bigint.make(12),
+			dst: bigint.make(3),
+			seqNumber: bigint.make(3999),
+			cNumber: bigint.make(324111),
+			label: 9,
+			data: bigint.serialize('abcdef')
+		    };
+		    assert(eq.eqv(packet, wrapAndUnwrap(
+			c.getFirstPacketStartingFrom.result, packet
+		    )));
+		}
+	    }
+	);
+    }
+);
