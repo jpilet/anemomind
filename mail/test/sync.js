40482c81ddf4ca70d14b5529675c62d18419a071
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-03-28 12:35:45 +0100

    factored out synchronize to its own file

--- mail/test/sync.js
+++ mail/test/sync.js
@@ -4,6 +4,7 @@ var assert = require('assert');
 var async = require("async");
 var pkt = require("../packet.js");
 var bigint = require('../bigint.js');
+var sync = require('../sync.js');
 
 var boxnames = ["a", "b", "c"];
 
@@ -64,166 +65,12 @@ function dispPacketCounts(boxes, cb) {
 }
 
 
-
-
-function finishSync(index, boxA, boxB, cb) {
-    // Where are done synchronizing. Save the index and
-    // move on.
-    boxA.setForeignDiaryNumber(
-	boxB.mailboxName,
-	index,
-	cb
-    );
-}
-
-function fetchFullPacket(index, boxA, boxB, cb) {
-    boxB.getFirstPacketStartingFrom(
-	index,
-	false,
-	function (err, row) {
-	    if (err == undefined) {
-
-		if (row == undefined) {
-		    console.log('WARNING: We wouldnt expect the packet to be empty');
-		    finishSync(index, boxA, boxB, cb);
-		} else {
-
-		    // Create a packet object
-		    var packet = row;
-
-		    // The actual index of the packet
-		    // that we fetched.
-		    var newIndex = row.diaryNumber;
-
-		    
-		    boxA.handleIncomingPacket(
-			packet,
-			function (err) {
-			    
-			    if (err == undefined) {
-				
-				// Recur with the next index.
-				synchronizeDirectedFrom(
-				    // IMPORTANT: Call with the index of the
-				    // packet fetched here.
-				    newIndex + 1,
-				    boxA, boxB, cb
-				);
-			    } else {
-				cb(err);
-			    }
-			}
-		    );
-		}
-		
-	    } else {
-		cb(err);
-	    }
-	}
-    );
-}
-
-function handleSyncPacketLight(index, lightPacket, boxA, boxB, cb) {
-    if (lightPacket == undefined) {
-
-	finishSync(index, boxA, boxB, cb);
-	
-    } else { 
-	boxA.isAdmissible(
-	    lightPacket.src, lightPacket.dst, lightPacket.seqNumber,
-	    function(err, admissible) {
-		if (err == undefined) {
-		    if (admissible) {
-
-			// It is admissible. Let's fetch the full packet.
-			fetchFullPacket(
-			    index,
-			    boxA,
-			    boxB,
-			    cb
-			);
-			
-		    } else {
-			// Recur, with next index.
-			synchronizeDirectedFrom(
-			    bigint.inc(lightPacket.diaryNumber),
-			    boxA, boxB, cb
-			);
-		    }
-		} else {
-		    cb(err);
-		}
-	    }
-	);
-    }
-}
-
-function synchronizeDirectedFrom(startFrom, boxA, boxB, cb) {
-    // Retrieve a light-weight packet
-    // just to see if we should accept it
-    boxB.getFirstPacketStartingFrom(
-	startFrom,
-	true,
-	function(err, row) {
-	    if (err == undefined) {
-		handleSyncPacketLight(startFrom, row, boxA, boxB, cb);
-	    } else {
-		cb(err);
-	    }	    
-	}
-    );
-}
-
-
-// Synchronize state in only one direction,
-// so that boxA will know everything that boxB knows,
-// but not the other way around.
-function synchronizeDirected(boxA, boxB, cb) {
-    // First retrieve the first number we should ask for
-    boxA.getForeignStartNumber(
-	boxB.mailboxName,
-	function(err, startFrom) {
-	    if (err == undefined) {
-		synchronizeDirectedFrom(
-		    startFrom, boxA, boxB,
-		    function() {
-			disp('Synchronized ' + boxA.mailboxName +
-				    ' from ' + boxB.mailboxName + '.]');
-			cb();
-		    }
-		);
-	    } else {
-		cb(err);
-	    }
-	}
-    );
-}
-
-
-// Perform a full synchronization of the contents in the two mailboxes.
-// boxB will share its packets with boxA, and boxA will share its packets with boxB.
-function synchronize(boxA, boxB, cb) {
-    synchronizeDirected(
-	boxA, boxB,
-	function(err) {
-	    if (err == undefined) {
-		synchronizeDirected(
-	    	    boxB, boxA,
-	    	    cb
-		);
-	    } else {
-		cb(err);
-	    }
-	}
-    );
-}
-
 // Perform pairwise synchronization of mailboxes, from left to right.
 function synchronizeArray(mailboxes, cb) {
     if (mailboxes.length < 2) {
 	cb();
     } else {
-	synchronize(
+	sync.synchronize(
 	    mailboxes[0],
 	    mailboxes[1],
 	    function (err) {
