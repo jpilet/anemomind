713ccdc53c963c716e8af8ee5d2030bdc926301d
Anemomind Edison <anemoson@ubilinux.(none)>
2015-03-30 07:57:58 +0000

    wrote a demo

--- mail/btserverdemo.js
+++ mail/btserverdemo.js
@@ -1,1 +1,14 @@
 var btserver = require('./btserver.js');
+var mb = require('mail.sqlite.js');
+
+var mailbox = new mb.Mailbox(
+    ':memory:',
+    'a',
+    function(err) {
+	if (err) {
+	    console.log('There was an error');
+	} else {
+	    btserver.runMailService('a', mailbox);
+	}
+    }
+);
