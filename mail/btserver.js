c1aa78e61e224ecfd286002fbc778560ecf3b7cd
Anemomind Edison <anemoson@ubilinux.(none)>
2015-03-30 08:23:18 +0000

    updates

--- mail/btserver.js
+++ mail/btserver.js
@@ -261,3 +261,5 @@ function runMailService(name, mailService) {
 	}
     });
 }
+
+module.exports.runMailService = runMailService;
--- mail/btserverdemo.js
+++ mail/btserverdemo.js
@@ -1,14 +1,15 @@
 var btserver = require('./btserver.js');
-var mb = require('mail.sqlite.js');
+var mb = require('./mail.sqlite.js');
+var bigint = require('./bigint.js');
 
 var mailbox = new mb.Mailbox(
     ':memory:',
-    'a',
+    bigint.make(0),
     function(err) {
 	if (err) {
 	    console.log('There was an error');
 	} else {
-	    btserver.runMailService('a', mailbox);
+	    btserver.runMailService('anemobox', mailbox);
 	}
     }
 );
