a99e507eba37fead675bac347bb835b544f220a3
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-04-01 16:24:40 +0200

    working login

--- mail/httpdemoclient.js
+++ mail/httpdemoclient.js
@@ -1,2 +1,42 @@
 // A demo client for the HTTP api
 
+var request = require('request');
+
+// Always have 'http://' at the beginning.
+var address = 'http://localhost:9000';
+var authurl = address + '/auth/local';
+
+// curl -d "{\"email\":\"kalle@abc.com\", \"password\":\"abc\"}" -H "Content-type: application/json" http://localhost:9000/auth/local
+
+// Test user that we use:
+//
+var testuser = {
+    'email': 'kalle@abc.com',
+    'password': 'abc'
+};
+
+function debugcb(err, response, body) {
+    console.log('err = %j', err);
+    console.log('response = %j', response);
+    console.log('body = %j', body);
+}
+
+
+function login(cb) {
+    var opts = {
+	url: authurl,
+	method: 'POST',
+	json: testuser
+    };
+
+    request(
+	opts,
+	debugcb
+    );
+}
+
+function test() {
+    request(address, debugcb);
+}
+
+login();
