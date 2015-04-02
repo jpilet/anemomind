25ae1141a580a8d56ca827c8fb968046f942c082
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-04-02 15:44:51 +0200

    a server connection

--- mail/httpdemoclient.js
+++ mail/httpdemoclient.js
@@ -1,15 +1,6 @@
 // A demo client for the HTTP api
 
-var assert = require('assert');
-var request = require('request');
-var JSONB = require('json-buffer');
-
-function Server(address, token) {
-    this.address = address;
-    this.authUrl = address + '/auth/local';
-    this.mailRpcUrl = address + '/api/mailrpc';
-    this.token = token;
-}
+var ServerConnection = require('./server-connection.js');
 
 // curl -d "{\"email\":\"kalle@abc.com\", \"password\":\"abc\"}" -H "Content-type: application/json" http://localhost:9000/auth/local
 
@@ -27,75 +18,6 @@ function debugcb(err, response, body) {
 }
 
 
-Server.prototype.login = function(userdata, cb) {
-    assert(userdata.email);
-    assert(userdata.password);
-    var self = this;
-    
-    var opts = {
-	url: this.authUrl,
-	method: 'POST',
-	json: userdata
-    };
-
-    request(
-	opts,
-	function(err, response, body) {
-	    if (err) {
-		cb(err);
-	    } else {
-		if (response.statusCode == 200) {
-		    cb(
-			undefined,
-			new Server(self.address, body.token)
-		    );
-		} else {
-		    cb(
-			undefined, this
-		    );
-		}
-	    }
-	}
-    );
-}
-
-Server.prototype.registerCalls = function(functions) {
-    for (var i = 0; i < functions.length; i++) {
-	var fn = functions[i];
-	assert(typeof fn == 'string');
-	this[fn] = function() {
-	    var n = Object.keys(arguments).length;
-
-	    // Omit the callback.
-	    var args = new Array(n-1);
-	    for (var i = 0; i < n - 1; i++) {
-		args[i] = arguments[i];
-	    }
-
-	    // This is the callback
-	    var cb = arguments[arguments.length-1];
-
-	    var opts = {
-		url: this.mailRpcUrl,
-		method: 'POST',
-
-		// Stringify it manually using JSONB, in order to
-		// get the buffers right.
-		json: {
-		    fn: fn,
-		    args: JSONB.stringify(args)
-		}
-	    };
-
-	    // Call it
-	    request(opts, function(err, response, body) {
-		cb(JSONB.parse(body.err), JSONB.parse(body.result));
-	    });
-	    return opts;
-	}
-	assert(this[fn] != undefined);
-    }
-}
 
 
 
@@ -106,7 +28,7 @@ Server.prototype.registerCalls = function(functions) {
 
 // Always have 'http://' at the beginning.
 var address = 'http://localhost:9000';
-var s = new Server(address);
+var s = new ServerConnection(address);
 s.login(testuser, function(err, server) {
     server.registerCalls(['add']);
     server.add(3, 4, 5, function (err, result) {
