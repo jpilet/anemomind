81a58605c2e35c181b89eb09a646f80ad107868b
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-04-02 14:40:26 +0200

    installed json buffer

--- www2/server/api/mailrpc/index.js
+++ www2/server/api/mailrpc/index.js
@@ -3,12 +3,32 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+  All rpc functions should, by convention,
+  deliver their results by calling a call-
+  back.
+*/  
+
+var rpc = {};
+
+// Just for testing it
+rpc.add = function() {
+    var sum = 0;
+    for (var i = 0; i < keys(arguments).length-1; i++) {
+	sum += arguments[i];
+    }
+    var cb(undefined, sum);
+}
+
+
+
+
+////
 function call(req, res) {
     console.log('req = %j', req);
     console.log('req.body = %j', req.body);
     return res.json(201, 'Here is the response');
 };
-
 
 function handleError(res, err) {
   return res.send(500, err);
