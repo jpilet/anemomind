a7f68072f4b172b5941099f0673c7aeaf4162253
Xavier Righetti <xavier@thefabric.com>
2015-04-03 11:21:07 +0200

    add animation controls

--- www2/client/app/map/map.controller.js
+++ www2/client/app/map/map.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('www2App')
-  .controller('MapCtrl', function ($scope, $stateParams, userDB, $http) {
+  .controller('MapCtrl', function ($scope, $stateParams, userDB, $http, $interval) {
     $scope.boat = { name: 'loading' };
 
     $http.get('/api/boats/' + $stateParams.boatId)
@@ -21,4 +21,23 @@ angular.module('www2App')
       // those can't be displayed because they are angles:
       // awa deviceTwdir externalTwa gpsBearing magHdg
     };
+
+    $scope.isPlaying = false;
+    var animationTimer;
+
+    $scope.togglePlayPause = function() {
+      if (!$scope.isPlaying) {
+        animationTimer = $interval(updatePosition, 100);
+      } else {
+        $interval.cancel(animationTimer);
+      }
+      $scope.isPlaying = !$scope.isPlaying;
+    }
+
+    function updatePosition() {
+      $scope.currentTime = new Date($scope.currentTime.getTime()+20 * 1000);
+      if ($scope.currentTime >= $scope.plotData[$scope.plotData.length - 1]['time']) {
+        $scope.currentTime = new Date($scope.plotData[0]['time']);
+      }
+    }
 });
