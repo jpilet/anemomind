0ec40f21036464eced6911d8ffe4ff6e9ee7bd8b
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-04-09 18:50:32 +0200

    added files if not already added.

---
+++ btclient/run-demo-client.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+var client = require('./btclient.js');
+
+// Register the functions that we would like to call.
+client.setCalls(['testAdd']);
+
+
+// A function that make a remote call in order to add two numbers.
+function makeRandomCall(cb) {
+    var a = 119*Math.random();
+    var b = 119*Math.random();
+    client.testAdd(a, b, function(err, sum) {
+	if (err) {
+	    cb(err);
+	} else {
+	    assert(a + b == sum);
+	    console.log('' + a + ' + ' + b + ' = ' + sum);
+	    cb();
+	}
+    });
+}
+
+// Make two different calls.
+makeRandomCall(
+    function(err) {
+	makeRandomCall(function() {});
+    }
+);
+
+
