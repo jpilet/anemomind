c9604ed8225144bd12a342c882a9ef7f791ebb0a
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-04-15 10:48:59 +0200

    work in progress

--- www2/server/components/mail/run-sync-http-demo.sh
+++ www2/server/components/mail/run-sync-http-demo.sh
@@ -1,4 +1,5 @@
-mongo --quiet anemomind-dev --eval "db.users.insert({name:'Test User','provider' : 'local', 'name' : 'test', 'email' : 'test@anemomind.com', 'hashedPassword' : 'bj0zHvlC/YIzEFOU7nKwr+OHEzSzfdFA9PMmsPGnWITGHp1zlL+29oa049o6FvuR2ofd8wOx2nBc5e2n2FIIsg==', 'salt' : 'bGwuseqg/L/do6vLH2sPVA==', 'role' : 'user' }"
+# --quiet
+mongo anemomind-dev --eval "db.users.insert({name:'Test User','provider' : 'local', 'name' : 'test', 'email' : 'test@anemomind.com', 'hashedPassword' : 'bj0zHvlC/YIzEFOU7nKwr+OHEzSzfdFA9PMmsPGnWITGHp1zlL+29oa049o6FvuR2ofd8wOx2nBc5e2n2FIIsg==', 'salt' : 'bGwuseqg/L/do6vLH2sPVA==', 'role' : 'user' });"
 # The password is 'anemoTest'
 
-node synchttpdemo.js
+node sync-http-demo.js
