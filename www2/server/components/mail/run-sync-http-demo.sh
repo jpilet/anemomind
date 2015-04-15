3ab3b18f625f324bbda5100e56711b62f9e443d1
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-04-15 11:02:35 +0200

    fixed comments in the run-sync-http-demo.sh script

--- www2/server/components/mail/run-sync-http-demo.sh
+++ www2/server/components/mail/run-sync-http-demo.sh
@@ -1,5 +1,5 @@
-# --quiet
+# To suppress the output: --quiet
 mongo anemomind-dev --eval "db.users.insert({name:'Test User','provider' : 'local', 'name' : 'test', 'email' : 'test@anemomind.com', 'hashedPassword' : 'bj0zHvlC/YIzEFOU7nKwr+OHEzSzfdFA9PMmsPGnWITGHp1zlL+29oa049o6FvuR2ofd8wOx2nBc5e2n2FIIsg==', 'salt' : 'bGwuseqg/L/do6vLH2sPVA==', 'role' : 'user' });"
-# The password is 'anemoTest'
+# The password for the registered user above is 'anemoTest'.
 
 node sync-http-demo.js
