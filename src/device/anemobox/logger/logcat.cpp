33bc1f0f77e6c4804b37f172df9ea61a0d4758dc
Julien Pilet <julien.pilet@gmail.com>
2015-05-11 09:13:04 +0200

    Add a tool to see the content of log files.

---
+++ src/device/anemobox/logger/logcat.cpp
@@ -0,0 +1,102 @@
+#include <device/anemobox/logger/Logger.h>
+#include <logger.pb.h>
+#include <server/common/logging.h>
+
+#include <iostream>
+#include <iomanip>
+
+using namespace sail;
+using namespace std;
+
+namespace {
+
+ostream& operator<<(ostream& out, const Angle<double>& angle) {
+  return out << angle.degrees() << " deg.";
+}
+ostream& operator<<(ostream& out, const Velocity<double>& value) {
+  return out << value.knots() << " kn.";
+}
+ostream& operator<<(ostream& out, const Length<double>& value) {
+  return out << value.nauticalMiles() << " miles.";
+}
+ostream& operator<<(ostream& out, const GeographicPosition<double>& value) {
+  // this format is compatible with google maps.
+  return out << setprecision(10) << "(" << value.lat().degrees() << ", "
+    << setprecision(10)<< value.lon().degrees() << ")";
+}
+
+template <class T>
+void formatValues(const vector<TimeStamp>& times,
+                  const vector<T>& values) {
+  if (times.size() != values.size()) {
+    LOG(WARNING) << "time and value array do not have the same size!";
+  }
+  const int len = min(times.size(), values.size());
+  for (int i = 0; i < len; ++i) {
+    cout << "  " << times[i].toString() << " " << values[i] << endl;
+  }
+}
+  
+void streamCat(const ValueSet& valueSet) {
+  cout << "\n" << valueSet.shortname() << ":\n";
+  vector<TimeStamp> times;
+  Logger::unpackTime(valueSet, &times);
+
+  if (valueSet.has_angles()) {
+    vector<Angle<double>> angles;
+    Logger::unpack(valueSet.angles(), &angles);
+    formatValues(times, angles);
+  }
+  if (valueSet.has_velocity()) {
+    vector<Velocity<double>> values;
+    Logger::unpack(valueSet.velocity(), &values);
+    formatValues(times, values);
+  }
+  if (valueSet.has_length()) {
+    vector<Length<double>> values;
+    Logger::unpack(valueSet.length(), &values);
+    formatValues(times, values);
+  }
+  if (valueSet.has_pos()) {
+    vector<GeographicPosition<double>> values;
+    Logger::unpack(valueSet.pos(), &values);
+    formatValues(times, values);
+  }
+}
+
+void logCat(const char* file) {
+  LogFile data;
+  if (!Logger::read(file, &data)) {
+    LOG(ERROR) << file << ": can't read log file.";
+    return;
+  }
+
+  if (data.has_anemobox()) {
+    cout << "Anemobox: " << data.anemobox() << endl;
+  }
+  if (data.has_boatid()) {
+    cout << "boatId: " << data.boatid() << endl;
+  }
+
+  if (data.has_boatname()) {
+    cout << "boatName: " << data.boatname() << endl;
+  }
+
+  for (int i = 0; i < data.stream_size(); ++i) {
+    streamCat(data.stream(i));
+  }
+}
+
+}  // namespace
+
+int main(int argc, const char* argv[]) {
+
+  if (argc < 1) {
+    LOG(FATAL) << "Usage: " << argv[0] << " <logfile> [<logfile> ...]";
+  }
+
+  for (int i = 1; i < argc; ++i) {
+    logCat(argv[i]);
+  }
+}
+
