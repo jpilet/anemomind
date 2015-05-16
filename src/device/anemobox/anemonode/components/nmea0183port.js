cfba72c1cd61d111017455e8ecd27adba53748a9
Julien Pilet <julien@anemomind.com>
2015-05-16 09:22:53 +0000

    Revert "Switched to fs.read/write to access serial port."
    This reverts commit 6d10efcd1f23b55852ac107a9846923bb2e4dbf3.

--- src/device/anemobox/anemonode/components/nmea0183port.js
+++ src/device/anemobox/anemonode/components/nmea0183port.js
@@ -1,43 +1,38 @@
 // Data source: NMEA0183
 
 var anemonode = require('../build/Release/anemonode');
-var exec = require('child_process').exec;
-var fs = require('fs');
+var SerialPort = require("serialport").SerialPort
 
-var nmeaPortFd;
+var nmea0183Port;
 
-function init(port) {
-exec("stty -F " + port + " 4800",  function (error, stdout, stderr) {
-  if (error) { console.log(error); }
+function init(nmea0183PortPath) {
+  var port = new SerialPort(nmea0183PortPath, {
+    baudrate: 4800
+  }, false); // this is the openImmediately flag [default is true]
 
-  fs.open(port, 'r+', function(err, fd) {
-
-    nmeaPortFd = fd;
-  var buffer = new Buffer(4096);
   var nmeaPortSource = new anemonode.Nmea0183Source(
-      "NMEA0183: " + port);
+      "NMEA0183: " + nmea0183PortPath);
 
-    setInterval(function() {
-      fs.read(fd, buffer, 0, 4096, null, function(err, bytes_read, buffer) {
-	 if (err) {
-	   console.log(err);
-	 } else {
-	   //console.log('received: ' +bytes_read + ' bytes: ' +  buffer.slice(0, bytes_read));
-	   nmeaPortSource.process(buffer.slice(0, bytes_read));
-	 }
-       });
-    }, 500);
+  port.open(function (error) {
+    if ( error ) {
+      console.log('failed to open: '+error);
+    } else {
+      console.log('Listening to NMEA0183 port ' + nmea0183PortPath);
+      nmea0183Port = port;
+      port.on('data', function(data) {
+        nmeaPortSource.process(data);
+        // TODO: log raw NMEA.
+      });
+    }
   });
-});
 }
 
 function emitNmea0183Sentence (sentence) {
-  if (nmeaPortFd) {
-    //console.log(sentence.toString('ascii'));
-    fs.write(nmeaPortFd, sentence, 0, sentence.length, function(err, written, buffer) {
-});
+  if (nmea0183Port) {
+    nmea0183Port.write(sentence);
   }
 }
 
 module.exports.init = init;
 module.exports.emitNmea0183Sentence = emitNmea0183Sentence;
+
