baf1c0e23b599c48816b59a38faf2d43eef21ed8
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-05-21 16:18:15 +0200

    reset a local mailbox

--- src/device/anemobox/anemonode/test/LocalMailbox.js
+++ src/device/anemobox/anemonode/test/LocalMailbox.js
@@ -1,1 +1,19 @@
-var 
+var lmb = require('../components/LocalMailbox.js');
+var assert = require('assert');
+
+it('LocalMailbox', function() {
+  describe(
+    'Instantiate a local mailbox and reset it',
+    function(done) {
+      lmb.setMailRoot('/tmp/localmail/');
+      lmb.open(function(err, mb) {
+	assert.equal(err, undefined);
+	assert(mb);
+	mb.reset(function(err) {
+	  assert.equal(err, undefined);
+	  done();
+	});
+      });
+    }
+  );
+});
