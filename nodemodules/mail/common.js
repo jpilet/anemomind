bba6ca063c3f15e3d16db264fc1b17306923cd23
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-05-22 12:03:26 +0200

    file utilities

--- nodemodules/mail/common.js
+++ nodemodules/mail/common.js
@@ -15,6 +15,14 @@ function isValidMailboxName(x) {
   return isIdentifier(x);
 }
 
+function isPacket(x) {
+  if (typeof x == 'object') {
+    return x.src && x.dst && x.label && x.seqNumber && x.data;
+  }
+  return false;
+}
+
 module.exports.isCounter = isCounter; 
 module.exports.isIdentifier = isIdentifier;
 module.exports.isValidMailboxName = isValidMailboxName;
+module.exports.isPacket = isPacket;
--- nodemodules/mail/file.js
+++ nodemodules/mail/file.js
@@ -3,30 +3,59 @@ var commont = require('./common.js');
 var mb = require('./mail.sqlite.js');
 var fs = require('fs');
 
+// fs.writeFile(filename, data, cb)
+
+
+function isFileMessage(x) {
+  if (typeof x == 'object') {
+    return x.path && x.info && x.data;
+  }
+  return false;
+}
 
 function packFileData(path, info, cb) {
   fs.readFile(path, function(err, data) {
     if (err) {
       cb(err);
     } else {
+      
       // We could just send the buffer,
       // but I think it makes sense to send it together with
       // some context.
-      packet = {
+      var message = {
 	path: path, // The path of the file on the sender file system.
 	info: info, // Extra information about the file, free format. E.g. a tag saying that
 	            // it is a log file.
 	data: data  // The file data.
       };
+
+      assert(isFileMessage(message));
+           
+      // Call the callback with the packed packet.
+      cb(null, msgpack.pack(message));
     }
   });
 }
 
-// Send a file from the current mailbox.
-function sendFile(mailbox, dst, path, desc, cb) {
-  if (!(mb.isMailbox(mailbox))) {
+
+
+
+// Send a file
+function sendFile(mailbox,   // The local mailbox
+		  dst,       // destination mailbox
+		  path,      // Path to the file to send
+		  info,      // Misc information for the user to choose.
+		  cb) {      // Called once the file has been put in the mailbox,
+                             //   waiting to be sent.
+  if (!(mb.isMailbox(mailbox) && common.isIdentifier(dst))) {
     cb(new Error('Bad input to sendFile'));
   } else {
-    packFileData(path, 
+    packFileData(path, info, function(err, buf) {
+      mailbox.sendPacket(dst, common.file, buf, cb);
+    });
   }
+}
+
+function unpackFile(packet) {
+  msgpack.unpack(buf);
 }
