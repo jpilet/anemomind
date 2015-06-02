d535e766715c5a319656f49555b3f57382908b46
Thomas Rubeli <thomas.j.rubeli@gmail.com>
2015-06-02 16:25:11 +0200

    basic panel with shapes only

--- www2/client/app/instrumentsPanel/instrumentsPanel.directive.js
+++ www2/client/app/instrumentsPanel/instrumentsPanel.directive.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('www2App')
-  .directive('instrumentsPanel', function () {
+.directive('instrumentsPanel', function () {
     return {
       templateUrl: 'app/instrumentsPanel/instrumentsPanel.html',
       restrict: 'EA',
-      link: function (scope, element, attrs) {
-      	Circle();
-      }
-    };
-  });
+      link: function ($scope, element, attrs) {
+        createPanel();
+
+        $scope.$watch('currentTime', function(newValue, oldValue) {
+          updateGraphs();
+      }, true);
+    }
+};
+});
--- www2/client/app/instrumentsPanel/instrumentsPanel.js
+++ www2/client/app/instrumentsPanel/instrumentsPanel.js
@@ -1,31 +1,70 @@
+var row=3;
+var column=2;
+var width=400;
+var height=400;
+
+var container;
 
 
+var createPanel = function(){
 
-var Circle = function(){
-	alert("salut");
+	var svgContainer = d3.select(document.getElementById("instruments_panel")).append("svg")
+	.attr("width", width)
+	.attr("height", height)
+	.append("g").attr("transform", "translate(" + 0 + "," + 0 + ")");
 
-	var svgContainer = d3.select("body").append("svg")
-	.attr("width", 200)
-	.attr("height", 600);
+	container=svgContainer;
 
+	//compute horizontal & vertical spacing
+	var hSpace=width/column;
+	var vSpace=height/row;
+	var hCenter=hSpace/2;
+	var vCenter=vSpace/2;
 
-
-	var jsonCircles = [
-	{ "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
-	{ "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
-	{ "x_axis": 110, "y_axis": 200, "radius": 20, "color" : "red"}];
+	//some testing component
+	var circleData = [
+	{ "x_axis": width/2, "y_axis": vCenter, "radius": 20, "color" : "green" },
+	{ "x_axis": hCenter, "y_axis": vSpace+vCenter, "radius": 20, "color" : "purple"},
+	{ "x_axis": hSpace+hCenter, "y_axis": vSpace+vCenter, "radius": 20, "color" : "red"},
+	{ "x_axis": hCenter, "y_axis": vSpace*2+vCenter, "radius": 20, "color" : "green" },
+	{ "x_axis": hSpace+hCenter, "y_axis": vSpace*2+vCenter, "radius": 20, "color" : "purple"}];
 
 
 	var circles = svgContainer.selectAll("circle")
-	.data(jsonCircles)
+	.data(circleData)
 	.enter()
 	.append("circle");
-
-
 
 	var circleAttributes = circles
 	.attr("cx", function (d) { return d.x_axis; })
 	.attr("cy", function (d) { return d.y_axis; })
 	.attr("r", function (d) { return d.radius; })
-	.style("fill", function(d) { return d.color; });
+	.style("fill", function(d) { return d.color; })
+
+
+
+	var mySquare=svgContainer.append("rect")
+  	.attr("x",60)
+  	.attr("y",60)
+  	.attr("width",60)
+    .attr("height",60)
+
+
+}
+
+function updateGraphs(){
+
+	//This function will update the graphs according to currentTime
+
+	container.selectAll("circle")
+	.transition()
+	.attr("r",3)
+	.duration(0)
+  	.delay(0);
+
+	container.selectAll("circle")
+	.transition()
+	.attr("r",60)
+	.duration(1000)
+  	.delay(100);
 }
