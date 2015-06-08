3d257f37da871f17891c10b2742bde42e923a8a6
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-06-08 15:23:46 +0200

    reschedule a delayed call later

--- src/device/anemobox/anemonode/test/DelayedCall.js
+++ src/device/anemobox/anemonode/test/DelayedCall.js
@@ -14,4 +14,21 @@ describe('DelayedCall', function() {
       }, 100);
     }, 100);
   });
+
+  it('Should reschedule a delayed call later', function(done) {
+    var performed = false;
+    var dc = new DelayedCall(function() {performed = true;});
+    dc.callDelayed(100); // At time 100
+    setTimeout(function() { // At time 50
+      assert(!performed);
+      dc.callDelayed(200); // At time 50 + 200 = 250
+      setTimeout(function() { // At time 50 + 100 = 150
+        assert(!performed);
+        setTimeout(function() { // At time 300
+          assert(performed);
+          done();
+        }, 150);
+      }, 100);
+    }, 50);
+  });
 });
