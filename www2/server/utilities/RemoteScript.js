5c9aae9900947540401700c82839b88bccb17d5c
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-06-09 12:00:46 +0200

    added a remote script

---
+++ www2/server/utilities/RemoteScript.js
@@ -0,0 +1,33 @@
+var naming = require('mail/naming.js');
+//var mb = require('../server/api/mailrpc/mailbox.js');
+var script = require('mail/scripts.js');
+var Boat = require('../server/api/mailrpc/boat.model.js');
+/*
+
+function getBoxId(boatId, cb) {
+  Boat.findById(boatId, function(err, boat) {
+    if (err) {
+      cb(err);
+    } else if (!boat) {
+      cb(new Error('No such boat'));
+    } else {
+      cb(null, boat.anemobox);
+    }
+  });
+}
+
+function listAllBoats(cb) {
+  Boat.find({}, cb);
+}
+
+// Used to send a script from a boat to a box.
+function executeScriptOnBoat(boatId, type, script, cb) {
+  mb.openMailbox(
+    naming.makeMailboxNameFromBoatId(boatId),
+    function(err, mailbox) {
+      script.runRemoteScript(
+        mailbox, naming.makeMailboxNameFromBoxId(),
+        type, script, cb);
+    });
+}
+*/
