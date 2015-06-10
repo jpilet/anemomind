32fcb55c814d69abdcd57d2c3ba748c8187fbe15
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-06-10 16:07:39 +0200

    added remote script

---
+++ utilitites/RemoteScript.js
@@ -0,0 +1,11 @@
+var mb = require('../mail/ht');
+
+var testuser = {
+    'email': 'test@anemomind.com',
+    'password': 'anemoTest'
+};
+
+var address = 'http://localhost:9000';
+
+function executeRemoteScript(user, ) {
+}
