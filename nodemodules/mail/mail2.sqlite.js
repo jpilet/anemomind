ef0c7354103280a2f7a2e171e1cd8989a14da8b7
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-06-12 23:19:18 +0200

    creating a new mailbox and resetting it works

--- nodemodules/mail/mail2.sqlite.js
+++ nodemodules/mail/mail2.sqlite.js
@@ -9,9 +9,32 @@ var fullschema = "CREATE TABLE IF NOT EXISTS packets (src TEXT, dst TEXT, \
 seqNumber TEXT, label INT, data BLOB, PRIMARY KEY(src, dst, seqNumber)); \
 CREATE TABLE IF NOT EXISTS lowerbounds (src TEXT, dst TEXT, lower TEXT, PRIMARY KEY(src, dst));";
 
+function beginTransaction(db, cb) {
+  //console.log("BEGIN TRANSACTION");
+  //inTransaction = true;
+  db.beginTransaction(cb);
+}
+
+function commit(T, cb) {
+  //console.log("END TRANSACTION");
+  //inTransaction = false;
+  T.commit(cb);
+}
+
+
 function createAllTables(db, cb) {
   db.exec(fullschema, cb);
 }
+
+function dropTables(db, cb) {
+  var names = ['packets', 'lowerbounds'];
+  var query = '';
+  for (var i = 0; i < names.length; i++) {
+    query += 'DROP TABLE IF EXISTS ' + names[i] + ';';
+  }
+  db.exec(query, cb);
+}
+
 
 function openDBWithFilename(dbFilename, cb) {
   var db = new TransactionDatabase(
@@ -37,25 +60,63 @@ function openDBWithFilename(dbFilename, cb) {
 
 
 
-function EndPoint(filename, mailboxName, db) {
+function EndPoint(filename, name, db) {
   this.db = db;
   this.dbFilename = filename;
-  this.mailboxName = mailboxName;
+  this.name = name;
 }
 
-function tryMakeEndPoint(filename, mailboxName, cb) {
-  if (!(common.isString(filename) && common.isIdentifier(mailboxName))) {
+function tryMakeEndPoint(filename, name, cb) {
+  if (!(common.isString(filename) && common.isIdentifier(name))) {
     cb(new Error('Invalid input to tryMakeEndPoint'));
   } else {
     openDBWithFilename(filename, function(err, db) {
       if (err) {
         cb(err);
       } else {
-        cb(null, new EndPoint(filename, mailboxName, db));
+        cb(null, new EndPoint(filename, name, db));
       }
     });
   }
 }
 
+EndPoint.prototype.reset = function(cb) {
+  beginTransaction(this.db, function(err, T) {
+    if (err) {
+      cb(err);
+    } else {
+      dropTables(T, function(err) {
+	if (err) {
+	  cb(err);
+	} else {
+	  createAllTables(T, function(err) {
+	    commit(T, function(err2) {
+	      cb(err || err2);
+	    });
+	  });
+	}
+      });
+    }
+  });
+}
+
+function tryMakeAndResetEndPoint(filename, name, cb) {
+  tryMakeEndPoint(filename, name, function(err, ep) {
+    if (err) {
+      cb(err);
+    } else {
+      ep.reset(function(err) {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null, ep);
+        }
+      });
+    }
+  });
+}
+
+
 module.exports.EndPoint = EndPoint;
 module.exports.tryMakeEndPoint = tryMakeEndPoint;
+module.exports.tryMakeAndResetEndPoint = tryMakeAndResetEndPoint;
--- nodemodules/mail/test/mail2.sqlite.js
+++ nodemodules/mail/test/mail2.sqlite.js
@@ -6,7 +6,17 @@ describe('EndPoint', function() {
     mb.tryMakeEndPoint("/tmp/newendpt.js", "newendpt", function(err, ep) {
       assert(!err);
       assert(ep instanceof mb.EndPoint);
-      assert.equal(ep.mailboxName, "newendpt");
+      assert.equal(ep.name, "newendpt");
+      done();
+    });
+  });
+  
+  it('Should instantiate a new end point without problems, and reset it', function(done) {
+    mb.tryMakeAndResetEndPoint("/tmp/newendpt.js", "newendpt", function(err, ep) {
+      console.log(err);
+      assert(!err);
+      assert(ep instanceof mb.EndPoint);
+      assert.equal(ep.name, "newendpt");
       done();
     });
   });
