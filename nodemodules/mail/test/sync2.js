eeb2e751f682f5a7862df8a1da9efa7e05b15026
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-06-13 09:39:31 +0200

    sync2

---
+++ nodemodules/mail/test/sync2.js
@@ -0,0 +1,48 @@
+var sync2 = require('../sync2.js');
+var mail2 = require('../mail2.sqlite.js');
+var assert = require('assert');
+
+function makeA(cb) {
+  mail2.tryMakeAndResetEndPoint('/tmp/epa.db', 'a', cb);
+}
+
+function makeB(cb) {
+  mail2.tryMakeAndResetEndPoint('/tmp/epa.db', 'a', cb);
+}
+
+function makeAB(cb) {
+  makeA(function(err, A) {
+    if (err) {
+      cb(err);
+    } else {
+      makeB(function(err, B) {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null, A, B);
+        }
+      });
+    }
+  });
+}
+
+function makeBuf(x) {
+  var dst = new Buffer(x.length);
+  for (var i = 0; i < x.length; i++) {
+    dst[i] = i;
+  }
+  return dst;
+}
+
+
+describe('sync2', function() {
+  it('Should synchronize one packet', function(done) {
+    makeAB(function(err, a, b) {
+      assert(!err);
+      a.sendPacket('b', 119, makeBuf([0, 1, 2]), function(err) {
+        assert(!err);
+        done();
+      });
+    });
+  });
+});
