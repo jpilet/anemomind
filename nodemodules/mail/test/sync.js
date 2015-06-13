0fee573450521f6671346cabb9a71854d1c87a08
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-06-13 15:41:16 +0200

    BUG CONFIRMEDmocha!

--- nodemodules/mail/test/sync.js
+++ nodemodules/mail/test/sync.js
@@ -7,39 +7,67 @@ var sync = require('../sync.js');
 
 var boxnames = ["a", "b", "c"];
 // Main call to run the demo
-describe(
-  'Synchronize',
-  function() {
-    it(
-      'Synchronize three mailboxes with each other',
-      function(done) {
 
-	this.timeout(4000);
 
-	async.map(
-	  boxnames,
-	  function (boxname, cb) {
-	    mb.tryMakeMailbox(":memory:", boxname, cb);
-	  },
-	  function(err, boxes) {
-	    assert.equal(err, undefined);
-	    demo.synchronizeThreeMailboxes(boxes, done);
-	  });
-      });
-  
-  it('propbug',
+function makeAndReset(name, cb) {
+  mb.tryMakeMailbox(
+    '/tmp/testbox' + name + '.db',
+    name, function(err, mailbox) {
+      if (err) {
+        cb(err);
+      } else {
+        mailbox.reset(function(err) {
+          if (err) {
+            cb(err);
+          } else {
+            cb(null, mailbox);
+          }
+        });
+      }
+    });
+}
+
+
+describe('Synchronize', function() {
+  it(
+    'Synchronize three mailboxes with each other',
     function(done) {
-      makeAndReset('a', function(err, a) {
+
+      this.timeout(4000);
+
+      async.map(
+	boxnames,
+	function (boxname, cb) {
+	  mb.tryMakeMailbox(":memory:", boxname, cb);
+	},
+	function(err, boxes) {
+	  assert.equal(err, undefined);
+	  demo.synchronizeThreeMailboxes(boxes, done);
+	});
+    });
+  
+  it('propbug', function(done) {
+    makeAndReset('a', function(err, a) {
+      assert(!err);
+      makeAndReset('b', function(err, b) {
         assert(!err);
-        makeAndReset('b', function(err, b) {
+        makeAndReset('a2', function(err, a2) {
           assert(!err);
-          makeAndReset('a2', function(err, c) {
+          a.sendPacket('b', 129, new Buffer(3), function(err) {
             assert(!err);
-            a.sendPacket('b', 129, new Buffer(3), function(err) {
+            sync.synchronize(a, b, function(err) {
               assert(!err);
-              sync.synchronize(a, b, function(err) {
+              b.getTotalPacketCount(function(err, count) {
                 assert(!err);
-                done();
+                assert.equal(count, 1);
+                sync.synchronize(a2, b, function(err) {
+                  assert(!err);
+                  a2.getTotalPacketCount(function(err, count) {
+                    assert(!err);
+                    assert.equal(count, 1);
+                    done();
+                  });
+                });
               });
             });
           });
@@ -47,5 +75,6 @@ describe(
       });
     });
   });
+});
 
 
