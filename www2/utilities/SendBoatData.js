edd479326b00f2fcf76119136bb4d5f4abee5e4f
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-06-24 18:46:35 +0200

    added file to send boat data

---
+++ www2/utilities/SendBoatData.js
@@ -0,0 +1,15 @@
+var common = require('./common.js');
+common.init();
+
+var boatId = process.argv[2];
+var srcFilename = process.argv[3];
+var dstFilename = process.argv[4];
+common.sendBoatData(boatId, srcFilename, dstFilename, function(err) {
+  if (err) {
+    console.log('Failed to send boat data, because');
+    console.log(err);
+  } else {
+    console.log('Successfully posted boat data');
+  }
+  process.exit();
+});
