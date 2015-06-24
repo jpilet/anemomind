31901533a74e8019a15e6d5f088ff18e0fcfff20
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-06-24 12:39:49 +0200

    posting files works

--- nodemodules/mail/test/files.js
+++ nodemodules/mail/test/files.js
@@ -1,6 +1,8 @@
 var files = require('../files.js');
 var fs = require('fs');
 var assert = require('assert');
+var Q = require('q');
+var mail2 = require('../mail2.sqlite.js');
 
 describe('File transfer code', function() {
   it('packfiles', function(done) {
@@ -24,5 +26,21 @@ describe('File transfer code', function() {
         });
       });
     });
+  });
+
+  it('transferfiles', function(done) {
+    Q.all([
+      Q.nfcall(mail2.tryMakeAndResetEndPoint, '/tmp/epa.db', 'a'),
+      Q.nfcall(mail2.tryMakeAndResetEndPoint, '/tmp/epb.db', 'b')
+      ]).then(function(eps) {
+        var a = eps[0];
+        var b = eps[1];
+        var srcFilename = '/tmp/boat.dat';
+        Q.nfcall(fs.writeFile, srcFilename, 'Interesting data')
+          .then(files.sendFiles(a, 'b', [{src: srcFilename, dst:'boat.dat'}]))
+          .then(function() {
+            done();
+          });
+      });
   });
 });
