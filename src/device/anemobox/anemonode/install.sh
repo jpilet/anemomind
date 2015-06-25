0873095c9f19531817cb6063a845a406c44802f4
Julien Pilet <julien@anemomind.com>
2015-06-25 06:10:08 +0000

    install.sh communicates with the release repository

--- src/device/anemobox/anemonode/install.sh
+++ src/device/anemobox/anemonode/install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
-rm -fR node_modules/mail || true
+#rm -fR node_modules/mail || true
+#npm install
+#node-gyp configure
+#node-gyp build
 
-npm install
+[ -f ~/.ssh/id_rsa ] || (mkdir -p ~/.ssh && cp id_rsa ~/.ssh)
 
-node-gyp configure
-node-gyp build
+[ -e /anemonode ] || git clone git+ssh://anemobox@vtiger.anemomind.com/home/anemobox/anemobox.git /anemonode
 
-rsync -ar . /anemonode
+rsync -ar --exclude=id_rsa --exclude=install.sh . /anemonode
 
 git rev-parse HEAD > /anemonode/commit
+
+echo "Installed. After testing, please validate the release files with: "
+echo "  cd /anemonode ; git add . ; git commit ; git push"
+echo "or invalidate with:"
+echo "  cd /anemonode ; git reset --hard"
+
