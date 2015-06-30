a74acf7a97572631653e5a74c72a213d5f6cb528
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-06-30 12:22:12 +0200

    removed stuff which should not be there

--- src/device/anemobox/anemonode/run.sh
+++ src/device/anemobox/anemonode/run.sh
@@ -5,8 +5,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 RTC=/bin/rtc
 [ -e "${RTC}" ] && "${RTC}" --load
 
-./format.sh
-
 set -e
 export ANEMOBOX_CONFIG_PATH=/home/anemobox
 export BLENO_DEVICE_NAME="Anemobox"
