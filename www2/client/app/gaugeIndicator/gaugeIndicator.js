c7cd4645b7ac442ee2ffd7a56f1e4cc927605e28
Thomas Rubeli <thomas@AechDebian>
2015-07-11 18:51:58 +0200

    gauge indicator has been fixed and renamed

--- www2/client/app/gaugeIndicator/gaugeIndicator.js
+++ www2/client/app/gaugeIndicator/gaugeIndicator.js
@@ -3,29 +3,31 @@
  */
 
 
-function Panel(){
+function Panel(rootElement){
 	this.row=3;
 	this.column=2
 	this.width=400;
 	this.height=400;
-	this.init();
 	this.deltaTransition=1000;
 	this.delayTransition=100;
+    this.root=rootElement;
+    this.init();
 }
 
 
 Panel.prototype.init = function(){
 
-	var panel = document.getElementById("instruments_panel");
+	var panel = this.root[0];
 
 	/*var svgContainer= this.container = d3.select(panel).append("svg")
 	.attr("width", 200)
 	.attr("height", 200);
 */
+    var panel_component=this;
 
-    d3.xml("assets/images/svg/gauge_opt.svg", "image/svg+xml", function(xml) {
+    d3.xml("assets/images/svg/gauge.svg", "image/svg+xml", function(xml) {
     var importedNode = document.importNode(xml.documentElement, true);
-    this.gauge1 = d3.select(panel).node().appendChild(importedNode);
+    panel_component.gauge1 = d3.select(panel).selectAll("#gauge-svg-container").node().appendChild(importedNode);
 
     });
 
@@ -35,10 +37,9 @@ Panel.prototype.init = function(){
 Panel.prototype.updatePanelGraphs = function(value){
 
 	if(value){
+        //console.log(this.gauge1.selectAll("g#needle"));
 
-        console.log(this.gauge1.selectAll("g#needle"));
-
-        this.gauge1.selectAll("g#needle")
+        d3.select(this.gauge1).selectAll("#needle_inside")
         .transition()
         .attr("transform", "rotate(" + value + ")")
         .duration(this.deltaTransition)
