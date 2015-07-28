1d16b932f9983aaeeb22668b4bb7e9be92d77b52
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-07-28 18:31:40 +0200

    check that lb0 is updated

--- nodemodules/mail/test/complexsync.js
+++ nodemodules/mail/test/complexsync.js
@@ -14,7 +14,7 @@ eval(dd.parse('(dafn makeEndPoints () (map (afn (fname name) ' +
               '(quote ("box119" "gateway" "gateway" "boat119"))))'));
 
 
-eval(dd.parse('(dafn test1 (eps) (let ((box g0 g1 boat) eps pair (object "src" "box119" "dst" "boat119") lb0 (.getLowerBounds boat (array pair))) (.sendPacket box "boat119" 139 (new Buffer (array 0 1 2 3 4))) (synchronize box g0) (assert (= 1 (.getTotalPacketCount g0))) (assert (= 0 (.getTotalPacketCount boat))) (synchronize g0 boat) (assert (= 0 (.getTotalPacketCount boat))) (assert (< (get lb0 0) (get (.getLowerBounds boat (array pair)) 0)))))'));
+eval(dd.parse('(dafn test1 (eps) (let ((box g0 g1 boat) eps pair (object "src" "box119" "dst" "boat119") lb0 (.getLowerBounds boat (array pair))) (.sendPacket box "boat119" 139 (new Buffer (array 0 1 2 3 4))) (synchronize box g0) (assert (= 1 (.getTotalPacketCount g0))) (assert (= 0 (.getTotalPacketCount boat))) (synchronize g0 boat) (assert (= 0 (.getTotalPacketCount boat))) (assert (= (get lb0 0) (get (.getLowerBounds boat (array pair)) 0)))))'));
 
 eval(dd.parse('(dafn runTest () (let (eps (makeEndPoints)) (test1 eps) (console.log "Done")))'));
 
