b661677c08c431ba97d735fd586e027374dd2c2b
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-07-28 18:40:41 +0200

    added macro

--- nodemodules/mail/test/complexsync.js
+++ nodemodules/mail/test/complexsync.js
@@ -13,10 +13,18 @@ eval(dd.parse('(dafn makeEndPoints () (map (afn (fname name) ' +
               '(quote ("a" "b" "c" "d")) ' +
               '(quote ("box119" "gateway" "gateway" "boat119"))))'));
 
+var withEps = dd.macro(function() {
+  var args = dd.argsToArray(arguments);
+  return ['let', [['boat', 'g0', 'g1', 'box'], args[0]]].concat(args.slice(1));
+});
 
+// Transfer a packet via the first gateway
 eval(dd.parse('(dafn test1 (eps) (let ((box g0 g1 boat) eps pair (object "src" "box119" "dst" "boat119") lb0 (.getLowerBounds boat (array pair))) (.sendPacket box "boat119" 139 (new Buffer (array 0 1 2 3 4))) (synchronize box g0) (assert (= 1 (.getTotalPacketCount g0))) (assert (= 0 (.getTotalPacketCount boat))) (synchronize g0 boat) (assert (= 0 (.getTotalPacketCount boat))) (assert (< (get lb0 0) (get (.getLowerBounds boat (array pair)) 0)))))'));
 
-eval(dd.parse('(dafn runTest () (let (eps (makeEndPoints)) (test1 eps) (console.log "Done")))'));
+// Transfer a packet via the second gateway
+eval(dd.parse('(dafn test2 (eps) (let ((box g0 g1 boat) eps)))'));
+
+eval(dd.parse('(dafn runTest () (let (eps (makeEndPoints)) (test1 eps) (test2 eps) (console.log "Done")))'));
 
 describe('complex', function() {
   it('Should synchronize in a complex fashion', function(done) {
