6caffca96f4eb3ccb7538c1112402afc0a9a80a2
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-07-28 23:27:34 +0200

    acall sendPacket

--- nodemodules/mail/test/complexsync.js
+++ nodemodules/mail/test/complexsync.js
@@ -6,8 +6,6 @@ var bigint = require('../bigint.js');
 var pprint = require('dentdoche/pprint.js');
 var synchronize = dd.makeAsync(require('../sync2.js').synchronize);
 
-dd.declareAsyncMethods(mail2.EndPoint, 'sendPacket' /*'getTotalPacketCount', 'getLowerBounds'*/);
-
 var pair = {src: "box119", dst: "boat119"};
 var pairs = [pair];
 
@@ -30,10 +28,10 @@ var withEps = dd.macro(function() {
 });
 
 // Transfer a packet via the first gateway
-eval(dd.parse('(dafn test1 (eps) (withEps eps (let (lb0 (acall .getLowerBounds boat pairs)) (.sendPacket box "boat119" 139 (new Buffer (array 0 1 2 3 4))) (synchronize box g0) (assert (= 1 (acall .getTotalPacketCount g0))) (assert (= 0 (acall .getTotalPacketCount boat))) (synchronize g0 boat) (assert (= 0 (acall .getTotalPacketCount boat))) (assert (< (get lb0 0) (get (acall .getLowerBounds boat (array pair)) 0))) (synchronize boat g0) (synchronize g0 box))))'));
+eval(dd.parse('(dafn test1 (eps) (withEps eps (let (lb0 (acall .getLowerBounds boat pairs)) (acall .sendPacket box "boat119" 139 (new Buffer (array 0 1 2 3 4))) (synchronize box g0) (assert (= 1 (acall .getTotalPacketCount g0))) (assert (= 0 (acall .getTotalPacketCount boat))) (synchronize g0 boat) (assert (= 0 (acall .getTotalPacketCount boat))) (assert (< (get lb0 0) (get (acall .getLowerBounds boat (array pair)) 0))) (synchronize boat g0) (synchronize g0 box))))'));
 
 // Transfer a packet via the second gateway
-eval(dd.parse('(dafn test2 (eps) (withEps eps (let (lb0 (first (acall .getLowerBounds boat pairs))) (.sendPacket box "boat119" 140 (new Buffer (array 3 4 5))) (synchronize box g1) (assert (= 1 (acall .getTotalPacketCount box))) (synchronize g1 boat)  (= (bigint.inc lb0) (first (acall .getLowerBounds boat pairs))))))'));
+eval(dd.parse('(dafn test2 (eps) (withEps eps (let (lb0 (first (acall .getLowerBounds boat pairs))) (acall .sendPacket box "boat119" 140 (new Buffer (array 3 4 5))) (synchronize box g1) (assert (= 1 (acall .getTotalPacketCount box))) (synchronize g1 boat)  (= (bigint.inc lb0) (first (acall .getLowerBounds boat pairs))))))'));
 
 // Transfer the two packets and check that they were received.
 eval(dd.parse('(dafn runTest () (let (eps (makeEndPoints)) (withEps eps (.addPacketHandler boat handler) (test1 eps) (test2 eps) (assert (= (quote (139 140)) received)) (assert (= 1 (acall .getTotalPacketCount box))))))'));
