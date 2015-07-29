d13f13c78b2bbba9ac13288e73b1be2f82d34462
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-07-29 11:42:26 +0200

    done

--- nodemodules/mail/test/complexsync.js
+++ nodemodules/mail/test/complexsync.js
@@ -27,14 +27,26 @@ var withEps = dd.macro(function() {
   return result;
 });
 
+var myAssertSub = dd.setAsync(function(label, value, cb) {
+  if (value) {
+    cb(null, value);
+  } else {
+    cb(new Error('Assertion ' + label + ' failed.'));
+  }
+});
+
+var myAssert = dd.macro(function(x) {
+  return [myAssertSub, pprint.str(x), x];
+});
+
 // Transfer a packet via the first gateway
-eval(dd.parse('(dafn test1 (eps) (withEps eps (let (lb0 (acall .getLowerBounds boat pairs)) (acall .sendPacket box "boat119" 139 (new Buffer (array 0 1 2 3 4))) (synchronize box g0) (assert (= 1 (acall .getTotalPacketCount g0))) (assert (= 0 (acall .getTotalPacketCount boat))) (synchronize g0 boat) (assert (= 0 (acall .getTotalPacketCount boat))) (assert (< (get lb0 0) (get (acall .getLowerBounds boat (array pair)) 0))) (synchronize boat g0) (synchronize g0 box))))'));
+eval(dd.parse('(dafn test1 (eps) (withEps eps (let (lb0 (acall .getLowerBounds boat pairs)) (acall .sendPacket box "boat119" 139 (new Buffer (array 0 1 2 3 4))) (synchronize box g0) (myAssert (= 1 (acall .getTotalPacketCount g0))) (myAssert (= 0 (acall .getTotalPacketCount boat))) (synchronize g0 boat) (myAssert (= 0 (acall .getTotalPacketCount boat))) (myAssert (< (get lb0 0) (get (acall .getLowerBounds boat (array pair)) 0))))))'));
 
 // Transfer a packet via the second gateway
-eval(dd.parse('(dafn test2 (eps) (withEps eps (let (lb0 (first (acall .getLowerBounds boat pairs))) (acall .sendPacket box "boat119" 140 (new Buffer (array 3 4 5))) (synchronize box g1) (assert (= 1 (acall .getTotalPacketCount box))) (synchronize g1 boat)  (= (bigint.inc lb0) (first (acall .getLowerBounds boat pairs))))))'));
+eval(dd.parse('(dafn test2 (eps) (withEps eps (let (lb0 (first (acall .getLowerBounds boat pairs))) (acall .sendPacket box "boat119" 140 (new Buffer (array 3 4 5))) (synchronize box g1) (myAssert (= 2 (acall .getTotalPacketCount box))) (synchronize g1 boat)  (= (bigint.inc lb0) (first (acall .getLowerBounds boat pairs))))))'));
 
 // Transfer the two packets and check that they were received.
-eval(dd.parse('(dafn runTest () (let (eps (makeEndPoints)) (withEps eps (.addPacketHandler boat handler) (test1 eps) (test2 eps) (assert (= (quote (139 140)) received)) (assert (= 1 (acall .getTotalPacketCount box))))))'));
+eval(dd.parse('(dafn runTest () (let (eps (makeEndPoints)) (withEps eps (.addPacketHandler boat handler) (test1 eps) (test2 eps) (myAssert (= (quote (139 140)) received)))))'));// (myAssert (= 2 (acall .getTotalPacketCount box))))))'));
 
 describe('complex', function() {
   it('Should synchronize in a complex fashion', function(done) {
