379be004e648351055d5decc181f73eb1f413c72
Julien Pilet <julien.pilet@gmail.com>
2015-07-30 15:26:03 +0200

    Cleaned-up boat summary
    The boat summary directive now works only with a single boat.
    the pagination code is the one from angular-ui.

--- www2/client/app/main/main.controller.js
+++ www2/client/app/main/main.controller.js
@@ -5,15 +5,10 @@ angular.module('www2App')
              function($scope, Auth, $http, boatList) {
     $scope.isLoggedIn = Auth.isLoggedIn();
 
-    // $scope.boats = boatList.boats();
-    // $scope.sessions = boatList.sessions();
-    // $scope.$on('boatList:updated', function(event, boats) {
-    //   $scope.boats = boats;
-    // });
-    // $scope.$on('boatList:sessionsUpdated', function(event, data) {
-    //   $scope.sessions = data;
-    // });
-    // $scope.boat = boatList.boat
+    $scope.boats = boatList.boats();
+    $scope.$on('boatList:updated', function(event, boats) {
+       $scope.boats = boats;
+    });
 
     $scope.$watch(Auth.isLoggedIn, function(newVal, oldVal) {
       $scope.isLoggedIn = newVal;
