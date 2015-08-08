59e6c8ba71d656adc229ff20514b87fa7b038bc7
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-08-08 13:32:13 +0200

    added file

---
+++ src/server/math/PiecewisePolynomials.h
@@ -0,0 +1,21 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef SERVER_MATH_PIECEWISEPOLYNOMIALS_H_
+#define SERVER_MATH_PIECEWISEPOLYNOMIALS_H_
+
+namespace sail { namespace PiecewisePolynomials {
+
+
+
+
+
+
+
+}}
+
+
+
+#endif /* SERVER_MATH_PIECEWISEPOLYNOMIALS_H_ */
