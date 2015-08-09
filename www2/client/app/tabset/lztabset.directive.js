e008b52fa5d13c6dfaca8b0dc70ede434b8a8715
Thomas Rubeli <thomas@AechDebian>
2015-08-09 23:14:22 +0200

    added a tab display for the instruments panel

---
+++ www2/client/app/tabset/lztabset.directive.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('www2App')
+  .directive('lztabset', function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    transclude: true,
+    controller: function($scope) {
+      $scope.templateUrl = '';
+      var tabs = $scope.tabs = [];
+      var controller = this;
+
+      this.selectTab = function (tab) {
+        angular.forEach(tabs, function (tab) {
+          tab.selected = false;
+        });
+        tab.selected = true;
+      };
+
+      this.setTabTemplate = function (templateUrl) {
+        $scope.templateUrl = templateUrl;
+      }
+
+      this.addTab = function (tab) {
+        if (tabs.length == 0) {
+          controller.selectTab(tab);
+        }
+        tabs.push(tab);
+      };
+    },
+    template:
+      '<div class="row-fluid">' +
+        '<div class="row-fluid">' +
+          '<div class="nav nav-tabs" ng-transclude></div>' +
+        '</div>' +
+        '<div class="row-fluid">' +
+          '<ng-include src="templateUrl">' +
+        '</ng-include></div>' +
+      '</div>'
+  };
+});
+
+
