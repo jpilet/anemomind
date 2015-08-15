ada000d6a68b8599b5a9b4b21632fafd1735088b
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-08-15 10:14:48 +0200

    restructured a bit

--- src/server/nautical/EvalMinCovCalib.cpp
+++ src/server/nautical/EvalMinCovCalib.cpp
@@ -5,6 +5,7 @@
 
 #include <server/nautical/synthtest/NavalSimulationPrecomp.h>
 #include <server/nautical/MinCovCalib.h>
+#include <server/common/EnvUtil.h>
 
 using namespace sail;
 
@@ -12,7 +13,20 @@ void calibrateForNavs(Array<Nav> navs) {
 
 }
 
-void evaluateForSimulation() {
+struct SynthResults {
+  NavalSimulation::SimulatedCalibrationResults before, after;
+};
+
+std::ostream &operator<<(std::ostream &s, SynthResults results) {
+  s << "\n****** Results on synthetic data ******\n";
+  s << "Before calibration:\n";
+  s << results.before;
+  s << "After calibration:\n";
+  s << results.after;
+  return s;
+}
+
+SynthResults evaluateForSimulation() {
   std::cout << "Prepare synthetic data..." << std::endl;
   NavalSimulation sim = getNavSimFractalWindOrientedLong();
   auto bd = sim.boatData(0);
@@ -23,13 +37,23 @@ void evaluateForSimulation() {
   std::cout << "Run the calibration..." << std::endl;
   MinCovCalib::Settings settings;
   Corrector<double> calibratedParameters = MinCovCalib::optimize(navs, settings);
+  return SynthResults{
+    bd.evaluateNoCalibration(),
+    bd.evaluateFitness(calibratedParameters)
+  };
+}
 
-  std::cout << "  With default parameters:   \n" << bd.evaluateNoCalibration();
-  std::cout << "  With calibrated paramters: \n" << bd.evaluateFitness(calibratedParameters);
+void evaluateForRealData(std::string datasetPath) {
+
 }
 
 void standardBenchmark() {
-  evaluateForSimulation();
+  auto synthResults = evaluateForSimulation();
+  evaluateForRealData(getDatasetPath("exocet").toString());
+  evaluateForRealData(getDatasetPath("psaros33_Banque_Sturdza").toString());
+  evaluateForRealData(getDatasetPath("Irene").toString());
+
+  std::cout << EXPR_AND_VAL_AS_STRING(synthResults) << std::endl;
 }
 
 int main(int argc, const char **argv) {
