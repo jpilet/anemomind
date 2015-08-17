2bee7bfd6d6f06f5e6a2b0862139873b3f75ea04
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-08-17 18:11:38 +0200

    bra

--- src/server/nautical/TemporalSplit.cpp
+++ src/server/nautical/TemporalSplit.cpp
@@ -6,14 +6,8 @@
 #include <server/nautical/TemporalSplit.h>
 #include <cassert>
 #include <server/common/logging.h>
-#include <server/common/Histogram.h>
-#include <server/common/ProportionateIndexer.h>
-#include <server/common/string.h>
-#include <server/common/ArrayIO.h>
-#include <server/common/PhysicalQuantityIO.h>
 
 namespace sail {
-
 
 namespace {
   int countGaps(const Array<Nav> &sortedNavs,
@@ -84,150 +78,22 @@ Array<Spani> recursiveTemporalSplit(Array<Nav> sortedNavs,
     return dst.get();
 }
 
-
-
-/*class SplitNode {
- public:
-  SplitNode(Array<Nav> navs, Spani span)
-};*/
-
-struct Dif {
- Duration<double> dur;
- int index;
-};
-
-std::ostream &operator<<(std::ostream &s, const Dif &x) {
-  s << "Dif of duration " << x.dur.seconds() << " seconds" << std::endl;
-  return s;
-}
-
-Array<Dif> getDifs(Array<Nav> navs) {
-  std::sort(navs.begin(), navs.end());
-  int n = navs.size() - 1;
-  Array<Dif> difs(n);
-  for (int i = 0; i < n; i++) {
-    difs[i] = Dif{navs[i+1].time() - navs[i].time(), i+1};
-  }
-
-  class Cmp {
-   public:
-    bool operator() (const Dif &a, const Dif &b) const {
-      return a.dur > b.dur;
-    }
-  };
-
-  std::sort(difs.begin(), difs.end(), Cmp());
-  return difs;
-}
-
-class NavSplitNode {
- public:
-  NavSplitNode(Array<Nav> navs) : _navs(navs), _span(0, navs.size()) {}
-
-  NavSplitNode(Array<Nav> navs, Spani span) : _navs(navs), _span(span) {}
-
-  bool isLeaf() const {
-    return !bool(_left);
-  }
-
-  void split(int index) {
-    if (isLeaf()) {
-      _left = std::shared_ptr<NavSplitNode>(new NavSplitNode(_navs, Spani(_span.minv(), index)));
-      _right = std::shared_ptr<NavSplitNode>(new NavSplitNode(_navs, Spani(index, _span.maxv())));
-    } else {
-      if (index < _left->span().maxv()) {
-        _left->split(index);
-      } else {
-        _right->split(index);
-      }
-    }
-  }
-
-  Spani span() const {
-    return _span;
-  }
-
-  void disp(std::ostream *dst, Duration<double> minGap, int depth = 0) {
-    indent(dst, 2*depth);
-    *dst << "[begin node of duration " << duration().seconds() << " seconds]\n";
-    if (!isLeaf()) {
-      auto gapDur = gapDuration();
-      if (gapDur > minGap) {
-        int nextDepth = depth+1;
-        _left->disp(dst, minGap, nextDepth);
-        indent(dst, 2*nextDepth);
-        *dst << "Gap duration " << gapDur.seconds() << " seconds" << std::endl;
-        _right->disp(dst, minGap, depth+1);
-      }
-    }
-    indent(dst, 2*depth);
-    *dst << "[end node of duration " << duration().seconds() << " seconds]\n";
-  }
-
-  const Nav &first() const {
-    return _navs[_span.minv()];
-  }
-
-  const Nav &last() const {
-    return _navs[_span.maxv()-1];
-  }
-
-  Duration<double> duration() const {
-    return last().time() - first().time();
-  }
-
-  int middle() const {
-    return _left->span().maxv();
-  }
-
-  Duration<double> gapDuration() const {
-    return _right->first().time() - _left->last().time();
-  }
-
-  Duration<double> meanPeriodTime() const {
-    return (1.0/(_span.width() - 1))*duration();
-  }
-
-  bool isRoot() const {
-    return _span.minv() == 0 && _span.maxv() == _navs.size();
-  }
-
- private:
-  Spani _span;
-  Array<Nav> _navs;
-  std::shared_ptr<NavSplitNode> _left, _right;
-};
-
-std::shared_ptr<NavSplitNode> makeSplitTree(Array<Nav> navs) {
-  auto difs = getDifs(navs);
-  std::shared_ptr<NavSplitNode> result(new NavSplitNode(navs));
-  for (auto dif: difs) {
-    result->split(dif.index);
-  }
-  return result;
-}
-
 void dispTemporalRaceOverview(Array<Spani> spans, Array<Nav> navs, std::ostream *out) {
   int spanCount = spans.size();
   for (int i = 0; i < spans.size(); i++) {
     Spani span = spans[i];
     Array<Nav> sub = navs.slice(span.minv(), span.maxv());
     *out << "[" << span.minv() << ", " << span.maxv() << "[" << std::endl;
-    *out << "   from          " << sub.first().time().toString() << std::endl;
-    *out << "   duration      " << (sub.last().time() - sub.first().time()).str() << std::endl;
-    *out << "   to            " << sub.last().time().toString() << std::endl;
-    Duration<double> periodTime = (1.0/sub.size())*(sub.last().time() - sub.first().time());
-    *out << "   period time   " << (periodTime).seconds() << " seconds\n" << std::endl;
+    *out << "   from     " << sub.first().time().toString() << std::endl;
+    *out << "   duration " << (sub.last().time() - sub.first().time()).str() << std::endl;
+    *out << "   to     " << sub.last().time().toString() << '\n' << std::endl;
     if (i < spans.size()-1) {
       Duration<double> gap = navs[spans[i+1].minv()].time() - sub.last().time();
       *out << "Gap of " << gap.str() << '\n' << std::endl;
     }
   }
   *out << "Total of " << spanCount << " race episodes." << std::endl;
-  std::cout << EXPR_AND_VAL_AS_STRING(getDifs(navs).sliceTo(180)) << std::endl;
 }
-
-
 
 
 
