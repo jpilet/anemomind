78b58cda19af63a394c4372920fd4dbf90cb1be9
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-08-20 15:35:25 +0200

    filtering that works

--- src/server/math/nonlinear/BandedSolverTest.cpp
+++ src/server/math/nonlinear/BandedSolverTest.cpp
@@ -65,45 +65,60 @@ double theSignal(double x) {
   return 1.0;
 }
 
-TEST(BandedSolver, Ramp) {
+/*
+ * This example shows that, if we use AbsCost for both the data and the regularization,
+ * the filtered signal will have the same shape, even if we scale both the underlying true
+ * signal and the noise.
+ */
+TEST(BandedSolver, RampMultiscale) {
+  bool visualize = false;
+
+  int scaleCount = 4;
+  LineKM scaleMap(0, scaleCount-1, log(1.0), log(120));
+
+
   AbsCost regCost(0.0001), dataCost(0.0001);
 
   // 30 samples ranging from -1 to 1.
   Sampling sampling(30, -1, 1);
 
-  int obsCount = 300;
-  Array<Observation<1> > obs(obsCount);
-  Arrayd obsX(obsCount), obsY(obsCount);
-  for (int i = 0; i < obsCount; i++) {
-    auto x = -1 + 2.0*positiveMod(sin(234.234*i) + sin(233.8*i), 1.0);
-    auto y = theSignal(x) + 0.1*sin(3443.0*i);
-    obs[i] = Observation<1>{sampling.represent(x), y};
-    obsX[i] = x;
-    obsY[i] = y;
-  }
+  for (int i = 0; i < scaleCount; i++) {
+    double scale = exp(scaleMap(i));
+    int obsCount = 300;
+    Array<Observation<1> > obs(obsCount);
+    Arrayd obsX(obsCount), obsY(obsCount);
+    for (int i = 0; i < obsCount; i++) {
+      auto x = -1 + 2.0*positiveMod(sin(234.234*i) + sin(233.8*i), 1.0);
+      auto noise = 0.1*sin(3443.0*i);
+      auto y = scale*(theSignal(x) + noise);
+      obs[i] = Observation<1>{sampling.represent(x), y};
+      obsX[i] = x;
+      obsY[i] = y;
+    }
 
-  BandedSolver::Settings s;
-  s.regOrder = 2;
-  s.lambda = 1.0;
-  MDArray2d Y = BandedSolver::solve(dataCost, regCost, sampling, obs, s);
+    BandedSolver::Settings s;
+    s.regOrder = 2;
+    s.lambda = 1.0;
+    s.iters = 4;
+    MDArray2d Y = BandedSolver::solve(dataCost, regCost, sampling, obs, s);
 
-  for (int i = 0; i < sampling.count(); i++) {
-    double x = sampling.indexToX()(i);
-    EXPECT_NEAR(Y(i, 0), theSignal(x), 0.04);
-  }
+    for (int i = 0; i < sampling.count(); i++) {
+      double x = sampling.indexToX()(i);
+      EXPECT_NEAR(Y(i, 0), scale*theSignal(x), scale*0.04);
+    }
 
-  bool visualize = false;
+    if (visualize) {
+      GnuplotExtra plot;
+      plot.set_style("lines");
+      Arrayd X = sampling.makeX();
+      plot.plot_xy(X, Y.getStorage());
+      plot.set_style("points");
+      plot.plot_xy(obsX, obsY);
+      plot.show();
 
-  if (visualize) {
-    GnuplotExtra plot;
-    plot.set_style("lines");
-    Arrayd X = sampling.makeX();
-    plot.plot_xy(X, Y.getStorage());
-    plot.set_style("points");
-    plot.plot_xy(obsX, obsY);
-    plot.show();
+      std::cout << EXPR_AND_VAL_AS_STRING(Y) << std::endl;
+    }
 
-    std::cout << EXPR_AND_VAL_AS_STRING(Y) << std::endl;
   }
 }
 
