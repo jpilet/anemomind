e923c4e01ee5c5223095b2b622a60b655655c690
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-08-20 15:24:55 +0200

    working test case

--- src/server/math/nonlinear/BandedSolverTest.cpp
+++ src/server/math/nonlinear/BandedSolverTest.cpp
@@ -52,7 +52,58 @@ TEST(BandedSolver, TVFilterFirstOrder) {
     plot.show();
 
     std::cout << EXPR_AND_VAL_AS_STRING(Y) << std::endl;
+  }
+}
 
+double theSignal(double x) {
+  double a = 0.2;
+  if (x < -a) {
+    return -1.0;
+  } else if (x < a) {
+    return LineKM(-a, a, -1, 1)(x);
+  }
+  return 1.0;
+}
+
+TEST(BandedSolver, Ramp) {
+  AbsCost regCost(0.0001), dataCost(0.0001);
+
+  // 30 samples ranging from -1 to 1.
+  Sampling sampling(30, -1, 1);
+
+  int obsCount = 300;
+  Array<Observation<1> > obs(obsCount);
+  Arrayd obsX(obsCount), obsY(obsCount);
+  for (int i = 0; i < obsCount; i++) {
+    auto x = -1 + 2.0*positiveMod(sin(234.234*i) + sin(233.8*i), 1.0);
+    auto y = theSignal(x) + 0.1*sin(3443.0*i);
+    obs[i] = Observation<1>{sampling.represent(x), y};
+    obsX[i] = x;
+    obsY[i] = y;
+  }
+
+  BandedSolver::Settings s;
+  s.regOrder = 2;
+  s.lambda = 1.0;
+  MDArray2d Y = BandedSolver::solve(dataCost, regCost, sampling, obs, s);
+
+  for (int i = 0; i < sampling.count(); i++) {
+    double x = sampling.indexToX()(i);
+    EXPECT_NEAR(Y(i, 0), theSignal(x), 0.04);
+  }
+
+  bool visualize = false;
+
+  if (visualize) {
+    GnuplotExtra plot;
+    plot.set_style("lines");
+    Arrayd X = sampling.makeX();
+    plot.plot_xy(X, Y.getStorage());
+    plot.set_style("points");
+    plot.plot_xy(obsX, obsY);
+    plot.show();
+
+    std::cout << EXPR_AND_VAL_AS_STRING(Y) << std::endl;
   }
 }
 
