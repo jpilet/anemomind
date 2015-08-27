00323e091140a46e98070f1c535ae11798a35079
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-08-27 17:09:36 +0200

    visualization

--- src/server/math/SparsityConstrainedTest.cpp
+++ src/server/math/SparsityConstrainedTest.cpp
@@ -5,6 +5,7 @@
 
 #include <server/math/SparsityConstrained.h>
 #include <gtest/gtest.h>
+#include <server/plot/extra.h>
 
 using namespace sail;
 
@@ -25,4 +26,41 @@ TEST(SparsityConstrained, DistributeWeights) {
   EXPECT_LT(W1[1], W1[0] + smallGap);
 }
 
+TEST(SparsityConstrained, SignalFit) {
+  Arrayd noisySignal(300);
+  Arrayd time(300);
+  for (int i = 0; i < 300; i++) {
+    noisySignal[i] = (i < 100 || i > 200? 1 : 0) + 0.1*sin(34*i*i);
+    time[i] = i;
+  }
 
+  int rows = 300 + 299;
+  Eigen::VectorXd B = Eigen::VectorXd::Zero(rows);
+  typedef Eigen::Triplet<double> Triplet;
+  std::vector<Triplet> triplets;
+  triplets.reserve(300 + 2*299);
+  for (int i = 0; i < 300; i++) {
+    triplets.push_back(Triplet(i, i, 1.0));
+    B(i) = noisySignal[i];
+  }
+  Array<Spani> cst(299);
+  for (int i = 0; i < 299; i++) {
+    int row = 300 + i;
+    triplets.push_back(Triplet(row, i, 1.0));
+    triplets.push_back(Triplet(row, i+1, -1.0));
+    cst[i] = Spani(row, row+1);
+  }
+  Eigen::SparseMatrix<double> A(rows, 300);
+  A.setFromTriplets(triplets.begin(), triplets.end());
+  SparsityConstrained::Settings settings;
+  auto X = SparsityConstrained::solve(A, B, cst, 297, settings);
+
+
+  if (true) {
+    GnuplotExtra plot;
+    plot.set_style("lines");
+    plot.plot_xy(time, noisySignal);
+    plot.plot_xy(time, Arrayd(X.size(), X.data()));
+    plot.show();
+  }
+}
