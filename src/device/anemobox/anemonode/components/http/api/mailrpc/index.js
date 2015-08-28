465ce4aa3b19ac87cd8993076a5dac6708a8135d
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-08-28 12:26:29 +0200

    merged remote

---
+++ src/device/anemobox/anemonode/components/http/api/mailrpc/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var express = require('express');
+var router = express.Router();
+var rpc = require('endpoint/endpoint.httpserver.js');
+var ep = require('../../../LocalEndpoint.js');
+
+function withEndpoint(endpointName, req, cbOperation, done) {
+  ep.withNamedLocalEndpoint(endpointName, cbOperation, done);
+}
+
+rpc.bindMethodHandlers(withEndpoint, router);
+module.exports = router;
--- src/device/anemobox/anemonode/components/http/routes.js
+++ src/device/anemobox/anemonode/components/http/routes.js
@@ -8,6 +8,7 @@ module.exports = function(app) {
 
   app.use('/api/id', require('./api/id'));
   app.use('/api/live', require('./api/live'));
+  app.use('/api/mailrpc', require('./api/mailrpc'));
 
   app.route('/')
     .get(function(req, res) {
