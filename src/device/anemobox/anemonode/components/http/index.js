1169201b9a4f78e83a0b845bc24507edf21f5d6c
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-08-28 20:30:54 +0200

    Rewritten code for HTTP rpc

--- src/device/anemobox/anemonode/components/http/index.js
+++ src/device/anemobox/anemonode/components/http/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
+
 var server = require('http').createServer(app);
 require('./routes')(app);
 
