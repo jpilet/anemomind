1326a820867ec384b5e3cfb1d820289c8370855f
Julien Pilet <julien@anemomind.com>
2015-08-31 13:31:11 +0000

    addressed comments

--- src/device/anemobox/anemonode/components/imu/index.js
+++ src/device/anemobox/anemonode/components/imu/index.js
@@ -8,10 +8,9 @@ function listen(cb) {
     return;
   }
 
-  imu = spawn('/bin/bash', ['-c', './app_bno055']);
+  imu = spawn('/bin/bash', ['-c', __dirname + '/app_bno055']);
   imu.stdout.pipe(split()).on('data', function(line) {
     if (line && line.length > 0) {
-      console.log('Parsing: "' + line + '"');
       var obj = JSON.parse(line);
       cb(obj);
     }
