baba4b8a70bfa38551ab3d644607133e4683ec2f
Julien Pilet <julien@anemomind.com>
2015-08-31 08:12:30 +0000

    Add RPC calls to api/live http interface.

---
+++ src/device/anemobox/anemonode/components/callrpc.js
@@ -0,0 +1,26 @@
+var http = require('./http/api/live/live.controller.js');
+var rpcble = require('./rpcble');
+
+exports.call = function(func, args, callback) {
+  if (http.isConnected()) {
+    http.callRpc(func, args, callback);
+  } else if (rpcble.isConnected()) {
+    rpcble.call(func, args, callback);
+  } else {
+    console.log('Can\'t call rpc ' + func + ': nobody is connected.');
+  }
+};
+
+setInterval(function() {
+  var now = new Date();
+  exports.call('ping', {data: now}, function(answer) {
+    if (answer && 'data' in answer) {
+      var delta = (new Date()) - new Date(answer.data);
+      console.log('RPC ping reply in ' + delta + ' ms');
+    } else {
+      console.log('bad ping answer:');
+      console.warn(answer);
+    }
+ });
+}, 900);
+  
--- src/device/anemobox/anemonode/components/http/api/live/live.controller.js
+++ src/device/anemobox/anemonode/components/http/api/live/live.controller.js
@@ -2,6 +2,11 @@
 
 var anemonode = require('../../../../build/Release/anemonode');
 
+var pendingCalls = {}
+var pendingCallPackets = [];
+
+var lastFetch;
+
 // Get list of boats
 exports.index = function(req, res) {
   var response = {};
@@ -12,9 +17,50 @@ exports.index = function(req, res) {
       response[i] = anemonode.dispatcher.values[i].value()
     }
   }
+  if (pendingCallPackets.length > 0) {
+    response.rpcCalls = pendingCallPackets;
+    pendingCallPackets = [];
+  }
+  lastFetch = new Date()
   res.json(response);
 };
 
 function handleError(res, err) {
   return res.status(500).send(err);
+}
+
+
+exports.callRpc = function(func, args, callback) {
+  var packet = {
+    callId: Math.round(Math.random() * 65535),
+    func: func,
+    args: args
+  };
+
+  pendingCalls[packet.callId] = callback || function() {};
+
+  pendingCallPackets.push(packet);
+}
+
+exports.rpcReply = function(req, res) {
+  var callId = req.params.callId;
+  if (callId == undefined) {
+    return res.status(403).send("no callId");
+  }
+  if (!(callId in pendingCalls)) {
+    return res.status(404).send("No such callId");
+  }
+  
+  var answer = ('answer' in req.body ? req.body.answer : undefined);
+  pendingCalls[callId](answer);
+
+  res.status(200).send();
+}
+
+exports.isConnected = function() {
+  if (!lastFetch) {
+    return false;
+  }
+  var now = new Date();
+  return (now.getTime() - lastFetch.getTime()) < 5000;
 }
