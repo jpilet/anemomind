b04daf7f729f174728983a23ee92b9a0f9b8141b
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-09-24 10:11:10 +0200

    Bra

--- src/server/math/nonlinear/SparseCurveFit.h
+++ src/server/math/nonlinear/SparseCurveFit.h
@@ -7,12 +7,13 @@
 #define SERVER_MATH_NONLINEAR_SPARSECURVEFIT_H_
 
 #include <server/math/SparsityConstrained.h>
+#include <server/common/logging.h>
 
 namespace sail {
 namespace SparseCurveFit {
 
 struct Settings {
-  // This is the settings of the underlying algorithm used to solve the problem.
+  // These are the settings of the underlying algorithm used to solve the problem.
   SparsityConstrained::Settings settings;
 
   // Select this fraction of all observations to use as inliers, ignore the others.
@@ -27,8 +28,9 @@ struct Settings {
   // rate is not lower than that fraction, because then it is going to break. And we should not choose
   // the fraction too low, because then we are going to overfit to a few samples. In either case,
   // no matter if we choose fraction-based outlier rejection or threshold-based rejection, we will
-  // have to tune a parameter because one size doesn't fit all. But in most cases, I believe that say,
-  // around the best 60% of the samples should be high quality observations even for crappy gps devices.
+  // have to tune a parameter because one size doesn't fit all. But in most cases, I believe that it
+  // is fair to say that at least around the best 60% of the samples should be high quality
+  // observations even for crappy gps devices.
   double inlierRate = 0.6;
 
   // How many discontinuities in some derivative (of order regOrder) we expect. The more discontinuities
@@ -37,8 +39,9 @@ struct Settings {
   // The discontinuity count can be tuned so that there is maybe one discontinuity every six seconds or
   // so in a temporal signal. Discontinuities are used to model features in the underlying noise-free signal.
   // I hope that this way of regularizing will be very robust to different amounts of noise in the
-  // measurements.
-  double discontinuityCount = 0;
+  // measurements. And the algorithm will automatically select the locations for the discontinuities where
+  // they are most needed, e.g. at buoy turns.
+  double discontinuityCount = -1;
 
   // For which derivative order we count the discontinuities.
   int regOrder = 3;
@@ -46,7 +49,7 @@ struct Settings {
 
 template <int Dim>
 MDArray2d fit(const Settings &settings, int sampleCount, Array<Observation<Dim> > observations) {
-
+  CHECK_LE(0, settings.discontinuityCount);
 }
 
 
---
+++ src/server/math/nonlinear/SparseCurveFitTest.cpp
@@ -0,0 +1,13 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <gtest/gtest.h>
+#include <server/math/nonlinear/SparseCurveFit.h>
+
+
+
+TEST(SparseCurveFitTest, NoiseFreeTest) {
+
+}
