41c58c95ab53634414f453263ae6714e34b33556
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-10-13 14:41:22 +0200

    nice

--- src/server/nautical/calibration/consensus/cons003.m
+++ src/server/nautical/calibration/consensus/cons003.m
@@ -1,24 +1,23 @@
 add_calib_paths;
 
 raw = true;
-[A, B] = get_calib_ds(0, raw);
+[A, B] = get_calib_ds(2, raw);
 n = get_observation_count(A);
 %%
 settings = make_indeparam_settings();
 
-Q = gram_schmidt(A);
-R = Q'*A;
-indeparam_optimizers = make_indeparam_optimizers(Q, B, settings);
+AB = [A B];
+Q = gram_schmidt(AB);
+R = Q'*AB;
+q = zeros(size(B));
+indeparam_optimizers = make_indeparam_optimizers(A, B, settings);
 
 
 
 %%
 close all;
 
-X = R*[1 0 0 0]';
-
-%X(1) = 0;
-
+X = [1 0 -50 -100]';
 
 plot_indeparam_optimizers(indeparam_optimizers, X);
 
