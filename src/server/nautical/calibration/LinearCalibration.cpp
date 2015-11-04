dd628a095a9f75a6c852dfa25a41627612e5c414
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-11-04 07:53:20 +0100

    more files

---
+++ src/server/nautical/calibration/LinearCalibration.cpp
@@ -0,0 +1,108 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#include <server/nautical/calibration/LinearCalibration.h>
+#include <armadillo>
+#include <server/math/BandMat.h>
+#include <server/common/ArrayIO.h>
+#include <server/common/ScopedLog.h>
+#include <server/common/string.h>
+#include <server/common/ArrayIO.h>
+
+
+namespace sail {
+namespace LinearCalibration {
+
+void initializeLinearParameters(bool withOffset, double *dst2or4) {
+  dst2or4[0] = 1.0;
+  int n = (withOffset? 4 : 2);
+  for (int i = 1; i < n; i++) {
+    dst2or4[i] = 0.0;
+  }
+}
+
+typedef Eigen::Triplet<double> Triplet;
+
+
+
+
+int calcPassiveCount(Duration<double> total, Duration<double> period) {
+  return 1 + int(floor(total/period));
+}
+
+CommonResults calibrateSparse(FlowMatrices rawMats, Duration<double> totalDuration,
+    CommonCalibrationSettings settings) {
+    return CommonResults();
+}
+
+LinearCorrector::LinearCorrector(const FlowSettings &flowSettings,
+    Arrayd windParams, Arrayd currentParams) :
+    _flowSettings(flowSettings), _windParams(windParams), _currentParams(currentParams) {}
+
+Array<CalibratedNav<double> > LinearCorrector::operator()(const Array<Nav> &navs) const {
+  return navs.map<CalibratedNav<double> >([&](const Nav &x) {
+    return (*this)(x);
+  });
+}
+
+arma::mat asMatrix(const MDArray2d &x) {
+  return arma::mat(x.ptr(), x.rows(), x.cols(), false, true);
+}
+
+arma::mat asMatrix(const Arrayd &x) {
+  return arma::mat(x.ptr(), x.size(), 1, false, true);
+}
+
+CalibratedNav<double> LinearCorrector::operator()(const Nav &nav) const {
+  MDArray2d Aw(2, _flowSettings.windParamCount()), Bw(2, 1);
+  MDArray2d Ac(2, _flowSettings.currentParamCount()), Bc(2, 1);
+
+  makeTrueWindMatrixExpression(nav, _flowSettings, &Aw, &Bw);
+  makeTrueCurrentMatrixExpression(nav, _flowSettings, &Ac, &Bc);
+  arma::vec2 windMat = asMatrix(Aw)*asMatrix(_windParams) + asMatrix(Bw);
+  arma::vec2 currentMat = asMatrix(Ac)*asMatrix(_currentParams) + asMatrix(Bc);
+
+  HorizontalMotion<double> wind{Velocity<double>::knots(windMat[0]),
+    Velocity<double>::knots(windMat[1])};
+  HorizontalMotion<double> current{Velocity<double>::knots(currentMat[0]),
+    Velocity<double>::knots(currentMat[1])};
+  CalibratedNav<double> dst;
+  dst.rawAwa.set(nav.awa());
+  dst.rawAws.set(nav.aws());
+  dst.rawMagHdg.set(nav.magHdg());
+  dst.rawWatSpeed.set(nav.watSpeed());
+  dst.gpsMotion.set(nav.gpsMotion());
+
+  // TODO: Fill in more things here.
+
+  dst.trueWindOverGround.set(wind);
+  dst.trueCurrentOverGround.set(current);
+  return dst;
+}
+
+std::string LinearCorrector::toString() const {
+  std::stringstream ss;
+  ss << "LinearCorrector(windParams="<< _windParams << ", currentParams=" << _currentParams << ")";
+  return ss.str();
+}
+
+
+Results calibrate(CommonCalibrationSettings commonSettings,
+    FlowSettings flowSettings, Array<Nav> navs) {
+    assert(std::is_sorted(navs.begin(), navs.end()));
+  auto totalDuration = navs.last().time() - navs.first().time();
+  auto windResults = calibrateSparse(makeTrueWindMatrices(navs, flowSettings),
+      totalDuration, commonSettings);
+  auto currentResults = calibrateSparse(makeTrueCurrentMatrices(navs, flowSettings),
+      totalDuration, commonSettings);
+  //LinearCorrector corrector(flowSettings, windResults.parameters, currentResults.parameters);
+  //return Results{corrector, windResults.recoveredFlow, currentResults.recoveredFlow};
+  return Results();
+}
+
+
+
+}
+}
---
+++ src/server/nautical/calibration/LinearCalibration.h
@@ -0,0 +1,196 @@
+/*
+ *  Created on: 2015
+ *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
+ */
+
+#ifndef SERVER_NAUTICAL_CALIBRATION_LINEARCALIBRATION_H_
+#define SERVER_NAUTICAL_CALIBRATION_LINEARCALIBRATION_H_
+
+#include <device/Arduino/libraries/PhysicalQuantity/PhysicalQuantity.h>
+#include <server/math/QuadForm.h>
+#include <server/math/SparsityConstrained.h>
+#include <device/Arduino/libraries/CalibratedNav/CalibratedNav.h>
+
+namespace sail {
+namespace LinearCalibration {
+
+inline int flowParamCount(bool withOffset) {
+  return (withOffset? 4 : 2);
+}
+
+struct FlowSettings {
+ bool windWithOffset = true;
+ bool currentWithOffset = true;
+
+ int windParamCount() const {
+   return flowParamCount(windWithOffset);
+ }
+
+ int currentParamCount() const {
+   return flowParamCount(currentWithOffset);
+ }
+};
+
+
+/*
+ * See docs/calib/linearcalib.tex
+ *
+ * If withOffset is false, dst is assumed to be a 2x2 matrix,
+ * otherwise a 2x4 matrix. The dst matrix, when multiplied with
+ * a parameter vector, results in a calibrated motion.
+ */
+template <typename MatrixType>
+void makeCalibratedMotionMatrix(
+    Angle<double> angle, Velocity<double> magnitude,
+    bool withOffset,
+    MatrixType *dst, Velocity<double> unit) {
+  double cosPhi = cos(angle);
+  double sinPhi = sin(angle);
+  double r = magnitude/unit;
+  (*dst)(0, 0) = r*sinPhi; (*dst)(0, 1) = -r*cosPhi;
+  (*dst)(1, 0) = r*cosPhi; (*dst)(1, 1) = r*sinPhi;
+  if (withOffset) {
+    (*dst)(0, 2) = sinPhi; (*dst)(0, 3) = -cosPhi;
+    (*dst)(1, 2) = cosPhi; (*dst)(1, 3) = sinPhi;
+  }
+}
+
+template <typename InstrumentAbstraction>
+HorizontalMotion<double> getGpsMotion(const InstrumentAbstraction &nav) {
+  return HorizontalMotion<double>::polar(nav.gpsSpeed(), nav.gpsBearing());
+}
+
+template <typename MatrixType>
+void makeGpsOffset(const HorizontalMotion<double> &m, MatrixType *dstB,
+    Velocity<double> unit) {
+  (*dstB)(0, 0) = m[0]/unit;
+  (*dstB)(1, 0) = m[1]/unit;
+}
+
+/*
+ * Use this function to express the true wind W as a function of the parameters X:
+ *
+ * W(X) = AX + B
+ *
+ * If withOffset = false, then A is 2x2, otherwise if it is true, then A is 2x4
+ * B is always 2x1
+ */
+template <typename InstrumentAbstraction, typename MatrixType>
+void makeTrueWindMatrixExpression(const InstrumentAbstraction &nav,
+  FlowSettings settings,
+  MatrixType *dstA, MatrixType *dstB,
+  Velocity<double> unit = Velocity<double>::knots(1.0)) {
+  auto absoluteDirectionOfWind = nav.magHdg() + nav.awa() + Angle<double>::degrees(180);
+  makeCalibratedMotionMatrix(absoluteDirectionOfWind, nav.aws(), settings.windWithOffset, dstA, unit);
+  makeGpsOffset(getGpsMotion(nav), dstB, unit);
+}
+
+
+struct FlowMatrices {
+ MDArray2d A, B;
+};
+
+template <typename InstrumentAbstraction>
+FlowMatrices makeTrueWindMatrices(Array<InstrumentAbstraction> navs, const FlowSettings &s) {
+  int n = navs.size();
+  int paramCount = s.windParamCount();
+  MDArray2d A(2*n, paramCount);
+  MDArray2d B(2*n, 1);
+  for (int i = 0; i < n; i++) {
+    int offset = 2*i;
+    auto a = A.sliceRowBlock(i, 2);
+    auto b = B.sliceRowBlock(i, 2);
+    makeTrueWindMatrixExpression(navs[i], s, &a, &b);
+  }
+  return FlowMatrices{A, B};
+}
+
+template <typename InstrumentAbstraction>
+FlowMatrices makeTrueCurrentMatrices(Array<InstrumentAbstraction> navs, const FlowSettings &s) {
+  int n = navs.size();
+  int paramCount = s.currentParamCount();
+  MDArray2d A(2*n, paramCount);
+  MDArray2d B(2*n, 1);
+  for (int i = 0; i < n; i++) {
+    int offset = 2*i;
+    auto a = A.sliceRowBlock(i, 2);
+    auto b = B.sliceRowBlock(i, 2);
+    makeTrueCurrentMatrixExpression(navs[i], s, &a, &b);
+  }
+  return FlowMatrices{A, B};
+}
+
+
+/*
+ * Use this function to express the true current C as a function of the parameters X:
+ *
+ * C(X) = AX + B
+ *
+ * If withOffset = false, then A is 2x2, otherwise if it is true, then A is 2x4
+ * B is always 2x1
+ */
+template <typename InstrumentAbstraction, typename MatrixType>
+void makeTrueCurrentMatrixExpression(const InstrumentAbstraction &nav,
+  const FlowSettings &s,
+  MatrixType *dstA, MatrixType *dstB,
+  Velocity<double> unit = Velocity<double>::knots(1.0)) {
+  auto oppositeDirectionOfBoatOverWater = nav.magHdg() + Angle<double>::degrees(180);
+  makeCalibratedMotionMatrix(oppositeDirectionOfBoatOverWater,
+      nav.watSpeed(), s.currentWithOffset, dstA, unit);
+  makeGpsOffset(getGpsMotion(nav), dstB, unit);
+}
+
+void initializeLinearParameters(bool withOffset, double *dst2or4);
+
+/*
+ * Common part of the calibration:
+ *
+ * Linear calibration with sparsity constraints.
+ * Can be used on either wind or current.
+ */
+struct CommonCalibrationSettings {
+};
+
+struct CommonResults {
+};
+
+CommonResults calibrateSparse(FlowMatrices mats, Duration<double> totalDuration,
+    CommonCalibrationSettings settings);
+
+
+
+
+
+
+
+/*
+ * Perform full calibration of wind and current.
+ */
+// A class used to map a raw nav to a corrected one, using the parameters recovered.
+class LinearCorrector : public CorrectorFunction {
+ public:
+  LinearCorrector() {}
+  LinearCorrector(const FlowSettings &flowSettings, Arrayd windParams, Arrayd currentParams);
+  Array<CalibratedNav<double> > operator()(const Array<Nav> &navs) const;
+  CalibratedNav<double> operator()(const Nav &navs) const;
+  std::string toString() const;
+ private:
+  FlowSettings _flowSettings;
+  Arrayd _windParams, _currentParams;
+};
+
+struct Results {
+  Results() {}
+  LinearCorrector corrector;
+  Array<HorizontalMotion<double> > windOverGround, currentOverGround;
+};
+
+Results calibrate(CommonCalibrationSettings commonSettings,
+    FlowSettings flowSettings, Array<Nav> navs);
+
+
+
+}
+}
+
+#endif
