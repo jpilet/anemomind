5884e74bd330830c6ac368054c3a2fc3374801f2
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-11-14 16:37:18 +0100

    FUNKAR BRA

--- src/server/nautical/calibration/LinearCalibrationTest.cpp
+++ src/server/nautical/calibration/LinearCalibrationTest.cpp
@@ -224,7 +224,7 @@ TEST(LinearCalibrationTest, RealData) {
   auto trueWind = makeTrueWindMatrices(navs, flowSettings);
   auto trueCurrent = makeTrueCurrentMatrices(navs, flowSettings);
 
-  auto flow = trueCurrent;
+  auto flow = trueWind;
 
   Eigen::MatrixXd Avelocities =
       Eigen::Map<Eigen::MatrixXd>(flow.A.ptr(), flow.rows(), flow.A.cols());
