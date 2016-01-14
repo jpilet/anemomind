173e39dde4b5c268fedea2e1ebbed918ecc495b5
Julien Pilet <julien.pilet@gmail.com>
2016-01-14 16:00:25 +0100

    Add comment about BitStream endianness

--- src/device/anemobox/n2k/BitStream.h
+++ src/device/anemobox/n2k/BitStream.h
@@ -4,6 +4,17 @@
 #include <stdint.h>
 #include <algorithm>
 
+/* Read a packed stream of arbirarily sized ints, not necessarily aligned on
+ * bytes.
+ *
+ * The encoding is "little endian": to encode 3 bits in a byte, the 3 low bits
+ * are used first.
+ * Thus, the stream of 4-bit int: 0x1, 0x2, 0x3, 0x4 is encoded:
+ * 0x21, 0x43
+ *
+ * It is possible to read up to 64 bits in a single read. A 64bit int is
+ * assumed to be encoded little endian.
+ */
 class BitStream {
  public:
   BitStream(uint8_t* data, size_t len);
