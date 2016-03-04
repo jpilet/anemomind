2eb1560da12efc817cdf1e732e2dd22d146077af
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-03-04 18:46:53 +0100

    almost works

--- src/server/nautical/logs/CsvTest.cpp
+++ src/server/nautical/logs/CsvTest.cpp
@@ -6,11 +6,20 @@
 #include <gtest/gtest.h>
 #include <server/nautical/logs/LogLoader.h>
 #include <server/common/Env.h>
+#include <server/common/PathBuilder.h>
 
 using namespace sail;
 
 TEST(CsvTest, Rowdy) {
-  LogLoader loader;
-
-
+  auto data = LogLoader::loadNavDataset(
+      PathBuilder::makeDirectory(Env::SOURCE_DIR)
+      .pushDirectory("datasets")
+      .pushDirectory("csvlog").get());
+  EXPECT_EQ(data.samples<AWA>().size(), 6);
+  EXPECT_EQ(data.samples<AWS>().size(), 6);
+  EXPECT_EQ(data.samples<MAG_HEADING>().size(), 6);
+  EXPECT_EQ(data.samples<WAT_SPEED>().size(), 6);
+  EXPECT_EQ(data.samples<GPS_SPEED>().size(), 6);
+  EXPECT_EQ(data.samples<GPS_BEARING>().size(), 6);
+  EXPECT_EQ(data.samples<GPS_POS>().size(), 6);
 }
