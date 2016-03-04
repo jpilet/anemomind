3f7038f70310f01e6ee8c0384dc9f4dfc7ed4fce
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-03-04 18:41:48 +0100

    Added some stuff

---
+++ src/server/nautical/logs/CsvTest.cpp
@@ -0,0 +1,16 @@
+/*
+ *  Created on: 2016
+ *      Author: Jonas Östlund <jonas@anemomind.com>
+ */
+
+#include <gtest/gtest.h>
+#include <server/nautical/logs/LogLoader.h>
+#include <server/common/Env.h>
+
+using namespace sail;
+
+TEST(CsvTest, Rowdy) {
+  LogLoader loader;
+
+
+}
