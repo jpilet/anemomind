d5b9d809ba60d49552285932b0881706db3eba13
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-03-04 19:38:52 +0100

    Load an ordinary log file

--- src/server/nautical/logs/ProtobufLogTest.cpp
+++ src/server/nautical/logs/ProtobufLogTest.cpp
@@ -3,6 +3,23 @@
  *      Author: Jonas Östlund <jonas@anemomind.com>
  */
 
+#include <gtest/gtest.h>
+#include <server/common/Env.h>
+#include <server/common/PathBuilder.h>
+#include <server/nautical/logs/LogLoader.h>
+
+using namespace sail;
+
+TEST(ProtobufLogTest, LoadAFile) {
+  auto data = LogLoader::loadNavDataset(
+      PathBuilder::makeDirectory(Env::SOURCE_DIR)
+        .pushDirectory("datasets")
+        .pushDirectory("protobuflog").get());
+  EXPECT_LT(0, data.samples<AWA>().size());
+  EXPECT_LT(0, data.samples<GPS_POS>().size());
+  EXPECT_LT(0, data.samples<GPS_SPEED>().size());
+  EXPECT_LT(0, data.samples<GPS_BEARING>().size());
+}
 
 
 
