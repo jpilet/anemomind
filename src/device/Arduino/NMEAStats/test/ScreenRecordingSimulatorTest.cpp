74f01482aa4323f0b550aecf90ad29153b0dbd5a
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-03-04 18:32:14 +0100

    Fixed this testcase too

--- src/device/Arduino/NMEAStats/test/ScreenRecordingSimulatorTest.cpp
+++ src/device/Arduino/NMEAStats/test/ScreenRecordingSimulatorTest.cpp
@@ -22,8 +22,8 @@ TEST(ScreenRecordingTest, screenAtTest) {
   NavDataset navs = LogLoader::loadNavDataset(nmeaFile);
 
   int n = getNavSize(navs);
-  EXPECT_EQ(5, n);
-  for (int i = 0; i < n; ++i) {
+  EXPECT_EQ(6, n);
+  for (int i = 1; i < 6; ++i) {
     ScreenInfo info;
     EXPECT_TRUE(simulator.screenAt(getNav(navs, i).time(), &info));
     EXPECT_NEAR((info.time - getNav(navs, i).time()).seconds(), 0, 2);
