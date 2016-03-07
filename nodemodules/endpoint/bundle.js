b07e87a90b079d3ef9532c739ad756f785fe8d18
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-03-07 08:25:12 +0100

    added test bundle

---
+++ nodemodules/endpoint/bundle.js
@@ -0,0 +1,20 @@
+/*
+
+= HOW TO USE =
+
+== ON THE SENDER SIDE ==
+bundle.sendBundle(dst, bundleSetup);
+
+where bundleSetup is a map: {
+ main: [path to file that should be run, relative to root. Can be .sh or .js]
+ dst: [path where the bundle should be unpacked]
+ src: [path to .bundle file]
+}
+
+== ON THE RECEIVER SIDE ==
+The receiver needs to add a bundle handler among the packet handler
+of the endpoint:
+
+endpoint.addPacketHandler(bundle.makeBundleHandler());
+
+*/
---
+++ nodemodules/endpoint/test/bundle.js
@@ -0,0 +1,8 @@
+var bundle = require('../bundle.js');
+var assert = require('assert');
+
+describe('bundle', function() {
+  it('Should prepare a setup where we can experiment', function(done) {
+    
+  });
+});
