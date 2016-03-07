213911bd838fb6c25b823e43fe5fda339b97c47f
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-03-07 08:44:18 +0100

    bra

--- nodemodules/endpoint/test/bundle.js
+++ nodemodules/endpoint/test/bundle.js
@@ -1,17 +1,42 @@
 var bundle = require('../bundle.js');
 var assert = require('assert');
 var Q = require('q');
+var mkdirp = require('mkdirp');
+var exec = require('child_process').exec;
+var Path = require('path');
 
+
+function makeDirName(name) {
+  return Path.join('/tmp/bundletest', name)
+}
+
+function makeDirectory(name) {
+  return Q.nfcall(mkdirp, makeDirName(name));
+}
+
+function gitInit(name) {
+  var x = makeDirName(name);
+  return Q.nfcall(exec, 'cd ' + x + '; git init');
+}
+
+function makeRepository(name) {
+  return makeDirectory(name)
+    .then(gitInit(name));
+
+}
 
 function prepareTestSetup() {
-  var x = Q.defer();
-  x.resolve();
-  return x.promise;  
+  return makeRepository('rulle');
 }
 
 describe('bundle', function() {
   it('Should prepare a setup where we can experiment', function(done) {
     prepareTestSetup()
-      .then(done);
+      .then(function(value) {
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      }).done();
   });
 });
