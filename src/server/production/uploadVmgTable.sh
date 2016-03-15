c37181822dd167acd7011658b95798b524fa103e
Julien Pilet <julien.pilet@gmail.com>
2016-03-15 09:32:19 +0100

    Calls uploadvmgtable from processNewLogs

---
+++ src/server/production/uploadVmgTable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+BIN="/home/anemomind/bin"
+PROCESSED_DIR="/home/anemomind/processed"
+SCRIPT="/tmp/update-vmgtable-$$"
+
+echo > $SCRIPT
+
+for boatdir in "$PROCESSED_DIR/"*; do
+  file="$boatdir/processed/boat.dat"
+  id="$(basename "${boatdir}"| sed 's/boat//')"
+
+  [ -r $file ] && \
+    "${BIN}/catTargetSpeed" --id "${id}" "$file" >> $SCRIPT
+done
+  
+cat $SCRIPT | ssh jpilet@anemolab.com mongo > /dev/null
