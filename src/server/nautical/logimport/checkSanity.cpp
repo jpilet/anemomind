917233e5ddee5aa212b4bc273338f45ed1d22b5c
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-03-17 10:50:14 +0100

    good

--- src/server/nautical/logimport/checkSanity.cpp
+++ src/server/nautical/logimport/checkSanity.cpp
@@ -1,36 +1,108 @@
 /*
  *  Created on: 2016
  *      Author: Jonas Östlund <jonas@anemomind.com>
+ *
  */
 
 #include <server/nautical/logimport/LogLoader.h>
 #include <server/common/logging.h>
+#include <device/anemobox/DispatcherUtils.h>
+#include <server/common/string.h>
 
 using namespace sail;
 
 namespace {
   Array<const char *> listPathsToLoad(int argc, const char **argv) {
     int n = argc - 1;
-    Array<std::string> dst(n);
+    Array<const char *> dst(n);
     if (n == 0) {
-      LOG(INFO) << "USAGE: Just pass all the paths that should be loaded, to this program";
+      std::cout <<
+          "USAGE: Just pass all the paths that should be loaded, to this program"
+          << std::endl;
     } else {
       for (int i = 0; i < n; i++) {
         auto p = argv[i + 1];
-        LOG(INFO) << "  To analyze: " << p;
+        std::cout << "  To analyze: " << p << std::endl;
         dst[i] = p;
       }
     }
     return dst;
   }
+
+  struct Problem {
+   std::string path, what;
+   void disp(std::ostream *dst);
+  };
+
+  void Problem::disp(std::ostream *dst) {
+    *dst << "   From path " << path << ": " << what << std::endl;
+  }
+
+
+  class DispatcherProblemVisitor {
+   public:
+    DispatcherProblemVisitor(
+        const char *p,
+        std::vector<Problem> *dst) : _path(p), _dst(dst) {}
+
+    template <DataCode Code, typename T>
+      void visit(const char *shortname, const std::string &srcName,
+          const typename TimedSampleCollection<T>::TimedVector &data) {
+      for (int i = 0; i < data.size(); i++) {
+        auto sample = data[i];
+        if (!sample.time.defined()) {
+          _dst->push_back(Problem{
+            _path,
+            stringFormat(
+                "Sample with index %d has undefined time in channel %s of type %s",
+                i, srcName.c_str(), shortname)
+          });
+        }
+      }
+    }
+   private:
+    const char *_path;
+    std::vector<Problem> *_dst;
+  };
+
+  void checkForDispatcherProblems(const char *p, const std::shared_ptr<Dispatcher> &d,
+    std::vector<Problem> *dst) {
+    DispatcherProblemVisitor v(p, dst);
+  }
+
+  void checkForDatasetProblems(const char *p, const NavDataset &ds, std::vector<Problem> *dst) {
+    checkForDispatcherProblems(p, ds.dispatcher(), dst);
+  }
+
+  void checkSanity(const char *path, std::vector<Problem> *dst) {
+    LogLoader loader;
+    loader.load(std::string(path));
+    auto dataset = loader.makeNavDataset();
+    checkForDatasetProblems(path, dataset, dst);
+  }
 }
 
 int main(int argc, const char **argv) {
   auto paths = listPathsToLoad(argc, argv);
-  for (int i = 0; i < paths.size(); i++) {
-    auto p = paths[i];
-    LOG(INFO) << "Processing path " << i + 1 << "/" << paths.size() << ": " << p;
+
+  if (paths.hasData()) {
+    std::vector<Problem> problems;
+
+    for (int i = 0; i < paths.size(); i++) {
+      auto p = paths[i];
+      std::cout << "Processing path " << i + 1 << "/" << paths.size() << ": " << p;
+      checkSanity(p, &problems);
+    }
+    if (problems.empty()) {
+      std::cout << "Congratulations, no problems detected!" << std::endl;
+    } else {
+      std::cout << problems.size() << " problems were detected:\n";
+      for (auto problem: problems) {
+        problem.disp(&(std::cout));
+      }
+    }
   }
+
   return 0;
 }
 
