7b9bfcf8c85deadf0122f454b2457357d26d8c3b
Julien Pilet <julien.pilet@gmail.com>
2016-03-18 19:01:41 +0100

    Create, cache and send update bundles (in progress)

--- www2/server/api/boxexec/boxexec.controller.js
+++ www2/server/api/boxexec/boxexec.controller.js
@@ -7,6 +7,7 @@
 
 var BoxExec = require('./boxexec.model');
 var RemoteOps = require('./remoteOps');
+var BundleUtils = require('./bundleutils');
 
 // Gets a list of Boxexecs
 exports.index = function(req, res) {
@@ -16,7 +17,7 @@ exports.index = function(req, res) {
   });
 }
 
-exports.create = function(req, res) {
+function createSendScriptBoxExec(req, res) {
   var validTypes = { js: true, sh: true };
   if (!validTypes[req.body.scriptType]) {
     res.status(400).send("Invalid type");
@@ -38,3 +39,76 @@ exports.create = function(req, res) {
         });
       });
 }
+
+function isFresh(file, maxAgeSeconds) {
+  var deferred = $q.defer();
+
+  fs.stat(file, function(err, stats) {
+    if (err || !stats.birthtime) {
+      deferred.reject(file + ': fs.stat failed');
+    }
+    var deltams = (new Date().getTime() - stats.birthtime.getTime());
+    if (deltams < maxAgeSeconds * 1000) {
+      deferred.resolve(file);
+    } else {
+      deferred.reject('not fresh');
+    }
+  });
+
+  return deferred;
+}
+
+function createSendBundleFile(req, res) {
+  if (!req.body.boatId
+      || !req.body.bundleSettings
+      || !req.body.bundleSettings.version) {
+    res.status(400).send("Bad bundle settings");
+  }
+
+  var boatId = req.body.boatId;
+
+  var sendFile = function(file) {
+    RemoteOps.sendBoatData(boatId, file, '/tmp/bundle', function(err, result) {
+      if (err) {
+        res.status(500).send("sendBoatData failed: " + err);
+      } else {
+        res.status(200).send();
+      }
+    });
+  };
+
+  var file = '/tmp/bundle-'
+    + BundleUtils.makeVersionString(req.body.bundleSettings.version);
+  var inprogress = file + '-in-progress';
+
+  // Is the file recent enough?
+  isFresh(file).then(
+     sendFile, // fresh
+     function() { // not fresh
+       BundleUtils.makeBundle({
+         localBundleFilename: inprogress,
+         version: req.body.bundleSettings.version})
+       .then(function() {
+           // bundle created, rename the file to ensure atomic creation
+           fs.rename(inprogress, file, function(err) {
+             sendFile(file);
+           });
+         },
+         function(err) {
+           res.status(500).send(err);
+         }
+       );
+     }
+   );
+}
+
+exports.create = function(req, res) {
+  if (req.body.scriptType) {
+    createSendScriptBoxExec(req, res);
+  } else if (req.body.bundleSettings) {
+    createSendBundleFile(req, res);
+  } else {
+    res.status(400).send();
+  }
+}
+
