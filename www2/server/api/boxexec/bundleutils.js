d39f14bd72cd5fd9c4e5bfb34b6d3c49ae1e2934
Jonas Östlund <jonas@anemomind.com>
2016-03-18 18:48:04 +0100

    Simplify even more the bundleutils

--- www2/server/api/boxexec/bundleutils.js
+++ www2/server/api/boxexec/bundleutils.js
@@ -12,39 +12,11 @@ var env = require('../../config/environment');
 var serverAddress = 'anemomind@vtiger.anemomind.com';
 var repositoryPath = '/home/anemobox/anemobox.git'
 
-// This is a template of the parameters 
-// for a bunde
-var settings = {
-  // You may want to give a unique name, in
-  // case 'makeBundle' is called concurrently on the server
-  // for different boats.
-  //
-  // This can be either an absolute path, or a path relative to 
-  // env.bundleDir. In either case, the path will be created if it does not exist.
-  bundleName: 'tmp.bundle',
-  
-  // For debugging, verbosity can be turned on.
-  verbose: true,
-
-  // Here you can either pass a string, such as 'v3..master' or just 'master',
-  // or you can pass an argument map, such as {from: 'v3'} or {from: 'v3', to: 'master'}
-  version: 'master'
-};
-
 function ensureBundleLocation(filename) {
   var p = path.dirname(filename);
   return q.nfcall(mkdirp, p);
 }
 
-function getFullBundleFilename(bundleName) {
-  if (path.isAbsolute(bundleName)) {
-    return bundleName;
-  } else {
-    return path.join(env.bundleDir, bundleName);
-  }
-}
-
-// TODO: Add settings map for 'anemolab'
 
 function exec2(cmd, verbose, cb) {
   if (verbose) {
@@ -89,32 +61,25 @@ function makeSshCommand(cmd) {
   return 'ssh ' + serverAddress + ' "' + cmd + '"';
 }
 
-
-
-// Make a bundle that is saved to the file, with name
-// as in settings.bundleName. The location of the file
-// depends on env.bundleDir.
-// This function returns the final filename as a promise when done.
-function makeBundle(settings) {
-  var v = makeVersionString(settings.version);
+// Make a bundle named as 'dstFilename'. The 'version' parameter
+// can be either a raw string, or a map with the 'from' key, and optionally
+// the 'to' key.
+function makeBundle(dstFilename, version, verbose) {
+  var v = makeVersionString(version);
   var cmd = 'cd ' + repositoryPath + '; '
       + 'git bundle create - ' + v;
-  var bname = getFullBundleFilename(settings.bundleName);
-  var fullCmd = makeSshCommand(cmd) + ' > ' + bname;
-  return ensureBundleLocation(bname)
+  var fullCmd = makeSshCommand(cmd) + ' > ' + dstFilename;
+  return ensureBundleLocation(dstFilename)
     .then(function() {
-      return q.nfcall(exec2, fullCmd, settings.verbose);
-    })
-    .then(function() {
-      return bname;
+      return q.nfcall(exec2, fullCmd, verbose);
     });
 }
 
 // Get all the possible version tags
-function getTags(settings) {
+function getTags(verbose) {
   var cmd = 'cd ' + repositoryPath + '; git tag';
   var fullCmd = makeSshCommand(cmd);
-  return q.nfcall(exec2, fullCmd, settings.verbose)
+  return q.nfcall(exec2, fullCmd, verbose)
     .then(function(x) {
       return x.stdout.split(/\s+/).filter(function(s) {
         return s.length > 0;
@@ -122,11 +87,5 @@ function getTags(settings) {
     });
 }
 
-
-// These two functions take a settings map as single argument
 module.exports.makeBundle = makeBundle;
 module.exports.getTags = getTags;
-
-// These are default settings that you can use as a template for
-// the arguments passed to the above functions
-module.exports.settings = settings;
