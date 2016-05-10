7179f1497199a3bd068b665f3e217338d49e9619
evaletolab <evaleto@gmail.com>
2016-05-10 12:11:21 +0200

    implement a fast query to collect photos and comments for a list of boats

--- www2/server/api/event/event.model.js
+++ www2/server/api/event/event.model.js
@@ -17,4 +17,61 @@ var EventSchema = new Schema({
   longitude: Number
 });
 
+EventSchema.statics.collectSocialDataByBoat=function (boatsId,callback) {
+  var promise=new mongoose.Promise;
+  if(callback){promise.addBack(callback);}
+
+	//
+	// filter relevants events
+	var select={
+	  boat:{
+	  	$in:boatsId||[]
+	  }, 
+	  $or:[
+	  	{'photo': {$exists:true}},{'comment':{$exists:true}}
+	  ]
+	};
+
+	//
+	// group photo & comment by boats
+	// It seems that $cond is not working with mongoose <4.0 !
+	this.aggregate(
+   {$match: select },
+   {$project:{
+       photo:1,
+       comment:1,
+       boat:1,
+       author:1,
+       when:1
+   }},
+   {$group:
+       {
+         _id:"$boat",
+         photos:{
+          $addToSet:{
+            "$cond": [
+                { "$ne": [ "$photo", undefined ] },
+                {src:"$photo",when:"$when",who:"$author"},undefined
+            ]
+          }
+         },
+         comments:{
+          $addToSet:{
+            "$cond": [
+                { "$ne": [ "$comment", undefined ] },
+                {txt:"$comment",when:"$when",who:"$author"},undefined
+            ]
+          }
+         }
+       }
+   },function(err,boats) {
+   		if(err){
+   			return promise.reject(err);
+   		}
+      promise.resolve(err,boats);
+
+   });
+}
+
+
 module.exports = mongoose.model('Event', EventSchema);
