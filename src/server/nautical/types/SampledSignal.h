8906aa4008ca314d18ad2a3886c37d16a53841ff
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-05-13 15:03:16 +0200

    Add UniformlySampledSignal

--- src/server/nautical/types/SampledSignal.h
+++ src/server/nautical/types/SampledSignal.h
@@ -20,6 +20,13 @@ public:
   virtual ~SampledSignal() {}
 };
 
+template <typename T>
+class UniformlySampledSignal : public SampledSignal<T> {
+public:
+  virtual Duration<double> samplingPeriod() const = 0;
+  virtual ~UniformlySampledSignal() {}
+};
+
 }
 
 #endif /* SERVER_NAUTICAL_TYPES_DISCRETETIMESIGNAL_H_ */
