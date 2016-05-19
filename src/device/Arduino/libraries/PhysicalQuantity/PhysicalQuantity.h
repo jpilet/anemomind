6626dfd7e17695cc4e74a362ad2c2c33881f3b45
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-05-19 17:20:27 +0200

    better

--- src/device/Arduino/libraries/PhysicalQuantity/PhysicalQuantity.h
+++ src/device/Arduino/libraries/PhysicalQuantity/PhysicalQuantity.h
@@ -16,154 +16,35 @@
 #ifndef PHYSICALQUANTITY_H_
 #define PHYSICALQUANTITY_H_
 
-#include <server/common/numerics.h>
-#ifdef ON_SERVER
-#include <cmath>
-#include <limits>
-#include <server/common/math.h>
-#include <sstream>
-#include <string>
-#else
-#include <math.h>
-#endif
-
-// TODO: Do we still need this?
-// workaround some arduino #define..
-#ifdef degrees
-#undef degrees
-#endif
-#ifdef radians
-#undef radians
-#endif
-
-#ifdef isnan
-#undef isnan
-#endif
-
-namespace sail {
-
 /*
- * These macros may not be in conformity with the
- * coding style guide but they reduce code duplication effort
- * and make the code much more readable than it otherwise would be.
- */
-#define MAKE_PHYSQUANT_UNIT_CONVERTERS(name, fromFactor) \
-  T name() const {return (T(1.0) / T(fromFactor))*(this->_x);} \
-  static ThisType name(T x) {return ThisType(T(fromFactor)*x);}
-
-#define DECLARE_PHYSQUANT_CONSTRUCTORS(ClassName, baseUnit) \
-  private: \
-    ClassName(T x) : PhysicalQuantity<ClassName<T>, T>(x) {} \
-  public: \
-    typedef ClassName<T> ThisType; \
-    ClassName() : PhysicalQuantity<ClassName<T>, T>() { } \
-    ClassName(const PhysicalQuantity<ClassName<T>, T> &x) : PhysicalQuantity<ClassName<T>, T>(x) {} \
-  T baseUnit() const { return this->_x; } \
-  static ThisType baseUnit(T x) { return ThisType(x); } \
-  template<class Other> \
-  ClassName<Other> cast() const { return ClassName<Other>::baseUnit(static_cast<Other>(this->baseUnit())); } \
-  template<class Other> operator ClassName<Other>() const { return cast<Other>(); }
-
-template <typename Quantity, typename Value>
-class PhysicalQuantity {
- public:
-#ifdef ON_SERVER
-  const static Value defaultValue;
-#endif
-  typedef PhysicalQuantity<Quantity, Value> ThisQuantity;
-  typedef Quantity QuantityType;
-  typedef Value ValueType;
-
-  // Additon/subtraction --> Quantity
-  Quantity operator+(ThisQuantity other) const {
-    return Quantity::makeFromX(_x + other.get());
-  }
-
-  ThisQuantity &operator += (ThisQuantity other) {
-    _x += other.get();
-    return *this;
-  }
-
-  Quantity operator-(ThisQuantity other) const {
-    return Quantity::makeFromX(_x - other.get());
-  }
-
-  Quantity operator-() const {
-    return Quantity::makeFromX(-_x);
-  }
-
-  ThisQuantity &operator -= (ThisQuantity other) {
-    _x -= other.get();
-    return *this;
-  }
-
-  static Quantity zero() {
-    return Quantity::makeFromX(0.0);
-  }
-
-  Quantity scaled(Value s) const {
-      return Quantity::makeFromX(_x * s);
-  }
-
-  // Syntactic sugar for 'scaled'
-  Quantity operator* (Value other) const {
-    return scaled(other);
-  }
-
-  Value operator/ (Quantity other) const {
-    return _x/other._x;
-  }
 
 
-  Quantity fabs() const { return Quantity::makeFromX(::fabs(_x)); }
-
-  // Please use sail::isFinite defined in
-  // <server/common/numeric.h> to test if
-  // PhysicalQuantity is finite. Same goes for nan.
-  bool isNaNQuantity() const { return sail::isNaN(_x); }
-  bool isFiniteQuantity() const { return sail::isFinite(_x); }
-
-  // Comparison --> bool
-  bool operator < (ThisQuantity other) const {return _x < other.get();}
-  bool operator <= (ThisQuantity other) const {return _x <= other.get();}
-  bool operator > (ThisQuantity other) const {return _x > other.get();}
-  bool operator >= (ThisQuantity other) const {return _x >= other.get();}
-  bool operator == (ThisQuantity other) const {return _x == other.get();}
+////////////////////////////////////
 
 #ifdef ON_SERVER
-  // Special method returning true for the comparison nan == nan.
-  bool eqWithNan(ThisQuantity other) const {
-    return strictEquality(_x, other.get());
+  std::string str() const {
+    std::stringstream ss;
+    Duration<T> remaining(*this);
+
+#define FORMAT_DURATION_UNIT(unit) \
+    if (remaining.unit() >= 1) { \
+      if (ss.str().size() > 0) ss << ", "; \
+      ss << floor(remaining.unit()) << " " #unit ; \
+      remaining -= unit(floor(remaining.unit())); \
+    }
+    FORMAT_DURATION_UNIT(weeks)
+    FORMAT_DURATION_UNIT(days)
+    FORMAT_DURATION_UNIT(hours)
+    FORMAT_DURATION_UNIT(minutes)
+    FORMAT_DURATION_UNIT(seconds)
+#undef FORMAT_DURATION_UNIT
+    return ss.str();
   }
-
-  bool nearWithNan(ThisQuantity other, double marg) const {
-    return sail::nearWithNan(_x, other.get(), marg);
-  }
-#endif
- protected:
-  Value get() const {return _x;}
-  PhysicalQuantity(Value x) : _x(x) {}
-#ifdef ON_SERVER
-  PhysicalQuantity() : _x(Quantity::defaultValue) {}
-#else
-  PhysicalQuantity() { }
-#endif
-  Value _x;
- private:
-  static Quantity makeFromX(Value X) { return Quantity(PhysicalQuantity<Quantity, Value>(X)); }
-};
-
-#ifdef ON_SERVER
-template <typename Quantity, typename Value>
-const Value PhysicalQuantity<Quantity, Value>::defaultValue =
-    Value(std::numeric_limits<double>::signaling_NaN());
 #endif
 
-template <typename T = double>
-class Angle : public PhysicalQuantity<Angle<T>, T> {
-  DECLARE_PHYSQUANT_CONSTRUCTORS(Angle, degrees)
- public:
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(radians, 180.0/M_PI);
+
+////////////////////////////////////
+
 
   Angle directionDifference(const Angle<T>& other) const {
     return (*this - other).normalizedAt0();
@@ -198,72 +79,285 @@ class Angle : public PhysicalQuantity<Angle<T>, T> {
     *sinAngle = sin(rad);
     *cosAngle = cos(rad);
   }
-};
 
-template <typename T = double>
-class Length : public PhysicalQuantity<Length<T>, T> {
-  DECLARE_PHYSQUANT_CONSTRUCTORS(Length, meters)
- public:
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(kilometers, 1000.0);
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(nauticalMiles, 1852.0);
-};
 
-template <typename T = double>
-class Velocity : public PhysicalQuantity<Velocity<T>, T> {
-  DECLARE_PHYSQUANT_CONSTRUCTORS(Velocity, knots)
- public:
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(metersPerSecond, 3600.0/1852);
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(kilometersPerHour, 1.0/1.852);
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(milesPerHour, 0.868976242);
-};
 
-template <typename T = double>
-class Duration : public PhysicalQuantity<Duration<T>, T> {
-  DECLARE_PHYSQUANT_CONSTRUCTORS(Duration, milliseconds)
- public:
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(seconds, 1000);
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(minutes, 60.0 * 1000.0);
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(hours, 3600.0 * 1000.0);
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(days, 24*3600.0 * 1000.0);
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(weeks, 7*24*3600.0 * 1000.0);
+
+////////////////////////////////////
+
 #ifdef ON_SERVER
-  std::string str() const {
-    std::stringstream ss;
-    Duration<T> remaining(*this);
+  // Special method returning true for the comparison nan == nan.
+  bool eqWithNan(ThisQuantity other) const {
+    return strictEquality(_x, other.get());
+  }
 
-#define FORMAT_DURATION_UNIT(unit) \
-    if (remaining.unit() >= 1) { \
-      if (ss.str().size() > 0) ss << ", "; \
-      ss << floor(remaining.unit()) << " " #unit ; \
-      remaining -= unit(floor(remaining.unit())); \
-    }
-    FORMAT_DURATION_UNIT(weeks)
-    FORMAT_DURATION_UNIT(days)
-    FORMAT_DURATION_UNIT(hours)
-    FORMAT_DURATION_UNIT(minutes)
-    FORMAT_DURATION_UNIT(seconds)
-#undef FORMAT_DURATION_UNIT
-    return ss.str();
+  bool nearWithNan(ThisQuantity other, double marg) const {
+    return sail::nearWithNan(_x, other.get(), marg);
   }
 #endif
+
+
+
+
+#ifdef ON_SERVER
+template <typename Quantity, typename Value>
+const Value PhysicalQuantity<Quantity, Value>::defaultValue =
+    Value(std::numeric_limits<double>::signaling_NaN());
+#endif
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////
+ *   // Additon/subtraction --> Quantity
+  Quantity operator+(ThisQuantity other) const {
+    return Quantity::makeFromX(_x + other.get());
+  }
+
+  ThisQuantity &operator += (ThisQuantity other) {
+    _x += other.get();
+    return *this;
+  }
+
+  Quantity operator-(ThisQuantity other) const {
+    return Quantity::makeFromX(_x - other.get());
+  }
+
+  Quantity operator-() const {
+    return Quantity::makeFromX(-_x);
+  }
+
+  ThisQuantity &operator -= (ThisQuantity other) {
+    _x -= other.get();
+    return *this;
+  }
+
+
+  Value operator/ (Quantity other) const {
+      return _x/other._x;
+    }
+
+
+ */
+
+
+#include <server/common/numerics.h>
+#ifdef ON_SERVER
+#include <cmath>
+#include <limits>
+#include <server/common/math.h>
+#include <sstream>
+#include <string>
+#else
+#include <math.h>
+#endif
+
+// TODO: Do we still need this?
+// workaround some arduino #define..
+#ifdef degrees
+#undef degrees
+#endif
+#ifdef radians
+#undef radians
+#endif
+
+#ifdef isnan
+#undef isnan
+#endif
+
+namespace sail {
+
+#define MAKE_PHYSQUANT_UNIT_CONVERTERS(t, l, a, m, name, fromFactor) \
+    static PhysicalQuantity<T, t, l, a, m> name(T x) { \
+      return PhysicalQuantity<T, t, l, a, m>(T(fromFactor)*x); \
+    } \
+  T name() const { \
+    static_assert(TimeDim == t, "Not applicable on this type"); \
+    static_assert(LengthDim == l, "Not applicable on this type"); \
+    static_assert(AngleDim == a, "Not applicable on this type"); \
+    static_assert(MassDim == m, "Not applicable on this type"); \
+    return (T(1.0) / T(fromFactor))*(this->_x); \
+  }
+
+
+#define MAKE_PHYSQUANT_TIME_CONVERTERS(name, fromFactor) \
+    MAKE_PHYSQUANT_UNIT_CONVERTERS(1, 0, 0, 0, name, fromFactor)
+
+#define MAKE_PHYSQUANT_LENGTH_CONVERTERS(name, fromFactor) \
+    MAKE_PHYSQUANT_UNIT_CONVERTERS(0, 1, 0, 0, name, fromFactor)
+
+#define MAKE_PHYSQUANT_VELOCITY_CONVERTERS(name, fromFactor) \
+    MAKE_PHYSQUANT_UNIT_CONVERTERS(-1, 1, 0, 0, name, fromFactor)
+
+#define MAKE_PHYSQUANT_ANGLE_CONVERTERS(name, fromFactor) \
+    MAKE_PHYSQUANT_UNIT_CONVERTERS(0, 0, 1, 0, name, fromFactor)
+
+#define MAKE_PHYSQUANT_MASS_CONVERTERS(name, fromFactor) \
+    MAKE_PHYSQUANT_UNIT_CONVERTERS(0, 0, 0, 1, name, fromFactor)
+
+template <typename T, int TimeDim/*t*/, int LengthDim/*l*/, int AngleDim/*a*/, int MassDim/*m*/>
+class PhysicalQuantity;
+
+template <typename T, int TimeDim/*t*/, int LengthDim/*l*/, int AngleDim/*a*/, int MassDim/*m*/>
+std::string toString(const PhysicalQuantity<T, TimeDim, LengthDim, AngleDim, MassDim> &x);
+
+template <typename T, int TimeDim/*t*/, int LengthDim/*l*/, int AngleDim/*a*/, int MassDim/*m*/>
+class PhysicalQuantity {
+public:
+  PhysicalQuantity() : _x(T(NAN)) {}
+
+  typedef PhysicalQuantity<T, TimeDim, LengthDim, AngleDim, MassDim> ThisType;
+
+  static PhysicalQuantity<T, 0, 0, 0, 0> scalar(T x) {return PhysicalQuantity<T, 0, 0, 0, 0>(x);}
+
+  MAKE_PHYSQUANT_TIME_CONVERTERS(milliseconds, 0.001)
+  MAKE_PHYSQUANT_TIME_CONVERTERS(seconds, 1.0);
+  MAKE_PHYSQUANT_TIME_CONVERTERS(minutes, 60.0); // Read as: One minute is 60 seconds
+  MAKE_PHYSQUANT_TIME_CONVERTERS(hours, 3600.0);
+  MAKE_PHYSQUANT_TIME_CONVERTERS(days, 24*3600.0);
+  MAKE_PHYSQUANT_TIME_CONVERTERS(weeks, 7*24*3600.0);
+
+  MAKE_PHYSQUANT_VELOCITY_CONVERTERS(knots, 1852.0/3600.0)
+  MAKE_PHYSQUANT_VELOCITY_CONVERTERS(metersPerSecond, 1.0);
+  MAKE_PHYSQUANT_VELOCITY_CONVERTERS(kilometersPerHour, 1000.0/3600.0);
+  MAKE_PHYSQUANT_VELOCITY_CONVERTERS(milesPerHour, 1609.0/3600.0);
+
+  MAKE_PHYSQUANT_MASS_CONVERTERS(kilograms, 1.0);
+  MAKE_PHYSQUANT_MASS_CONVERTERS(skeppund, 170.0);
+  MAKE_PHYSQUANT_MASS_CONVERTERS(lispund, 170.0/20.0);
+
+  MAKE_PHYSQUANT_LENGTH_CONVERTERS(meters, 1.0)
+  MAKE_PHYSQUANT_LENGTH_CONVERTERS(kilometers, 1000.0);
+  MAKE_PHYSQUANT_LENGTH_CONVERTERS(nauticalMiles, 1852.0);
+
+  MAKE_PHYSQUANT_ANGLE_CONVERTERS(radians, 1.0)
+  MAKE_PHYSQUANT_ANGLE_CONVERTERS(degrees, M_PI/180.0);
+
+  /*operator T () const {
+    static_assert(TimeDim == 0, "Only dimensionless units");
+    static_assert(LengthDim == 0, "Only dimensionless units");
+    static_assert(AngleDim == 0, "Only dimensionless units");
+    static_assert(MassDim == 0, "Only dimensionless units");
+    return _x;
+  }*/
+
+  static PhysicalQuantity<T, TimeDim, LengthDim, AngleDim, MassDim> wrap(T x) {
+    return PhysicalQuantity<T, TimeDim, LengthDim, AngleDim, MassDim>(x);
+  }
+
+
+
+  template <int t, int l, int a, int m>
+  PhysicalQuantity<T, TimeDim + t, LengthDim + l, AngleDim + a, MassDim + m> operator*(
+      const PhysicalQuantity<T, t, l, a, m> &other) {
+    PhysicalQuantity<T, TimeDim + t, LengthDim + l, AngleDim + a, MassDim + m>(_x*other._x);
+  }
+
+  template <int t, int l, int a, int m>
+  PhysicalQuantity<T, TimeDim - t, LengthDim - l, AngleDim - a, MassDim - m> operator/(
+      const PhysicalQuantity<T, t, l, a, m> &other) {
+    PhysicalQuantity<T, TimeDim + t, LengthDim + l, AngleDim + a, MassDim + m>(_x/other._x);
+  }
+
+  ThisType operator*(T s) const {
+    return ThisType(s*_x);
+  }
+
+  ThisType scaled(T s) const {
+    return ThisType(s*_x);
+  }
+
+  static ThisType zero() {
+    return ThisType(T(0.0));
+  }
+
+  ThisType operator+(const ThisType &other) const {
+    return ThisType(_x + other._x);
+  }
+
+  ThisType operator-(const ThisType &other) const {
+    return ThisType(_x - other._x);
+  }
+
+  ThisType fabs() const { return ThisType(::fabs(_x)); }
+
+  bool isNaNQuantity() const { return sail::isNaN(_x); }
+
+  bool isFiniteQuantity() const { return sail::isFinite(_x); }
+
+  bool operator < (ThisType other) const {return _x < other._x;}
+  bool operator <= (ThisType other) const {return _x <= other._x;}
+  bool operator > (ThisType other) const {return _x > other._x;}
+  bool operator >= (ThisType other) const {return _x >= other._x;}
+  bool operator == (ThisType other) const {return _x == other._x;}
+
+
+#ifdef ON_SERVER
+  std::string str() const {
+    return toString(*this);
+  }
+#endif
+private:
+  PhysicalQuantity(T x) : _x(x) {}
+  T _x;
 };
 
-template <typename T = double>
-class Mass : public PhysicalQuantity<Mass<T>, T> {
-  DECLARE_PHYSQUANT_CONSTRUCTORS(Mass, kilograms)
- public:
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(skeppund, 170.0);
-  MAKE_PHYSQUANT_UNIT_CONVERTERS(lispund, 170.0/20.0);
 
-};
+template <typename T=double>
+using Duration = PhysicalQuantity<T, 1, 0, 0, 0>;
 
-template <typename Q, typename T>
-PhysicalQuantity<Q, T> operator* (T s, PhysicalQuantity<Q, T> x) {
+template <typename T=double>
+using Length = PhysicalQuantity<T, 0, 1, 0, 0>;
+
+template <typename T=double>
+using Velocity = PhysicalQuantity<T, -1, 1, 0, 0>;
+
+template <typename T=double>
+using Angle = PhysicalQuantity<T, 0, 0, 1, 0>;
+
+template <typename T=double>
+using Mass = PhysicalQuantity<T, 0, 0, 0, 1>;
+
+template <typename T, int t, int l, int a, int m>
+PhysicalQuantity<T, t, l, a, m> operator*(T s, const PhysicalQuantity<T, t, l, a, m> &x) {
   return x*s;
+}
+
+template <typename T, int TimeDim, int LengthDim, int AngleDim, int MassDim>
+std::string toString(const PhysicalQuantity<T, TimeDim, LengthDim, AngleDim, MassDim> &x) {
+  static_assert(TimeDim == 1, "Only for time");
+   static_assert(LengthDim == 0, "Only for time");
+   static_assert(AngleDim == 0, "Only for time");
+   static_assert(MassDim == 0, "Only for time");
+   std::stringstream ss;
+   Duration<T> remaining(*this);
+#define FORMAT_DURATION_UNIT(unit) \
+   if (remaining.unit() >= 1) { \
+     if (ss.str().size() > 0) ss << ", "; \
+     ss << floor(remaining.unit()) << " " #unit ; \
+     remaining -= unit(floor(remaining.unit())); \
+   }
+   FORMAT_DURATION_UNIT(weeks)
+   FORMAT_DURATION_UNIT(days)
+   FORMAT_DURATION_UNIT(hours)
+   FORMAT_DURATION_UNIT(minutes)
+   FORMAT_DURATION_UNIT(seconds)
+#undef FORMAT_DURATION_UNIT
+   return ss.str();
 }
 
 
 
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////// Extra stuff
 template<typename T, int N>
 class FixedArray {
  public:
@@ -424,12 +518,12 @@ class HorizontalMotion : public Vectorize<Velocity<T>, 2> {
     };
 };
 
-template <typename Q, typename V>
-bool isFinite(const PhysicalQuantity<Q, V> &x) {
+template <typename T, int t, int l, int a, int m>
+bool isFinite(const PhysicalQuantity<T, t, l, a, m> &x) {
   return x.isFiniteQuantity();
 }
-template <typename Q, typename V>
-bool isNaN(const PhysicalQuantity<Q, V> &x) {
+template <typename T, int t, int l, int a, int m>
+bool isNaN(const PhysicalQuantity<T, t, l, a, m> &x) {
   return x.isNaNQuantity();
 }
 
@@ -440,8 +534,8 @@ bool isNaN(const PhysicalQuantity<Q, V> &x) {
 #undef MAKE_PHYSQUANT_UNIT_CONVERTERS
 #undef DECLARE_PHYSQUANT_CONSTRUCTORS
 
-template <typename Quantity, typename Value>
-Quantity fabs(sail::PhysicalQuantity<Quantity, Value> x) {
+template <typename T, int t, int l, int a, int m>
+sail::PhysicalQuantity<T, t, l, a, m> fabs(sail::PhysicalQuantity<T, t, l, a, m> x) {
   return x.fabs();
 }
 
