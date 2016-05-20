7f9c98d51b136b9d2a3cef2e0678dfbea2162fe5
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-05-20 10:16:17 +0200

    Hard code it for TimeStamp and Duration

--- src/server/math/Resampler.h
+++ src/server/math/Resampler.h
@@ -8,70 +8,27 @@
 #ifndef SERVER_MATH_RESAMPLER_H_
 #define SERVER_MATH_RESAMPLER_H_
 
-#include <algorithm>
-#include <server/common/ArrayBuilder.h>
+#include <server/common/TimeStamp.h>
 
 namespace sail {
 namespace Resampler {
 
-template <typename T>
 struct Endpoint {
-  typedef Endpoint<T> ThisType;
-
-  T pos;
+  sail::TimeStamp pos;
   bool rising;
 
-  bool operator<(const ThisType &other) {
+  bool operator<(const Endpoint &other) {
     return pos < other.pos;
   }
 };
 
-template <typename T>
-Array<Endpoint<T> > listEndpoints(const Array<T> &samples, T period) {
-  int n = samples.size();
-  Array<Endpoint<T> > eps(2*n);
-  for (int i = 0; i < n; i++) {
-    auto x = samples[i];
-    auto at = 2*i;
-    // Here we are essentially dilating every sample, by replacing it
-    // by an interval of width 2*period
-    eps[at + 0] = Endpoint<T>{x - period, true};
-    eps[at + 1] = Endpoint<T>{x + period, false};
-  }
-  std::sort(eps.begin(), eps.end());
-  return eps;
-}
+Array<Endpoint> listEndpoints(const Array<TimeStamp> &samples,
+    Duration<double> period);
+Array<TimeStamp> makeNewSamplesFromEndpoints(const Array<Endpoint> &eps,
+    Duration<double> period);
+Array<TimeStamp> resample(const Array<TimeStamp> &irregularSamples,
+    Duration<double> newSamplingPeriod);
 
-template <typename T>
-void addNewSamples(ArrayBuilder<T> *dst, T from, T to, T period) {
-  for (auto i = from; i <= to; i += period) {
-    dst->add(i);
-  }
-}
-
-template <typename T>
-Array<T> makeNewSamplesFromEndpoints(const Array<Endpoint<T> > & eps, T period) {
-  int sum = 0;
-  T start = T();
-  ArrayBuilder<T> newSamples;
-  for (auto ep: eps) {
-    sum += (ep.rising? 1 : -1);
-    if (sum == 1 && ep.rising) {
-      start = ep.pos;
-    } else if (sum == 0 && !ep.rising) {
-      // Here we are eroding it, by cutting 'period' from each side
-      addNewSamples(&newSamples, start+period, ep.pos-period, period);
-    }
-  }
-  return newSamples.get();
-}
-
-template <typename T>
-Array<T> resample(const Array<T> &irregularSamples, T newSamplingPeriod) {
-  return makeNewSamplesFromEndpoints(
-      listEndpoints(irregularSamples, newSamplingPeriod),
-      newSamplingPeriod);
-}
 
 }
 }
--- src/server/math/ResamplerTest.cpp
+++ src/server/math/ResamplerTest.cpp
@@ -9,38 +9,65 @@
 #include <server/common/ArrayIO.h>
 #include <server/math/Resampler.h>
 #include <gtest/gtest.h>
+#include <server/common/Functional.h>
 
 using namespace sail;
 using namespace sail::Resampler;
 
+namespace {
+  auto offset = TimeStamp::UTC(2016, 5, 20, 9, 59, 0);
+
+  Duration<double> unit = Duration<double>::days(1.0);
+
+  TimeStamp toTime(int i) {
+    return offset + double(i)*unit;
+  }
+
+  int fromTime(TimeStamp t) {
+    return int(round((t - offset).days()));
+  }
+
+  Array<TimeStamp> toTimeArray(const Arrayi &x) {
+    return sail::map(x, &toTime);
+  }
+
+  Arrayi fromTimeArray(const Array<TimeStamp> &x) {
+    return sail::map(x, &fromTime);
+  }
+}
+
 TEST(ResamplerTest, TestEps) {
   Arrayi samples{30, 40};
-  int period = 1;
-  auto eps = listEndpoints(samples, period);
+  auto period = 1.0*unit;
+  auto eps = listEndpoints(toTimeArray(samples), period);
   EXPECT_EQ(eps.size(), 4);
   int times[4] = {29, 31, 39, 41};
   for (int i = 0; i < 4; i++) {
-    EXPECT_EQ(times[i], eps[i].pos);
+    EXPECT_EQ(toTime(times[i]), eps[i].pos);
     EXPECT_EQ(eps[i].rising, i % 2 == 0);
   }
-  auto newSamples = makeNewSamplesFromEndpoints(eps, period);
+  auto newSamples = fromTimeArray(
+      makeNewSamplesFromEndpoints(eps, period));
   EXPECT_EQ(newSamples, (Arrayi{30, 40}));
 }
 
 
 TEST(ResamplerTest, TestEpsDense) {
   Arrayi samples{30, 31, 34, 35, 37, 40};
-  int period = 2;
-  auto eps = listEndpoints(samples, period);
+  auto period = 2.0*unit;
+  auto eps = listEndpoints(toTimeArray(samples), period);
   EXPECT_EQ(eps.size(), 12);
   int times[12] = {28, 29, 32, 32, 33, 33, 35, 36, 37, 38, 39, 42};
   for (int i = 0; i < 12; i++) {
-    EXPECT_EQ(times[i], eps[i].pos);
+    EXPECT_EQ(toTime(times[i]), eps[i].pos);
   }
-  auto newSamples = makeNewSamplesFromEndpoints(eps, period);
+  auto newSamples = fromTimeArray(makeNewSamplesFromEndpoints(
+      eps, period));
 
 
   auto expected = Arrayi{30, 32, 34, 36, 38, 40};
   EXPECT_EQ(newSamples, expected);
-  EXPECT_EQ(resample(samples, period), expected);
+  EXPECT_EQ(
+      fromTimeArray(resample(toTimeArray(samples), period)),
+      expected);
 }
