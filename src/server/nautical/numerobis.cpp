4431a472241541c51e701d87845278d4bc228737
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-05-26 11:09:31 +0200

    add numerobis

---
+++ src/server/nautical/numerobis.cpp
@@ -0,0 +1,21 @@
+/*
+ * numerobis.cpp
+ *
+ *  Created on: May 26, 2016
+ *      Author: jonas
+ */
+
+#include <server/nautical/logimport/LogLoader.h>
+
+using namespace sail;
+
+int main() {
+  std::string dir = "/home/jonas/prog/anemomind/datasets/boat573b14bbf437a48c0ce3ad54";
+
+  LogLoader loader;
+  loader
+
+  return 0;
+}
+
+
