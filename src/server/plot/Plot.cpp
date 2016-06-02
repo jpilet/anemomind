f8d37d0d67a888b50673fd80758154edffde9436
jonasseglare <uppfinnarjonas@gmail.com>
2016-06-02 15:08:32 +0200

    Add visualization tools and various fixes to the smooth GPS filter (#698)
    * Add visualization tools and various fixes to the smooth GPS filter
    
    * Remove redundant trait

---
+++ src/server/plot/Plot.cpp
@@ -0,0 +1,45 @@
+/*
+ * Plot.cpp
+ *
+ *  Created on: May 19, 2016
+ *      Author: jonas
+ */
+
+#include <server/plot/Plot.h>
+
+namespace sail {
+
+
+RenderSettings::RenderSettings() : rgbColor(0, 0, 0),
+    lineWidth(1.0), markerSize(3.0), type(SolidLine) {}
+
+PlottablePoints::PlottablePoints(const Array<Eigen::Vector3d> &pts,
+    const RenderSettings &rs) : _pts(pts), _rs(rs) {
+  for (auto x: pts) {
+    _box.extend(x.data());
+  }
+}
+
+void PlottablePoints::render(PlotModel *dst) {
+
+  // TODO: replace this by IndexableWrap class of jo-array-wrappers2
+  class AA : public AbstractArray<Eigen::Vector3d> {
+  public:
+    AA(const Array<Eigen::Vector3d> &src) : _src(src) {}
+
+    Eigen::Vector3d operator[](int i) const override {
+      return _src[i];
+    }
+
+    int size() const override {
+      return _src.size();
+    }
+  private:
+    const Array<Eigen::Vector3d> &_src;
+  };
+
+  dst->render(AA(_pts), _rs);
+}
+
+
+}
