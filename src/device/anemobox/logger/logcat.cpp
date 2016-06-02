08210bc2263c9cc007cd38862325266b9a60b362
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-06-02 14:18:01 +0200

    Add code to display times in logcat

--- src/device/anemobox/logger/logcat.cpp
+++ src/device/anemobox/logger/logcat.cpp
@@ -99,11 +99,13 @@ void streamCat(const ValueSet& valueSet, vector<TimedString>* entries) {
     vector<AbsoluteOrientation> values;
     Logger::unpack(valueSet.orient(), &values);
     formatValues(times, values, prefix, entries);
-  }
-  /*if (valueSet.has_exttimes()) {
+  }{
     vector<TimeStamp> extTimes;
-    Logger::unpackTime(extTimes, &times);
-  }*/
+    if (!extTimes.empty()) {
+      Logger::unpack(valueSet.exttimes(), &extTimes);
+      formatValues(times, extTimes, prefix, entries);
+    }
+  }
 
   for (int i = 0; i < valueSet.text_size(); ++i) {
     entries->push_back(TimedString(times[i], prefix + ": " + valueSet.text(i)));
