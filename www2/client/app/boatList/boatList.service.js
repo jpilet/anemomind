fefd982c5d6ef8b286bebf1f4bd286f391af1d25
evaletolab <evaleto@gmail.com>
2016-06-02 15:39:15 +0200

    add promise, may be temporary

--- www2/client/app/boatList/boatList.service.js
+++ www2/client/app/boatList/boatList.service.js
@@ -1,13 +1,31 @@
 'use strict';
 
 angular.module('www2App')
-  .service('boatList', function (Auth, $http, socket, $rootScope) {
+  .service('boatList', function (Auth, $http, $q,socket, $rootScope) {
     var boats = [ ];
     var boatDict = { };
     var curves = { };
     var sessionsForBoats = {};
 
+    var deferred = $q.defer();
+    //
+    // return promise on this.boats();
+    var promise;
+
+
+    //
+    // return the default boat to display at home
+    function getDefaultBoat() {
+      if(!boats.length){
+        return {};
+      }
+
+      // default super implementation
+      return boats[boats.length-1];
+    }
+
     function update() {
+        promise = deferred.promise;
         // Specifically ask for public boat, too.
         // We could have a user option to hide public boats.
         $http.get('/api/boats?public=1')
@@ -19,7 +37,9 @@ angular.module('www2App')
                var boat = data[i];
                boatDict[boat._id] = boat;
              }
-
+             //
+             // resolve
+             deferred.resolve(boats);
              $rootScope.$broadcast('boatList:updated', boats);
           });
         $http.get('/api/session')
@@ -38,6 +58,8 @@ angular.module('www2App')
     }
 
     update();
+    //
+    // TODO you can replace this watch by a broadcast/listen event 
     $rootScope.$watch(Auth.isLoggedIn, function(newVal, oldVal) {
       if (newVal && newVal != oldVal) {
         update();
@@ -54,10 +76,11 @@ angular.module('www2App')
 
     return {
       boat: function(id) { return boatDict[id]; },
-      boats: function() { return boats.slice(0); },
+      boats: function() { return promise; },
       sessions: function() { return $.extend({}, sessionsForBoats); },
       sessionsForBoat: function(boatId) { return sessionsForBoats[boatId]; },
       getCurveData: function(curveId) { return curves[curveId]; },
+      getDefaultBoat: getDefaultBoat,
       locationForCurve: locationForCurve,
       update: update,
     };
