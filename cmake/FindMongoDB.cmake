b3d2d6ac4fd1c97e7df1aad7b99badefc4b65e92
Julien Pilet <julien.pilet@gmail.com>
2016-07-01 14:30:15 +0200

    Installs and compile mongodb client library from cmake

--- cmake/FindMongoDB.cmake
+++ cmake/FindMongoDB.cmake
@@ -8,49 +8,94 @@
 #  MongoDB_FOUND, If false, do not try to use MongoDB.
 #  MongoDB_EXPOSE_MACROS, If true, mongo_ros should use '#define MONGO_EXPOSE_MACROS'
 
-set(MongoDB_EXPOSE_MACROS "NO")
+set(MongoDB_BUILD_FROM_SOURCES "YES")
 
-set(MongoDB_PossibleIncludePaths
-  /usr/include/
-  /usr/local/include/
-  /usr/include/mongo/
-  /usr/local/include/mongo/
-  /opt/mongo/include/
-  $ENV{ProgramFiles}/Mongo/*/include
-  $ENV{SystemDrive}/Mongo/*/include
-  )
-find_path(MongoDB_INCLUDE_DIR mongo/client/dbclient.h
-  ${MongoDB_PossibleIncludePaths})
+find_package(Boost COMPONENTS filesystem regex thread system REQUIRED)
 
-if(MongoDB_INCLUDE_DIR)
-  find_path(MongoDB_dbclientinterface_Path mongo/client/dbclientinterface.h
+if (MongoDB_BUILD_FROM_SOURCES)
+
+  message(STATUS "Building mongodb in ${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-src")
+  message(STATUS "MongoDB config:"
+        "scons --prefix=${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-install"
+        " --c++11=on"
+        " --cpppath=${Boost_INCLUDE_DIR}"
+        " --libpath=${Boost_LIBRARY_DIRS}"
+        " install")
+
+  ExternalProject_Add(mongodb_ext
+        GIT_REPOSITORY "https://github.com/mongodb/mongo-cxx-driver.git"
+        GIT_TAG legacy
+        BINARY_DIR "${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-src"
+        SOURCE_DIR "${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-src"
+        INSTALL_DIR "${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-install"
+
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND cd "${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-src"
+        && test "-f" "${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-install/lib/libmongoclient.a"
+        || scons "--prefix=${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-install"
+        --c++11=on
+        "--cpppath=${Boost_INCLUDE_DIR}"
+        "--libpath=${Boost_LIBRARY_DIRS}"
+        install
+        INSTALL_COMMAND ""
+        )
+
+  set(MongoDB_INCLUDE_DIR  "${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-install/include")
+
+  set(MongDB_FOUND "YES")
+
+  add_library(mongoclient STATIC IMPORTED)
+  set_property(TARGET mongoclient PROPERTY IMPORTED_LOCATION  "${CMAKE_BINARY_DIR}/third-party/mongocxxdriver-install/lib/libmongoclient.a")
+  add_dependencies(mongoclient mongodb_ext)
+
+  set (MongoDB_LIBRARIES mongoclient)
+
+else (MongoDB_BUILD_FROM_SOURCES)
+
+  set(MongoDB_EXPOSE_MACROS "NO")
+
+  set(MongoDB_PossibleIncludePaths
+    /usr/include/
+    /usr/local/include/
+    /usr/include/mongo/
+    /usr/local/include/mongo/
+    /opt/mongo/include/
+    $ENV{ProgramFiles}/Mongo/*/include
+    $ENV{SystemDrive}/Mongo/*/include
+    )
+  find_path(MongoDB_INCLUDE_DIR mongo/client/dbclient.h
     ${MongoDB_PossibleIncludePaths})
-  if (MongoDB_dbclientinterface_Path)
-    set(MongoDB_EXPOSE_MACROS "YES")
-  endif()
-endif()
 
-if(WIN32)
-  find_library(MongoDB_LIBRARIES NAMES mongoclient
-    PATHS
-    $ENV{ProgramFiles}/Mongo/*/lib
-    $ENV{SystemDrive}/Mongo/*/lib
-    )
-else(WIN32)
-  find_library(MongoDB_LIBRARIES NAMES libmongoclient.a mongoclient
-    PATHS
-    /usr/lib
-    /usr/lib64
-    /usr/lib/mongo
-    /usr/lib64/mongo
-    /usr/local/lib
-    /usr/local/lib64
-    /usr/local/lib/mongo
-    /usr/local/lib64/mongo
-    /opt/mongo/lib
-    /opt/mongo/lib64
-    )
-endif(WIN32)
+  if(MongoDB_INCLUDE_DIR)
+    find_path(MongoDB_dbclientinterface_Path mongo/client/dbclientinterface.h
+      ${MongoDB_PossibleIncludePaths})
+    if (MongoDB_dbclientinterface_Path)
+      set(MongoDB_EXPOSE_MACROS "YES")
+    endif()
+  endif()
+
+  if(WIN32)
+    find_library(MongoDB_LIBRARIES NAMES mongoclient
+      PATHS
+      $ENV{ProgramFiles}/Mongo/*/lib
+      $ENV{SystemDrive}/Mongo/*/lib
+      )
+  else(WIN32)
+    find_library(MongoDB_LIBRARIES NAMES libmongoclient.a mongoclient
+      PATHS
+      /usr/lib
+      /usr/lib64
+      /usr/lib/mongo
+      /usr/lib64/mongo
+      /usr/local/lib
+      /usr/local/lib64
+      /usr/local/lib/mongo
+      /usr/local/lib64/mongo
+      /opt/mongo/lib
+      /opt/mongo/lib64
+      )
+  endif(WIN32)
+endif (MongoDB_BUILD_FROM_SOURCES)
 
 if(MongoDB_INCLUDE_DIR AND MongoDB_LIBRARIES)
   set(MongoDB_FOUND TRUE)
@@ -72,9 +117,11 @@ if (UNIX)
                         ${Boost_REGEX_LIBRARY}
                         ${Boost_SYSTEM_LIBRARY}
                         ${Boost_THREAD_LIBRARY}
+                        ${Boost_REGEX_LIBRARY}
                         ${Boost_FILESYSTEM_LIBRARY}
 			${CMAKE_THREAD_LIBS_INIT}
 			ssl crypto)
 endif (UNIX)
 
 mark_as_advanced(MongoDB_INCLUDE_DIR MongoDB_LIBRARIES MongoDB_EXPOSE_MACROS)
+
