e3d6468c86dfed3f74c9a6b1f0c157345fd311e8
Julien Pilet <julien.pilet@gmail.com>
2016-07-05 15:56:26 +0200

    Fix and enhance BoatLogProcessorTest

--- src/server/nautical/BoatLogProcessorTest.cpp
+++ src/server/nautical/BoatLogProcessorTest.cpp
@@ -3,6 +3,7 @@
  *      Author: Jonas Östlund <uppfinnarjonas@gmail.com>
  */
 
+#include <device/Arduino/libraries/TargetSpeed/TargetSpeed.h>
 #include <gtest/gtest.h>
 #include <server/nautical/BoatLogProcessor.h>
 #include <server/common/PathBuilder.h>
@@ -33,13 +34,12 @@ TEST(BoatLogProcessor, ProcessingTest) {
   processBoatDataFullFolder(false, srcpath);
   PathBuilder output = PathBuilder::makeDirectory(srcpath).pushDirectory("processed");
 
-  const int fileCount = 5;
-  std::string files[fileCount] = {"all_navs.js", "all_tree_node_info.js", "all_tree.js", "some_other_file.js", "boat.dat"};
-  bool shouldExist[fileCount] = {true, true, true, false, true};
+  Poco::Path boatDatPath = output.makeFile("boat.dat").get();
+  EXPECT_TRUE(Poco::File(boatDatPath).exists());
 
-  for (int i = 0; i < fileCount; i++) {
-    Poco::Path filename = output.makeFile(files[i]).get();
-    EXPECT_TRUE(shouldExist[i] == Poco::File(filename).exists());
-  }
-  Poco::File(srcpath).remove(true);
+  TargetSpeedTable table;
+  EXPECT_TRUE(loadTargetSpeedTable(boatDatPath.toString().c_str(), &table));
+
+  EXPECT_NEAR(2, (double)table._upwind[4], 0.5);
+  EXPECT_NEAR(-1, (double)table._downwind[4], 0.5);
 }
