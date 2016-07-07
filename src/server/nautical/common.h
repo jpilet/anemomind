469999b11ceb2574db446df0b9bd1bcd6688d663
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-07-07 09:52:49 +0200

    Make a function computeTwdirFromTrueWind

--- src/server/nautical/common.h
+++ src/server/nautical/common.h
@@ -8,11 +8,23 @@
 #ifndef SERVER_NAUTICAL_COMMON_H_
 #define SERVER_NAUTICAL_COMMON_H_
 
+#include <device/Arduino/libraries/PhysicalQuantity/PhysicalQuantity.h>
+
+namespace sail {
+
 template <typename T>
 T computeCurrentFromBoatMotion(
     const T &boatMotionOverWater,
     const T &boatMotionOverGround) {
   return boatMotionOverGround - boatMotionOverWater;
+}
+
+template <typename T>
+Angle<T> computeTwdirFromTrueWind(const HorizontalMotion<T> &tw) {
+  return tw.angle() + Angle<double>::degrees(180.0);
+}
+
+
 }
 
 
