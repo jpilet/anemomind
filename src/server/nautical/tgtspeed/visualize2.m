0c66270ba98cc5b0f95b32c4f50db4ae1e7c0e12
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-07-08 14:00:04 +0200

    bra

--- src/server/nautical/tgtspeed/visualize2.m
+++ src/server/nautical/tgtspeed/visualize2.m
@@ -1,4 +1,4 @@
-prefix = '/tmp/tilestep1_dispatcher_exported';
+prefix = '/tmp/targetspeeddata.txt';
 data = load(strcat(prefix, '_data.txt'));
 data(1, :) = data(1, :) - data(1, 1);
 
@@ -9,5 +9,5 @@ unit_names = read_lines(strcat(prefix, '_units.txt'));
 validate_time(data(1, :));
 t = data(1, :);
 
-[data_subset, label_subset] = plot_for_shortname('gpsBearing', 
+[data_subset, label_subset] = plot_for_shortname('vmg', 
    short_names, source_names, unit_names, data);
