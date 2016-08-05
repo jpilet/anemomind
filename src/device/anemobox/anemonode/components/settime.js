eee4126caf10fc740de7f7090581526027301d12
Julien Pilet <julien.pilet@gmail.com>
2016-08-05 15:18:10 +0200

    Set system time only once on the box

--- src/device/anemobox/anemonode/components/settime.js
+++ src/device/anemobox/anemonode/components/settime.js
@@ -1,6 +1,12 @@
 var anemonode = require('../build/Release/anemonode');
 
+var maxSetTime = 1;
+var numSetTime = 0;
+
 function adjustTimeFromDispatcher() {
+  if (numSetTime >= maxSetTime) {
+    return;
+  }
   var sysTime = anemonode.dispatcher.values.dateTime.time(0);
   var gpsTime = anemonode.dispatcher.values.dateTime.value(0);
   if (!sysTime || !gpsTime) {
@@ -12,7 +18,12 @@ function adjustTimeFromDispatcher() {
   if (Math.abs(delta) > .2) {
     console.log('Calling adjtime with delta: ' + delta);
     var r= anemonode.adjTime(delta);
+    ++numSetTime;
+    if (numSetTime >= maxSetTime) {
+      anemonode.dispatcher.values.dateTime.unsubscribe(subscriptionIndex);
+    }
   }
 }
-anemonode.dispatcher.values.dateTime.subscribe(adjustTimeFromDispatcher);
+var subscriptionIndex =
+  anemonode.dispatcher.values.dateTime.subscribe(adjustTimeFromDispatcher);
 
