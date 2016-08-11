8b50a719266c2a3793c64b4d7a6f9273ca694c65
Julien Pilet <julien@anemomind.com>
2016-08-11 17:21:30 +0200

    add forgotten anemonode.h

---
+++ src/device/anemobox/anemonode/src/anemonode.h
@@ -0,0 +1,12 @@
+#ifndef ANEMONODE_ANEMONODE_H
+#define ANEMONODE_ANEMONODE_H
+
+namespace sail {
+
+class Dispatcher;
+
+extern Dispatcher *globalAnemonodeDispatcher;
+
+}  // namespace sail
+
+#endif  // ANEMONODE_ANEMONODE_H
