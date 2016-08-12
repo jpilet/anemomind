f7e47bc4cb35c5eca796a5b311a8d63d5e65e162
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-08-12 15:20:21 +0200

    bra

--- src/server/nautical/investigateMemoryIssue.cpp
+++ src/server/nautical/investigateMemoryIssue.cpp
@@ -5,6 +5,38 @@
  *      Author: jonas
  */
 
-#include <server>
+#include <server/nautical/logimport/LogLoader.h>
+#include <iostream>
+
+using namespace sail;
+
+int main() {
+  LogLoader loader;
+  loader.load("/Users/jonas/data/boat573b14bbf437a48c0ce3ad54");
+  auto ds = loader.makeNavDataset();
+  int counter = 0;
+
+  std::cout << "Please note the current memory consumption." << std::endl;
+  std::cout << "Do you want to access a GPS sample? 1 or 0" << std::endl;
+
+  int answer;
+  std::cin >> answer;
+
+  std::vector<NavDataset> shallowCopies;
+  while (true) {
+    auto copy = ds.dup();
+
+    if (answer != 0) {
+      std::cout << "Access the first sample in the copy: " <<
+          copy.samples<GPS_POS>().first().time.toString();
+    }
+
+    shallowCopies.push_back(copy);
+    std::cout << " Number of shallow copies so far: "
+        << shallowCopies.size() << std::endl;
+  }
+
+  return 0;
+}
 
 
