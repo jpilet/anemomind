5edd3d84a0ea2454cc8a2641f09cf30f84056a85
Julien Pilet <julien.pilet@gmail.com>
2016-08-17 16:36:46 +0200

    Fix wrong merge resolution

--- www2/client/app/boatList/boatList.service.js
+++ www2/client/app/boatList/boatList.service.js
@@ -45,9 +45,10 @@ angular.module('www2App')
         promise=deferred.promise;      
       }
 
-      // Specifically ask for public boat, too.
-      // We could have a user option to hide public boats.
-      $http.get('/api/boats?public=1')
+      // Specifically do not ask for public boats.
+      // We could have a user option to show public boats with:
+      // $http.get('/api/boats?public=1')
+      $http.get('/api/boats')
         .then(function(payload) {
           boats = payload.data;
           socket.syncUpdates('boat', boats);
