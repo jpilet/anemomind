65f3e7e5e01fc7e1a8b5c2b4d02546b75bc05764
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-08-18 18:43:58 +0200

    brae

--- src/server/common/ReduceTreeTest.cpp
+++ src/server/common/ReduceTreeTest.cpp
@@ -7,6 +7,7 @@
 
 #include <gtest/gtest.h>
 #include <server/common/ReduceTree.h>
+#include <server/common/ArrayIO.h>
 
 using namespace sail;
 
@@ -17,11 +18,14 @@ TEST(ReduceTreeTest, SumTree) {
       [](int a, int b) {return a + b;}, numbers);
 
   EXPECT_EQ(tree.top(), 1 + 2 + 3 + 4 + 5);
+  EXPECT_EQ(tree.allData().last(), 5);
   tree.setLeafValue(4, 0);
   EXPECT_EQ(tree.top(), 10);
   tree.setLeafValue(1, 0);
   EXPECT_EQ(tree.top(), 8);
   EXPECT_EQ(1, tree.getLeafValue(0));
   EXPECT_EQ(8, tree.getNodeValue(0));
+
+  std::cout << "All data: " << tree.allData() << std::endl;
 }
 
