4b8269042058508ce37cb56a462a129c4d09ddf2
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-08-19 17:53:11 +0200

    Fix indentation

--- www2/client/app/boatSummary/boatSummary.directive.js
+++ www2/client/app/boatSummary/boatSummary.directive.js
@@ -37,52 +37,49 @@ var app = angular.module('www2App')
             "N", "NE", "E", "SE", "S", "SW", "W", "NW" ];
           return windrose[index];
         };
+        scope.formatSpan = function(fromDateS, toDateS) {
+          var fromDate = new Date(fromDateS);
+          var toDate = new Date(toDateS);
+          function makeDurString(durSeconds) {
+            var dur = durSeconds;
+            var multipliers = [60, 60, 24, 7, 1000000000];
+            var labels = ['second', 'minute', 'hour', 'day', 'week'];
+            var formatDur = function(x, s) {
+              return (x == 0? null : x + ' ' + s + (x == 1? "" : "s"));
+            }
+            var dst = [];
+            for (var i = 0; i < multipliers.length; i++) {
+              var f = multipliers[i];
+              var a = Math.floor(dur/f);
+              var b = Math.floor(dur - f*a);
+              var s = formatDur(b, labels[i]);
+              if (s) {
+	              dst.push(s);
+              }
+              dur = a;
+            }
+            var finalParts = dst.reverse();
+            if (finalParts.length == 0) {
+              return "0 seconds";
+            } else if (finalParts.length == 1) {
+	            return finalParts[0];
+            } else {
+              return finalParts[0] + ' and ' + finalParts[1];
+            }
+          }
 
-	  scope.formatSpan = function(fromDateS, toDateS) {
-	      var fromDate = new Date(fromDateS);
-	      var toDate = new Date(toDateS);
-	      function makeDurString(durSeconds) {
-		  var dur = durSeconds;
-		  var multipliers = [60, 60, 24, 7, 1000000000];
-		  var labels = ['second', 'minute', 'hour', 'day', 'week'];
-		  var formatDur = function(x, s) {
-		      return (x == 0? null : x + ' ' + s + (x == 1? "" : "s"));
-		  }
-		  var dst = [];
-		  for (var i = 0; i < multipliers.length; i++) {
-		      var f = multipliers[i];
-		      var a = Math.floor(dur/f);
-		      var b = Math.floor(dur - f*a);
-		      var s = formatDur(b, labels[i]);
-		      if (s) {
-			  dst.push(s);
-		      }
-		      dur = a;
-		  }
-		  var finalParts = dst.reverse();
-		  if (finalParts.length == 0) {
-		      return "0 seconds";
-		  } else if (finalParts.length == 1) {
-		      return finalParts[0];
-		  } else {
-		      return finalParts[0] + ' and ' + finalParts[1];
-		  }
-	      }
+          var offsetIso = fromDate.toISOString();
 
-	      var offsetIso = fromDate.toISOString();
-
-	      // What is both an easily readable and 
-	      // somewhat standard format for dates? 
-	      // Raw ISO dates are standard,
-	      // but look a bit technical in this context, 
-	      // so let's declutter it a bit...
-	      var offsetString = offsetIso.substring(0, 10) 
-		  + " " + offsetIso.substring(11, 16);
-
-	      var durSeconds = 0.001*(toDate.getTime() - fromDate.getTime());
-
-	      return offsetString + " (" + makeDurString(durSeconds) + ")";
-	  }
+          // What is both an easily readable and 
+          // somewhat standard format for dates? 
+          // Raw ISO dates are standard,
+          // but look a bit technical in this context, 
+          // so let's declutter it a bit...
+          var offsetString = offsetIso.substring(0, 10) 
+              + " " + offsetIso.substring(11, 16);
+          var durSeconds = 0.001*(toDate.getTime() - fromDate.getTime());
+          return offsetString + " (" + makeDurString(durSeconds) + ")";
+        }
 
         scope.knotsToBeaufort = function(knots) {
           if (knots < 1) { return 0; }
