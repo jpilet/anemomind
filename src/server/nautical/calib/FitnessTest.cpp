f95fa62077e8140f2c2fb8d5f0857d32c7d8b38e
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-08-26 17:17:47 +0200

    good

--- src/server/nautical/calib/FitnessTest.cpp
+++ src/server/nautical/calib/FitnessTest.cpp
@@ -15,18 +15,30 @@ TEST(SensorTest, Instantiate) {
   SensorSet<double> sensorSet;
   EXPECT_EQ(0, sensorSet.paramCount());
   sensorSet.AWA["NMEA2000asdfasdfas"] = SensorModel<double, AWA>();
-  EXPECT_EQ(1, sensorSet.paramCount());
 
-  double params[1] = {324.43};
-  sensorSet.writeTo(params);
-  EXPECT_EQ(params[0], 0.0);
+  {
+    EXPECT_EQ(1, sensorSet.paramCount());
 
-  params[0] = 0.25;
-  sensorSet.readFrom(params);
-  params[0] = 234324.324;
-  sensorSet.writeTo(params);
-  EXPECT_EQ(0.25, params[0]);
+    double params[1] = {324.43};
+    sensorSet.writeTo(params);
+    EXPECT_NEAR(params[0], 0.0, 1.0e-6);
 
+    params[0] = 0.25;
+    sensorSet.readFrom(params);
+    params[0] = 234324.324;
+    sensorSet.writeTo(params);
+    EXPECT_NEAR(params[0], 0.25, 1.0e-6);
+  }
+
+  sensorSet.WAT_SPEED["NMEA0183Speedo"]
+      = SensorModel<double, WAT_SPEED>();
+  {
+    EXPECT_EQ(2, sensorSet.paramCount());
+    double params[2] = {0.0, 0.0};
+    sensorSet.writeTo(params);
+    EXPECT_NEAR(params[0], 0.25, 1.0e-6);
+    EXPECT_NEAR(params[1], 1.0, 1.0e-6);
+  }
 }
 
 
