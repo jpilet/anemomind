d10e61fe88a7430930db5f0cc53aaee2d8f7e348
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-08-26 09:29:08 +0200

    good

--- src/server/nautical/BoatStateTest.cpp
+++ src/server/nautical/BoatStateTest.cpp
@@ -7,8 +7,23 @@
 
 #include <server/nautical/BoatState.h>
 #include <gtest/gtest.h>
+#include <Eigen/Dense>
 
+using namespace sail;
 
+TEST(BoatStateTest, Orthonormality) {
+  typedef BoatState<double> BS;
+
+  auto expectedMast = BS::starboardVector().cross(BS::headingVector());
+  EXPECT_NEAR((expectedMast - BS::mastVector()).norm(), 0.0, 1.0e-6);
+
+  Eigen::Vector3d v[3] = {BS::starboardVector(), BS::headingVector(), BS::mastVector()};
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      EXPECT_NEAR(v[i].dot(v[j]), i == j? 1.0 : 0.0, 1.0e-6);
+    }
+  }
+}
 
 
 
