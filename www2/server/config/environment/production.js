06b017a9024aa0cbb7bdac1e24c6b601c283338d
Julien Pilet <julien.pilet@gmail.com>
2016-09-13 08:12:40 +0200

    Tuning for new production server (#841)

--- www2/server/config/environment/production.js
+++ www2/server/config/environment/production.js
@@ -21,14 +21,8 @@ module.exports = {
             'mongodb://localhost/anemomind'
   },
 
-  uploadDir: '/var/uploads',
-  endpointDir: '/var/mail2',
+  uploadDir: '/db/uploads',
+  endpointDir: '/db/mail2',
   backupDestination: 'anemomind@vtiger.anemomind.com:userlogs',
 
-  ssl: {
-    key: '/etc/ssl/anemolab.com/anemolab.com.key',
-    cert: '/etc/ssl/anemolab.com/anemolab.com.cert',
-    ip: '0.0.0.0',
-    port: 443
-  }
 };
