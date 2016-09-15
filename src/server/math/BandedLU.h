ea378296aaafb0363f840a6fd342a570d77094fa
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-09-15 10:10:26 +0200

    bea

--- src/server/math/BandedLU.h
+++ src/server/math/BandedLU.h
@@ -8,10 +8,19 @@
 #ifndef SERVER_MATH_BANDEDLU_H_
 #define SERVER_MATH_BANDEDLU_H_
 
+#include <server/math/lapack/BandMatrix.h>
+
 namespace sail {
+namespace BandedLU {
 
+/*template <typename T>
+void solveInPlace(
+    BandMatrix<T> *A Eigen::Matrix<T, Eigen::Dynamic,
+    Eigen::Dynamic> *B) {
 
+}*/
 
+}
 }
 
 
