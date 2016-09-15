991719a3314249fc34d7444b4b1d8a34934507ca
Julien Pilet <julien.pilet@gmail.com>
2016-09-15 09:59:04 +0200

    Removed useless code, added a space.
    The space makes formatting easier since it allows the browser to break the line.

--- www2/client/app/filters/geopos/geopos.js
+++ www2/client/app/filters/geopos/geopos.js
@@ -8,12 +8,6 @@ angular.module('www2App')
       var m = Math.floor(minfloat);
       var secfloat = (minfloat-m)*60;
       var s = secfloat;
-      // After rounding, the seconds might become 60. These two
-      // if-tests are not necessary if no rounding is done.
-      if (m==60) {
-        d++;
-        m=0;
-      }
       return ("" + d + "°" + m + "′" + s.toFixed(3) + "″");
     };
 
@@ -23,6 +17,6 @@ angular.module('www2App')
         return 'n/a';
       }
       return deg_to_dms(Math.abs(latLon.lat)) + (latLon.lat >= 0 ? 'N' : 'S')
-      + ',' + deg_to_dms(Math.abs(latLon.lon)) + (latLon.lon >= 0 ? 'E' : 'W');
+      + ', ' + deg_to_dms(Math.abs(latLon.lon)) + (latLon.lon >= 0 ? 'E' : 'W');
     };
   });
