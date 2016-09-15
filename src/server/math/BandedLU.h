d81f7b8bc5c1ea593c500de7f4fdedeac30d78c2
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-09-15 10:07:06 +0200

    added the banded LU algor

---
+++ src/server/math/BandedLU.h
@@ -0,0 +1,19 @@
+/*
+ * BandedLU.h
+ *
+ *  Created on: 15 Sep 2016
+ *      Author: jonas
+ */
+
+#ifndef SERVER_MATH_BANDEDLU_H_
+#define SERVER_MATH_BANDEDLU_H_
+
+namespace sail {
+
+
+
+}
+
+
+
+#endif /* SERVER_MATH_BANDEDLU_H_ */
