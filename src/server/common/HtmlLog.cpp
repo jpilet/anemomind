68261aff2046cef11e30ad762c7f880fd9c0d784
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-09-16 16:35:28 +0200

    added html log

---
+++ src/server/common/HtmlLog.cpp
@@ -0,0 +1,21 @@
+/*
+ * HtmlLog.cpp
+ *
+ *  Created on: 16 Sep 2016
+ *      Author: jonas
+ */
+
+#include "HtmlLog.h"
+
+namespace sail {
+
+HtmlLog::HtmlLog() {
+  // TODO Auto-generated constructor stub
+
+}
+
+HtmlLog::~HtmlLog() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace sail */
---
+++ src/server/common/HtmlLog.h
@@ -0,0 +1,21 @@
+/*
+ * HtmlLog.h
+ *
+ *  Created on: 16 Sep 2016
+ *      Author: jonas
+ */
+
+#ifndef SERVER_COMMON_HTMLLOG_H_
+#define SERVER_COMMON_HTMLLOG_H_
+
+namespace sail {
+
+class HtmlLog {
+public:
+  HtmlLog();
+  virtual ~HtmlLog();
+};
+
+} /* namespace sail */
+
+#endif /* SERVER_COMMON_HTMLLOG_H_ */
