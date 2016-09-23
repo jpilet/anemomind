36b168ab72e6fb36177b6d3fc81422934650ad51
Jonas Ostlund <jonas@Jonass-MacBook-Pro.local>
2016-09-23 14:22:23 +0200

    basdf

--- src/server/nautical/calib/BoatStateReconstructorTest.cpp
+++ src/server/nautical/calib/BoatStateReconstructorTest.cpp
@@ -21,6 +21,11 @@ TEST(BoatStateReconstructor, TimeMapper) {
   EXPECT_EQ(mapper.offset, offsetTime);
   EXPECT_EQ(mapper.period, 2.0_s);
   EXPECT_EQ(mapper.sampleCount, 4);
+  EXPECT_EQ(mapper.map(offsetTime - 1.0_s), -1);
+  EXPECT_EQ(mapper.map(offsetTime - 9.0_s), -1);
+  EXPECT_EQ(mapper.map(offsetTime + 1.0_s), 0);
+  EXPECT_EQ(mapper.map(offsetTime + 3.0_s), 1);
+  EXPECT_EQ(mapper.map(offsetTime + 9.0_s), -1);
 }
 
 /*TEST(BoatStateReconstructor, BasicTest) {
