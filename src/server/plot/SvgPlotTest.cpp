b77397253decae3d5001ea6ee51d5c7ee36f6f2d
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-13 14:32:57 +0200

    bra va

--- src/server/plot/SvgPlotTest.cpp
+++ src/server/plot/SvgPlotTest.cpp
@@ -7,3 +7,36 @@
 
 #include <server/plot/SvgPlot.h>
 #include <gtest/gtest.h>
+
+using namespace sail;
+using namespace sail::SvgPlot;
+
+BBox3d makeEmptyBBox() {
+  BBox3d b;
+  double a[3] = {0, 0, 0};
+  b.extend(a);
+  return b;
+}
+
+BBox3d makeBox123() {
+  double a[3] = {0, 0, 0};
+  double b[3] = {1, 2, 3};
+  BBox3d box;
+  box.extend(a);
+  box.extend(b);
+  return box;
+}
+
+TEST(SvgPlotTest, Geometry) {
+  EXPECT_TRUE(isEmpty(makeEmptyBBox()));
+  auto box = makeBox123();
+
+
+  //Eigen::Vector4d getCorner(const BBox3d &box, int cornerIndex0);
+  auto c0 = getCorner(box, 0);
+  EXPECT_NEAR((c0 - Eigen::Vector4d(0, 0, 0, 1)).norm(), 0.0, 1.0e-6);
+
+  //BBox3d projectBBox(const Eigen::Matrix4d &pose, const BBox3d &box);
+  //Eigen::Matrix<double, 2, 4> computeTotalProjection(
+  //const BBox3d &a, const Settings2d &settings);
+}
