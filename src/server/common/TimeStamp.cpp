8acf7102cf77b1d9b6921d2d3f8292f1442d3056
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-13 10:16:41 +0200

    bra

--- src/server/common/TimeStamp.cpp
+++ src/server/common/TimeStamp.cpp
@@ -138,6 +138,7 @@ TimeStamp TimeStamp::parse(const std::string &x0) {
   // parse fractions of seconds.
   auto x = removeFractionalParts(x0);
 
+  TRY_PARSE_TIME("%D", x);
   TRY_PARSE_TIME("%D %T", x);
   TRY_PARSE_TIME("%m/%d/%Y %r", x);
   LOG(WARNING) << "Failed to parse time: " << x0;
--- src/server/nautical/illustrate.cpp
+++ src/server/nautical/illustrate.cpp
@@ -6,9 +6,38 @@
  */
 
 #include <server/common/ArgMap.h>
+#include <server/common/TimeStamp.h>
 
 using namespace sail;
 
+struct Setup {
+  std::string path;
+  std::string from, to;
+};
+
+void makeIllustrations(const Setup &setup) {
+
+}
+
 int main(int argc, const char **argv) {
+
+  Setup setup;
+  ArgMap amap;
+  amap.registerOption("--path", "Path to dataset")
+      .store(&(setup.path))
+      .required();
+  amap.registerOption("--from", "YYYY-MM-DD").store(&(setup.to));
+  amap.registerOption("--to", "YYYY-MM-DD").store(&(setup.from));
+
+  auto status = amap.parse(argc, argv);
+  switch (status) {
+  case ArgMap::Done:
+    return 0;
+  case ArgMap::Continue:
+    makeIllustrations(setup);
+    return 0;
+  case ArgMap::Error:
+    return -1;
+  };
   return 0;
 }
