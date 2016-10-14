73bd554d73980d5f328ae658621103c272e71724
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-14 13:11:33 +0200

    god

--- src/server/plot/PlotUtils.h
+++ src/server/plot/PlotUtils.h
@@ -8,9 +8,35 @@
 #ifndef SERVER_PLOT_PLOTUTILS_H_
 #define SERVER_PLOT_PLOTUTILS_H_
 
+#include <Eigen/Dense>
+#include <server/common/Optional.h>
+#include <server/common/BBox.h>
+
 namespace sail {
+namespace PlotUtils {
+
+BBox3d makeDefaultBBox();
+
+struct Settings2d {
+  static constexpr double masterMargin = 30;
+  double xMargin = masterMargin*1.61803398875; // Golden ratio :-)
+  double yMargin = masterMargin;
+  double width = 640;
+  double height = 480;
+  bool axisIJ = false;
+  bool orthogonal = true;
+  BBox3d defaultBBox = makeDefaultBBox();
+};
+
+bool isEmpty(const BBox3d &box);
+BBox3d ensureGoodBBox(const BBox3d &box);
+Eigen::Vector4d getCorner(const BBox3d &box, int cornerIndex0);
+BBox3d projectBBox(const Eigen::Matrix4d &pose, const BBox3d &box);
+Eigen::Matrix<double, 2, 4> computeTotalProjection(
+    const BBox3d &a, const Settings2d &settings);
 
 
+}
 }
 
 #endif /* SERVER_PLOT_PLOTUTILS_H_ */
--- src/server/plot/PlotUtilsTest.cpp
+++ src/server/plot/PlotUtilsTest.cpp
@@ -4,7 +4,61 @@
  *  Created on: 14 Oct 2016
  *      Author: jonas
  */
+#include <gtest/gtest.h>
+#include <server/plot/PlotUtils.h>
+#include <server/common/Env.h>
+
+using namespace sail;
+using namespace sail::PlotUtils;
+
+BBox3d makeEmptyBBox() {
+  BBox3d b;
+  double a[3] = {0, 0, 0};
+  b.extend(a);
+  return b;
+}
+
+BBox3d makeBox123() {
+  double a[3] = {0, 0, 0};
+  double b[3] = {1, 2, 3};
+  BBox3d box;
+  box.extend(a);
+  box.extend(b);
+  return box;
+}
+
+Eigen::Vector3d getLower(const BBox3d &x) {
+  return Eigen::Vector3d(
+      x.getSpan(0).minv(),
+      x.getSpan(1).minv(),
+      x.getSpan(2).minv());
+}
+
+Eigen::Vector3d getUpper(const BBox3d &x) {
+  return Eigen::Vector3d(
+      x.getSpan(0).maxv(),
+      x.getSpan(1).maxv(),
+      x.getSpan(2).maxv());
+}
+
+TEST(PlotUtilsTest, Geometry) {
+  EXPECT_TRUE(isEmpty(makeEmptyBBox()));
+  auto box = makeBox123();
 
 
+  //Eigen::Vector4d getCorner(const BBox3d &box, int cornerIndex0);
+  EXPECT_NEAR((getCorner(box, 0) - Eigen::Vector4d(0, 0, 0, 1)).norm(), 0.0, 1.0e-6);
+  EXPECT_NEAR((getCorner(box, 1) - Eigen::Vector4d(1, 0, 0, 1)).norm(), 0.0, 1.0e-6);
+  EXPECT_NEAR((getCorner(box, 7) - Eigen::Vector4d(1, 2, 3, 1)).norm(), 0.0, 1.0e-6);
 
+  Eigen::Matrix4d pose = Eigen::Matrix<double, 4, 4>::Identity();
+  pose(1, 1) = -1.0;
+  pose(2, 2) = -1.0;
 
+  auto projected = projectBBox(pose, box);
+
+  EXPECT_NEAR((getLower(projected) - Eigen::Vector3d(0, -2 , -3)).norm(),
+      0.0, 1.0e-6);
+  EXPECT_NEAR((getUpper(projected) - Eigen::Vector3d(1, 0, 0)).norm(),
+      0.0, 1.0e-6);
+}
