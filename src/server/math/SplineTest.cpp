8d8e953255ec7f33baf26e5dce3aba83410ebb53
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-16 16:45:30 +0200

    asdf

--- src/server/math/SplineTest.cpp
+++ src/server/math/SplineTest.cpp
@@ -11,9 +11,14 @@
 using namespace sail;
 
 TEST(SplineTest, PolyTest) {
-  typedef Polynomial<double, 1> P1;
+  typedef Polynomial<double, 1> P1; // constant
+  typedef Polynomial<double, 2> P2; // straight line
+  typedef Polynomial<double, 3> P3; // second-order polynomial
   auto p = P1{3} + P1{4};
   EXPECT_EQ(p, P1{7});
+  EXPECT_FALSE(P1{8} == P1{7});
+
+  EXPECT_EQ((P1{4}.primitive()), (P2{0, 4}));
 }
 
 
