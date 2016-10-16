7e6dc495c049f299bf2c86a48db38793838f7587
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-16 16:04:02 +0200

    add the reconstructor

---
+++ src/server/nautical/calib/Reconstructor.cpp
@@ -0,0 +1,14 @@
+/*
+ * Reconstructor.cpp
+ *
+ *  Created on: 16 Oct 2016
+ *      Author: jonas
+ */
+
+#include "Reconstructor.h"
+
+namespace sail {
+
+
+
+}
---
+++ src/server/nautical/calib/Reconstructor.h
@@ -0,0 +1,17 @@
+/*
+ * Reconstructor.h
+ *
+ *  Created on: 16 Oct 2016
+ *      Author: jonas
+ */
+
+#ifndef SERVER_NAUTICAL_CALIB_RECONSTRUCTOR_H_
+#define SERVER_NAUTICAL_CALIB_RECONSTRUCTOR_H_
+
+namespace sail {
+
+
+
+}
+
+#endif /* SERVER_NAUTICAL_CALIB_RECONSTRUCTOR_H_ */
