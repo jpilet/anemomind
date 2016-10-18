2c9cb5d436ce049c1a05ec22b8419d35005aa655
Julien Pilet <julien@anemomind.com>
2016-10-18 14:59:16 +0000

    Add /map route; update index.html

--- www2/server/routes.js
+++ www2/server/routes.js
@@ -15,6 +15,7 @@ module.exports = function(app) {
   app.use('/api/events', require('./api/event'));
   app.use('/api/boats', require('./api/boat'));
   app.use('/api/mailrpc', require('./api/mailrpc'));
+  app.use('/api/map', require('./api/map'));
   app.use('/api/session', require('./api/session'));  
   app.use('/api/tiles', require('./api/tiles'));
   app.use('/api/upload', require('./api/upload'));
