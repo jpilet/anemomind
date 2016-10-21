7842036cd0a7f3b6e6c7db61980ccf107d9e2b94
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-21 10:21:01 +0200

    bra va

--- src/server/common/CmdArgTest.cpp
+++ src/server/common/CmdArgTest.cpp
@@ -12,12 +12,21 @@ using namespace sail;
 
 
 TEST(CmdArgTest, InputFormTest) {
+  double a = 0.0;
+  double b = 0.0;
+
   auto form = inputForm(
       [&](double amp, double phase) {
+    a = amp;
+    b = phase;
     return true;
   },
   Arg<double>("amp").describe("The amplitude, in meters"),
   Arg<double>("phase").describe("The phase, in radians"));
+
+  EXPECT_TRUE(bool(form.parse({"3", "4"})));
+  EXPECT_NEAR(a, 3.0, 1.0e-5);
+  EXPECT_NEAR(b, 4.0, 1.0e-5);
 }
 
 TEST(CmdArgTest, BasicUsage) {
