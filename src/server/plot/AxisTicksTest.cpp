bea5aee5ab3885c7f37b7da62d5ca7192fdefbe9
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-23 12:03:35 +0200

    bra

--- src/server/plot/AxisTicksTest.cpp
+++ src/server/plot/AxisTicksTest.cpp
@@ -11,4 +11,8 @@ using namespace sail;
 
 TEST(AxisTicksTest, TestBasicIterator) {
   BasicTickIterator iter(0, "meters");
+  EXPECT_NEAR(iter.tickSpacing(), 1.0, 1.0e-6);
+  auto x = iter.get(4);
+  EXPECT_NEAR(x.position, 4.0, 1.0e-6);
+  EXPECT_EQ(x.tickLabel, "4 meters");
 }
