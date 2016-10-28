eb84b2eaa29ec882fdae7eb55017633c166fb16f
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-28 18:43:07 +0200

    grymt

--- src/server/math/SplineUtilsTest.cpp
+++ src/server/math/SplineUtilsTest.cpp
@@ -18,16 +18,22 @@ TEST(SplineUtilsTest, TemporalTest) {
     offset + 0.0_s,
     offset + 1.0_s,
     offset + 2.0_s,
-    offset + 3.0_s
+    offset + 3.0_s,
+    offset + 4.3_s
   };
-  Span<TimeStamp> span(offset, offset + 4.0_s);
+  Span<TimeStamp> span(offset, offset + 5.0_s);
 
-  Arrayd dst{9, 7, 4, 17};
+  Arrayd dst{9, 7, 4, 17, -3.4};
+
+  std::cout << "Build the curve" << std::endl;
 
   TemporalSplineCurve c(span, 1.0_s, src, dst);
+
+  std::cout << "Built the curve" << std::endl;
 
   EXPECT_NEAR(c.evaluate(offset + 0.0_s), 9.0, 1.0e-4);
   EXPECT_NEAR(c.evaluate(offset + 1.0_s), 7.0, 1.0e-4);
   EXPECT_NEAR(c.evaluate(offset + 2.0_s), 4.0, 1.0e-4);
   EXPECT_NEAR(c.evaluate(offset + 3.0_s), 17.0, 1.0e-4);
+  EXPECT_NEAR(c.evaluate(offset + 4.3_s), -3.4, 1.0e-4);
 }
