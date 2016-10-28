7ed7561776e1f76bfdf88867493d79c0b053d962
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-28 17:56:34 +0200

    bra

---
+++ src/server/math/SplineUtils.cpp
@@ -0,0 +1,14 @@
+/*
+ * SplineUtils.cpp
+ *
+ *  Created on: 28 Oct 2016
+ *      Author: jonas
+ */
+
+#include "SplineUtils.h"
+
+namespace sail {
+
+
+
+} /* namespace sail */
---
+++ src/server/math/SplineUtils.h
@@ -0,0 +1,17 @@
+/*
+ * SplineUtils.h
+ *
+ *  Created on: 28 Oct 2016
+ *      Author: jonas
+ */
+
+#ifndef SERVER_MATH_SPLINEUTILS_H_
+#define SERVER_MATH_SPLINEUTILS_H_
+
+namespace sail {
+
+
+
+}
+
+#endif /* SERVER_MATH_SPLINEUTILS_H_ */
