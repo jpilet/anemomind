205b395cdbd4d01f558ff898aa2cc653d6356f51
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-28 18:32:33 +0200

    good

--- src/server/math/SplineUtilsTest.cpp
+++ src/server/math/SplineUtilsTest.cpp
@@ -5,6 +5,23 @@
  *      Author: jonas
  */
 
+#include <server/math/SplineUtils.h>
+#include <gtest/gtest.h>
+
+using namespace sail;
 
 
+TEST(SplineUtilsTest, TemporalTest) {
+  auto offset = TimeStamp::UTC(2016, 5, 4, 3, 3, 4);
 
+  Array<TimeStamp> src{
+    offset + 0.0_s,
+    offset + 1.0_s,
+    offset + 2.0_s
+  };
+  Span<TimeStamp> span(offset, offset + 3.0_s);
+
+  Arrayd dst{9, 7, 4};
+
+  TemporalSplineCurve basis(span, 1.0_s, src, dst);
+}
