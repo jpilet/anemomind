8f815d46ee7023585a3b7d355b1caadf2a55fc81
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-28 18:45:43 +0200

    bra

--- src/server/math/SplineUtils.cpp
+++ src/server/math/SplineUtils.cpp
@@ -28,32 +28,20 @@ TemporalSplineCurve::TemporalSplineCurve(
   for (int i = 0; i < sampleCount; i++) {
     lhs.add(i, i, 1.0e-12);
   }
-  std::cout << "SAMPLE COUNT = " << sampleCount << std::endl;
   int n = src.size();
   for (int k = 0; k < n; k++) {
     double x = toLocal(src[k]);
     double y = dst[k];
-    std::cout << "Build weights" << std::endl;
     auto w = _basis.build(x);
-    std::cout << "Weights built" << std::endl;
-    for (int r = 0; r < w.dim; r++) {
-      std::cout << "   i=" << w.inds[r] << " w=" << w.weights[r] << std::endl;
-
-    }
     for (int i = 0; i < w.dim; i++) {
       for (int j = 0; j < w.dim; j++) {
-        std::cout << "Inc lhs" << std::endl;
         int I = w.inds[i];
         int J = w.inds[j];
         lhs.add(I, J, w.weights[i]*w.weights[j]);
-        std::cout << "Done\n";
       }
-      std::cout << "Inc rhs" << std::endl;
       rhs(w.inds[i], 0) += w.weights[i]*y;
-      std::cout << "Done" << std::endl;
     }
   }
-  std::cout << "SOLVE IT" << std::endl;
   Pbsv<double>::apply(&lhs, &rhs);
   _coefs = rhs.getStorage();
 }
