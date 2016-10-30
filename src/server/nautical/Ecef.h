5f8eceee36bf61a0ee9d1ba0dac2e9282a430399
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-30 17:20:03 +0100

    asdf

---
+++ src/server/nautical/Ecef.h
@@ -0,0 +1,22 @@
+/*
+ * ECEF.h
+ *
+ *  Created on: 30 Oct 2016
+ *      Author: jonas
+ */
+
+#ifndef SERVER_NAUTICAL_ECEF_H_
+#define SERVER_NAUTICAL_ECEF_H_
+
+namespace sail {
+
+// Implementation of
+// https://microem.ru/files/2012/08/GPS.G1-X-00006.pdf
+
+struct ECEF {
+static constexpr
+};
+
+}
+
+#endif /* SERVER_NAUTICAL_ECEF_H_ */
---
+++ src/server/nautical/EcefTest.cpp
@@ -0,0 +1,10 @@
+/*
+ * EcefTest.cpp
+ *
+ *  Created on: 30 Oct 2016
+ *      Author: jonas
+ */
+
+
+
+
