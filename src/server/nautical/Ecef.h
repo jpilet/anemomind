3a7c4ff4ed69f16aa6da50507d8ad96323da67e0
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-10-30 17:58:02 +0100

    bra va

--- src/server/nautical/Ecef.h
+++ src/server/nautical/Ecef.h
@@ -8,14 +8,118 @@
 #ifndef SERVER_NAUTICAL_ECEF_H_
 #define SERVER_NAUTICAL_ECEF_H_
 
+#include <server/math/JetUtils.h>
+#include <device/Arduino/libraries/PhysicalQuantity/PhysicalQuantity.h>
+
 namespace sail {
 
 // Implementation of
 // https://microem.ru/files/2012/08/GPS.G1-X-00006.pdf
 
-struct ECEF {
-static constexpr
+template <typename T>
+struct ECEFCoords {
+  Length<T> xyz[3];
 };
+
+template <typename T>
+struct LLACoords {
+  Angle<T> lon;
+  Angle<T> lat;
+  Length<T> height;
+};
+
+
+struct ECEF {
+  static constexpr double a = 6378137;
+  static constexpr double a2 = a*a;
+  static constexpr double f = 1.0/298.257223563;
+  static constexpr double b = a*(1.0 - f);
+  static constexpr double b2 = b*b;
+  static constexpr double e2 = (a2 - b2)/a2;
+  static constexpr double ep2 = (a2 - b2)/b2;
+  static double e() {return sqrt(e2);}
+  static double ep() {return sqrt(ep2);}
+
+  template <typename T>
+  static T computeNFromSinPhi2(T sinPhi2) {
+    return MakeConstant<T>::apply(a)/
+            sqrt(MakeConstant<T>::apply(1.0)
+                - MakeConstant<T>::apply(e2)*sinPhi2);
+  }
+
+  template <typename T>
+  static T computeNFromPhi(T phi) {
+    T sinPhi = sin(phi);
+    return computeNFromSinPhi2<T>(sinPhi*sinPhi);
+  }
+
+  template <typename T>
+  static ECEFCoords<T> convert(const LLACoords<T> &src) {
+    T x, y, z;
+    lla2ecef<T>(src.lon.radians(),
+        src.lat.radians(), src.height.meters(),
+        &x, &y, &z);
+    return ECEFCoords<T>{
+      Length<T>::meters(x),
+      Length<T>::meters(y),
+      Length<T>::meters(z),
+    };
+  }
+
+  template <typename T>
+  static LLACoords<T> convert(const ECEFCoords<T> &src) {
+    T lon, lat, height;
+    ecef2lla<T>(
+        src.xyz[0].meters(),
+        src.xyz[1].meters(),
+        src.xyz[2].meters(),
+        &lon, &lat, &height);
+    return LLACoords<T>{
+      Angle<T>::radians(lon),
+      Angle<T>::radians(lat),
+      Length<T>::meters(height)
+    };
+  }
+
+  template <typename T>
+  static void lla2ecef(T lonRad, T latRad, T heightMeters,
+      T *xMetersOut, T *yMetersOut, T *zMetersOut) {
+    T phi = latRad;
+    T lambda = lonRad;
+    T sinPhi = sin(phi);
+    T sinPhi2 = sinPhi*sinPhi;
+    T N = computeNFromSinPhi2<T>(sinPhi2);
+    T factor = (N + heightMeters)*cos(phi);
+    *xMetersOut = factor*cos(lambda);
+    *yMetersOut = factor*sin(lambda);
+    *zMetersOut = (MakeConstant<T>::apply(b2/a2)*N
+        + heightMeters)*sinPhi;
+  }
+
+  template <typename T>
+  static void ecef2lla(
+      T xMeters, T yMeters, T zMeters,
+      T *lonRadOut, T *latRadOut, T *heightMetersOut) {
+    T lambda = atan2(yMeters, xMeters);
+    T p = sqrt(xMeters*xMeters + yMeters*yMeters);
+    T theta = atan2(
+        zMeters*MakeConstant<T>::apply(a),
+        p*MakeConstant<T>::apply(b));
+    T sinTheta = sin(theta);
+    T sin3Theta = sinTheta*sinTheta*sinTheta;
+    T cosTheta = cos(theta);
+    T cos3Theta = cosTheta*cosTheta*cosTheta;
+    T phi = atan2(
+        zMeters + MakeConstant<T>::apply(ep2*b)*sin3Theta,
+        p - MakeConstant<T>::apply(e2*a)*cos3Theta);
+    *heightMetersOut = p/cos(phi) - computeNFromPhi<T>(phi);
+    *lonRadOut = lambda;
+    *latRadOut = phi;
+  }
+
+
+};
+
 
 }
 
--- src/server/nautical/EcefTest.cpp
+++ src/server/nautical/EcefTest.cpp
@@ -5,6 +5,27 @@
  *      Author: jonas
  */
 
+#include <server/nautical/Ecef.h>
+#include <gtest/gtest.h>
+
+using namespace sail;
+
+TEST(EcefTest, BasicBackAndForth) {
+  auto lon = 34.0_deg;
+  auto lat = 49.0_deg;
+  auto h = 5.7_m;
+  LLACoords<double> src{
+    lon, lat, h
+  };
+
+  auto dst = ECEF::convert(src);
+  auto src2 = ECEF::convert(dst);
+
+  EXPECT_NEAR(cos(src.lat), cos(src2.lat), 1.0e-6);
+  EXPECT_NEAR(sin(src.lat), sin(src2.lat), 1.0e-6);
+  EXPECT_NEAR(cos(src.lon), cos(src2.lon), 1.0e-6);
+  EXPECT_NEAR(sin(src.lon), sin(src2.lon), 1.0e-6);
+}
 
 
 
