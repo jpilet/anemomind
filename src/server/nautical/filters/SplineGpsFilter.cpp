07ee33155ee5153489a0d63e777508966d2f99da
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-11-04 11:45:41 +0100

    good

--- src/server/nautical/filters/SplineGpsFilter.cpp
+++ src/server/nautical/filters/SplineGpsFilter.cpp
@@ -550,7 +550,7 @@ void buildProblemPerCurve(
   addPositionDataTerms(settings, c, sampleSpan, pd, dst);
   //addMotionDataTerms(settings, c, sampleSpan, md, dst);
   addDataRegTerms(settings, c, sampleSpan, dst);
-  //addStabilizeTerms(settings, sampleSpan, dst);
+  addStabilizeTerms(settings, sampleSpan, dst);
 }
 
 void buildProblem(
