3244108acea817dd8cc3aef3c672abd12d998a1f
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-11-04 10:14:42 +0100

    brae

--- src/server/common/DiscreteOutlierFilterTest.cpp
+++ src/server/common/DiscreteOutlierFilterTest.cpp
@@ -10,7 +10,7 @@
 
 using namespace sail;
 
-TEST(DiscreteOutlierFilterTest, BasicTest) {
+TEST(DiscreteOutlierFilterTest, BasicTest1) {
 
   Array<double> values{7, 8, 130, 3, 4};
   DiscreteOutlierFilter::Settings settings;
@@ -23,5 +23,21 @@ TEST(DiscreteOutlierFilterTest, BasicTest) {
 
   for (int i = 0; i < 5; i++) {
     EXPECT_EQ(mask[i], i != 2);
+  }
+}
+
+TEST(DiscreteOutlierFilterTest, BasicTest2) {
+
+  Array<double> values{7, 130, 0, 139, 4};
+  DiscreteOutlierFilter::Settings settings;
+  settings.threshold = 20;
+
+  auto mask = DiscreteOutlierFilter::computeOutlierMask<double*, double>(
+      values.begin(), values.end(),
+      [](double a, double b) {return std::abs(a - b);},
+      settings);
+
+  for (int i = 0; i < 5; i++) {
+    EXPECT_EQ(mask[i], (i != 1 && i != 3));
   }
 }
