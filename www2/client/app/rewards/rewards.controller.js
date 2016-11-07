ce534ef9062c643859a919449ed3d39dce3f8d6a
Nico Pag-ong <n.pagong@arcanys.com>
2016-11-07 16:45:22 +0800

    Added function to calculate number of levels and number of bars inside per level

--- www2/client/app/rewards/rewards.controller.js
+++ www2/client/app/rewards/rewards.controller.js
@@ -4,4 +4,33 @@ angular.module('www2App')
   .controller('RewardsCtrl', function ($scope, Auth) {
     $scope.isAdmin = Auth.isAdmin;
     $scope.isLoggedIn = Auth.isLoggedIn();
+
+    $scope.maxLevel = 10;
+    var maxBars = 5;
+    $scope.currentLevel = 0;
+
+    // The number of levels inside a bar
+    // are actually 5 and not 3
+    var displayLevels = function(currentLevel) {
+      var max = currentLevel + 4;
+      var min = max >= 11 ? 7 : currentLevel;
+
+      // Instead of 10, we set it by 11
+      // but we don't have to fill this level
+      // with bars
+      max = max >= 11 ? 11 : max;      
+
+      return {min: min, max: max };
+    };
+    console.log(displayLevels(5));
+
+    var fillUpWithBars = function(valueGiven, valueToReach) {
+      var diff = valueToReach - valueGiven;
+      var perc = diff / valueToReach;
+      var bars = Math.round(maxBars - (maxBars * perc));
+
+      return bars;
+    };
+    console.log(fillUpWithBars(15, 20));
+    
   });
