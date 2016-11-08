83acccf67d31c983925adf20900c65d7f44c983d
evaletolab <evaleto@gmail.com>
2016-11-08 20:51:06 +0100

    directive to display html element only if user boat admin

--- www2/client/app/boatList/boatList.directive.js
+++ www2/client/app/boatList/boatList.directive.js
@@ -1,6 +1,7 @@
 'use strict';
   
-
+//
+// directive that display html element if user is admin of the current boat 
 angular.module('www2App')
   .directive('showIfBoatadmin', function ( $window, boatList, Auth) {
     return {
@@ -30,32 +31,3 @@ angular.module('www2App')
       }
     };
   })
-
-  .directive('showIfBoatreader', function ( $window, boatList, Auth) {
-    return {
-      restrict: 'A',
-      link: function (scope, element, attrs) {
-        //
-        // default action is driven by CSS
-        var defaultClass="ng-hide", successAction="ng-show";
- 
-        attrs.$observe('showIfBoatreader',function(boatId) {
-          element.addClass(defaultClass);
-          if(!boatId){
-            // not ready yet
-            return;
-          }
-
-          scope.user=Auth.getCurrentUser();
-          boatList.boats().then(function(boats) {
-            var boat=boatList.boat(boatId);
-            
-            if(!!~boat.admins.indexOf(scope.user._id)){
-              element.addClass(successAction).removeClass(defaultClass);
-            }
-
-          });
-        });
-      }
-    };
-  });
