5259b336e06d8aa9e00c88932c562cfd2f40594e
Nico Pag-ong <n.pagong@arcanys.com>
2016-11-15 15:15:01 +0800

    Replaced the map share modal by using the angular-modal-service plugin. Moved the codes used for map share modal to the Share controller.

--- www2/client/app/share/share.controller.js
+++ www2/client/app/share/share.controller.js
@@ -1,39 +1,90 @@
 'use strict';
 
 angular.module('www2App')
-  .controller('ShareCtrl', function ($scope, $element, ModalService) {
+  .controller('ShareCtrl', function ($scope, $stateParams, $location, $element, boatList, ModalService) {
+
+      $scope.boat = { _id: $stateParams.boatId, name: 'loading' };
+
+      $scope.isVisible = (typeof ModalService.isVisible === 'undefined' || ModalService.isVisible == null) ? false : ModalService.isVisible;
 
       $scope.iconList = [
       {
-        name: 'linkedin',
-        url: '#'
+        name:'facebook',
+        icon:'fa-facebook',
+        url: 'https://www.facebook.com/sharer/sharer.php?u=',
+        use:true
       },
       {
-        name: 'facebook',
-        url: '#'
+        name: 'linkedin',
+        icon:'fa-linkedin',
+        url: 'https://www.linkedin.com/shareArticle?mini=true&url=',//&title=_TITLE_&summary=_TXT_'
+        use:false
       },
       {
         name: 'pinterest',
-        url: '#'
+        url: '#',
+        use:false
       },
       {
         name: 'twitter',
-        url: '#'
+        icon:'fa-twitter',
+        url: 'https://twitter.com/home?status=',
+        use:true
       },
       {
         name: 'google',
-        url: '#'
+        icon:'fa-google-plus',
+        url: 'https://plus.google.com/share?url=',
+        use:true
       },
       {
         name: 'mail',
-        url: '#'
+        url: '#',
+        use:false
       }
     ];
+    $scope.selectedIcon=$scope.iconList[0];
+    
 
-    $scope.shareMe = ModalService.data.boat+' and her team made a great performance with an average speed of '+ModalService.data.speed+' .'+ModalService.data.speedUnit;
-    $scope.shareMe += '\n\nAdd text ...';
+    // This is used for the modal template in Challenges page
+    if(typeof ModalService.data !== 'undefined' || ModalService.data != null) {
+      $scope.shareMe = ModalService.data.boat+' and her team made a great performance with an average speed of '+ModalService.data.speed+' .'+ModalService.data.speedUnit;
+      $scope.shareMe += '\n\nAdd text ...';
+    }
+    
+    // This is used for the modal template in Map page
+    if(typeof $stateParams.boatId !== 'undefined' || $stateParams.boatId != null) {
+      boatList.boats().then(function (boats) {
+        var aveSpeedText = '32 Kts';
+        var windBlowedText = '22 Kts';
+        var performanceText = '91%';
+        $scope.boat=boatList.boat($stateParams.boatId);
+        $scope.sharedLink=$location.absUrl();
+        $scope.shareText = 'Share '+$scope.boat.name+' and her team performance';
+      });
+
+      $scope.changePublicAccess=function() {
+        $scope.boat.publicAccess = ! $scope.boat.publicAccess;      
+        boatList.save($stateParams.boatId, $scope.boat)
+          .success(function(boat) { 
+            $scope.boat = boat; 
+          });
+      };
+    }
+    
+
+    $scope.goShare=function(icon){
+      window.open(icon.url+$scope.sharedLink, "_new");
+    };
+
+    $scope.selecteIcon=function(icon){
+      $scope.selectedIcon=icon;
+    };
+
 
     $scope.closeModal = function() {
-      $element.hide();
+      // The background of the modal
+      $element.next().remove();
+      $element.remove();      
     };
   });
