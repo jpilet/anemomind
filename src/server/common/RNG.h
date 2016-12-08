178d407e29badd9dd37b4e5309509dbaf9dd621c
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-12-08 16:33:42 +0100

    FIRST VERSION OF MAG HEDG

---
+++ src/server/common/RNG.h
@@ -0,0 +1,21 @@
+/*
+ * RNG.h
+ *
+ *  Created on: 8 Dec 2016
+ *      Author: jonas
+ */
+
+#ifndef SERVER_COMMON_RNG_H_
+#define SERVER_COMMON_RNG_H_
+
+#include <random>
+
+namespace sail {
+
+typedef std::mt19937 RNG;
+
+}
+
+
+
+#endif /* SERVER_COMMON_RNG_H_ */
