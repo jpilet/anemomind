8ada27d278358aaa147aa0d6be5deff1c67b8a2d
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-12-30 11:44:03 +0100

    Game calib on the way

---
+++ src/server/nautical/calib/GameCalib.cpp
@@ -0,0 +1,16 @@
+/*
+ * GameCalib.cpp
+ *
+ *  Created on: 30 Dec 2016
+ *      Author: jonas
+ */
+
+#include "GameCalib.h"
+
+namespace sail {
+namespace GameCalib {
+
+
+
+}
+}
---
+++ src/server/nautical/calib/GameCalib.h
@@ -0,0 +1,19 @@
+/*
+ * GameCalib.h
+ *
+ *  Created on: 30 Dec 2016
+ *      Author: jonas
+ */
+
+#ifndef SERVER_NAUTICAL_CALIB_GAMECALIB_H_
+#define SERVER_NAUTICAL_CALIB_GAMECALIB_H_
+
+namespace sail {
+namespace GameCalib {
+
+void solve
+
+}
+}
+
+#endif /* SERVER_NAUTICAL_CALIB_GAMECALIB_H_ */
