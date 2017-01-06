12d3ed4d1e4ac4c266dc650ade10fe64dc3821aa
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-06 17:40:38 +0100

    bra va

--- src/server/math/nonlinear/LsqNashSolver.h
+++ src/server/math/nonlinear/LsqNashSolver.h
@@ -8,10 +8,30 @@
 #ifndef SERVER_MATH_NONLINEAR_LSQNASHSOLVER_H_
 #define SERVER_MATH_NONLINEAR_LSQNASHSOLVER_H_
 
+#include <Eigen/Dense>
+#include <ceres/jet.h>
+
 namespace sail {
 namespace LsqNashSolver {
 
+class SubFunction {
+public:
+  virtual void eval(
+      const double *X, double *dst) const = 0;
+  virtual void eval(const double *X,
+      double *dst, int jacobianRow,
+      std::vector<Eigen::Triplet<double>> *dst) = 0;
+  virtual ~SubFunction() {}
+};
 
+template <typename F, int ... InputDim>
+class ADSubFunction {
+  ADSubFunction(Array<Spani> ... inputSpans) :
+    _inputSpans({inputSpans...}) {}
+public:
+private:
+  Array<Spani> _inputSpans;
+};
 
 }
 }
