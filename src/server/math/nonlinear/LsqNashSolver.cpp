b292fba0c26bfa9578e2523e2c13a4285285b0fa
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-06 17:23:17 +0100

    added the nash solvers

---
+++ src/server/math/nonlinear/LsqNashSolver.cpp
@@ -0,0 +1,16 @@
+/*
+ * LsqNashSolver.cpp
+ *
+ *  Created on: 6 Jan 2017
+ *      Author: jonas
+ */
+
+#include "LsqNashSolver.h"
+
+namespace sail {
+namespace LsqNashSolver {
+
+
+
+}
+}
---
+++ src/server/math/nonlinear/LsqNashSolver.h
@@ -0,0 +1,19 @@
+/*
+ * LsqNashSolver.h
+ *
+ *  Created on: 6 Jan 2017
+ *      Author: jonas
+ */
+
+#ifndef SERVER_MATH_NONLINEAR_LSQNASHSOLVER_H_
+#define SERVER_MATH_NONLINEAR_LSQNASHSOLVER_H_
+
+namespace sail {
+namespace LsqNashSolver {
+
+
+
+}
+}
+
+#endif /* SERVER_MATH_NONLINEAR_LSQNASHSOLVER_H_ */
