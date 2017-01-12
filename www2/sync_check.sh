dd226bb1134338f3f81689dbbe365f5583e1ad19
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-12 10:18:34 +0100

    Made the md5 check more accurate

--- www2/sync_check.sh
+++ www2/sync_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 truth="MD5 (uploads/anemologs/boat57f678e612063872e749d481/dpkg.log) = d7b92cb70c333ff82616ff660799a078"
-result=$(md5 uploads/anemologs/boat57f678e612063872e749d481/dpkg.log)
+result=$(md5 uploads/anemologs/boat57f678e612063872e749d481/*)
 if [ "$result" = "$truth" ]; then
   echo "PASSED :-)"
   exit 0
