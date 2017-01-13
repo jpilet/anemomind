02d010f7d677e64a94c15d7add6062cd0f87ff56
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-13 12:18:32 +0100

    Tweaks to sync test

--- src/device/anemobox/anemonode/sync_start_test.sh
+++ src/device/anemobox/anemonode/sync_start_test.sh
@@ -2,13 +2,12 @@
 set -e
 bash /root/disable_watchdog.sh
 rm -rf /media/sdcard/logs/*
-cp /usr/bin/dpkg dpkg.log
-cd /media/sdcard/mail2
-rm -rf *
+cp /usr/bin/dpkg /media/sdcard/logs/dpkg.log
+rm -rf /media/sdcard/mail2
 cd /anemonode
 if sh sync_check.sh; then
   echo "sync_check should *not* succeed, something is wrong!"
   exit 1
 fi
 echo "############## perform_sync_test_anemobox.sh: Launch the service!"
-./run.sh > synctest.log
+./run.sh
