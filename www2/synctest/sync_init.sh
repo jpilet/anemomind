a33d8b4791337acff13492c2364f24bc69f9f6a8
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-20 18:08:02 +0100

    addressed it all

--- www2/synctest/sync_init.sh
+++ www2/synctest/sync_init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e # Stop on first error
-cd ..
 killall mongod || true
 killall grunt || true
 killall node || true
-WWW2_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-LOGFILE="${WWW2_DIR}/uploads/anemologs/boat57f678e612063872e749d481/dpkg.log"
+export WWW2_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+echo "######################### WWW2_DIR = $WWW2_DIR"
+export LOGFILE="${WWW2_DIR}/uploads/anemologs/boat57f678e612063872e749d481/dpkg.log"
 rm -rf /tmp/endpoints || true
 rm -r /tmp/synctest_message.txt || true
 rm -rf "${LOGFILE}"
