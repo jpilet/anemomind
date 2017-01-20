a9b9757a8ffdefd91242b01ca3e1f49ddcbba619
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-20 12:36:34 +0100

    this is good

--- www2/synctest/sync_run1.sh
+++ www2/synctest/sync_run1.sh
@@ -1,5 +1,5 @@
 set -e # Stop on first error
-
+cd ..
 killall mongod || true
 killall grunt || true
 killall node || true
@@ -9,7 +9,8 @@ rm -rf /tmp/endpoints || true
 rm -r /tmp/synctest_message.txt || true
 rm -rf "${www2_dir}/uploads/anemologs/boat57f678e612063872e749d481"
 echo "Launching the server!"
-if sh sync_check.sh; then
+cd synctest
+if eval "bash sync_check${1}.sh"; then
   echo "The sync_check.sh should not pass, something is wrong!"
   exit 1
 fi
