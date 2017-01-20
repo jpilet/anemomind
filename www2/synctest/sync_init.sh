c7029d36b38355afe2b6775a32ac17c9df69c006
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-20 14:03:47 +0100

    both bow

---
+++ www2/synctest/sync_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e # Stop on first error
+cd ..
+killall mongod || true
+killall grunt || true
+killall node || true
+export WWW2_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "WWW2_DIR = $WWW2_DIR"
+rm -rf /tmp/endpoints || true
+rm -r /tmp/synctest_message.txt || true
+rm -rf "${WWW2_DIR}/uploads/anemologs/boat57f678e612063872e749d481"
+echo "Launching the server!"
+cd synctest
+if eval "bash sync_check${1}.sh"; then
+  echo "The sync_check.sh should not pass, something is wrong!"
+  exit 1
+fi
+grunt serve:dev &
+echo "### The server was started"
---
+++ www2/synctest/sync_run2.sh
@@ -0,0 +1,3 @@
+set -e
+bash sync_init.sh
+echo "################ Now you only have to wait for the file to arrive, and then call the command printed out on the anemobox!"
