5ccd6837dd4bff8601408a7f32fed710ba533856
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-01-26 09:42:49 +0100

    Removed unnecessary assigment

--- www2/synctest/sync_check2.sh
+++ www2/synctest/sync_check2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-truth="d7b92cb70c333ff82616ff660799a078"
 result=$(md5 -q ${WWW2_DIR}/uploads/anemologs/boat57f678e612063872e749d481/${1} || true)
 truth="${2}"
 if [ "$result" = "$truth" ]; then
