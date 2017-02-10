1c33e75410a8d5c6c997e2a49f417ef167230135
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-02-10 14:07:36 +0100

    Add a factory reset script

---
+++ src/device/anemobox/anemonode/factory_reset.sh
@@ -0,0 +1,10 @@
+## TODO: Should we just do 'rm -rf /media/sdcard/*' or would it be too much?
+##       Should we just do 'rm -rf /home/anemobox/*' or would it be too much?
+rm -rf /media/sdcard/mail2/*
+rm -rf /media/sdcard/logs/*
+rm /home/anemobox/config.json 
+bootcount=$(</home/anemobox/bootcount)
+if ! [[ $bootcount =~ ^[[:space:]]*[[:digit:]]+[[:space:]]*$ ]] ; then
+  echo "WARNING: Boot count '$bootcount' was not a number, reset it to 0."
+  echo "0" > /home/anemobox/bootcount
+fi
