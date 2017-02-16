3e4adde61d1958f869205c36ba29dbaca91f4de7
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-02-16 10:24:00 +0100

    Address comments related to ping check

--- www2/synctest/ping_check.sh
+++ www2/synctest/ping_check.sh
@@ -2,12 +2,11 @@
 set -e
 WWW2_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 truth="109482d245aa982cb550f7c5be67ffa3" ## md5 on mock_boat.dat
-to_execute=$(cat /tmp/calltmp.txt)
-echo "What to execute: $to_execute"
 if (cd "${WWW2_DIR}/utilities" ; bash /tmp/calltmp2.txt ) | grep -q "${truth}" ; then
   echo "PASSED :-)"
+  rm /tmp/calltmp2.txt
   exit 0
 else
-  echo "Failed, got '$result' but expected '$truth'"
+  echo "Failed :-("
   exit 1
 fi
