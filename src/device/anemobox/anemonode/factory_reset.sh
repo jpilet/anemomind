3273aef869c8dda180cb6a8cfd4a999abec2af02
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-02-17 13:57:34 +0100

    Update factory_reset.sh

--- src/device/anemobox/anemonode/factory_reset.sh
+++ src/device/anemobox/anemonode/factory_reset.sh
@@ -4,8 +4,11 @@
 rm -rf /media/sdcard/mail2/*
 rm -rf /media/sdcard/logs/*
 rm /home/anemobox/config.json 
+rm /home/anemobox/boat.dat
+rm /home/anemobox/imu.cal
 bootcount=$(</home/anemobox/bootcount)
 if ! [[ $bootcount =~ ^[[:space:]]*[[:digit:]]+[[:space:]]*$ ]] ; then
   echo "WARNING: Boot count '$bootcount' was not a number, reset it to 0."
   echo "0" > /home/anemobox/bootcount
 fi
+echo "Successfully factory reset"
