ec4eb1cc25f3d51f5df3d37a980fd5f0b6e1c8a2
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-02-23 18:46:31 +0100

    Simplify mongo call

--- www2/synctest/get_devbox_boatid.sh
+++ www2/synctest/get_devbox_boatid.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mongo --quiet anemomind-dev --eval 'db.boats.find({anemobox: "784b87a0b162"}).toArray()[0]["_id"].valueOf()'
+mongo --quiet anemomind-dev --eval 'db.boats.findOne({anemobox: "784b87a0b162"})["_id"].valueOf()'
