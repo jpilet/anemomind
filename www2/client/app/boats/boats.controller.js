1dc8604644a48be525d92c44077181b6e1f50ec0
Julien Pilet <julien.pilet@gmail.com>
2017-03-10 17:42:52 +0100

    Fix boat creation from web interface (#1035)
    Also add error reporting

--- www2/client/app/boats/boats.controller.js
+++ www2/client/app/boats/boats.controller.js
@@ -3,6 +3,16 @@
 angular.module('www2App')
   .controller('BoatsCtrl', function ($scope, $http, $stateParams,$location, socket, boatList,Auth,$log,$timeout) {
     $scope.isLoggedIn = Auth.isLoggedIn();
+    $scope.errorMessage = undefined;
+    $scope.boatCreated = undefined;
+
+    // For some reason, for the two-way binding to work properly between the
+    // html and the controller, scope members have to be initialized with something
+    // valid. So to initialize a variable to something non-empty but containing an
+    // empty string, we make an object containing just one member.
+    // Trick found on
+    // http://stackoverflow.com/questions/12618342/ng-model-does-not-update-controller-value
+    $scope.newBoat = { name: '' };
 
     $scope.$watch(Auth.isLoggedIn, function(newVal, oldVal) {
       $scope.isLoggedIn = newVal;
@@ -24,12 +34,24 @@ angular.module('www2App')
     }
 
     $scope.addBoat = function() {
-      if(!$scope.newBoat||$scope.newBoat === '') {
+      $scope.errorMessage = undefined;
+
+      var newBoatName = $scope.newBoat.name = ('' + $scope.newBoat.name).trim();
+      if(!newBoatName || newBoatName === '') {
+        $scope.errorMessage = 'Please enter a boat name';
         return;
       }
-      $http.post('/api/boats', { name: $scope.newBoat });
-      $scope.newBoat = '';
-      boatList.update();
+
+      $scope.newBoat.name = '';
+
+      $http.post('/api/boats', { name: newBoatName })
+        .then(function(boat) {
+          boatList.update();
+          $scope.boatCreated = boat.data;
+        })
+      .catch(function(err) {
+        $scope.errorMessage = 'Failed to create boat.';
+      });
     };
 
 
