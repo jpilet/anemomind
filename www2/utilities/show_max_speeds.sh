0bb5bfad4bef8cf194e972444af95f17994bdfd7
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-30 12:26:36 +0200

    Address the NODE_ENV confusion

--- www2/utilities/show_max_speeds.sh
+++ www2/utilities/show_max_speeds.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 echo "Using NODE_ENV='${NODE_ENV}'"
-cat show_max_speeds_mongo.txt | mongo "${NODE_ENV}"
+uri=$(node catconfig.js %s x.mongo.uri)
+echo "Using URI='${uri}'"
+cat show_max_speeds_mongo.txt | mongo $uri
