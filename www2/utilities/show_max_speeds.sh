68bc1994f5d632455b008061c1b2f186577b0417
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-30 12:36:59 +0200

    Address Juliens comment on Here_document

--- www2/utilities/show_max_speeds.sh
+++ www2/utilities/show_max_speeds.sh
@@ -3,4 +3,13 @@ set -e
 echo "Using NODE_ENV='${NODE_ENV}'"
 uri=$(node catconfig.js %s x.mongo.uri)
 echo "Using URI='${uri}'"
-cat show_max_speeds_mongo.txt | mongo $uri
+mongo $uri << 'EOF'
+  db.sailingsessions.aggregate(
+    {
+      $group: {
+        _id: "$boat",
+        maxSpeedOverGround: { $max: "$maxSpeedOverGround" } 
+      } 
+    },
+    { $sort : { maxSpeedOverGround : -1 } });
+EOF
