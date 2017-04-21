43007e4e0fd94f69ad5dafe24451b90c6c111c52
Julien Pilet <julien.pilet@gmail.com>
2017-04-21 17:22:06 +0200

    Fix minor server issues (#1063)
    * Fix minor server issues
    
    * Address Jonas comment

--- www2/server/social/map.js
+++ www2/server/social/map.js
@@ -35,17 +35,22 @@ module.exports = function(app) {
 	// open a shared map
 	controller.get=function (req,res) {
 
-	  var l=req.query.l,
-	      boatId=req.params.boatId,
-	      curve=req.query.c,
-	      start=curveStartTimeStr(curve),
-	      end=curveEndTimeStr(curve),
-	      width=1024, height=536;
+	  var l = req.query.l;
+	  var boatId = req.params.boatId;
+	  var curve = req.query.c;
+	  var start = curve ? curveStartTimeStr(curve) : undefined;
+	  var end = curve ? curveEndTimeStr(curve) : undefined;
+	  var width = 1024, height=536;
 
 	  // build image url that keep ideal FB ratio 1.91:1 (600x315, 800x418, 1024x536)
-	  var ogImg=[
-	    "/api/map",boatId,l,start,end,width+"-"+height+".png"
-	  ].join('/');
+	  var ogImg = [
+            "/api/map",
+            boatId,
+            l || '',
+            start ||'',
+            end || '',
+            width+"-"+height+".png"
+          ].join('/');
 
 	  // Load current boat
 	  mongoose.model('Boat').findById(boatId).select('name').exec(function(err,boat) {
