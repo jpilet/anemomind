bc5a8be9a10d14a12ae60238a18a6d9427af12c9
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-05-19 17:10:03 +0200

    Add reboot

--- src/device/anemobox/anemonode/components/http/api/reset/reset.controller.js
+++ src/device/anemobox/anemonode/components/http/api/reset/reset.controller.js
@@ -1,5 +1,6 @@
 'use strict'
 var factoryReset = require('../../../factoryReset.js');
+var reboot = require('../../../reboot.js').reboot;
 
 module.exports.reset = function(req, res) {
   factoryReset(function(err) {
@@ -7,6 +8,12 @@ module.exports.reset = function(req, res) {
       res.status(500).send("Factory reset error: " + err);
     } else {
       res.status(200).end();
+      
+      // Leave some time for the HTTP-response to get away
+      // before rebooting.
+      setTimeout(function() {
+        reboot();
+      }, 3000);
     }
   });
 };
