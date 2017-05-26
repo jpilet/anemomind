374b349965611cf43b85bc4ddb307f5c74a78ad0
jonasseglare <uppfinnarjonas@gmail.com>
2017-05-26 17:30:19 +0200

    Add a web interface on the anemobox to reset it (#1096)

--- src/device/anemobox/anemonode/components/http/routes.js
+++ src/device/anemobox/anemonode/components/http/routes.js
@@ -8,6 +8,7 @@ var express = require('express');
 
 module.exports = function(app) {
 
+  app.use('/api/reset', require('./api/reset'));
   app.use('/api/id', require('./api/id'));
   app.use('/api/live', require('./api/live'));
   app.use('/api/rpc', require('./api/rpc'));
