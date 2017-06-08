f5a528f2ac40f9258ab1033b53e903507e1bd194
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-06-08 17:08:59 +0200

    good

--- src/device/anemobox/anemonode/components/nmea2000raw.js
+++ src/device/anemobox/anemonode/components/nmea2000raw.js
@@ -2,16 +2,25 @@
 var j1939socket = require('j1939socket');
 var can = require('socketcan');
 var buffer = require('buffer');
-var anemonode = require('anemonode');
-
+var anemonode = require('../build/Release/anemonode');
 var channel = can.createRawChannel("can0", true /* ask for timestamps */);
 channel.start();
 
-function dumpPacket(msg) {
-  var tsMilliseconds = 1000*msg.ts_sec + 0.001*msg.ts_usec;
+function logRawPacket(msg) {
+  var systemTime0 = 1000*msg.ts_sec + 0.001*msg.ts_usec;
+  var systemTime1 = new Date().getTime();
+  var monotonicTime1 = anemonode.currentTime();
   
+  // monotonicTime1 - monotonicTime0 = systemTime1 - systemTime0
+  
+  monotonicTime0 = monotonicTime1 - (systemTime1 - systemTime0);
 
-    toHex(msg.id).toUpperCase() + '#' + msg.data.toString('hex').toUpperCase());
+  anemonode.logRawNmea2000(
+    monotonicTime0, 
+    msg.id, msg.data.toString());
+  
+  // This will, hopefully, trigger a packet to be
+  // delivered, inside "nmea2000.js".
   j1939socket.fetch();
 }
 
