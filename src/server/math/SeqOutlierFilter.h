2cf047437878aa41e934401cc313f3990f5ed9fc
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-06-09 14:16:41 +0200

    well good

--- src/server/math/SeqOutlierFilter.h
+++ src/server/math/SeqOutlierFilter.h
@@ -35,9 +35,18 @@ public:
     : _settings(settings), _prototype(prototype) {}
 
   struct LocalState {
-    LocalModel model;
-    int lastUpdate = 0;
+    // The index of its associated segment.
     int index = 0;
+
+    // The current local model.
+    LocalModel model;
+
+    // When the last update happened. Used to identify the oldest
+    // LocalState that we replace by a new one in case we need
+    // a new state.
+    int lastUpdate = 0;
+
+    // How many times we have inserted something.
     int insertedCount = 0;
 
     LocalState(
@@ -46,9 +55,60 @@ public:
       : model(x), lastUpdate(itemCount), index(i) {}
   };
 
+  struct Candidate {
+    LocalModel* stateToUpdate = nullptr;
+    T nextModel;
+
+    Candidate() {}
+    Candidate(double maxCost, LocalState* state, const T& x) {
+      auto next = state->model.insert(x);
+      auto nextCost = next.cost();
+
+      // Check if the fitness to the local model
+      // is good enough.
+      if (nextCost < maxCost) {
+
+        /*
+         * Here we decide how to choose the best state to
+         * update. For instance, we can choose the best fit. Or
+         * we can choose the longest segment.
+         */
+
+        selCost = nextCost;
+      }
+    }
+
+    double selCost = std::numeric_limits<double>::infinity();
+
+    bool operator<(const Candidate& other) {
+      return selCost < other.selCost;
+    }
+  };
+
+  Candidate getModelToUpdate(const T& x) {
+    Candidate best;
+    for (auto& m: _localModels) {
+      best = std::min(best,
+          Candidate(_settings.maxLocalModelCost,
+              &m, x));
+    }
+    return best;
+  }
+
   // Inserts an item and assigns it to an existing or new segment.
   int filter(const T& x) {
     _itemCounter++;
+
+    // First try to see if there is already an acceptable local state,
+    // and in that case, choose the best state by some criterion.
+    auto best = getModelToUpdate(x);
+    if (x.stateToUpdate) {
+      auto& u = *(x.stateToUpdate);
+      u.model = x.nextModel;
+      u.lastUpdate = _itemCounter;
+      u.insertedCount++;
+      return u.index;
+    }
 
     LocalState newState(_segmentCounter++, _itemCounter, x);
     if (_localModels.size() < _settings.maxLocalModelCount) {
@@ -56,6 +116,7 @@ public:
     } else {
       _localModels[oldestIndex()] = newState;
     }
+    return newState.index;
   }
 private:
   Settings _settings;
