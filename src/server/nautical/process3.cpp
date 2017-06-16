35fec31e1d1bc67248fd087350c12e7c591f3c36
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-06-16 11:00:19 +0200

    good

---
+++ src/server/nautical/process3.cpp
@@ -0,0 +1,18 @@
+/*
+ * process3.cpp
+ *
+ *  Created on: 15 Jun 2017
+ *      Author: jonas
+ */
+
+#include <string>
+
+struct ProcessSettings {
+
+};
+
+int main(int argc, const char** argv) {
+  std::string directory;
+
+  return 0;
+}
