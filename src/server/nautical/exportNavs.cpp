2eacf56197a8b91938768f166f55ecf882f4f32d
Julien Pilet <julien.pilet@gmail.com>
2017-09-05 10:14:11 +0200

    Add a few options to extend exportNavs
    So that I can produce a CSV tailored to what Alinghi wants.

--- src/server/nautical/exportNavs.cpp
+++ src/server/nautical/exportNavs.cpp
@@ -15,6 +15,7 @@
 #include <sstream>
 
 using namespace sail;
+using namespace std;
 using namespace sail::NavCompat;
 
 enum Format  {CSV, MATLAB, JSON};
@@ -26,6 +27,9 @@ struct ExportSettings {
   bool withHeader = true;
   bool verbose = false;
   bool downSampleGps = false;
+  bool preferInternalGps = false;
+  std::string preferredSource;
+  std::string timeFormat = "us";
 };
 
 NavDataset loadNavsFromArgs(Array<ArgMap::Arg*> args) {
@@ -80,7 +84,23 @@ std::string timeToLiteralHumanReadable(TimeStamp t, const ExportSettings& settin
   if (settings.format == MATLAB) { // Don't export text, only numbers.
     return timeToLiteral(t);
   }
-  return t.toString("%Y-%m-%d %T");
+  // Round to the nearest second.
+  int64_t msec = t.toMilliSecondsSince1970();
+  msec = 1000 * ((msec + 500) / 1000);
+
+  const char* timeFormat = 0;
+
+  if (settings.timeFormat == "us") {
+    timeFormat = "%m/%d/%Y %I:%M:%S %p"; // en-US
+  } else if (settings.timeFormat == "fr") {
+    timeFormat =  "%d.%m.%Y %T"; // FR/CH locale
+  } else if (settings.timeFormat == "iso") {
+    timeFormat = "%Y-%m-%dT%H:%M:%SZ";
+  } else {
+    LOG(FATAL) << "Unknown timeFormat: " << settings.timeFormat
+      << ". Available: us, fr or iso.";
+  }
+  return TimeStamp::fromMilliSecondsSince1970(msec).toString(timeFormat);
 }
 
 Array<NavField> getNavFields(const ExportSettings& format) {
@@ -90,7 +110,8 @@ Array<NavField> getNavFields(const ExportSettings& format) {
       return timeToLiteralHumanReadable(x.time(), format);
     }},
     NavField{"AWA (degrees)", [=](const Nav &x) {
-      return angleToLiteral(x.awa(), format, 180);
+      string lit = angleToLiteral(x.awa(), format, 180);
+      return lit;
     }},
     NavField{"AWS (knots)", [=](const Nav &x) {
       return velocityToLiteral(x.aws(), format);
@@ -175,15 +196,20 @@ std::string makeHeader(Array<NavField> fields, bool quoted) {
   return result;
 }
 
-std::string makeLiteralString(const Array<NavField> &fields,
-    const Nav &x, std::string sep) {
-  std::string result;
-  int last = fields.size() - 1;
+std::vector<std::string> makeLiterals(
+    const Array<NavField> &fields, const Nav &x) {
+  std::vector<std::string> result;
+  result.reserve(fields.size());
+
   for (int i = 0; i < fields.size(); i++) {
-    auto v = fields[i].getLiteral(x);
-    result += v + (i == last? "" : sep);
+    result.push_back(fields[i].getLiteral(x));
   }
   return result;
+}
+
+std::string makeLiteralString(const Array<NavField> &fields,
+    const Nav &x, std::string sep) {
+  return join(makeLiterals(fields, x), sep);
 }
 
 int exportCsv(bool withHeader, Array<NavField> fields,
@@ -191,8 +217,14 @@ int exportCsv(bool withHeader, Array<NavField> fields,
   if (withHeader) {
     *dst << makeHeader(fields, true) << "\n";
   }
+
+  std::string lastTime;
   for (auto nav: navs) {
-    *dst << makeLiteralString(fields, nav, ", ") << "\n";
+    std::vector<std::string> row{makeLiterals(fields, nav)};
+    if (row[0] != lastTime) {
+      lastTime = row[0];
+      *dst << join(row, ",") << "\n";
+    }
   }
   return 0;
 }
@@ -240,13 +272,21 @@ NavDataset performCalibration(NavDataset navs0,const ExportSettings& settings) {
 }
 
 int exportNavs(Array<ArgMap::Arg*> args, const ExportSettings& settings, std::string output) {
-  NavDataset navs = loadNavsFromArgs(args);
+  NavDataset navs = removeStrangeGpsPositions(loadNavsFromArgs(args));
 
-  navs = navs.createMergedChannels();
+  if (settings.preferredSource.size() > 0) {
+    navs.preferSourceAll(settings.preferredSource);
+  }
+
+  if (settings.preferInternalGps) {
+    navs.preferSource(
+       std::set<DataCode>{GPS_POS, GPS_SPEED, GPS_BEARING}, "Internal GPS");
+  }
 
   if (settings.downSampleGps) {
     navs = downSampleGpsTo1Hz(navs);
   }
+
 
   Array<NavField> fields = getNavFields(settings);
   if (settings.simulatedTrueWindData) {
@@ -256,6 +296,9 @@ int exportNavs(Array<ArgMap::Arg*> args, const ExportSettings& settings, std::st
   LOG(INFO) << "Navs successfully loaded, export them to "
       << output << " with format " << format;
   std::ofstream file(output);
+
+  //navs.outputSummary(&std::cout);
+  std::cout << navs << endl;
 
   auto sampled = makeArray(navs);
   LOG(INFO) << "Number of navs to export: " << sampled.size();
@@ -284,6 +327,12 @@ int main(int argc, const char **argv) {
   amap.registerOption("--no-simulate", "Skip simulated true wind columns");
   amap.registerOption("-v", "Verbose output");
   amap.registerOption("-1Hz", "Downsample gps to 1Hz");
+  amap.registerOption("-i", "Prefer internal GPS source");
+  amap.registerOption("-s", "set default preferred source")
+    .store(&settings.preferredSource);
+  amap.registerOption("-t", "set time format: us, fr or iso")
+    .store(&settings.timeFormat);
+
   amap.setHelpInfo(
       std::string("") +
       "Exports nav data to other formats. In addition to the named arguments,\n" +
@@ -301,6 +350,7 @@ int main(int argc, const char **argv) {
                            CSV : (settings.formatStr == "json"? JSON : MATLAB));
         settings.withHeader = !amap.optionProvided("--no-header");
         settings.verbose = amap.optionProvided("-v");
+	settings.preferInternalGps = amap.optionProvided("-i");
         settings.simulatedTrueWindData = !amap.optionProvided("--no-simulate");
         settings.downSampleGps = amap.optionProvided("-1Hz");
         return exportNavs(amap.freeArgs(), settings, output);
