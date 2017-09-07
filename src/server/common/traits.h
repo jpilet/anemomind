1e5843511e53af75c93d1ccf3bc310566e1d649e
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-09-07 17:52:36 +0200

    Improve type traits

--- src/server/common/traits.h
+++ src/server/common/traits.h
@@ -122,20 +122,121 @@ struct IsStringMap {
       && std::is_same<std::string, KeyTypeOf<T>>::value;
 };
 
+template <typename Iterator, typename = void>
+struct IteratorInputType {
+  typedef typename std::iterator_traits<Iterator>::value_type type;
+};
+
+template <typename Iterator>
+struct IteratorInputType<
+  Iterator, typename void_<
+    typename Iterator::container_type>::type> {
+  typedef typename Iterator::container_type C;
+  typedef typename C::value_type type;
+};
+
+template <typename ... T> struct TypeList {};
+
+
+
+// Based on https://stackoverflow.com/a/7943765
+template <typename T>
+struct CallableTraits : public CallableTraits<decltype(&T::operator())> {};
+
+template <typename ClassType, typename ReturnType, typename... Args>
+struct CallableTraits<ReturnType(ClassType::*)(Args...) const> {
+  typedef ReturnType result_type;
+  typedef TypeList<Args...> arg_types;
+};
+
+template <typename T>
+struct FunctionTraits : public CallableTraits<T> {};
+
 template <typename F>
-struct FunctionTraits;
+using FunctionResultType = typename FunctionTraits<F>::result_type;
 
-template <typename Y, typename X>
-struct FunctionTraits<std::function<Y(X)>> {
-  typedef X input_type;
-  typedef Y output_type;
+template <typename F>
+using FunctionArgTypes = typename FunctionTraits<F>::arg_types;
+
+template <typename Y, typename ... T>
+struct FunctionTraits<Y(*)(T...)> {
+  typedef Y result_type;
+  typedef TypeList<T...> arg_types;
 };
 
-template <typename Y, typename X>
-struct FunctionTraits<Y (*)(X)> {
-  typedef X input_type;
-  typedef Y output_type;
+template <typename T> struct TypeListOps;
+template <typename X, typename ... Y> struct TypeListOps<TypeList<X, Y...>> {
+  typedef X first;
+  typedef TypeList<Y...> rest;
 };
+
+template <typename X, typename Y> struct Cons {};
+template <typename X, typename ... T>
+struct Cons<X, TypeList<T...>> {
+  typedef TypeList<X, T...> type;
+};
+
+template <typename T>
+using FirstType = typename TypeListOps<T>::first;
+
+template <typename T>
+using RestTypes = typename TypeListOps<T>::rest;
+
+template <typename T>
+using SecondType = FirstType<RestTypes<T>>;
+
+template <typename T> struct TypeListSummary {};
+
+template <typename X, typename ... Y>
+struct TypeListSummary<TypeList<X, Y...>> {
+  static const int size = 1 + TypeListSummary<TypeList<Y...>>::size;
+};
+
+template <>
+struct TypeListSummary<TypeList<>> {
+  static const int size = 0;
+};
+
+template <typename F>
+struct Arity {
+  static const int value = TypeListSummary<FunctionArgTypes<F>>::size;
+};
+
+template <typename F, typename T>
+struct MapTypes {};
+
+template <typename F, typename X>
+struct TypeMapper {};
+
+template <typename OpKey, typename X, typename ... Y>
+struct MapTypes<OpKey, TypeList<X, Y...>> {
+  typedef typename Cons<
+      typename TypeMapper<OpKey, X>::type,
+      typename MapTypes<OpKey, TypeList<Y...>>::type>::type type;
+};
+
+template <typename F>
+struct MapTypes<F, TypeList<>> {
+  typedef TypeList<> type;
+};
+
+struct CleanTypeOpKey {};
+template <typename X>
+struct TypeMapper<CleanTypeOpKey, X> {
+  typedef typename std::remove_const<
+      typename std::remove_reference<X>::type>::type type;
+};
+
+template <typename T>
+using CleanTypeList = typename MapTypes<
+    CleanTypeOpKey, T>::type;
+
+template <typename T> struct The_value_is;
+
+template <typename T>
+using Disp = The_value_is<T>;
+
+//#define AUTO_BODY(arglist, body) -> decltype(([](arglist) body )()) body
 
 }
 
