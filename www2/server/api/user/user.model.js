6171207ea1e5a97f42b03c6202bf80cbd716c74d
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-09-15 16:09:55 +0200

    FIXUP make unique:true for user

--- www2/server/api/user/user.model.js
+++ www2/server/api/user/user.model.js
@@ -7,8 +7,7 @@ var authTypes = ['github', 'twitter', 'facebook', 'google'];
 
 var UserSchema = new Schema({
   name: String,
-  email: { type: String, lowercase: true /* <-- TODO: Would it be 
-                      useful to have 'unique: true' here?*/},
+  email: { type: String, lowercase: true, unique: true},
   role: {
     type: String,
     default: 'user'
