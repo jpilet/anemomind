d2ce9936e6cba8ecdc8a3f82bba1d02ccbe8a5de
jonasseglare <uppfinnarjonas@gmail.com>
2017-09-15 16:12:00 +0200

    Fix failing unit test on server (#1133)
    * Fix failing unit test on server
    
    * FIXUP make unique:true for user

--- www2/server/api/user/user.model.js
+++ www2/server/api/user/user.model.js
@@ -7,7 +7,7 @@ var authTypes = ['github', 'twitter', 'facebook', 'google'];
 
 var UserSchema = new Schema({
   name: String,
-  email: { type: String, lowercase: true },
+  email: { type: String, lowercase: true, unique: true},
   role: {
     type: String,
     default: 'user'
