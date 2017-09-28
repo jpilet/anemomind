4144de7b02e958947cdac1ded29ca54f2c795591
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-09-28 13:47:11 +0200

    Test SessionRenderer

--- www2/test/SessionRenderer_test.js
+++ www2/test/SessionRenderer_test.js
@@ -4,6 +4,8 @@ var assert = require('assert');
 var _ = require('lodash');
 var SessionOps = require('../client/app/SessionOps.js');
 var anemoutils = require('../client/app/anemoutils.js');
+
+// Not sure how to require JS code that has not been wrapped in a module.
 eval(fs.readFileSync('../client/app/boatList/SessionRenderer.js', 'utf8'));
 
 var sessions = require('./raw_sessions.json');
