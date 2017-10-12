3296dbf98efa140b66f7316e9b3cd22de503817b
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-10-12 12:25:58 +0200

    good

---
+++ src/server/common/BundleTransducer.h
@@ -0,0 +1,22 @@
+/*
+ * BundleTransducer.h
+ *
+ *  Created on: 12 Oct 2017
+ *      Author: jonas
+ */
+
+#ifndef SERVER_COMMON_BUNDLETRANSDUCER_H_
+#define SERVER_COMMON_BUNDLETRANSDUCER_H_
+
+#include <server/common/Transducer.h>
+#include <server/common/Array.h>
+
+namespace sail {
+
+
+
+}
+
+
+
+#endif /* SERVER_COMMON_BUNDLETRANSDUCER_H_ */
