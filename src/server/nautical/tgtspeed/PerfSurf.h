d62f283adac3a0de5f5a21423df8b95bdf80f9d0
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-11-17 16:12:14 +0100

    pretty good

--- src/server/nautical/tgtspeed/PerfSurf.h
+++ src/server/nautical/tgtspeed/PerfSurf.h
@@ -53,7 +53,7 @@ struct PerfSurfSettings {
    * requiring more iterations of the algorithm.
    *
    */
-  Velocity<double> sigma = 0.1_kn;
+  Velocity<double> sigma = 0.02_kn;
 
   /**
    * A weight for every point will make the surface
