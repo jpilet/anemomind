efbb5819eca90a20050c8078fccf8deef177a004
jonasseglare <uppfinnarjonas@gmail.com>
2018-01-19 11:04:24 +0100

    Add a FastPacketSplitter class that does the opposite of what FastPac… (#1181)
    * Add a FastPacketSplitter class that does the opposite of what FastPacketBuffer does
    
    * Improve unit test regarding seq counters
    
    * split a fastpacket
    
    * Don't return CanPacket by value
    
    * Fixed packet splitting calculation

--- src/device/anemobox/n2k/FastPacket.cpp
+++ src/device/anemobox/n2k/FastPacket.cpp
@@ -1,6 +1,7 @@
 #include <device/anemobox/n2k/FastPacket.h>
-
 #include <algorithm>
+#include <server/common/math.h>
+#include <server/common/logging.h>
 
 namespace PgnClasses {
 
@@ -15,6 +16,7 @@ const int SEQUENCE_COUNTER_NUM_BITS = 3;
 // b0 – b4 = 1 to 31, 5 bit frame counter,  b0  =LSB 
 // b5  –  b7  = 3 -bit sequence counter which shall be the same value as in the
 // first frame, b5 is the LSB of the counter. 
+//Byte 2: (only for the first frame: Expected length in bytes of the entire payload)
 FastPacketBuffer::ReceptionKey FastPacketBuffer::keyForPacket(
     const CanPacket& packet) {
   return
@@ -52,8 +54,15 @@ void FastPacketBuffer::add(const CanPacket& packet) {
       reception->second.expectedLength = packet.data[1];
       reception->second.packet.data.resize(reception->second.expectedLength);
     }
+
+    // This is where, in the destination buffer, the data
+    // should be inserted.
     int dstOffset = dataDstOffset(packet);
+
+    // This is where, in the small incoming source buffer,
+    // we should start reading the data
     int srcOffset = dataSrcOffset(packet);
+
     int newLength = dstOffset + packet.data.size() - srcOffset;
     if (reception->second.expectedLength) {
       newLength = std::min(int(reception->second.expectedLength), newLength);
@@ -76,6 +85,78 @@ void FastPacketBuffer::add(const CanPacket& packet) {
       _buffer.erase(reception);
     }
   }
+}
+
+namespace {
+  uint8_t encodeFirstByte(uint8_t seqCounter, uint8_t frameCounter) {
+    return // Is this bit fiddling OK?
+        (seqCounter << SEQUENCE_COUNTER_SHIFT)
+        | (frameCounter & FRAME_COUNTER_MASK);
+  }
+
+  struct FastPacketSplitState {
+    FastPacketSplitState(int s) : seqCounter(s) {}
+
+    uint8_t seqCounter = 0;
+    uint8_t frameCounter = 0;
+    int bytesSent = 0;
+
+    void initializeNewPacket(const CanPacket& src, CanPacket* dst) {
+      dst->pgn = src.pgn;
+      dst->longSrc = src.longSrc;
+      dst->shortSrc = src.shortSrc;
+      dst->data.assign(8, 0xFF);
+      dst->data[0] = encodeFirstByte(seqCounter, frameCounter);
+      frameCounter++;
+    }
+
+    int computePayloadSize(int maxPayload, const CanPacket& src) const {
+      return std::min(int(maxPayload), int(src.data.size() - bytesSent));
+    }
+
+    void sendBytes(
+        int count,
+        int dstOffset,
+        const CanPacket& src, CanPacket* dst) {
+      auto srcBegin = src.data.begin() + bytesSent;
+      auto srcEnd = srcBegin + count;
+      auto dstBegin = dst->data.begin() + dstOffset;
+      std::copy(srcBegin, srcEnd, dstBegin);
+      bytesSent += count;
+    }
+
+    void makeFirstPacket(const CanPacket& src, CanPacket* dst) {
+      initializeNewPacket(src, dst);
+      int n = computePayloadSize(6, src);
+      dst->data[1] = src.data.size(); // Expected bytes (only first message)
+      sendBytes(n, 2, src, dst);
+    }
+
+    void makeRemainingPacket(const CanPacket& src, CanPacket* dst) {
+      initializeNewPacket(src, dst);
+      int n = computePayloadSize(7, src);
+      sendBytes(n, 1, src, dst);
+    }
+
+    bool remainsDataToBeSent(const CanPacket& src) const {
+      return bytesSent < src.data.size();
+    }
+  };
+}
+
+std::vector<CanPacket> FastPacketSplitter::split(const CanPacket& src) {
+  FastPacketSplitState state(_pgn2seqCounter[src.pgn].next());
+  std::vector<CanPacket> dst;
+  int packetCount = sail::div1(src.data.size()+1, 7);
+  dst.resize(packetCount);
+  state.makeFirstPacket(src, &(dst[0]));
+  using namespace sail;
+  for (int i = 1; i < packetCount; i++) {
+    CHECK(state.remainsDataToBeSent(src));
+    state.makeRemainingPacket(src,&(dst[i]));
+  }
+  CHECK(!state.remainsDataToBeSent(src));
+  return dst;
 }
 
 }  // namespace PgnClasses
--- src/device/anemobox/n2k/FastPacket.h
+++ src/device/anemobox/n2k/FastPacket.h
@@ -11,7 +11,7 @@ class FastPacketBuffer {
   public:
 
     void add(const CanPacket& packet);
-
+    virtual ~FastPacketBuffer() {}
   protected:
     virtual void fullPacketReceived(const CanPacket& fullPacket) = 0;
 
@@ -46,6 +46,29 @@ class FastPacketBuffer {
     static int dataDstOffset(const CanPacket& packet);
 
     std::map<ReceptionKey, ReceptionBuffer> _buffer;
+};
+
+/* Performs the opposite of the FastPacketBuffer class: Given a
+   big packet, a so called "Fast packet", split it up
+   into smaller packets.
+
+   Note: Not all packets are "Fast packets".
+   This is handled by the PgnClasses file. In particular, see
+   the method PgnVisitor::pushAndLinkPacket. Given an incoming packet,
+   that method determines whether it should be linked into a fast packet
+   or if it is a single packet. TODO: We need to write the opposite of
+   that in the code generator.
+   */
+class FastPacketSplitter {
+public:
+  std::vector<CanPacket> split(const CanPacket& src);
+private:
+  struct FastPacketSeqCounter {
+    uint8_t value = 0;
+    uint8_t next() {value = (value + 1) % 8; return value;}
+  };
+
+  std::map<int, FastPacketSeqCounter> _pgn2seqCounter;
 };
 
 }  // namespace PgnClasses
