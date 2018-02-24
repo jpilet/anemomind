b93e6888eb086563a2eb0c1f3508319738ca2a9c
jonasseglare <5850088+jonasseglare@users.noreply.github.com>
2018-02-24 14:29:48 +0100

    Fixes to boxid (#1240)

--- www2/synctest/get_devbox_boatid.sh
+++ www2/synctest/get_devbox_boatid.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 boxid=$(cat boxid.txt)
-echo "Looking up boat for box '${boxid}'"
-mongo --quiet anemomind-dev --eval 'db.boats.findOne({anemobox: "${boxid}"})["_id"].valueOf()'
+mongo --quiet anemomind-dev --eval "db.boats.findOne({anemobox: '${boxid}'})._id.valueOf()"
