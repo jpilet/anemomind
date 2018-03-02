56b0bee791e1b31db9b6daa88245f425f0006a42
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-03-02 15:19:59 +0100

    wip

--- src/server/nautical/tgtspeed/RealPerfSurf.h
+++ src/server/nautical/tgtspeed/RealPerfSurf.h
@@ -13,43 +13,58 @@
 #define SERVER_NAUTICAL_TGTSPEED_REALPERFSURF_H_
 
 #include <server/common/Transducer.h>
+#include <server/common/TimedValue.h>
 
 namespace sail {
 
-/*template <typename Iterator, typename Step>
-class TimedValueMerge : public Step,
-  public Transducer<TimedValueMerge<Iterator, Step>> {
+// This is a stateful transducer,
+// but there is no state to flush.
+template <typename Iterator, typename Step>
+class TimedValuePairs : public Step,
+  public Transducer<TimedValuePairs<Iterator, Step>> {
 public:
-  TimedValueMerge(Iterator begin, Iterator end,
-      const Step& s = Step()) : _begin(begin), _end(end), Step(s) {}
+  // Obs: butEnd is a *valid iterator*. We can dereference it.
+  // It is the iterator pointing just before the end of the range.
+  TimedValuePairs(Iterator begin, Iterator butEnd,
+      const Step& s = Step()) : _begin(begin), _butEnd(butEnd), Step(s) {}
 
   typedef typename Step::input_type input_type;
   typedef typename Step::result_type result_type;
 
   // Use it as a step function
   result_type step(result_type y, input_type x) {
-    if (_begin == _end) {
+    if (_begin >= _butEnd || x.time < _begin->time) {
       return y;
-    } else {
-
     }
-    return _f(x)? Step::step(y, x) : y;
+
+
+    // Advance
+    while (_begin < _butEnd && (_begin+1)->time <= x.time) {
+      _begin++;
+    }
+    if (_begin == _butEnd) {
+      return y;
+    }
+
+    return Step::step(Step::step(y, {*_begin, x}), {*_butEnd, x});
   }
 
   // Use it as a transducer
   template <typename S>
-  TimedValueMerge<Iterator, S> apply(S s) const {
-    return TimedValueMerge<Iterator, S>(_begin, _end, s);
+  TimedValuePairs<Iterator, S> apply(S s) const {
+    return TimedValuePairs<Iterator, S>(_begin, _butEnd, s);
   }
 private:
-  Iterator _begin, _end;
+  Iterator _begin, _butEnd;
 };
 
 template <typename Iterator>
-TimedValueMerge<Iterator, UndefinedStep> trTimedValueMerge(
-    Iterator b, Iterator e) {
-  return TimedValueMerge<Iterator, UndefinedStep>(b, e);
-}*/
+TimedValuePairs<Iterator, UndefinedStep> trTimedValuePairs(
+    Iterator b, Iterator e0) {
+  auto butEnd = e0;
+  butEnd--;
+  return TimedValuePairs<Iterator, UndefinedStep>(b, butEnd);
+}
 
 
 } /* namespace sail */
--- src/server/nautical/tgtspeed/RealPerfSurfTest.cpp
+++ src/server/nautical/tgtspeed/RealPerfSurfTest.cpp
@@ -5,6 +5,30 @@
  *      Author: jonas
  */
 
+#include <gtest/gtest.h>
+#include <server/nautical/tgtspeed/RealPerfSurf.h>
+
+using namespace sail;
+
+TimedValue<int> tv(double t, int value) {
+  return TimedValue<int>(
+      TimeStamp::UTC(2018, 3, 2, 15, 11, 0) + t*1.0_s, value);
+}
+
+TEST(RealPerfSurfTest, TimedValuePairs) {
+  std::vector<TimedValue<int>> A{
+    tv(0.0, 1),
+    tv(2.0, 3)
+  };
+
+  std::vector<TimedValue<int>> B{
+    tv(1.0, 119),
+    tv(4.0, 120)
+  };
+
+  std::vector<std::pair<TimedValue<int>, TimedValue<int>>> dst;
+  transduceIntoColl(trTimedValuePairs(A.begin(), A.end()), &dst, B);
+}
 
 
 
