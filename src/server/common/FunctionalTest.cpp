edc925b2083ab4cf379e6dc926acbaa5cf9a0868
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-03-16 11:56:16 +0100

    Update FunctionalTest

--- src/server/common/FunctionalTest.cpp
+++ src/server/common/FunctionalTest.cpp
@@ -10,3 +10,12 @@
 #include <server/common/string.h>
 
 using namespace sail;
+
+TEST(FunctionalTest, TestComplement) {
+  auto isOdd = [](int i) {return i % 2 == 1;};
+
+  EXPECT_TRUE(isOdd(119));
+  auto isEven = complementFunction(isOdd);
+
+  EXPECT_FALSE(isEven(119));
+}
