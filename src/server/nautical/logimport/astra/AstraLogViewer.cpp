0add0b6b4666923c9f8d93ff292e58bff0be450b
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-04-06 12:59:19 +0200

    good

---
+++ src/server/nautical/logimport/astra/AstraLogViewer.cpp
@@ -0,0 +1,15 @@
+/*
+ * AstraLogViewer.cpp
+ *
+ *  Created on: 6 Apr 2018
+ *      Author: jonas
+ *
+ * Display the contents of an Astra log file in an easy-to-understand fashion,
+ * mainly for debugging.
+ */
+
+int main() {
+  return 0;
+}
+
+
