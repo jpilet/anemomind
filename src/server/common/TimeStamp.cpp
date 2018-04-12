776d4140799d69ca300602b54e984ac726772312
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-04-12 17:02:43 +0200

    Undo unnecessary changes to TimeStamp

--- src/server/common/TimeStamp.cpp
+++ src/server/common/TimeStamp.cpp
@@ -136,6 +136,12 @@ TimeStamp TimeStamp::parse(const std::string &x0) {
   // TODO: Rewrite this parsing, so that we can also
   // parse fractions of seconds.
   auto x = removeFractionalParts(x0);
+
+  TRY_PARSE_TIME("%D", x);
+  TRY_PARSE_TIME("%FT%TZ", x);
+  TRY_PARSE_TIME("%F", x);
+  TRY_PARSE_TIME("%D %T", x);
+  TRY_PARSE_TIME("%m/%d/%Y %r", x);
   LOG(WARNING) << "Failed to parse time: " << x0;
   return TimeStamp();
 }
