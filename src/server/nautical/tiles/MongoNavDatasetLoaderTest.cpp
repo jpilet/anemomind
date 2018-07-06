aee654472397d6cb60b571f6bf9f131120fcebdf
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-07-06 15:10:18 +0200

    Improve the unit test

--- src/server/nautical/tiles/MongoNavDatasetLoaderTest.cpp
+++ src/server/nautical/tiles/MongoNavDatasetLoaderTest.cpp
@@ -24,14 +24,54 @@ TEST(MongoNavDatasetLoader, TestIt) {
   }
   LOG(INFO) << "Successfully connected";
 
-  // Boat id of Jaisalmer
-  std::string boatId = "552b806a35ce7cb254dc9515";
+  std::string boatId = "k4ttsk1t";
+  {
+    auto collection = UNIQUE_MONGO_PTR(
+          mongoc_collection,
+          mongoc_database_get_collection(
+              db.db.get(),
+              "events"));
+    if (!collection) {
+      LOG(WARNING) << "Failed to obtain events";
+      return;
+    }
 
-  auto dataset = loadEvents(db, boatId);
-  for (auto s: dataset.samples<USER_DEF_SESSION>()) {
-    LOG(INFO) << "  Session edge at " << s.time.toIso8601String()
-        << " of type " << (s.value == BinaryEdge::ToOn? "ToOn" : "ToOff");
+    { // Remove existing docs.
+      bson_error_t removeError;
+      WrapBson toRemove;
+      bsonAppendAsOid(&toRemove, "boat", boatId);
+      if (!mongoc_collection_remove (
+          collection.get(),
+          MONGOC_REMOVE_NONE,
+          &toRemove,
+          nullptr,
+          &removeError)) {
+        LOG(WARNING) << "Failed to remove because: " << removeError.message;
+      }
+    }
+    for (int i = 0; i < 9; i++) {
+      bson_error_t error;
+      WrapBson toInsert;
+      bsonAppendAsOid(&toInsert, "boat", boatId);
+      bsonAppend(&toInsert, "when", TimeStamp::UTC(2018, 7, 5, 18, 3, 4 + i));
+      bsonAppend(&toInsert, "structuredMessage", (i % 2 == 0)?
+          "New session" : "End session");
+      EXPECT_TRUE(mongoc_collection_insert (
+          collection.get(),
+          MONGOC_INSERT_NONE,
+          &toInsert, nullptr, &error));
+    }
   }
+
+  //boatId = "552b806a35ce7cb254dc9515";
+  auto dataset = loadEvents(db, boatId);
+  int toOnCount = 0;
+  int toOffCount = 0;
+  for (auto s: dataset.samples<USER_DEF_SESSION>()) {
+    (*(s.value == BinaryEdge::ToOn? &toOnCount : &toOffCount))++;
+  }
+  EXPECT_EQ(toOnCount, 5);
+  EXPECT_EQ(toOffCount, 4);
 }
 
 
