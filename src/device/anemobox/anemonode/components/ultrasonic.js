c21258a3e15ec6154d093560a56ecd1932d0a5df
Julien Pilet <julien.pilet@gmail.com>
2018-07-06 09:58:28 +0200

    Add config option to start/stop calypso ultrasonic support (#1306)

--- src/device/anemobox/anemonode/components/ultrasonic.js
+++ src/device/anemobox/anemonode/components/ultrasonic.js
@@ -1,17 +1,37 @@
 var calypso = require('calypso_ultrasonic');
 var anemonode = require('../build/Release/anemonode');
 
+var connectedPeripheral = undefined;
+var supportEnabled = false;
+
 function Watchdog(cb) {
-  this.cb = cb;
+  var t = this;
+  this.cb = function() { t.timeout = undefined; cb(); };
   this.timeout = undefined;
 }
 
-Watchdog.prototype.next = function(t) {
+Watchdog.prototype.cancel = function() {
   if (this.timeout) {
     clearTimeout(this.timeout);
+    this.timeout = undefined;
   }
+}
+
+Watchdog.prototype.next = function(t) {
+  this.cancel();
   this.timeout = setTimeout(this.cb, t);
 };
+
+// In case of timeout, we close the connection and start scanning again.
+var watchdog = new Watchdog(function() {
+  if (connectedPeripheral) {
+    console.warn('No more data from calypso ' + connectedPeripheral.address
+                 + ', reconnecting');
+    connectedPeripheral.disconnect();
+    connectedPeripheral = undefined;
+  }
+  startScanning();
+});
 
 function peripheralDiscovered(err, peripheral) {
   if (err) {
@@ -22,29 +42,34 @@ function peripheralDiscovered(err, peripheral) {
 
   calypso.stopScanning();
 
-  // In case of timeout, we close the connection and start scanning again.
-  var watchdog = new Watchdog(function() {
-    console.warn('No more data from calypso ' + peripheral.address + ', reconnecting');
-    peripheral.disconnect();
-    startScanning();
-  });
+  connectedPeripheral = peripheral;
 
   // we will try to connect during 1 minute
   watchdog.next(60 * 1000);
 
   calypso.connect(peripheral, function(err) {
     if (err) {
+      console.warn('Error connecting to Calypso Ultrasonic');
+      watchdog.cancel();
+      connectedPeripheral = undefined;
       startScanning();
       return;
     }
+
+    console.log('Connected to Calypso Ultrasonic ' + peripheral.address);
+
     var noDataTimeout;
     peripheral.listenData(function (err, sensorData) {
       if (err) {
+        watchdog.cancel();
         console.warn('Device error, trying to reconnect: ', err);
+        connectedPeripheral = undefined;
+        peripheral.disconnect();
         calypso.scan(peripheralDiscovered);
         return;
       }
       var source = 'Calypso ' + peripheral.address;
+
       var values = anemonode.dispatcher.values;
       values.awa.setValue(source, sensorData.awa);
       values.aws.setValue(source, sensorData.aws);
@@ -58,11 +83,35 @@ function peripheralDiscovered(err, peripheral) {
     });
   });
 }
-
 calypso.setDiscoveredCallback(peripheralDiscovered);
 
 function startScanning() {
-  calypso.scan(function(err) { console.warn(err); });
+  if (supportEnabled) {
+    calypso.scan(function(err) { console.warn(err); });
+  }
 }
 
-module.exports.startScanning = startScanning;
+function start() {
+  if (!supportEnabled) {
+    console.log('Starting up Calypso Ultrasonic support');
+  }
+  supportEnabled = true;
+  startScanning();
+}
+
+function shutdown() {
+  if (supportEnabled) {
+    supportEnabled = false;
+
+    console.log('Shutting down Calypso Ultrasonic support');
+    watchdog.cancel();
+    if (connectedPeripheral) {
+      connectedPeripheral.disconnect();
+      connectedPeripheral = undefined;
+    }
+    calypso.stopScanning();
+  }
+}
+
+module.exports.start = start;
+module.exports.shutdown = shutdown;
