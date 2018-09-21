65ee6827dca07ef2c1520268829bb86ce7af2fd0
Julien Pilet <julien.pilet@gmail.com>
2018-09-21 11:15:33 +0200

    Add AbsoluteOrientation.str()

--- src/server/nautical/AbsoluteOrientation.h
+++ src/server/nautical/AbsoluteOrientation.h
@@ -14,6 +14,13 @@ struct AbsoluteOrientation {
     return heading == other.heading &&
         roll == other.roll && pitch == other.pitch;
   }
+
+#ifdef ON_SERVER
+  std::string str() const {
+    return "{ h:" +  heading.str() + ", r:", roll.str()
+      + ", p:" + pitch.str() + " }";
+  }
+#endif
 };
 
 bool isFinite(const AbsoluteOrientation &x);
