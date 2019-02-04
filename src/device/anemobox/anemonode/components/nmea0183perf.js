5209362b166a9b069429b7ae194234664156505c
Julien Pilet <julien.pilet@gmail.com>
2019-02-04 22:52:35 +0100

    Output VMG perf% in NKE proprietary format
    this is 100% finished yet: I have to add a config entry
    to choose the format. Will be NKE or TackTick.

---
+++ src/device/anemobox/anemonode/components/nmea0183perf.js
@@ -0,0 +1,136 @@
+var anemonode = require('../build/Release/anemonode');
+
+var minResendTime = 490; // ms
+var maxVmgTargetAgeMs = 5000;
+
+var subscriptions = undefined;
+var callback = function() { };
+var outputFormat = undefined;
+var updateInterval;
+
+var lastSentTime;
+var lastEmptySent;
+
+function getPerf() {
+  var now = anemonode.currentTime();
+
+  if (lastSentTime != undefined
+      && (now - lastSentTime) < minResendTime) {
+    return;
+  }
+  
+  var vmgDispData = anemonode.dispatcher.values.vmg;
+  if (!vmgDispData) {
+    return;
+  }
+  var targetVmgDispData = anemonode.dispatcher.values.targetVmg;
+  if (!targetVmgDispData) {
+    return;
+  }
+
+  if ((lastSentTime != undefined
+       && vmgDispData.time() < lastSentTime) // Nothing new to send?
+      || targetVmgDispData.time() < (now - maxVmgTargetAgeMs)) {  // too old
+    return;
+  }
+
+  var targetVmg = targetVmgDispData.value();
+
+  if (targetVmg > 0) {
+    lastSentTime = now;
+    return {
+      'vmg': vmgDispData.value(),
+      'targetVmg': targetVmg
+    };
+  }
+}
+
+function nmeaPacket(data) {
+  var sum = 0;
+  for (var i = 0; i < data.length; ++i) {
+    sum ^= data.charCodeAt(i);
+  }
+  var hexsum = sum.toString(16);
+  if (hexsum.length == 1) {
+    hexsum = "0" + hexsum;
+  }
+
+  return "$" + data + "*" + hexsum + "\r\n";
+}
+
+/* NKE specific code */
+function nkeDynamic(data2CharCode, channelNumber, format, value, dataLabel, unitLabel) {
+  return nmeaPacket(
+      [
+      'PNKEA',
+      data2CharCode,
+      channelNumber,
+      format,
+      value,
+      dataLabel,
+      unitLabel].join(','));
+}
+
+function nkePerfSentences(perf, targetVmg) {
+  return (
+      nkeDynamic(
+         'PV', 1, 4 /* XXX_X 0 to 999.9 number*/,
+         Math.round(perf * 10),
+         'VMG Perf', 'Prcent')
+      + nkeDynamic(
+          'TV', 2, 0, /* xx_xx 0.00 to 99.99 number */
+          Math.round(targetVmg * 100),
+          'VMG Target', 'Nds'));
+}
+
+function update() {
+  var perf = getPerf();
+  var nmea;
+  if (outputFormat == 'NKE') {
+    if (perf) {
+      nmea = nkePerfSentences(perf.vmg / perf.targetVmg, perf.targetVmg)
+    } else {
+      var now = anemonode.currentTime().getTime();
+      if (lastEmptySent == undefined || ((now - lastEmptySent) > 990)) {
+        nmea = nkePerfSentences(0, 0);
+        lastEmptySent = now;
+      }
+    }
+    if (nmea) {
+      callback(nmea);
+    }
+  }
+}
+
+function activateNmea0183PerfOutput(nmea0183cb, format) {
+  if (format != 'NKE') {
+    nmea0183cb = undefined;
+  }
+
+  if (nmea0183cb) {
+    callback = nmea0183cb;
+    outputFormat = format;
+    if (subscriptions == undefined) {
+      subscriptions = {
+        vmg: anemonode.dispatcher.values.vmg.subscribe(update),
+        targetVmg: anemonode.dispatcher.values.targetVmg.subscribe(update),
+      };
+    }
+    if (updateInterval == undefined) {
+      updateInterval = setInterval(update, 1000);
+    }
+  } else {
+    callback = function() {};
+    outputFormat = undefined;
+    if (subscriptions != undefined) {
+      anemonode.dispatcher.values.vmg.unsubscribe(subscriptions.vmg);
+      anemonode.dispatcher.values.targetVmg.unsubscribe(subscriptions.targetVmg);
+    }
+    if (updateInterval != undefined) {
+      clearInterval(updateInterval);
+      updateInterval = undefined;
+    }
+  }
+}
+
+module.exports.activateNmea0183PerfOutput = activateNmea0183PerfOutput;
--- src/device/anemobox/anemonode/test_rs422.js
+++ src/device/anemobox/anemonode/test_rs422.js
@@ -41,15 +41,63 @@ function init(nmea0183PortPath) {
 
 function emitNmea0183Sentence (sentence) {
   if (nmea0183Port) {
-    console.log('[sending: ' + sentence + ']');
+    console.log('  sending: ' + sentence);
     nmea0183Port.write(sentence);
   }
 }
 
 init('/dev/ttyMFD1');
 
+var sentences = '\n$IIRMB,A,0.11,R,,YVOI,,,,,015.92,242,02.4,V,A*73\n$IIRMC,164510,A,4629.736,N,00639.790,E,03.6,197,200808,,,A*4F\n$IIVHW,,,195,M,03.4,N,,*6E\n$IIVWR,028,L,07.7,N,,,,*75 \n$IIDPT,062.0,-1.0,*44\n$IIGLL,4629.736,N,00639.790,E,164510,A,A*5E\n$IIHDG,195,,,,*5A\n$IIMTW,+21.0,C*3B\n$IIMWV,332,R,07.7,N,A*11\n$IIMWV,312,T,04.8,N,A*19';
+
+sentences = '$PNKEA,PV,1,4,110,VMG Perf,Pourcent,*1b\r\n'
+//+ '$PNKEA,TV,2,0,0,VMG Target,Nds*75\n'
+;
+
+function nmeaPacket(data) {
+  var sum = 0;
+  for (var i = 0; i < data.length; ++i) {
+    sum ^= data.charCodeAt(i);
+  }
+  var hexsum = sum.toString(16);
+  if (hexsum.length == 1) {
+    hexsum = "0" + hexsum;
+  }
+
+  return "$" + data + "*" + hexsum + "\r\n";
+}
+
+function nkeDynamic(data2CharCode, channelNumber, format, value, dataLabel, unitLabel) {
+  return nmeaPacket(
+      [
+      'PNKEA',
+      data2CharCode,
+      channelNumber,
+      format,
+      value,
+      dataLabel,
+      unitLabel].join(','));
+}
+
+var start = new Date().getTime();
 var counter = 0;
 setInterval(function() {
-  emitNmea0183Sentence('Hello ' + ++counter);
-  }, 1000);
+  //emitNmea0183Sentence('Hello ' + ++counter);
+  //emitNmea0183Sentence(sentences);
+  var dt = (new Date()).getTime() - start;
+  
+  var perf = (100 + Math.sin(dt / 9) * 30);
+  var targetVmg = (5 + Math.sin(7 + dt / 9) * 5);
+
+  //perf = (Math.round(dt / 1000) % 2) == 1 ? 0 : 100;
+  perf = 0;
+  targetVmg = 0;
+  emitNmea0183Sentence(nkeDynamic('PV', 1, 4,
+                                  Math.round(10 * perf),
+                                  'VMG Perf', '%'));
+  emitNmea0183Sentence(nkeDynamic('TV', 2, 0,
+                                  Math.round(100 * targetVmg),
+                                  'VMG Target', 'Nds'));
+                                  
+  }, 500);
 
