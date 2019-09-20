3438f01ed45dfa2009d644f41678a601c672a2e5
Julien Pilet <julien.pilet@gmail.com>
2019-09-20 09:48:54 +0200

    Improve build scripts (details)

--- src/server/build.sh
+++ src/server/build.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$(dirname "$( which "$0" )")
 SOURCE_DIR="$( cd ${SCRIPT_DIR}/../../ && pwd )"
 BUILD_DIR=${1:pwd}
 cd "${BUILD_DIR}"
-cmake "${SOURCE_DIR}" -DCMAKE_BUILD_TYPE=RelWidthDebInfo && \
+cmake "${SOURCE_DIR}" -DCMAKE_BUILD_TYPE=RelWidthDebInfo -DWITH_SAILROOT=OFF && \
 make -j1 nautical_processBoatLogs logimport_summary anemobox_logcat logimport_try_load nautical_catTargetSpeed
