4f692b731f8d2266452f98c1c698ce1246bbd124
wasimkhan-cognologix <wasim.khan@cognologix.com>
2019-12-17 15:50:51 +0530

    changes have been made to run data flow with gke in VPC network of GCP.

--- experimental/dataflow-pipeline/processBoatLogs.sh
+++ experimental/dataflow-pipeline/processBoatLogs.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+# Load up .env
+export $(egrep -v '^#' .env | xargs)
+echo "Fetching mongo db's ip for dataflow."
+>temp.txt
+for i in mongod-0 mongod-1 mongod-2
+ do
+ a=`kubectl describe services $i | grep "10.0.*" | cut -d ":" -f2`
+ if [ $i != "mongod-2" ]; then
+ echo -n "$a:27017,">>temp.txt
+ else
+ echo -n "$a:27017">>temp.txt
+ fi
+ done
+mongo_ips=`cat temp.txt | tr -d ' '`
+rm -f temp.txt
+
 export GOOGLE_APPLICATION_CREDENTIALS="../../www2/anemomind-9b757e3fbacb.json"
 mvn compile exec:java -Dexec.mainClass=com.anemomind.ProcessNewLogs \
-     -Dexec.args="--runner=DataflowRunner --project=anemomind --region=europe-west1 \
+     -Dexec.args="--runner=DataflowRunner --project=${PROJECT_NAME} \
                   --subnetwork=https://www.googleapis.com/compute/v1/projects/anemomind/regions/europe-west1/subnetworks/anemovpc-sn-europe-west1b \
                   --gcpTempLocation=gs://boat_logs/tmp \
                   --sourcePath=gs://boat_binaries/bins/ \
                   --workerPath=/home/anemomind/ \
-                  --mongoDbUri=mongodb://main_admin:anemo123@10.0.0.62:27017,10.0.0.61:27017,10.0.0.60:27017/anemomind-dev?replicaSet=MainRepSet&authSource=admin \
+                  --region=${REGION} \
+                  --subnetwork=https://www.googleapis.com/compute/v1/projects/anemomind/regions/${REGION}/subnetworks/${SUBNET} \
+                  --mongoDbUri=mongodb://main_admin:${MONGO_PWD}@${mongo_ips}/anemomind-dev?replicaSet=MainRepSet&authSource=admin \
                   --concurrency=10" \
-     -Pdataflow-runner 
+     -Pdataflow-runner
