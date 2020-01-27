562be0b6bcd3b5f8ca2cad029ab62c1615a1fce8
Julien Pilet <julien.pilet@gmail.com>
2020-01-27 09:55:44 +0100

    Remove outdated files

--- www/app/scripts/controllers/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('anemomindApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    
-});
--- www/app/scripts/services/session.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('anemomindApp')
-  .factory('Session', function ($resource) {
-    return $resource('/api/session/');
-  });
