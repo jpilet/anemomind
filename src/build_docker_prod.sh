7c1b0c7d28645b16d6a1c907713bf6da9f54e626
Julien Pilet <julien.pilet@gmail.com>
2020-03-27 10:08:15 +0100

    Update docker-compose and pass build args to docker build in build_docker.sh

--- src/build_docker_prod.sh
+++ src/build_docker_prod.sh
@@ -12,6 +12,12 @@ TAG=${2:latest}
 # TODO: add -t option with the appropriate image name and tag.
 docker build -f Dockerfile.prod -t gcr.io/$PROJECT_NAME/anemomind_anemocppserver:$TAG .
 
-docker build -t gcr.io/$PROJECT_NAME/anemomind_anemowebapp:latest -f www2/Dockerfile \
+docker build -t gcr.io/$PROJECT_NAME/anemomind_anemowebapp:${TAG} -f www2/Dockerfile \
+         --build-arg MONGO_URL=mongodb://anemomongo:27017/anemomind-dev \
+         --build-arg GCLOUD_PROJECT=anemomind\
+         --build-arg GCS_KEYFILE=/anemomind/www2/anemomind-9b757e3fbacb.json\
+         --build-arg GCS_BUCKET=boat_logs\
+         --build-arg USE_GS="true" \
+         --build-arg PUBSUB_TOPIC_NAME=anemomind_log_topic \
          --build-arg CPP_DOCKER_IMAGE=gcr.io/$PROJECT_NAME/anemomind_anemocppserver:$TAG .
 
